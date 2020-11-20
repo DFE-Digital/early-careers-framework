41767cc3ec8d949d39b6aa64e4d3c1faa79f74b4
Ethan Mills <ethan.mills@softwire.com>
2020-11-20 10:56:02 +0000

    ECF-NONE: Fix lint errors

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -1,9 +1,10 @@
-require.context('govuk-frontend/govuk/assets');
+/* eslint-disable import/first */
+require.context("govuk-frontend/govuk/assets");
 
-import '../styles/application.scss';
-import Rails from 'rails-ujs';
-import Turbolinks from 'turbolinks';
-import { initAll } from 'govuk-frontend';
+import "../styles/application.scss";
+import Rails from "rails-ujs";
+import Turbolinks from "turbolinks";
+import { initAll } from "govuk-frontend";
 
 Rails.start();
 Turbolinks.start();
