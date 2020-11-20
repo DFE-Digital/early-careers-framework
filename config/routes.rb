ffb8187b80376014d8ef8ccb57c9a423f7fea5ce
Michal Kreft <michal.kreft@softwire.com>
2020-11-20 15:11:20 +0000

    ECF-32: Add school search form

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get "/pages/:page", to: "pages#show"
 
   resource :supplier_dashboard, controller: :supplier_dashboard, only: :show
+  resource :invite_schools
 
   get "/404", to: "errors#not_found", via: :all
   get "/422", to: "errors#unprocessable_entity", via: :all
