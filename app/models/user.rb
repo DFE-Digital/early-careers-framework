c740cfcdd101430a2263db0411b87eb2fc6163d0
Michal Kreft <michal.kreft@softwire.com>
2020-12-03 10:23:02 +0000

    ecf-29: Remove password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ require 'byebug'
 
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :trackable,
-         :recoverable, :rememberable, :validatable, :confirmable,
+         :rememberable, :validatable, :confirmable,
          :passwordless_authenticatable
 
   def password_required?
