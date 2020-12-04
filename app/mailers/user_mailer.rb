f4c3093ce83b20efdf22c599ea18ee46ad65e2f7
Alfred Mazimbe <alfred.mazimbe@404-digital.co.uk>
2020-12-04 10:15:31 +0000

    use notify templates for sign in email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,16 @@
 class UserMailer < ApplicationMailer
-  default from: "from@example.com"
+  SIGN_IN_EMAIL_TEMPLATE = "7ab8db5b-9842-4bc3-8dbb-f590a3198d9e".freeze
 
   def sign_in_email(user, url)
-    @user = user
-    @url  = url
-    mail to: @user.email, subject: "Sign in into ECF"
+    template_mail(
+      SIGN_IN_EMAIL_TEMPLATE,
+      to: user.email,
+      rails_mailer: mailer_name,
+      rails_mail_template: action_name,
+      personalisation: {
+        first_name: user.first_name.capitalize,
+        sign_in_url: url,
+      },
+    )
   end
 end
