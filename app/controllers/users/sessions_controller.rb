09ad14899ae3f849bc07fcb0ca2c61bb164f3f23
Alfred Mazimbe <alfred.mazimbe@404-digital.co.uk>
2020-12-06 00:20:06 +0000

    added request spec

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,7 +2,7 @@ class Users::SessionsController < Devise::SessionsController
   def sign_in_with_token
     user = User.find_by(login_token: params[:login_token])
 
-    if user.present?
+    if user.present? && Time.zone.now < user.login_token_valid_until
       user.update!(login_token: nil, login_token_valid_until: 1.year.ago)
       sign_in(user, scope: :user)
       redirect_to dashboard_path
