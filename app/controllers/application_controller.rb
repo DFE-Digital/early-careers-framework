a67c2ea497425e41b161f73e581c194577b71b4e
Alfred Mazimbe <alfred.mazimbe@404-digital.co.uk>
2020-12-07 09:54:18 +0000

    registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   default_form_builder GOVUKDesignSystemFormBuilder::FormBuilder
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email first_name last_name])
+  end
 end
