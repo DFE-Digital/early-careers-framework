4ab338b1e99f9049f46ef920d9e54d080da367c7
Alfred Mazimbe <alfred.mazimbe@404-digital.co.uk>
2020-12-07 11:21:55 +0000

    pr feedback

--- db/migrate/20201130093110_devise_create_users.rb
+++ db/migrate/20201130093110_devise_create_users.rb
@@ -14,10 +14,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :login_token
       t.datetime :login_token_valid_until
 
-      ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
-
       ## Rememberable
       t.datetime :remember_created_at
 
@@ -37,7 +33,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
     end
 
     add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
     add_index :users, :confirmation_token,   unique: true
   end
 end
