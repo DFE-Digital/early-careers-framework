684043ba8da0c32ec516543b1e178c672a2b3d4e
Ethan Mills <ethan.mills@softwire.com>
2020-12-09 09:49:01 +0000

    ECF-TECH-33: Make git commit hash visible at check endpoint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,13 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def check
+    render json: { status: "OK", version: ENV["SHA"] }, status: :ok
+  end
+
 protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[email first_name last_name])
-  end
-
-  def check
-    render json: { status: "OK" }, status: :ok
   end
 end
