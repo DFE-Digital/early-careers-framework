4d7722239ebe4fbe1f8be8c421dc7adca8c5f169
Michal Kreft <michal.kreft@softwire.com>
2020-12-10 13:04:06 +0000

    Make linting happy

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,6 @@ require "action_view/railtie"
 require "rails/test_unit/railtie"
 require "govspeak"
 
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
