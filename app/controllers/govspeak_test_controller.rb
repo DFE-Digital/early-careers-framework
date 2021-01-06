39d6c1bc89e1035b59c1a5f10115f102e0b82e19
Michal Kreft <42969249+kicferk1@users.noreply.github.com>
2021-01-06 13:41:03 +0000

    Add youtube in govspeak (#40)
    * ECF-2-38: Bump govspeak version to the one with youtube embeddings
    
    * ECF-2-38: Quality of life improvements on govspeak test page

--- app/controllers/govspeak_test_controller.rb
+++ app/controllers/govspeak_test_controller.rb
@@ -5,11 +5,12 @@ require "govspeak"
 class GovspeakTestController < ApplicationController
   def show
     @content = Govspeak::Document.new("").to_html
+    @preview_string = ""
   end
 
   def preview
-    preview_string = params[:preview_string]
-    @content = Govspeak::Document.new(preview_string).to_html
+    @preview_string = params[:preview_string]
+    @content = Govspeak::Document.new(@preview_string).to_html
     render :show
   end
 end
