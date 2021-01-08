2d8781af5be342b5bb9d53f42f7972868d0a3ce5
Michal Kreft <michal.kreft@softwire.com>
2021-01-08 14:40:55 +0000

    Remove unnecessary javascript until we decide we need it

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -1,9 +1,6 @@
 /* eslint-disable import/first */
 require.context("govuk-frontend/govuk/assets");
 
-//= require govuk_publishing_components/dependencies
-//= require govuk_publishing_components/all_components
-
 import "../styles/application.scss";
 import { initAll } from "govuk-frontend";
 
