a46ed3c504af40f5b053871c537ff6bdf0fcc36e
Michal Kreft <michal.kreft@softwire.com>
2021-01-12 14:12:31 +0000

    ECF-RP-100: Add current user and lead provider to school search controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   default_form_builder GOVUKDesignSystemFormBuilder::FormBuilder
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_current_user
 
   def check
     render json: { status: "OK", version: ENV["SHA"], environment: Rails.env }, status: :ok
@@ -13,5 +14,9 @@ protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[email first_name last_name])
+  end
+
+  def set_current_user
+    @current_user = current_user
   end
 end
