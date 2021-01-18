4c9c00eb8a21986299af6e1fa554cc9cf0c532ad
Michal Kreft <michal.kreft@softwire.com>
2021-01-18 12:11:44 +0000

    ECF-EL-73: Add tests

---
+++ spec/models/course_lesson_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe CourseLesson, type: :model do
+  it "can be created" do
+    expect {
+      CourseLesson.create(
+        title: "Test Course lesson",
+        content: "No content",
+        course_module: FactoryBot.create(:course_module),
+      )
+    }.to change { CourseLesson.count }.by(1)
+  end
+
+  describe "associations" do
+    it { is_expected.to belong_to(:course_module) }
+  end
+
+  describe "validations" do
+    subject { FactoryBot.create(:course_lesson) }
+    it { is_expected.to validate_presence_of(:title).with_message("Enter a title") }
+    it { is_expected.to validate_presence_of(:content).with_message("Enter content") }
+  end
+end
