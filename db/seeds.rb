1b6a03f3c5e0846eb4a5c905b4d318fe053b5142
Michal Kreft <42969249+kicferk1@users.noreply.github.com>
2021-01-22 13:36:26 +0000

    ECF-EL-73: Add cip content (#64)
    * Add seeding files to populate our database
    
    * Appeal linter, fix a crash

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,15 @@
 # frozen_string_literal: true
 
-# TODO: Remove network and school seeding when we have a way of getting them from GIAS
-unless Network.first
-  network = Network.create!(name: "Test school network")
-  School.create!(urn: "TEST_URN_1", name: "Test school one", address_line1: "Test address", country: "England", postcode: "TEST1", network: network, domains: %w[testschool1.sch.uk network.com digital.education.gov.uk])
-  School.create!(urn: "TEST_URN_2", name: "Test school two", address_line1: "Test address London", country: "England", postcode: "TEST2", network: network, domains: %w[testschool2.sch.uk network.com digital.education.gov.uk])
-  School.create!(urn: "TEST_URN_3", name: "Test school three", address_line1: "Test address Oxford", country: "England", postcode: "TEST3", domains: ["testschool3.sch.uk"])
-  School.create!(urn: "TEST_URN_4", name: "Test school four", address_line1: "Test address Newcastle", country: "England", postcode: "TEST4", domains: ["testschool4.sch.uk"])
-end
+# store all seeds inside the folder db/seeds
 
-# TODO: Remove this when we have a way of adding lead providers, or expand to include all of them
-unless LeadProvider.first
-  LeadProvider.create!(name: "Test Lead Provider")
-end
+Dir[Rails.root.join("db/seeds/dummy_structures.rb")].each { |seed| load seed }
 
-# TODO: Remove this when we have a way of adding partnerships
-unless Partnership.first || Rails.env.production?
-  Partnership.create!(school: School.first, lead_provider: LeadProvider.first)
-end
+CURRENT_CIP_VERSION = 2
+CourseLesson.where("version < ?", CURRENT_CIP_VERSION).delete_all
+CourseModule.where("version < ?", CURRENT_CIP_VERSION).delete_all
+CourseYear.where("version < ?", CURRENT_CIP_VERSION).delete_all
 
-unless AdminProfile.first || Rails.env.production?
-  user = User.find_or_create_by!(email: "ecf@mailinator.com") do |u|
-    u.full_name = "Admin User"
-  end
-  AdminProfile.create!(user: user)
-end
-
-unless CourseYear.first || Rails.env.production?
-  course_year = CourseYear.create!(
-    lead_provider: LeadProvider.first, is_year_one: true, title: "Test title", content: "Test **content**",
-  )
-  course_module = CourseModule.create!(course_year: course_year, title: "Test title", content: "Test **content**")
-  CourseLesson.create!(course_module: course_module, title: "Test title", content: "Test **content**")
-end
+Dir[Rails.root.join("db/seeds/cip_ambition.rb")].each { |seed| load seed }
+Dir[Rails.root.join("db/seeds/cip_edt.rb")].each { |seed| load seed }
+Dir[Rails.root.join("db/seeds/cip_teachfirst.rb")].each { |seed| load seed }
+Dir[Rails.root.join("db/seeds/cip_ucl.rb")].each { |seed| load seed }
---
+++ db/seeds/dummy_structures.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+# TODO: Remove network and school seeding when we have a way of getting them from GIAS
+unless Network.first
+  network = Network.create!(name: "Test school network")
+  School.create!(urn: "TEST_URN_1", name: "Test school one", address_line1: "Test address", country: "England", postcode: "TEST1", network: network, domains: %w[testschool1.sch.uk network.com digital.education.gov.uk])
+  School.create!(urn: "TEST_URN_2", name: "Test school two", address_line1: "Test address London", country: "England", postcode: "TEST2", network: network, domains: %w[testschool2.sch.uk network.com digital.education.gov.uk])
+  School.create!(urn: "TEST_URN_3", name: "Test school three", address_line1: "Test address Oxford", country: "England", postcode: "TEST3", domains: ["testschool3.sch.uk"])
+  School.create!(urn: "TEST_URN_4", name: "Test school four", address_line1: "Test address Newcastle", country: "England", postcode: "TEST4", domains: ["testschool4.sch.uk"])
+end
+
+# TODO: Remove this when we have a way of adding lead providers, or expand to include all of them
+unless LeadProvider.first
+  LeadProvider.create!(name: "Test Lead Provider")
+end
+
+# TODO: Remove this when we have a way of adding partnerships
+unless Partnership.first || Rails.env.production?
+  Partnership.create!(school: School.first, lead_provider: LeadProvider.first)
+end
+
+unless AdminProfile.first || Rails.env.production?
+  user = User.find_or_create_by!(email: "ecf@mailinator.com") do |u|
+    u.full_name = "Admin User"
+  end
+  AdminProfile.create!(user: user)
+end
