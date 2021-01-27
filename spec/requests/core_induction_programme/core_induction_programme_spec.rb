12cbc16ff914fcf86cf7f0b7a124528e65f5e26f
Michal Kreft <michal.kreft@softwire.com>
2021-01-27 10:42:24 +0000

    Update cip case to kebab, add link to it in the header

--- spec/requests/core_induction_programme/core_induction_programme_spec.rb
+++ spec/requests/core_induction_programme/core_induction_programme_spec.rb
@@ -4,7 +4,7 @@ require "rails_helper"
 
 RSpec.describe "Core Induction Programme", type: :request do
   it "renders the core_induction_programme page" do
-    get "/core_induction_programme"
+    get "/core-induction-programme"
     expect(response).to render_template(:show)
   end
 
--- spec/requests/core_induction_programme/course_module_spec.rb
+++ spec/requests/core_induction_programme/course_module_spec.rb
@@ -5,7 +5,7 @@ require "rails_helper"
 RSpec.describe "Core Induction Programme Module", type: :request do
   it "renders the cip module page" do
     course_module = FactoryBot.create(:course_module)
-    get "/core_induction_programme/#{course_module.course_year.id}/#{course_module.id}"
+    get "/core-induction-programme/#{course_module.course_year.id}/#{course_module.id}"
     expect(response).to render_template(:show)
   end
 end
