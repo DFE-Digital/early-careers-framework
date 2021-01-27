e74d5d82d9524e7963b9d0f995fc0179702d7a90
Michal Kreft <michal.kreft@softwire.com>
2021-01-27 11:57:27 +0000

    Add test for user being admin

--- spec/requests/core_induction_programme/core_induction_programme_spec.rb
+++ spec/requests/core_induction_programme/core_induction_programme_spec.rb
@@ -12,4 +12,13 @@ RSpec.describe "Core Induction Programme", type: :request do
     get "/core-induction-programme/download-export"
     expect(response).to redirect_to(cip_path)
   end
+
+  it "downloads a file when user is admin" do
+    admin_user = create(:user, :admin)
+    sign_in admin_user
+
+    get "/core-induction-programme/download-export"
+    expect(response.body).to include("CourseYear.import(")
+    response.header["Content-Type"].should eql "text/plain"
+  end
 end
