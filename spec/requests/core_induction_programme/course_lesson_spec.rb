98a96fd2e5756adecb72e2a27ed40ad1f6c983e9
Steven <steven.woods@digital.education.gov.uk>
2021-01-28 15:29:37 +0000

    add request tests for lessons

--- spec/requests/core_induction_programme/course_lesson_spec.rb
+++ spec/requests/core_induction_programme/course_lesson_spec.rb
@@ -3,9 +3,76 @@
 require "rails_helper"
 
 RSpec.describe "Core Induction Programme Lesson", type: :request do
-  it "renders the cip lesson page" do
-    course_lesson = FactoryBot.create(:course_lesson)
-    get "/core_induction_programme/#{course_lesson.course_module.course_year.id}/#{course_lesson.course_module.id}/#{course_lesson.id}"
-    expect(response).to render_template(:show)
+  let(:course_lesson) { FactoryBot.create(:course_lesson) }
+  let(:course_lesson_url) { "/core_induction_programme/#{course_lesson.course_module.course_year.id}/#{course_lesson.course_module.id}/#{course_lesson.id}" }
+
+  describe "when an admin user is logged in" do
+    before do
+      admin_user = create(:user, :admin)
+      sign_in admin_user
+    end
+    describe "GET /core_induction_programme/:years/:modules/:lessons" do
+      it "renders the cip lesson page" do
+        get course_lesson_url
+        expect(response).to render_template(:show)
+      end
+      it "renders the cip edit lesson page" do
+        get "#{course_lesson_url}/edit"
+        expect(response).to render_template(:edit)
+      end
+    end
+
+    describe "PUT /core_induction_programme/:years/:modules/:lessons/edit" do
+      it "renders a preview of changes to lesson" do
+        put "#{course_lesson_url}/edit"
+
+        expect(response).to render_template(:edit)
+      end
+    end
+    describe "PUT /core_induction_programme/:years/:modules/:lessons" do
+      it "redirects to the lesson page when saving content" do
+        put course_lesson_url, params: { commit: "Save changes", lesson_preview: course_lesson.content }
+        expect(response).to redirect_to(course_lesson_url)
+      end
+    end
+  end
+
+  describe "when a non-admin user is logged in" do
+    before do
+      user = create(:user)
+      sign_in user
+    end
+    describe "GET /core_induction_programme/:years/:modules/:lessons" do
+      it "renders the cip lesson page" do
+        get course_lesson_url
+        expect(response).to render_template(:show)
+      end
+    end
+    describe "GET /core_induction_programme/:years/:modules/:lessons/edit" do
+      it "redirects to the sign in page" do
+        expect { get "#{course_lesson_url}/edit" }.to raise_error Pundit::NotAuthorizedError
+      end
+    end
+  end
+
+  describe "when a non-user is accessing the lesson page" do
+    describe "GET /core_induction_programme/:years/:modules/:lessons" do
+      it "renders the cip lesson page" do
+        get course_lesson_url
+        expect(response).to render_template(:show)
+      end
+    end
+    describe "GET /core_induction_programme/:years/:modules/:lessons/edit" do
+      it "redirects to the sign in page" do
+        get "#{course_lesson_url}/edit"
+        expect(response).to redirect_to("/users/sign_in")
+      end
+    end
+    describe "PUT /core_induction_programme/:years/:modules/:lessons" do
+      it "redirects to the sign in page" do
+        put course_lesson_url, params: { commit: "Save changes", lesson_preview: course_lesson.content }
+        expect(response).to redirect_to("/users/sign_in")
+      end
+    end
   end
 end
