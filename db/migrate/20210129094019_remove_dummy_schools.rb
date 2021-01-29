668b056663d18efe2ceafceadd012c704366cadb
Michal Kreft <michal.kreft@softwire.com>
2021-01-29 09:56:59 +0000

    Bring back some dummy content for partnerships

--- db/migrate/20210129094019_remove_dummy_schools.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class RemoveDummySchools < ActiveRecord::Migration[6.1]
-  def change
-    School.where(local_authority: nil).delete_all
-    School.where(local_authority_district: nil).delete_all
-  end
-end
--- db/seeds/dummy_structures.rb
+++ db/seeds/dummy_structures.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
+# TODO: Remove network and school seeding when we have a way of getting them from GIAS
+unless Network.first
+  local_authority = LocalAuthority.create!(code: "TEST01", name: "Test local authority")
+  local_authority_district = LocalAuthorityDistrict.create!(code: "TEST01", name: "Test local authority")
+  network = Network.create!(name: "Test school network")
+  School.create!(urn: "TEST_URN_1", name: "Test school one", address_line1: "Test address", country: "England", postcode: "TEST1", network: network, domains: %w[testschool1.sch.uk network.com digital.education.gov.uk], local_authority: local_authority, local_authority_district: local_authority_district)
+  School.create!(urn: "TEST_URN_2", name: "Test school two", address_line1: "Test address London", country: "England", postcode: "TEST2", network: network, domains: %w[testschool2.sch.uk network.com digital.education.gov.uk], local_authority: local_authority, local_authority_district: local_authority_district)
+end
+
 # TODO: Remove this when we have a way of adding lead providers, or expand to include all of them
 unless LeadProvider.first
   LeadProvider.create!(name: "Test Lead Provider")
