d6743752b1ba73f25c2511e4e552c15d4b5d0688
Steven <steven.woods@digital.education.gov.uk>
2021-02-04 11:34:54 +0000

    add functionality to edit year content

--- app/controllers/core_induction_programme/years_controller.rb
+++ app/controllers/core_induction_programme/years_controller.rb
@@ -1,7 +1,36 @@
 # frozen_string_literal: true
 
 class CoreInductionProgramme::YearsController < ApplicationController
-  def show
-    @course_year = CourseYear.includes(:course_modules).find(params[:id])
+  include Pundit
+  include GovspeakHelper
+
+  after_action :verify_authorized, except: :show
+  before_action :authenticate_user!, except: :show
+  before_action :load_and_authorize_course_year
+
+  def show; end
+
+  def edit
+    @year_preview = params[:year_preview] || @course_year.content
+    @html_content = content_to_html(@year_preview)
+  end
+
+  def update
+    if params[:commit] == "Save changes"
+      @course_year.update!(content: params[:year_preview])
+      flash[:success] = "Your changes have been saved"
+      redirect_to cip_year_url
+    else
+      @year_preview = params[:year_preview]
+      @html_content = content_to_html(@year_preview)
+      render action: "edit"
+    end
+  end
+
+private
+
+  def load_and_authorize_course_year
+    @course_year = CourseYear.find(params[:id])
+    authorize @course_year
   end
 end
