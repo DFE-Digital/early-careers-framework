0fdf8ea2b37b738a137759bfeecb1a9afdc3863f
Michal Kreft <42969249+kicferk1@users.noreply.github.com>
2021-02-05 12:58:31 +0000

    Lesson order in modules (#83)
    * Remove unused reference to next module / lesson from cip models
    
    * Fix lesson order staying the same after editing
    
    * Simplify list displays in course year / lesson
    
    * Comment fixing
    
    * Improve test readability

--- app/models/course_module.rb
+++ app/models/course_module.rb
@@ -2,9 +2,13 @@
 
 class CourseModule < ApplicationRecord
   belongs_to :course_year
-  has_one :next_module, class_name: "CourseModule", foreign_key: :next_module_id
-  belongs_to :previous_module, class_name: "CourseModule", inverse_of: :next_module, optional: true
   has_many :course_lessons, dependent: :delete_all
+
+  # We use previous_module_id to store the connections between modules
+  # The id telling us which module is next lives on the next module, where it is called 'previous_module_id'
+  # That's why the foreign key is named contrary to the field name
+  has_one :next_module, class_name: "CourseModule", foreign_key: :previous_module_id
+  belongs_to :previous_module, class_name: "CourseModule", inverse_of: :next_module, optional: true
 
   validates :title, presence: { message: "Enter a title" }
   validates :content, presence: { message: "Enter content" }
