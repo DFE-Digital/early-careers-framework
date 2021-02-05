da11c24edfa9c874e2e58d23e62489b5016d2b78
Ethan Mills <ethan.mills@softwire.com>
2021-02-05 11:49:53 +0000

    Add stored_location_for(user) to enhance post-login redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(user)
-    helpers.profile_dashboard_url(user)
+    stored_location_for(user) || helpers.profile_dashboard_url(user)
   end
 
 protected
