c2ecd8613d1ac1e7da853996778a6aa31776d978
Alfred Mazimbe <alfred.mazimbe@404-digital.co.uk>
2021-02-08 11:30:31 +0000

    added question one view

--- app/controllers/school_registration/question_one_controller.rb
+++ app/controllers/school_registration/question_one_controller.rb
@@ -1,5 +1,9 @@
 class SchoolRegistration::QuestionOneController < ApplicationController
   def index
-
+    @school = School.new
+    @options = [
+      OpenStruct.new(id: 1, name: "Yes"),
+      OpenStruct.new(id: 2, name: "I don't know yet")
+    ]
   end
 end
