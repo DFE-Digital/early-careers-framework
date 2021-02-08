16fdd081451d55e0ecd95a4e406ba9f09c84b145
Michal Kreft <michal.kreft@softwire.com>
2021-02-08 15:33:14 +0000

    Add title editing to years

--- app/controllers/core_induction_programme/years_controller.rb
+++ app/controllers/core_induction_programme/years_controller.rb
@@ -6,31 +6,31 @@ class CoreInductionProgramme::YearsController < ApplicationController
 
   after_action :verify_authorized, except: :show
   before_action :authenticate_user!, except: :show
-  before_action :load_and_authorize_course_year
+  before_action :load_course_year
 
   def show; end
 
-  def edit
-    @year_preview = params[:year_preview] || @course_year.content
-    @html_content = content_to_html(@year_preview)
-  end
+  def edit; end
 
   def update
     if params[:commit] == "Save changes"
-      @course_year.update!(content: params[:year_preview])
+      @course_year.save!
       flash[:success] = "Your changes have been saved"
       redirect_to cip_year_url
     else
-      @year_preview = params[:year_preview]
-      @html_content = content_to_html(@year_preview)
       render action: "edit"
     end
   end
 
 private
 
-  def load_and_authorize_course_year
+  def load_course_year
     @course_year = CourseYear.find(params[:id])
     authorize @course_year
+    @course_year.assign_attributes(course_year_params)
+  end
+
+  def course_year_params
+    params.permit(:content, :title)
   end
 end
