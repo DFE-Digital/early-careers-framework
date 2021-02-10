8fb1cb34f407085fd1fce08112c63c1abd693471
Michal Kreft <42969249+kicferk1@users.noreply.github.com>
2021-02-10 11:50:44 +0000

    Edit course titles (#89)
    * Add title editing to lessons
    
    * Add title editing to modules
    
    * Add title editing to years
    
    * Add small spaces everywhere
    
    * Fix markdown display when editing years

--- app/controllers/core_induction_programme/modules_controller.rb
+++ app/controllers/core_induction_programme/modules_controller.rb
@@ -6,31 +6,31 @@ class CoreInductionProgramme::ModulesController < ApplicationController
 
   after_action :verify_authorized, except: :show
   before_action :authenticate_user!, except: :show
-  before_action :load_and_authorize_course_module
+  before_action :load_course_module
 
   def show; end
 
-  def edit
-    @module_preview = params[:module_preview] || @course_module.content
-    @html_content = content_to_html(@module_preview)
-  end
+  def edit; end
 
   def update
     if params[:commit] == "Save changes"
-      @course_module.update!(content: params[:module_preview])
+      @course_module.save!
       flash[:success] = "Your changes have been saved"
       redirect_to cip_year_module_url
     else
-      @module_preview = params[:module_preview]
-      @html_content = content_to_html(@module_preview)
       render action: "edit"
     end
   end
 
 private
 
-  def load_and_authorize_course_module
+  def load_course_module
     @course_module = CourseModule.find(params[:id])
     authorize @course_module
+    @course_module.assign_attributes(course_module_params)
+  end
+
+  def course_module_params
+    params.permit(:content, :title)
   end
 end
