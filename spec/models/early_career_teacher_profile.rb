e61381f35b2a98ec1d572169ead3dc1dd0070264
Steven <steven.woods@digital.education.gov.uk>
2021-02-11 13:38:51 +0000

    update ect profile model test with new assoc

--- spec/models/early_career_teacher_profile.rb
+++ spec/models/early_career_teacher_profile.rb
@@ -7,5 +7,7 @@ RSpec.describe EarlyCareerTeacherProfile, type: :model do
     it { is_expected.to belong_to(:user) }
     it { is_expected.to belong_to(:core_induction_programme).optional }
     it { is_expected.to belong_to(:cohort).optional }
+    it { is_expected.to have_many(:course_lesson_progresses) }
+    it { is_expected.to have_many(:course_lessons).through(:course_lesson_progresses) }
   end
 end
