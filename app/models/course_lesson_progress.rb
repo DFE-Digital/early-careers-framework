66b6cf6d532a4ae8bfb117b8695a4a3bb921ab75
Steven <steven.woods@digital.education.gov.uk>
2021-02-11 16:48:09 +0000

    add enum to course lesson progress model

--- app/models/course_lesson_progress.rb
+++ app/models/course_lesson_progress.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class CourseLessonProgress < ApplicationRecord
+  enum progress: { not_started: "not_started", complete: "complete" }
+
   belongs_to :course_lesson
   belongs_to :early_career_teacher_profile
 
   validates :course_lesson_id, uniqueness: { scope: :early_career_teacher_profile_id }
-  validates :progress, inclusion: { in: %w[not_started complete] }
 end
