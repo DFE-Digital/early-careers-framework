848bd4382fba3d876008e740839a2e9e15752d34
Sebastian Siemianowski <sebastian.siemianowski@digital.education.gov.uk>
2021-02-15 14:18:57 +0000

    Adds api for returning list of schools

--- spec/serializers/school_serializer_spec.rb
+++ spec/serializers/school_serializer_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe SchoolSerializer do
     let(:school) { create(:school, name: "Western Armstrong", address_line1: "52634 Gloria Circle", address_line2: "Pagac Extensions", postcode: "SE23 1SA") }
     let(:decorated_school) { ::Decorators::SchoolDecorator.new(school) }
     let(:serialized_school) { SchoolSerializer.render(decorated_school) }
+
     it "outputs correctly formatted serialized school" do
       expected_json_string = "{\"id\":\"#{school.id}\",\"full_address_formatted\":\"52634 Gloria Circle, Pagac Extensions, SE23 1SA\",\"name\":\"Western Armstrong\"}"
       expect(serialized_school).to eq expected_json_string
