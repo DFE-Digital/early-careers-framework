5cdcb8c20dd9a667fab9573d4bbfae0764a1ba27
Sebastian Siemianowski <sebastian.siemianowski@digital.education.gov.uk>
2021-02-15 14:56:08 +0000

    Adds api for returning list of schools

--- app/controllers/api/school_search_controller.rb
+++ app/controllers/api/school_search_controller.rb
@@ -5,6 +5,7 @@ class Api::SchoolSearchController < Api::ApiController
     if params[:search_key]
       schools = School.search_by_name_or_urn(params[:search_key]).limit(10)
       decorated_schools = schools.map { |school| ::Decorators::SchoolDecorator.new(school) }
+
       render json: SchoolSerializer.render(decorated_schools)
     else
       render json: []
