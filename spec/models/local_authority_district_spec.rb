e95e40051a4c2b05cde93fee127adf609bc5968a
Ethan Mills <ethan.mills@softwire.com>
2021-02-17 14:51:43 +0000

    ECF-RP-129: Add spec for scope on LAD

--- spec/models/local_authority_district_spec.rb
+++ spec/models/local_authority_district_spec.rb
@@ -43,4 +43,26 @@ RSpec.describe LocalAuthorityDistrict, type: :model do
       end
     end
   end
+
+  describe "scope :only_with_uplift" do
+    let!(:new_sparsity) { create(:local_authority_district, district_sparsities: [build(:district_sparsity, start_year: 2021)]) }
+    let!(:existing_sparsity) { create(:local_authority_district, district_sparsities: [build(:district_sparsity, start_year: 2020)]) }
+    let!(:previous_sparsity) { create(:local_authority_district, district_sparsities: [build(:district_sparsity, start_year: 2019, end_year: 2020)]) }
+
+    it "includes existing sparsity" do
+      expect(LocalAuthorityDistrict.only_with_uplift(2020)).to include(existing_sparsity)
+    end
+
+    it "does not include new sparsity" do
+      expect(LocalAuthorityDistrict.only_with_uplift(2020)).not_to include(new_sparsity)
+    end
+
+    it "does not include previous sparsity" do
+      expect(LocalAuthorityDistrict.only_with_uplift(2020)).not_to include(previous_sparsity)
+    end
+
+    it "includes previous sparsity for the correct year" do
+      expect(LocalAuthorityDistrict.only_with_uplift(2019)).to include(previous_sparsity)
+    end
+  end
 end
