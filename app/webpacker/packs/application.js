b392d8839925fecdc65e9075a8f9f731664d1ae6
Ethan Mills <ethan.mills@softwire.com>
2021-02-25 11:09:45 +0000

    Use core-js to make up for what IE11 lacks

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/first */
 require.context("govuk-frontend/govuk/assets");
 
+import "core-js";
 import "../styles/application.scss";
 import { initAll } from "govuk-frontend";
 import "./admin/supplier-users";
