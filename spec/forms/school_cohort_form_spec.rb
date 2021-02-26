8b2e4b1a4a3b606ea01fc6cfded01492f2d3c993
Alfred Mazimbe <alfred.mazimbe@404-digital.co.uk>
2021-02-26 09:45:38 +0000

    fix style of specs in school cohort form

--- spec/forms/school_cohort_form_spec.rb
+++ spec/forms/school_cohort_form_spec.rb
@@ -6,13 +6,7 @@ RSpec.describe SchoolCohortForm, type: :model do
   describe "validations" do
     it { is_expected.to validate_presence_of(:estimated_mentor_count).with_message("Enter your expected number of mentors") }
     it { is_expected.to validate_presence_of(:estimated_teacher_count).with_message("Enter your expected number of ECTs") }
-
-    it do
-      should validate_numericality_of(:estimated_mentor_count).is_greater_than_or_equal_to(0)
-    end
-
-    it do
-      should validate_numericality_of(:estimated_teacher_count).is_greater_than_or_equal_to(0)
-    end
+    it { is_expected.to validate_numericality_of(:estimated_mentor_count).is_greater_than_or_equal_to(0) }
+    it { is_expected.to validate_numericality_of(:estimated_teacher_count).is_greater_than_or_equal_to(0) }
   end
 end
