9b1088727f993ea218026e1d2fe511a1ffc4bd7b
xswoodsx <69353998+xswoodsx@users.noreply.github.com>
2021-03-03 09:19:19 +0000

    Add current page user is visiting to breadcrumbs (#20)
    * Add current page user is visiting to breadcrumbs
    
    * Add tests for CipBreadcrumbHelper
    
    * provide additional tests checking behaviour of breadcrumbs with different actions
    
    * additional tests for each breadcrumb and refactored.

--- app/helpers/cip_breadcrumb_helper.rb
+++ app/helpers/cip_breadcrumb_helper.rb
@@ -1,22 +1,30 @@
 # frozen_string_literal: true
 
 module CipBreadcrumbHelper
-  def course_year_breadcrumbs(user)
-    [home_crumb(user)]
+  def course_year_breadcrumbs(user, course_year)
+    year_crumb = course_year_crumb(course_year)
+    [
+      home_crumb(user),
+      end_crumb(year_crumb),
+    ]
   end
 
   def course_module_breadcrumbs(user, course_module)
+    module_crumb = course_module_crumb(course_module)
     [
       home_crumb(user),
-      course_module_crumb(course_module),
+      course_year_crumb(course_module.course_year),
+      end_crumb(module_crumb),
     ]
   end
 
   def course_lesson_breadcrumbs(user, course_lesson)
+    lesson_crumb = course_lesson_crumb(course_lesson)
     [
       home_crumb(user),
+      course_year_crumb(course_lesson.course_module.course_year),
       course_module_crumb(course_lesson.course_module),
-      course_lesson_crumb(course_lesson),
+      end_crumb(lesson_crumb),
     ]
   end
 
@@ -26,11 +34,19 @@ private
     ["Home", user ? dashboard_path : cip_path]
   end
 
+  def course_year_crumb(course_year)
+    [course_year.title, cip_year_path(course_year)]
+  end
+
   def course_module_crumb(course_module)
-    [course_module.course_year.title, cip_year_path(course_module.course_year)]
+    [course_module.title, cip_year_module_path(course_module.course_year, course_module)]
   end
 
   def course_lesson_crumb(course_lesson)
-    [course_lesson.course_module.title, cip_year_module_path(course_lesson.course_module.course_year, course_lesson.course_module)]
+    [course_lesson.title, cip_year_module_lesson_path(course_lesson.course_module.course_year, course_lesson.course_module, course_lesson)]
+  end
+
+  def end_crumb(crumb)
+    action_name == "show" ? crumb[0] : crumb
   end
 end
---
+++ spec/helpers/cip_breadcrumb_helper_spec.rb
@@ -0,0 +1,70 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe CipBreadcrumbHelper, type: :helper do
+  before :each do
+    @user = FactoryBot.create(:user, :admin)
+    @course_year = FactoryBot.create(:course_year)
+    @course_year_crumb = [@course_year.title, "/core-induction-programme/years/#{@course_year.id}"]
+  end
+
+  describe "#year_breadcrumb" do
+    let(:course_year_breadcrumb) { helper.course_year_breadcrumbs(@user, @course_year) }
+    it "returns an array for the course year breadcrumb" do
+      expect(course_year_breadcrumb).to eq([["Home", "/dashboard"], @course_year_crumb])
+    end
+
+    it "returns a url for the end crumb when the action_name is edit" do
+      allow(helper).to receive(:action_name) { "edit" }
+      expect(course_year_breadcrumb).to eql([["Home", "/dashboard"], @course_year_crumb])
+    end
+
+    it "returns just the title for the end crumb when the action_name is show" do
+      allow(helper).to receive(:action_name) { "show" }
+      expect(course_year_breadcrumb).to eql([["Home", "/dashboard"], @course_year.title])
+    end
+  end
+
+  describe "#module_breadcrumb" do
+    let(:course_module) { create(:course_module, course_year: @course_year) }
+    let(:course_module_crumb) { [course_module.title, "/core-induction-programme/years/#{@course_year.id}/modules/#{course_module.id}"] }
+    let(:course_module_breadcrumb) { helper.course_module_breadcrumbs(@user, course_module) }
+
+    it "returns an array for the course module breadcrumb" do
+      expect(course_module_breadcrumb).to eq([["Home", "/dashboard"], @course_year_crumb, course_module_crumb])
+    end
+
+    it "returns a url for the end crumb when the action_name is edit" do
+      allow(helper).to receive(:action_name) { "edit" }
+      expect(course_module_breadcrumb).to eq([["Home", "/dashboard"], @course_year_crumb, course_module_crumb])
+    end
+
+    it "returns just the title for the end crumb when the action_name is show" do
+      allow(helper).to receive(:action_name) { "show" }
+      expect(course_module_breadcrumb).to eql([["Home", "/dashboard"], @course_year_crumb, course_module.title])
+    end
+  end
+
+  describe "#lesson_breadcrumb" do
+    let(:course_module) { create(:course_module, course_year: @course_year) }
+    let(:course_lesson) { create(:course_lesson, course_module: course_module) }
+    let(:course_module_crumb) { [course_module.title, "/core-induction-programme/years/#{@course_year.id}/modules/#{course_module.id}"] }
+    let(:course_lesson_crumb) { [course_lesson.title, "/core-induction-programme/years/#{@course_year.id}/modules/#{course_module.id}/lessons/#{course_lesson.id}"] }
+    let(:course_lesson_breadcrumb) { helper.course_lesson_breadcrumbs(@user, course_lesson) }
+
+    it "returns an array for the course lesson breadcrumb" do
+      expect(course_lesson_breadcrumb).to eq([["Home", "/dashboard"], @course_year_crumb, course_module_crumb, course_lesson_crumb])
+    end
+
+    it "returns the url for the end crumb when the action_name is edit" do
+      allow(helper).to receive(:action_name) { "edit" }
+      expect(course_lesson_breadcrumb).to eq([["Home", "/dashboard"], @course_year_crumb, course_module_crumb, course_lesson_crumb])
+    end
+
+    it "returns just the title for the end crumb when the action_name is show" do
+      allow(helper).to receive(:action_name) { "show" }
+      expect(course_lesson_breadcrumb).to eq([["Home", "/dashboard"], @course_year_crumb, course_module_crumb, course_lesson.title])
+    end
+  end
+end
