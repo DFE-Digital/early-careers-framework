c4612efaa4d79a38b3af694ae41cc59f5590f149
Sebastian Siemianowski <sebastian.siemianowski@digital.education.gov.uk>
2021-03-04 02:55:41 +0000

    Added partnership spec

--- spec/models/partnership_spec.rb
+++ spec/models/partnership_spec.rb
@@ -3,4 +3,9 @@
 require "rails_helper"
 
 RSpec.describe Partnership, type: :model do
+  describe "associations" do
+    it { is_expected.to belong_to(:school) }
+    it { is_expected.to belong_to(:lead_provider) }
+    it { is_expected.to belong_to(:cohort) }
+  end
 end
