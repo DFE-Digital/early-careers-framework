1185540b895ebe4d2febbae9358a20f222aa7432
Ethan Mills <ethan.mills@softwire.com>
2021-03-05 14:33:21 +0000

    ECF-RP-313: Refactor cypress test

--- spec/cypress/integration/admin-create-administrator.js
+++ spec/cypress/integration/admin-create-administrator.js
@@ -5,7 +5,7 @@ describe("Admin user creating another admin user", () => {
 
   it("should show a create administrator button", () => {
     cy.visit("/admin/administrators");
-    cy.get("a.govuk-button").contains("Create a new administrator");
+    cy.get(".govuk-button").should("contain", "Create a new administrator");
   });
 
   it("should create a new user", () => {
@@ -13,24 +13,30 @@ describe("Admin user creating another admin user", () => {
     const email = "j.smith@example.com";
 
     cy.visit("/admin/administrators");
-    cy.get("a.govuk-button").contains("Create a new administrator").click();
+    cy.get(".govuk-button").contains("Create a new administrator").click();
 
-    cy.get("h1").should("contain", "Create a new administrator user");
+    cy.location("pathname").should("equal", "/admin/administrators/new");
     cy.get("input[name='user[full_name]']").type(fullName);
     cy.get("input[name='user[email]']").type(email);
-    cy.get("input.govuk-button").contains("Continue").click();
+    cy.get(".govuk-button").contains("Continue").click();
 
-    cy.get("h1").should("contain", "Confirm these details");
-    cy.contains(fullName);
-    cy.contains(email);
+    cy.location("pathname").should(
+      "equal",
+      "/admin/administrators/new/confirm"
+    );
+    cy.get("main").should("contain", fullName);
+    cy.get("main").should("contain", email);
     cy.get("input.govuk-button").contains("Create administrator user").click();
 
-    cy.get("h1.govuk-panel__title").contains("Administrator created");
-    cy.get("a.govuk-button").contains("View all administrators").click();
+    cy.location("pathname").should(
+      "equal",
+      "/admin/administrators/new/success"
+    );
+    cy.get(".govuk-button").contains("View all administrators").click();
 
-    cy.get("h1").contains("Administrators");
-    cy.contains(fullName);
-    cy.contains(email);
+    cy.location("pathname").should("equal", "/admin/administrators");
+    cy.get("main").should("contain", fullName);
+    cy.get("main").should("contain", email);
 
     cy.appEval(`User.find_by(email: "${email}").admin?`).then((result) =>
       expect(result).to.equal(true)
