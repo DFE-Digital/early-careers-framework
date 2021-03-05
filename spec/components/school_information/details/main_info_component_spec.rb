e67d6210e957a9d5600858fb908e29b15c796bb0
Sebastian Siemianowski <sebastian.siemianowski@digital.education.gov.uk>
2021-03-05 11:46:52 +0000

    Updates main info component spec

--- spec/components/school_information/details/main_info_component_spec.rb
+++ spec/components/school_information/details/main_info_component_spec.rb
@@ -3,23 +3,38 @@
 require "rails_helper"
 
 RSpec.describe ::SchoolInformation::Details::MainInfoComponent, type: :component do
-  let(:school){ create(:school) }
-  let(:rendered_component){ render_inline(described_class.new(school: school)).to_html }
+  let(:local_authority) { create(:local_authority, name: 'York') }
+  let(:school) { create(:school) }
+  let(:rendered_component) { render_inline(described_class.new(school: school)).to_html }
 
-  it 'Contains School Information text' do
-    expect(rendered_component).to include "School information"
+  context 'with local authority assigned' do
+    before(:each) do
+      create(:school_local_authority, school: school, local_authority: local_authority, start_year: 2021)
+    end
+
+    it 'Contains School Information text' do
+      expect(rendered_component).to include "School information"
+    end
+
+    it 'Contains URN' do
+      expect(rendered_component).to include "URN"
+      expect(rendered_component).to include "#{school.urn}"
+    end
+
+    it 'Contains Local Authority' do
+      expect(rendered_component).to include "Local authority"
+      expect(rendered_component).to include "#{school.local_authority.name}"
+    end
+
+    it 'Contains School contact' do
+      expect(rendered_component).to include "School contact"
+    end
   end
 
-  it 'Contains URN' do
-    expect(rendered_component).to include "URN"
-    expect(rendered_component).to include "#{school.urn}"
-  end
-
-  it 'Contains Local Authority' do
-    expect(rendered_component).to include "Local authority"
-  end
-
-  it 'Contains School contact' do
-    expect(rendered_component).to include "School contact"
+  context 'without local authority assigned' do
+    it 'Contains Local Authority' do
+      expect(rendered_component).to include "Local authority"
+      expect(rendered_component).to include "No local authority assigned"
+    end
   end
 end
