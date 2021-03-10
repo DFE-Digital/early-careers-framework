78702f80498ae7c10111ef7d63cbffc38f2114a9
Ethan Mills <ethan.mills@softwire.com>
2021-03-10 11:42:46 +0000

    ECF-RP-315: Add unit tests for cohorts_with_lead_provider

--- spec/models/delivery_partner_spec.rb
+++ spec/models/delivery_partner_spec.rb
@@ -14,6 +14,29 @@ RSpec.describe DeliveryPartner, type: :model do
     it { is_expected.to have_many(:lead_providers).through(:provider_relationships) }
   end
 
+  describe "#cohorts_with_lead_provider" do
+    let(:delivery_partner) { create(:delivery_partner) }
+    let(:partnered_cohort) { create(:cohort) }
+    let(:unpartnered_cohort) { create(:cohort) }
+    let(:lead_provider) { create(:lead_provider, cohorts: [partnered_cohort, unpartnered_cohort]) }
+
+    before do
+      ProviderRelationship.create!(
+        delivery_partner: delivery_partner,
+        lead_provider: lead_provider,
+        cohort: partnered_cohort,
+      )
+    end
+
+    it "includes a cohort for a lead provider where a provider relationship exists" do
+      expect(delivery_partner.cohorts_with_provider(lead_provider)).to include(partnered_cohort)
+    end
+
+    it "does not include a cohort for a lead provider where no provider relationship exists" do
+      expect(delivery_partner.cohorts_with_provider(lead_provider)).not_to include(unpartnered_cohort)
+    end
+  end
+
   describe "soft delete" do
     let!(:delivery_partner) { create(:delivery_partner) }
     it "can be discarded" do
