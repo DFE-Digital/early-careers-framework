f52286a1d8ea205bb1fae4c15640f62a9bc271a5
Sebastian Siemianowski <sebastian.siemianowski@digital.education.gov.uk>
2021-03-12 11:50:12 +0000

    Removes profiles

--- config/routes.rb
+++ config/routes.rb
@@ -94,12 +94,6 @@ Rails.application.routes.draw do
         end
       end
     end
-    scope :profiles, module: "profiles" do
-      root to: redirect("/admin/profiles/admin-profiles")
-      resources :admin_profiles, only: %i[index show destroy], path: "/admin-profiles"
-      resources :induction_coordinator_profiles, only: %i[index show destroy], path: "/induction-coordinator-profiles"
-      resources :lead_provider_profiles, only: %i[index show destroy], path: "/lead-provider-profiles"
-    end
 
     scope :administrators, module: "administrators" do
       resources :administrators, only: %i[index new create edit update destroy delete], path: "/" do
