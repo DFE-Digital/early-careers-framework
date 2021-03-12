b3dfc7dd2e49690e547dda482be7f80534fcc377
Ethan Mills <40488007+ethanmills@users.noreply.github.com>
2021-03-12 11:15:13 +0000

    ECF-RP-332: create supplier updates (#131)
    * ECF-RP-332: Remove lead provider creation and success page
    
    * ECF-RP-332: Remove dead code

--- spec/support/new_supplier_helper.rb
+++ spec/support/new_supplier_helper.rb
@@ -2,40 +2,12 @@
 
 module NewSupplierHelper
   # rubocop:disable Naming/MethodName
-  def when_I_choose_a_supplier_name(name)
-    get "/admin/suppliers/new"
-    post "/admin/suppliers/new", params: { new_supplier_form: { name: name } }
+  def when_I_choose_a_delivery_partner_name(name)
+    get "/admin/suppliers/new/delivery-partner/choose-name"
+    post "/admin/suppliers/new/delivery-partner/choose-name", params: { delivery_partner_form: { name: name } }
   end
 
-  alias_method :given_I_have_chosen_supplier_name, :when_I_choose_a_supplier_name
-
-  def when_I_choose_a_delivery_partner_type
-    get "/admin/suppliers/new/supplier-type"
-    post "/admin/suppliers/new/supplier-type", params: { new_supplier_form: { type: "delivery_partner" } }
-  end
-
-  alias_method :given_I_have_chosen_delivery_partner_type, :when_I_choose_a_delivery_partner_type
-
-  def when_I_choose_a_lead_provider_type
-    get "/admin/suppliers/new/supplier-type"
-    post "/admin/suppliers/new/supplier-type", params: { new_supplier_form: { type: "lead_provider" } }
-  end
-
-  alias_method :given_I_have_chosen_lead_provider_type, :when_I_choose_a_lead_provider_type
-
-  def when_I_choose_a_cip(cip)
-    get "/admin/suppliers/new/lead-provider/choose-cip"
-    post "/admin/suppliers/new/lead-provider/choose-cip", params: { lead_provider_form: { cip: cip.id } }
-  end
-
-  alias_method :given_I_have_chosen_cip, :when_I_choose_a_cip
-
-  def when_I_choose_cohorts(cohorts)
-    get "/admin/suppliers/new/lead-provider/choose-cohorts"
-    post "/admin/suppliers/new/lead-provider/choose-cohorts", params: { lead_provider_form: { cohorts: cohorts.map(&:id) } }
-  end
-
-  alias_method :given_I_have_chosen_cohorts, :when_I_choose_cohorts
+  alias_method :given_I_have_chosen_delivery_partner_name, :when_I_choose_a_delivery_partner_name
 
   def when_I_choose_lps_and_cohorts(lead_providers, cohorts)
     get "/admin/suppliers/new/delivery-partner/choose-lps"
@@ -55,11 +27,6 @@ module NewSupplierHelper
 
   alias_method :given_I_have_confirmed_my_choices, :when_I_confirm_my_choices
 
-  def when_I_confirm_my_lead_provider_choices
-    post "/admin/suppliers/new/lead-provider", params: {}
-  end
-
-  alias_method :given_I_have_confirmed_my_lead_provider_choices, :when_I_confirm_my_lead_provider_choices
   # rubocop:enable Naming/MethodName
 
   def provider_relationship_value(lead_provider, cohort)
