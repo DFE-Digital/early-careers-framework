c5701f8c236387be5eb26428df1c0e63cc69d52b
Sebastian Siemianowski <sebastian.siemianowski@digital.education.gov.uk>
2021-03-12 11:14:31 +0000

    Adds the specs

--- spec/requests/admin/lead_providers/lead_provider_users_spec.rb
+++ spec/requests/admin/lead_providers/lead_provider_users_spec.rb
@@ -5,10 +5,14 @@ require "rails_helper"
 RSpec.describe "Admin::LeadProviders::LeadProviderUsers", type: :request do
   let(:lead_provider_profile) { create(:lead_provider_profile) }
   let(:lead_provider_user) { lead_provider_profile.user }
+  let(:lead_provider_user_two) { create(:user, :lead_provider, full_name: "Emma Dow", email: "emma-dow@example.com") }
+  let(:lead_provider_profile_two) { lead_provider_user_two.lead_provider_profile }
 
   before do
     admin_user = create(:user, :admin)
     sign_in admin_user
+    lead_provider_user
+    lead_provider_user_two
   end
 
   describe "GET /admin/suppliers/lead_providers/users/:id/edit" do
@@ -20,7 +24,7 @@ RSpec.describe "Admin::LeadProviders::LeadProviderUsers", type: :request do
     end
   end
 
-  describe "PATCH /admin/suppliers/lead_providers/:id/users/:id" do
+  describe "PATCH /admin/suppliers/lead_providers/users/:id" do
     let(:email) { "user@example.com" }
 
     it "updates the user and redirects to users page" do
@@ -42,6 +46,24 @@ RSpec.describe "Admin::LeadProviders::LeadProviderUsers", type: :request do
         expect(response.body).to include("Enter an email")
         expect(response).to render_template("admin/suppliers/lead_provider_users/edit")
       end
+    end
+  end
+
+  describe "DELETE /admin/suppliers/lead_providers/users/:id/" do
+    it "marks the lead_provider profile as deleted" do
+      delete "/admin/suppliers/lead-providers/users/#{lead_provider_user_two.id}"
+
+      lead_provider_profile_two.reload
+      lead_provider_user_two.reload
+      expect(lead_provider_profile_two.discarded?).to be true
+      expect(lead_provider_user_two.discarded?).to be true
+    end
+
+    it "redirects to the lead_provider users index page" do
+      delete "/admin/suppliers/lead-providers/users/#{lead_provider_user_two.id}"
+
+      expect(response).to redirect_to("/admin/suppliers/users?user_deleted=true")
+      expect(response.body).not_to include(CGI.escapeHTML(lead_provider_profile_two.user.full_name))
     end
   end
 end
