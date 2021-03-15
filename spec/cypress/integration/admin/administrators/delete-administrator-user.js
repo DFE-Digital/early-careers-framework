d52e8a4148510933307b10f95119c23a064d1b47
Sebastian Siemianowski <sebastian.siemianowski@digital.education.gov.uk>
2021-03-15 10:55:20 +0000

    changes data-cy to data test

--- spec/cypress/integration/admin/administrators/delete-administrator-user.js
+++ spec/cypress/integration/admin/administrators/delete-administrator-user.js
@@ -9,8 +9,8 @@ describe("Admin user deleting another admin user", () => {
     cy.appScenario("/admin/administrators/create_admin_user");
     cy.visit("/admin/administrators");
     cy.get("main").should("contain", adminUserName);
-    cy.get("[data-cy=edit-admin-link]").contains(adminUserName).click();
-    cy.get("[data-cy=delete-button]").click();
+    cy.get("[data-test=edit-admin-link]").contains(adminUserName).click();
+    cy.get("[data-test=delete-button]").click();
     cy.get("main").should("contain", "Do you want to delete this user?");
     cy.get("main").should("contain", "Admin user: Emma Dow");
     cy.get(".govuk-button--warning").click();
--- spec/cypress/integration/admin/suppliers/delete-delivery-partner-user.js
+++ spec/cypress/integration/admin/suppliers/delete-delivery-partner-user.js
@@ -10,10 +10,10 @@ describe("Admin user deleting delivery partner user", () => {
     cy.visit("/admin/suppliers");
     cy.get("a").contains("All users").click();
     cy.get("main").should("contain", deliveryPartnerName);
-    cy.get("[data-cy=edit-supplier-user-link]")
+    cy.get("[data-test=edit-supplier-user-link]")
       .contains(deliveryPartnerName)
       .click();
-    cy.get("[data-cy=delete-button]").click();
+    cy.get("[data-test=delete-button]").click();
     cy.get("main").should("contain", "Do you want to delete this user?");
     cy.get("main").should("contain", "Supplier user: John Wick");
     cy.get(".govuk-button--warning").click();
