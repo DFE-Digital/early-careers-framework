18b30981eed8143fb1b234b0cf27da4bab7baaa5
Sebastian Siemianowski <sebastian.siemianowski@digital.education.gov.uk>
2021-03-15 10:51:22 +0000

    modifies cypress test

--- spec/cypress/integration/admin/administrators/delete-administrator-user.js
+++ spec/cypress/integration/admin/administrators/delete-administrator-user.js
@@ -9,9 +9,11 @@ describe("Admin user deleting another admin user", () => {
     cy.appScenario("/admin/administrators/create_admin_user");
     cy.visit("/admin/administrators");
     cy.get("main").should("contain", adminUserName);
-    cy.get(".cypress-test-edit-admin-link").contains(adminUserName).click();
-    cy.get(".cypress-test-delete-button").click();
-    cy.get(".cypress-test-delete-button").click();
+    cy.get("[data-cy=edit-admin-link]").contains(adminUserName).click();
+    cy.get("[data-cy=delete-button]").click();
+    cy.get("main").should("contain", "Do you want to delete this user?");
+    cy.get("main").should("contain", "Admin user: Emma Dow");
+    cy.get(".govuk-button--warning").click();
     cy.get("main").should("not.contain", adminUserName);
     cy.get("main").should("contain", "User deleted");
   });
--- spec/cypress/integration/admin/suppliers/delete-delivery-partner-user.js
+++ spec/cypress/integration/admin/suppliers/delete-delivery-partner-user.js
@@ -10,11 +10,13 @@ describe("Admin user deleting delivery partner user", () => {
     cy.visit("/admin/suppliers");
     cy.get("a").contains("All users").click();
     cy.get("main").should("contain", deliveryPartnerName);
-    cy.get(".cypress-test-edit-supplier-user-link")
+    cy.get("[data-cy=edit-supplier-user-link]")
       .contains(deliveryPartnerName)
       .click();
-    cy.get(".cypress-test-delete-button").click();
-    cy.get(".cypress-test-delete-button").click();
+    cy.get("[data-cy=delete-button]").click();
+    cy.get("main").should("contain", "Do you want to delete this user?");
+    cy.get("main").should("contain", "Supplier user: John Wick");
+    cy.get(".govuk-button--warning").click();
     cy.get("main").should("not.contain", deliveryPartnerName);
     cy.get("main").should("contain", "User deleted");
   });
