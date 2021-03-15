f8d91f22f0cd00f49f57475122f8cacae863dc1f
Ethan Mills <ethan.mills@softwire.com>
2021-03-15 15:09:48 +0000

    Update back links

--- spec/cypress/integration/admin/suppliers/delete-delivery-partner.js
+++ spec/cypress/integration/admin/suppliers/delete-delivery-partner.js
@@ -24,4 +24,23 @@ describe("Admin user deleting delivery partner", () => {
     cy.location("pathname").should("equal", "/admin/suppliers");
     cy.get("main").should("not.contain", deliveryPartnerName);
   });
+
+  it("has a back button to the edit page", () => {
+    cy.appScenario("admin/suppliers/manage_delivery_partner");
+
+    cy.visit("/admin/suppliers");
+    cy.get("a").contains(deliveryPartnerName).click();
+
+    cy.location("pathname").should(
+      "contain",
+      "/admin/suppliers/delivery-partner"
+    );
+    cy.get(".govuk-button").contains("Delete").click();
+
+    cy.location("pathname").should("contain", "/delete");
+
+    cy.get(".govuk-button").contains("Back").click();
+    cy.location("pathname").should("contain", "Edit");
+    cy.get("main").should("contain", deliveryPartnerName);
+  });
 });
