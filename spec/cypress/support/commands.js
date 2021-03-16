524c68c092e912599726e980b790199b01496459
Michal Kreft <michal.kreft@softwire.com>
2021-03-16 17:32:00 +0000

    RP-151-2: Remove unused email verification methods from cypress

--- spec/cypress/support/commands.js
+++ spec/cypress/support/commands.js
@@ -49,8 +49,6 @@ Cypress.Commands.add("clickCommitButton", () => {
 });
 
 const SIGN_IN_EMAIL_TEMPLATE = "7ab8db5b-9842-4bc3-8dbb-f590a3198d9e";
-const EMAIL_CONFIRMATION_TEMPLATE = "50059d26-c65d-4e88-831a-8bfb9f4116cd";
-const PRIMARY_CONTACT_TEMPLATE = "a7cc4d19-c0cb-4187-a71b-1b1ea029924f";
 
 const computeHeadersFromEmail = (email) =>
   email.header.reduce(
@@ -68,26 +66,5 @@ Cypress.Commands.add("verifySignInEmailSent", (user) => {
     expect(headersHash["template-id"]).to.eq(SIGN_IN_EMAIL_TEMPLATE);
     expect(headersHash.personalisation.full_name).to.eq(user.full_name);
     expect(headersHash.To).to.eq(user.email);
-  });
-});
-
-Cypress.Commands.add("verifyConfirmationEmailSent", (user) => {
-  cy.appEval("ActionMailer::Base.deliveries").then((emails) => {
-    expect(emails).to.have.lengthOf(1);
-    const headersHash = computeHeadersFromEmail(emails[0]);
-    expect(headersHash["template-id"]).to.eq(EMAIL_CONFIRMATION_TEMPLATE);
-    expect(headersHash.personalisation.full_name).to.eq(user.full_name);
-    expect(headersHash.To).to.eq(user.email);
-  });
-});
-
-Cypress.Commands.add("verifyPrimaryContactEmailSent", (coordinator, email) => {
-  cy.appEval("ActionMailer::Base.deliveries").then((emails) => {
-    expect(emails).to.have.lengthOf(1);
-    const headersHash = computeHeadersFromEmail(emails[0]);
-    expect(headersHash["template-id"]).to.eq(PRIMARY_CONTACT_TEMPLATE);
-    expect(headersHash.personalisation.full_name).to.eq(coordinator.full_name);
-    expect(headersHash.personalisation.email).to.eq(coordinator.email);
-    expect(headersHash.To).to.eq(email);
   });
 });
