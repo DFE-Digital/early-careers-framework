b4f2c90fc02e8782d4054fd287ac77c620efbfc1
Michal Kreft <michal.kreft@softwire.com>
2021-03-16 12:38:05 +0000

    RP-151: Add unit tests for confirmation emails of induction coordinators

--- spec/requests/registrations/user_profile_spec.rb
+++ spec/requests/registrations/user_profile_spec.rb
@@ -23,38 +23,103 @@ RSpec.describe "Registrations::UserProfile", type: :request do
   end
 
   describe "POST /registrations/user-profile" do
-    it "creates an induction coordinator profile" do
-      post "/registrations/user-profile", params: { user: {
-        full_name: full_name,
-        email: email,
-      } }
+    before do
+      mail = instance_double(ActionMailer::MessageDelivery, deliver_now: true)
+      allow(UserMailer).to receive(:confirmation_instructions).and_return(mail)
+    end
 
-      expect(created_user.full_name).to eq(full_name)
-      expect(response).to redirect_to(:registrations_verification_sent)
-      expect(created_user.induction_coordinator_profile.schools).to include(school)
+    context "when request is valid" do
+      let(:send_request) do
+        post "/registrations/user-profile", params: { user: {
+          full_name: full_name,
+          email: email,
+        } }
+      end
+
+      it "creates an induction coordinator profile" do
+        send_request
+        expect(created_user.full_name).to eq(full_name)
+        expect(created_user.induction_coordinator_profile.schools).to include(school)
+      end
+
+      it "redirects to verification sent page" do
+        send_request
+        expect(response).to redirect_to(:registrations_verification_sent)
+      end
+
+      it "sends a confirmation email" do
+        send_request
+        expect(UserMailer).to have_received(:confirmation_instructions).with(created_user, /.*/, {})
+      end
+
+      context "when school has old unconfirmed registrations" do
+        let(:user) { create(:user, confirmed_at: nil) }
+        let!(:coordinator) { create(:induction_coordinator_profile, user: user, schools: [school]) }
+
+        before do
+          user.update!(confirmation_sent_at: 2.days.ago)
+        end
+
+        it "registers new induction coordinator" do
+          send_request
+          expect(school.reload.induction_coordinator_profiles).to include created_user.induction_coordinator_profile
+        end
+
+        it "removes the old coordinator profiles" do
+          send_request
+          expect(school.induction_coordinator_profiles).not_to include user.reload.induction_coordinator_profile
+        end
+
+        it "sends a confirmation email to the new induction coordinator" do
+          send_request
+          expect(UserMailer).to have_received(:confirmation_instructions).with(created_user, /.*/, {})
+        end
+      end
     end
 
     context "when email is missing" do
+      let(:send_request) do
+        post "/registrations/user-profile", params: { user: {
+          full_name: full_name,
+        } }
+      end
+
       it "does not create the user" do
-        expect {
-          post "/registrations/user-profile", params: { user: {
-            full_name: full_name,
-          } }
-        }.not_to(change { User.count })
+        expect { send_request }.not_to(change { User.count })
+      end
+
+      it "shows validation error" do
+        send_request
         expect(response.body).to include("Enter an email")
         expect(response).to render_template("registrations/user_profile/new")
+      end
+
+      it "does not send a confirmation email" do
+        send_request
+        expect(UserMailer).not_to have_received(:confirmation_instructions)
       end
     end
 
     context "when full name is missing" do
+      let(:send_request) do
+        post "/registrations/user-profile", params: { user: {
+          email: email,
+        } }
+      end
+
       it "does not create the user" do
-        expect {
-          post "/registrations/user-profile", params: { user: {
-            email: email,
-          } }
-        }.not_to(change { User.count })
+        expect { send_request }.not_to(change { User.count })
+      end
+
+      it "shows validation error" do
+        send_request
         expect(response.body).to include("Enter a full name")
         expect(response).to render_template("registrations/user_profile/new")
+      end
+
+      it "does not send a confirmation email" do
+        send_request
+        expect(UserMailer).not_to have_received(:confirmation_instructions)
       end
     end
 
@@ -72,23 +137,7 @@ RSpec.describe "Registrations::UserProfile", type: :request do
             email: email,
           } }
         }.to raise_error(ActionController::BadRequest)
-      end
-    end
-
-    context "when school has old unconfirmed registrations" do
-      let(:user) { create(:user, confirmed_at: nil) }
-      let!(:coordinator) { create(:induction_coordinator_profile, user: user, schools: [school]) }
-
-      before { user.update(confirmation_sent_at: 2.days.ago) }
-
-      it "registers new induction coordinator and removes the old coordinator profiles" do
-        post "/registrations/user-profile", params: { user: {
-          full_name: full_name,
-          email: email,
-        } }
-
-        expect(school.reload.induction_coordinator_profiles).to include created_user.induction_coordinator_profile
-        expect(school.induction_coordinator_profiles).not_to include user.reload.induction_coordinator_profile
+        expect(UserMailer).not_to have_received(:confirmation_instructions)
       end
     end
   end
