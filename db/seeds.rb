c651e0873cf1f0e1772674e979d9a1f706d05dc6
Ethan Mills <ethan.mills@softwire.com>
2021-03-17 16:53:42 +0000

    ECF-RP-379: Move environment check to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 Dir[Rails.root.join("db/seeds/initial_seed.rb")].each { |seed| load seed }
-Dir[Rails.root.join("db/seeds/dummy_structures.rb")].each { |seed| load seed }
+
+if Rails.env.development? || Rails.env.deployed_development?
+  Dir[Rails.root.join("db/seeds/dummy_structures.rb")].each { |seed| load seed }
+end
--- db/seeds/dummy_structures.rb
+++ db/seeds/dummy_structures.rb
@@ -1,44 +1,24 @@
 # frozen_string_literal: true
 
-if Rails.env.development? || Rails.env.deployed_development?
-  SchoolDataImporter.new(Rails.logger).delay.run
+SchoolDataImporter.new(Rails.logger).delay.run
 
-  standard_user_emails = ["admin@example.com", "lead-provider@example.com", "second-school-leader@example.com", "school-leader@example.com"]
-  (1..5).each do |number|
-    standard_user_emails << "second-admin-#{number}@example.com"
-    standard_user_emails << "second-lead-provider-#{number}@example.com"
-    standard_user_emails << "second-school-leader-#{number}@example.com"
-  end
+standard_user_emails = ["admin@example.com", "lead-provider@example.com", "second-school-leader@example.com", "school-leader@example.com"]
+(1..5).each do |number|
+  standard_user_emails << "second-admin-#{number}@example.com"
+  standard_user_emails << "second-lead-provider-#{number}@example.com"
+  standard_user_emails << "second-school-leader-#{number}@example.com"
+end
 
-  User.with_discarded.where(email: standard_user_emails).each do |user|
-    user.undiscard
-    user.admin_profile&.undiscard
-    user.lead_provider_profile&.undiscard
-    user.induction_coordinator_profile&.undiscard
-  end
+User.with_discarded.where(email: standard_user_emails).each do |user|
+  user.undiscard
+  user.admin_profile&.undiscard
+  user.lead_provider_profile&.undiscard
+  user.induction_coordinator_profile&.undiscard
+end
 
-  (1..5).each do |number|
-    user = User.with_discarded.find_or_create_by!(email: "second-admin-#{number}@example.com") do |u|
-      u.full_name = "John Doe - #{number}"
-      u.confirmed_at = Time.zone.now.utc
-    end
-
-    user.undiscard
-
-    AdminProfile.with_discarded.find_or_create_by!(user: user).undiscard
-
-    user = User.with_discarded.find_or_create_by!(email: "second-lead-provider-#{number}@example.com") do |u|
-      u.full_name = "Merry Doe - #{number}"
-      u.confirmed_at = Time.zone.now.utc
-    end
-
-    user.undiscard
-
-    LeadProviderProfile.with_discarded.find_or_create_by!(user: user, lead_provider: LeadProvider.first).undiscard
-  end
-
-  user = User.with_discarded.find_or_create_by!(email: "admin@example.com") do |u|
-    u.full_name = "Admin User"
+(1..5).each do |number|
+  user = User.with_discarded.find_or_create_by!(email: "second-admin-#{number}@example.com") do |u|
+    u.full_name = "John Doe - #{number}"
     u.confirmed_at = Time.zone.now.utc
   end
 
@@ -46,54 +26,72 @@ if Rails.env.development? || Rails.env.deployed_development?
 
   AdminProfile.with_discarded.find_or_create_by!(user: user).undiscard
 
-  user = User.find_or_create_by!(email: "lead-provider@example.com") do |u|
-    u.full_name = "Lp User"
+  user = User.with_discarded.find_or_create_by!(email: "second-lead-provider-#{number}@example.com") do |u|
+    u.full_name = "Merry Doe - #{number}"
     u.confirmed_at = Time.zone.now.utc
   end
 
   user.undiscard
 
-  LeadProviderProfile.find_or_create_by!(user: user, lead_provider: LeadProvider.first).undiscard
+  LeadProviderProfile.with_discarded.find_or_create_by!(user: user, lead_provider: LeadProvider.first).undiscard
+end
 
-  school_urns_twenty_twenty_one = %w[136089 105448 128702 113280 138229 143094 140667 127834 146786 113199 126346 133936 132971 107126 102887 102418 129369 140980 116848 112236]
+user = User.with_discarded.find_or_create_by!(email: "admin@example.com") do |u|
+  u.full_name = "Admin User"
+  u.confirmed_at = Time.zone.now.utc
+end
 
-  School.where(urn: school_urns_twenty_twenty_one).each do |school|
-    Partnership.find_or_create_by!(school: school, lead_provider: LeadProvider.first, cohort: Cohort.find_or_create_by!(start_year: 2021))
-  end
+user.undiscard
 
-  school_urns_twenty_twenty_two = %w[119378 134847 113870 127979 144744 121499 147505 105626 402027 100173]
+AdminProfile.with_discarded.find_or_create_by!(user: user).undiscard
 
-  School.where(urn: school_urns_twenty_twenty_two).each do |school|
-    Partnership.find_or_create_by!(school: school, lead_provider: LeadProvider.first, cohort: Cohort.find_or_create_by!(start_year: 2022))
-  end
+user = User.find_or_create_by!(email: "lead-provider@example.com") do |u|
+  u.full_name = "Lp User"
+  u.confirmed_at = Time.zone.now.utc
+end
 
-  user = User.with_discarded.find_or_create_by!(email: "second-school-leader@example.com") do |u|
-    u.full_name = "School Leader User - Induction Coordinator"
-    u.confirmed_at = Time.zone.now.utc
-  end
+user.undiscard
 
-  user.undiscard
+LeadProviderProfile.find_or_create_by!(user: user, lead_provider: LeadProvider.first).undiscard
 
-  if School.any?
-    InductionCoordinatorProfile.joins(:schools).find_or_create_by!(user: user, schools: [School.first]).undiscard
-  end
+school_urns_twenty_twenty_one = %w[136089 105448 128702 113280 138229 143094 140667 127834 146786 113199 126346 133936 132971 107126 102887 102418 129369 140980 116848 112236]
 
-  user = User.with_discarded.find_or_create_by!(email: "school-leader@example.com") do |u|
-    u.full_name = "School Leader User"
-    u.confirmed_at = Time.zone.now.utc
-  end
+School.where(urn: school_urns_twenty_twenty_one).each do |school|
+  Partnership.find_or_create_by!(school: school, lead_provider: LeadProvider.first, cohort: Cohort.find_or_create_by!(start_year: 2021))
+end
 
-  user.undiscard
+school_urns_twenty_twenty_two = %w[119378 134847 113870 127979 144744 121499 147505 105626 402027 100173]
 
-  if School.any?
-    InductionCoordinatorProfile.joins(:schools).find_or_create_by!(user: user, schools: [School.first]).undiscard
-  end
+School.where(urn: school_urns_twenty_twenty_two).each do |school|
+  Partnership.find_or_create_by!(school: school, lead_provider: LeadProvider.first, cohort: Cohort.find_or_create_by!(start_year: 2022))
+end
 
-  user = User.find_or_create_by!(email: "early-career-teacher@example.com") do |u|
-    u.full_name = "ECT User"
-    u.confirmed_at = Time.zone.now.utc
-  end
-  if School.any?
-    EarlyCareerTeacherProfile.find_or_create_by!(user: user, school: School.first, cohort: Cohort.first, core_induction_programme: CoreInductionProgramme.first)
-  end
+user = User.with_discarded.find_or_create_by!(email: "second-school-leader@example.com") do |u|
+  u.full_name = "School Leader User - Induction Coordinator"
+  u.confirmed_at = Time.zone.now.utc
+end
+
+user.undiscard
+
+if School.any?
+  InductionCoordinatorProfile.joins(:schools).find_or_create_by!(user: user, schools: [School.first]).undiscard
+end
+
+user = User.with_discarded.find_or_create_by!(email: "school-leader@example.com") do |u|
+  u.full_name = "School Leader User"
+  u.confirmed_at = Time.zone.now.utc
+end
+
+user.undiscard
+
+if School.any?
+  InductionCoordinatorProfile.joins(:schools).find_or_create_by!(user: user, schools: [School.first]).undiscard
+end
+
+user = User.find_or_create_by!(email: "early-career-teacher@example.com") do |u|
+  u.full_name = "ECT User"
+  u.confirmed_at = Time.zone.now.utc
+end
+if School.any?
+  EarlyCareerTeacherProfile.find_or_create_by!(user: user, school: School.first, cohort: Cohort.first, core_induction_programme: CoreInductionProgramme.first)
 end
