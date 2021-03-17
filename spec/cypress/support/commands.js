d97e7322076ed5c4c239d679a269bc3241df60e9
Michal Kreft <42969249+kicferk1@users.noreply.github.com>
2021-03-17 15:31:12 +0000

    Email testing in cypress (#143)
    * RP-151-2: Add an email test in cypress
    
    * Prettier somehow didn't work in our hook
    
    * RP-151-2: Remove unused email verification methods from cypress
    
    * RP-151-2: Separate getting sent emails into a cypress command

--- spec/cypress/support/commands.js
+++ spec/cypress/support/commands.js
@@ -48,6 +48,30 @@ Cypress.Commands.add("clickCommitButton", () => {
   cy.get("[name=commit]").click();
 });
 
+const SIGN_IN_EMAIL_TEMPLATE = "7ab8db5b-9842-4bc3-8dbb-f590a3198d9e";
+
+const computeHeadersFromEmail = (email) =>
+  email.header.reduce(
+    (hashSoFar, element) => ({
+      ...hashSoFar,
+      [element.name]: element.unparsed_value,
+    }),
+    {}
+  );
+Cypress.Commands.add("appSentEmails", () =>
+  cy.appEval("ActionMailer::Base.deliveries")
+);
+
+Cypress.Commands.add("verifySignInEmailSent", (user) => {
+  cy.appSentEmails().then((emails) => {
+    expect(emails).to.have.lengthOf(1);
+    const headersHash = computeHeadersFromEmail(emails[0]);
+    expect(headersHash["template-id"]).to.eq(SIGN_IN_EMAIL_TEMPLATE);
+    expect(headersHash.personalisation.full_name).to.eq(user.full_name);
+    expect(headersHash.To).to.eq(user.email);
+  });
+});
+
 Cypress.Commands.add("chooseNameAndEmailForUser", (name, email) => {
   cy.get("input[name*=full_name").type(name);
   cy.get("input[name*=email").type(email);
