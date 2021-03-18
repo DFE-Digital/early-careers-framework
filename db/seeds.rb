2fe4eee80d98dc15ae75181e51f70061736cb7d8
Ethan Mills <ethan.mills@softwire.com>
2021-03-18 11:24:47 +0000

    ECF-RP-379: Change seeds to require_relative

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-Dir[Rails.root.join("db/seeds/initial_seed.rb")].each { |seed| load seed }
+require_relative "seeds/initial_seed"
 
 if Rails.env.development? || Rails.env.deployed_development?
-  Dir[Rails.root.join("db/seeds/dummy_structures.rb")].each { |seed| load seed }
+  require_relative "seeds/dummy_structures"
 end
