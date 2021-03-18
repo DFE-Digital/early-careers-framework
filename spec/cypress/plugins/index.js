63afc88e076460bd620fe7ba8d539863841290b5
Callum Macrae <callum.macrae@digital.education.gov.uk>
2021-03-18 15:48:33 +0000

    Fix cucumber import syntax

--- spec/cypress/plugins/index.js
+++ spec/cypress/plugins/index.js
@@ -12,7 +12,7 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-import cucumber from "cypress-cucumber-preprocessor";
+const cucumber = require("cypress-cucumber-preprocessor").default;
 
 /**
  * @type {Cypress.PluginConfig}
