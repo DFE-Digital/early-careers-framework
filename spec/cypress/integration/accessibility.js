5567272b4dc13e6135d667d67024498e18eb6223
Ethan Mills <ethan.mills@softwire.com>
2021-03-19 12:07:36 +0000

    ECF-RP-329: Extract title matching to helper

--- spec/cypress/integration/accessibility.js
+++ spec/cypress/integration/accessibility.js
@@ -10,7 +10,7 @@ describe("Accessibility", () => {
 
     // School not registered page
     cy.get('[name="user[email]"]').type("doesntexist@example.com{enter}");
-    cy.title().should("equal", "Check your email - Early career framework");
+    cy.titleShouldEqual("Check your email");
     cy.checkA11y();
     cy.appSentEmails().then((emails) => {
       expect(emails).to.have.lengthOf(0);
@@ -24,7 +24,7 @@ describe("Accessibility", () => {
       });
 
     cy.get('[name="commit"]').contains("Sign in").click();
-    cy.title().should("equal", "Check your email - Early career framework");
+    cy.titleShouldEqual("Check your email");
     cy.checkA11y();
 
     cy.get("@userData").then(([user]) => {
