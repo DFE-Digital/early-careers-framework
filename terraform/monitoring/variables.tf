3ae4d52d1a48291d9cf4fabc09d8d10d18e260a2
Ethan Mills <40488007+ethanmills@users.noreply.github.com>
2021-03-23 14:59:28 +0000

    Set up grafana monitoring for dev (#172)
    * Set up grafana monitoring for dev
    
    * Markups

---
+++ terraform/monitoring/variables.tf
@@ -0,0 +1,24 @@
+variable paas_api_url {
+  default = "https://api.london.cloud.service.gov.uk"
+}
+
+variable paas_space_name {
+}
+
+variable monitoring_instance_name {
+}
+
+variable paas_user {
+}
+
+variable paas_password {
+}
+
+variable google_client_id {
+}
+
+variable google_client_secret {
+}
+
+variable grafana_admin_password {
+}
