9dbf33e6e52f1725be1ee857db470e8615cf73d7
Alfred Mazimbe <alfred@404-digital.co.uk>
2021-03-26 18:45:20 +0000

    added school name to nomination email

--- app/mailers/school_mailer.rb
+++ app/mailers/school_mailer.rb
@@ -3,15 +3,16 @@
 class SchoolMailer < ApplicationMailer
   NOMINATION_EMAIL_TEMPLATE = "a7cc4d19-c0cb-4187-a71b-1b1ea029924f"
 
-  def nomination_email(recipient_email, nomination_url, reference)
+  def nomination_email(params)
     template_mail(
       NOMINATION_EMAIL_TEMPLATE,
-      to: recipient_email,
-      reference: reference,
+      to: params[:recipient],
+      reference: params[:reference],
       rails_mailer: mailer_name,
       rails_mail_template: action_name,
       personalisation: {
-        nomination_link: nomination_url,
+        school_name: params[:school_name],
+        nomination_link: params[:nomination_url],
       },
     )
   end
--- spec/mailers/school_mailer_spec.rb
+++ spec/mailers/school_mailer_spec.rb
@@ -6,10 +6,15 @@ RSpec.describe SchoolMailer, type: :mailer do
   describe "#nomination_email" do
     let(:token) { "fedd83c06d5747f1" }
     let(:primary_contact_email) { "contact@example.com" }
-    let(:nomination_link) { "https://ecf-dev.london.cloudapps/nominations?token=#{token}" }
+    let(:nomination_url) { "https://ecf-dev.london.cloudapps/nominations?token=#{token}" }
 
     let(:nomination_email) do
-      SchoolMailer.nomination_email(primary_contact_email, nomination_link, token)
+      SchoolMailer.nomination_email(
+        recipient: primary_contact_email,
+        nomination_url: nomination_url,
+        reference: token,
+        school_name: "Great Ouse Academy",
+      ).deliver_now
     end
 
     it "renders the right headers" do
