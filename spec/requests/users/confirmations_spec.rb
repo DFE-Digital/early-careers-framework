7ef0c9478df8c6d1b7167418b5580552db66a7ee
Michal Kreft <michal.kreft@softwire.com>
2021-03-29 15:43:29 +0100

    ECF-RP-396: Remove null safety in tests

--- spec/requests/users/confirmations_spec.rb
+++ spec/requests/users/confirmations_spec.rb
@@ -5,7 +5,7 @@ require "rails_helper"
 RSpec.describe "Users::Confirmations", type: :request do
   let(:confirmation_token) { "soC7rF8i_BgYdUxafFcP" }
   let(:induction_coordinator) { user.induction_coordinator_profile }
-  let(:school) { induction_coordinator&.schools&.first }
+  let(:school) { induction_coordinator.schools.first }
 
   let(:user) do
     create(:user, :induction_coordinator, confirmed_at: nil, confirmation_token: confirmation_token)
@@ -48,7 +48,7 @@ RSpec.describe "Users::Confirmations", type: :request do
       end
 
       it "does not notify the school primary contact" do
-        expect(UserMailer).not_to receive(:primary_contact_notification).with(user, school)
+        expect(UserMailer).not_to receive(:primary_contact_notification).with(anything, anything)
 
         get "/users/confirmation?confirmation_token=#{confirmation_token}"
       end
