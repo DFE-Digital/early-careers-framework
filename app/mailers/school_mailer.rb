c03b878746abdf93f918e47b534878d97d362393
Alfred Mazimbe <alfred@404-digital.co.uk>
2021-03-29 10:27:57 +0100

    added school mailer keyword arguments

--- app/mailers/school_mailer.rb
+++ app/mailers/school_mailer.rb
@@ -3,16 +3,16 @@
 class SchoolMailer < ApplicationMailer
   NOMINATION_EMAIL_TEMPLATE = "a7cc4d19-c0cb-4187-a71b-1b1ea029924f"
 
-  def nomination_email(params)
+  def nomination_email(recipient:, reference:, school_name:, nomination_url:)
     template_mail(
       NOMINATION_EMAIL_TEMPLATE,
-      to: params[:recipient],
-      reference: params[:reference],
+      to: recipient,
+      reference: reference,
       rails_mailer: mailer_name,
       rails_mail_template: action_name,
       personalisation: {
-        school_name: params[:school_name],
-        nomination_link: params[:nomination_url],
+        school_name: school_name,
+        nomination_link: nomination_url,
       },
     )
   end
