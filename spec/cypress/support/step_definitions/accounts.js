b192d84abf947d636a278a039b772525eaa65552
Sebastian Siemianowski <sebastian.siemianowski@digital.education.gov.uk>
2021-03-30 23:30:58 +0100

    fixes cucumber method for calling generic factories

--- spec/cypress/support/step_definitions/accounts.js
+++ spec/cypress/support/step_definitions/accounts.js
@@ -1,5 +1,6 @@
 import { Given } from "cypress-cucumber-preprocessor/steps";
 
 Given("Following Factory set up was run {string}", (factoryString) => {
-  cy.appFactories([factoryString.split(" ")]).as("factoryData");
+  const factoryArray = [[factoryString.split(" ")]].flat;
+  cy.appFactories(factoryArray).as("factoryData");
 });
