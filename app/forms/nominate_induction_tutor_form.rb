6bab1365c0d58060bbfda665d5a12e429e5283fd
Sebastian Siemianowski <sebastian.siemianowski@digital.education.gov.uk>
2021-03-30 11:25:38 +0100

    Work in progress - adds the infrastructure for saving the induction coordinator data

---
+++ app/forms/nominate_induction_tutor_form.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+class NominateInductionTutorFormForm
+  include ActiveModel::Model
+
+  attr_accessor :full_name, :email, :token
+
+  validates :full_name, presence: { message: "Enter a name" }, on: :details
+  validates :email, presence: { message: "Enter email" }, on: :details
+
+  def save!
+    school = NominationEmail.find_by(token: token).school
+    profile = InductionCoordinatorProfile.new
+
+    user = ActiveRecord::Base.transaction do
+      user = User.create!(
+        full_name: full_name,
+        email: email,
+        confirmed_at: Time.zone.now.utc, # Question Should we confirm the user or not?
+      )
+      profile.user = user
+      profile.save!
+      school.induction_coordinator_profiles << profile
+      user
+    end
+
+    user
+  end
+end
--- app/models/nomination_email.rb
+++ app/models/nomination_email.rb
@@ -2,4 +2,14 @@
 
 class NominationEmail < ApplicationRecord
   belongs_to :school
+
+  NOMINATION_EXPIRY_TIME = 7.days
+
+  def nomination_expired?
+    sent_at > NOMINATION_EXPIRY_TIME
+  end
+
+  def tutor_already_nominated?
+    school.induction_coordinator_profiles.count.positive?
+  end
 end
