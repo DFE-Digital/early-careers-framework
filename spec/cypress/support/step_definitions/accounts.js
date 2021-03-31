79922a3832bf6462a630b978e63473606e1bdd9a
Sebastian Siemianowski <sebastian.siemianowski@digital.education.gov.uk>
2021-03-31 01:03:08 +0100

    Fixes Cucumber steps relating to running Nomination Email Factories

--- spec/cypress/support/step_definitions/accounts.js
+++ spec/cypress/support/step_definitions/accounts.js
@@ -4,3 +4,11 @@ Given("Following Factory set up was run {string}", (factoryString) => {
   const factoryArray = [[factoryString.split(" ")]].flat;
   cy.appFactories(factoryArray).as("factoryData");
 });
+
+Given(
+  "Following Factory set up was run {string} with trait {string}",
+  (factoryName, traitName) => {
+    // cy.appFactories(factoryArray).as("factoryData");
+    cy.appFactories([["create", factoryName, traitName]]).as("factoryData");
+  }
+);
