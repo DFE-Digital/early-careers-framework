70748c80ec310fe295a15ca9911a0eda9b7d393e
Sebastian Siemianowski <sebastian.siemianowski@digital.education.gov.uk>
2021-04-01 08:25:29 +0100

    Addresses coded comments around unit testing nomination email model

--- spec/models/nomination_email_spec.rb
+++ spec/models/nomination_email_spec.rb
@@ -6,4 +6,12 @@ RSpec.describe NominationEmail, type: :model do
   describe "associations" do
     it { is_expected.to belong_to(:school) }
   end
+
+  describe "expired email" do
+    let(:expired_nomination_email) { create(:nomination_email, :expired_nomination_email) }
+
+    it "shows as expired once it goes beyond expiration date" do
+      expect(expired_nomination_email.expired?).to eq true
+    end
+  end
 end
