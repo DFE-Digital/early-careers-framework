881d06e6de3e63d2d8fa8d1f39e8ecd9c165b06a
Alfred Mazimbe <alfred@404-digital.co.uk>
2021-04-01 13:48:15 +0100

    rubocop fixes

--- app/controllers/schools/partnerships_controller.rb
+++ app/controllers/schools/partnerships_controller.rb
@@ -5,12 +5,19 @@ class Schools::PartnershipsController < Schools::BaseController
   skip_after_action :verify_policy_scoped
 
   def index
-    school = current_user.induction_coordinator_profile.schools.first
+    @school = current_user.induction_coordinator_profile.schools.first
+    @partnership = Partnership.find_by(school: @school, cohort: cohort)
 
     # if school.chosen_programme?(Cohort.current)
     #   redirect_to helpers.profile_dashboard_url(current_user)
     # else
     #   @induction_choice_form = InductionChoiceForm.new
     # end
+  end
+
+private
+
+  def cohort
+    @cohort ||= Cohort.find_by(start_year: params[:id])
   end
 end
