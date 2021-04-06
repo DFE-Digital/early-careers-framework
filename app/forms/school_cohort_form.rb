bf2aa94f5400c9cb21997ae0941ead9396255af8
maxmills <8balldigitalsolutions@gmail.com>
2021-04-06 17:55:34 +0100

    rm whitespace and only_integers on cohort form

--- app/forms/school_cohort_form.rb
+++ app/forms/school_cohort_form.rb
@@ -7,6 +7,6 @@ class SchoolCohortForm
 
   validates :estimated_mentor_count, presence: { message: "Enter your expected number of mentors" }
   validates :estimated_teacher_count, presence: { message: "Enter your expected number of ECTs" }
-  validates :estimated_mentor_count, numericality: { greater_than_or_equal_to: 0, less_than: 1000 }
-  validates :estimated_teacher_count, numericality: { greater_than_or_equal_to: 0, less_than: 1000 }
+  validates :estimated_mentor_count, numericality: { greater_than_or_equal_to: 0, less_than: 1000, only_integer: true }
+  validates :estimated_teacher_count, numericality: { greater_than_or_equal_to: 0, less_than: 1000, only_integer: true }
 end
