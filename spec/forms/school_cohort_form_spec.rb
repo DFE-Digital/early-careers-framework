c63bd2101c3da9ac70576b1aaabb4cbf7aeae289
maxmills <8balldigitalsolutions@gmail.com>
2021-04-07 09:37:19 +0100

    rm estimates_spec

--- spec/forms/school_cohort_form_spec.rb
+++ spec/forms/school_cohort_form_spec.rb
@@ -3,6 +3,9 @@
 require "rails_helper"
 
 RSpec.describe SchoolCohortForm, type: :model do
+  let(:school_cohort) { create(:school_cohort) }
+  subject { described_class.new(school_cohort) }
+
   describe "validations" do
     it { is_expected.to validate_presence_of(:estimated_mentor_count).with_message("Enter your expected number of mentors") }
     it { is_expected.to validate_presence_of(:estimated_teacher_count).with_message("Enter your expected number of ECTs") }
