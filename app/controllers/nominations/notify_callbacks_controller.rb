a176960dee72d7407031ece95636763f44ed6dc7
Alfred Mazimbe <alfred@404-digital.co.uk>
2021-04-09 15:07:18 +0100

    ECF-1-402 Added notify callback (#227)
    * added notify callback
    
    * rubocop fixes
    
    * pr feedback to add logging and gracefully handle messages without a reference
    
    * rubobcop fixes
    
    * turn failed log into a 1 liner

---
+++ app/controllers/nominations/notify_callbacks_controller.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class Nominations::NotifyCallbacksController < ActionController::API
+  def create
+    return head :no_content unless params[:reference]
+
+    email = NominationEmail.find_by(token: params[:reference])
+    email.update!(notify_status: params[:status]) if email
+    log_email if failed_email?(email)
+
+    head :no_content
+  end
+
+private
+
+  def log_email
+    Rails.logger.info "Email could not be sent - notify_id: #{params[:id]}, reference: #{params[:reference]}, template_id: #{params[:template_id]}"
+  end
+
+  def failed_email?(email)
+    return false if email.nil?
+
+    params[:status] != "sending" && params[:status] != "delivered"
+  end
+end
