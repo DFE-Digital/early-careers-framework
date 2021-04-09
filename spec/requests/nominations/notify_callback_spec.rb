28daa29d5b72b4c99878a1e24a0a1ed601c4bf0f
Alfred Mazimbe <alfred@404-digital.co.uk>
2021-04-09 08:39:54 +0100

    rubocop fixes

--- spec/requests/nominations/notify_callback_spec.rb
+++ spec/requests/nominations/notify_callback_spec.rb
@@ -7,13 +7,12 @@ RSpec.describe "Nominations::NotifyCallbacks", type: :request do
     let(:nomination_email) { create(:nomination_email) }
 
     it "updates matching nomination email" do
-      post "/nominations/notify-callback", params: { 
+      post "/nominations/notify-callback", params: {
         reference: nomination_email.token,
         status: "failure",
       }
-      
+
       expect(nomination_email.reload.notify_status).to eq "failure"
     end
   end
 end
-
