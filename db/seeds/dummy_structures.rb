1d68bf4677e5f1f3c9482f63896332336c037216
Ethan Mills <ethan.mills@softwire.com>
2021-04-12 11:34:20 +0100

    Update seeds for pentest emails and users

--- db/seeds/dummy_structures.rb
+++ db/seeds/dummy_structures.rb
@@ -1,30 +1,30 @@
 # frozen_string_literal: true
 
-# SchoolDataImporter.new(Rails.logger).delay.run
-#
-# User.find_or_create_by!(email: "admin@example.com") do |user|
-#   user.update!(full_name: "Admin User")
-#   AdminProfile.find_or_create_by!(user: user)
-# end
-#
-# User.find_or_create_by!(email: "lead-provider@example.com") do |user|
-#   user.update!(full_name: "LeadProvider User")
-#   LeadProviderProfile.find_or_create_by!(user: user, lead_provider: LeadProvider.first)
-# end
-#
-# User.find_or_create_by!(email: "school-leader@example.com") do |user|
-#   user.update!(full_name: "InductionTutor User")
-#   school = School.find_or_create_by!(
-#     name: "Example school",
-#     postcode: "BB1 1BB",
-#     address_line1: "3 Madeup Street",
-#     primary_contact_email: "school-info@example.com",
-#     school_status_code: 1,
-#     school_type_code: 1,
-#     administrative_district_code: "E123",
-#     urn: "999999",
-#   )
-#   InductionCoordinatorProfile.find_or_create_by!(user: user) do |profile|
-#     profile.update!(schools: [school])
-#   end
-# end
+SchoolDataImporter.new(Rails.logger).delay.run
+
+User.find_or_create_by!(email: "admin@example.com") do |user|
+  user.update!(full_name: "Admin User")
+  AdminProfile.find_or_create_by!(user: user)
+end
+
+User.find_or_create_by!(email: "lead-provider@example.com") do |user|
+  user.update!(full_name: "LeadProvider User")
+  LeadProviderProfile.find_or_create_by!(user: user, lead_provider: LeadProvider.first)
+end
+
+User.find_or_create_by!(email: "school-leader@example.com") do |user|
+  user.update!(full_name: "InductionTutor User")
+  school = School.find_or_create_by!(
+    name: "Example school",
+    postcode: "BB1 1BB",
+    address_line1: "3 Madeup Street",
+    primary_contact_email: "school-info@example.com",
+    school_status_code: 1,
+    school_type_code: 1,
+    administrative_district_code: "E123",
+    urn: "999999",
+  )
+  InductionCoordinatorProfile.find_or_create_by!(user: user) do |profile|
+    profile.update!(schools: [school])
+  end
+end
--- db/seeds/test_data.rb
+++ db/seeds/test_data.rb
@@ -9,7 +9,7 @@ School.find_or_create_by!(urn: "000001") do |school|
     name: "ZZ Test School 1",
     postcode: "AA1 1AA",
     address_line1: "1 Nowhere lane",
-    primary_contact_email: "cpd-test+school-1#{DOMAIN}",
+    primary_contact_email: "cpd-pentest-1#{DOMAIN}",
     school_status_code: 1,
     school_type_code: 1,
     administrative_district_code: "E123",
@@ -22,7 +22,7 @@ School.find_or_create_by!(urn: "000002") do |school|
     name: "ZZ Test School 2",
     postcode: "AA2 2AA",
     address_line1: "2 Nowhere lane",
-    primary_contact_email: "cpd-test+school-2#{DOMAIN}",
+    primary_contact_email: "cpd-pentest-1#{DOMAIN}",
     school_status_code: 1,
     school_type_code: 1,
     administrative_district_code: "E123",
@@ -35,7 +35,7 @@ School.find_or_create_by!(urn: "000003") do |school|
     name: "ZZ Test School 3",
     postcode: "AA3 3AA",
     address_line1: "3 Nowhere lane",
-    primary_contact_email: "cpd-test+school-3#{DOMAIN}",
+    primary_contact_email: "cpd-pentest-2#{DOMAIN}",
     school_status_code: 1,
     school_type_code: 1,
     administrative_district_code: "E123",
@@ -43,7 +43,7 @@ School.find_or_create_by!(urn: "000003") do |school|
   SchoolLocalAuthority.find_or_create_by!(school: school, local_authority: local_authority, start_year: 2019)
   NominationEmail.find_or_create_by!(
     token: "abc123",
-    sent_to: "cpd-test+school-3#{DOMAIN}",
+    sent_to: "cpd-pentest-2#{DOMAIN}",
     sent_at: 1.year.ago,
     school: school,
   )
@@ -54,13 +54,13 @@ School.find_or_create_by!(urn: "000004") do |school|
     name: "ZZ Test School 4",
     postcode: "AA4 4AA",
     address_line1: "4 Nowhere lane",
-    primary_contact_email: "cpd-test+school-4#{DOMAIN}",
+    primary_contact_email: "cpd-pentest-1#{DOMAIN}",
     school_status_code: 1,
     school_type_code: 1,
     administrative_district_code: "E123",
   )
   SchoolLocalAuthority.find_or_create_by!(school: school, local_authority: local_authority, start_year: 2019)
-  user = User.find_or_create_by!(full_name: "Induction Tutor for School 4", email: "cpd-test+tutor-1#{DOMAIN}")
+  user = User.find_or_create_by!(full_name: "Induction Tutor for School 4", email: "cpd-pentest-3#{DOMAIN}")
   InductionCoordinatorProfile.find_or_create_by!(user: user) do |profile|
     profile.update!(schools: [school])
   end
@@ -70,17 +70,37 @@ end
 School.find_or_create_by!(urn: "000005") do |school|
   school.update!(
     name: "ZZ Test School 5",
-    postcode: "AA4 4AA",
-    address_line1: "4 Nowhere lane",
-    primary_contact_email: "cpd-test+school-5#{DOMAIN}",
+    postcode: "AA5 5AA",
+    address_line1: "5 Nowhere lane",
+    primary_contact_email: "cpd-pentest-1#{DOMAIN}",
     school_status_code: 1,
     school_type_code: 1,
     administrative_district_code: "E123",
   )
   SchoolLocalAuthority.find_or_create_by!(school: school, local_authority: local_authority, start_year: 2019)
-  user = User.find_or_create_by!(full_name: "Induction Tutor for School 5", email: "cpd-test+tutor-2#{DOMAIN}")
+  user = User.find_or_create_by!(full_name: "Induction Tutor for School 5", email: "cpd-pentest-4#{DOMAIN}")
   InductionCoordinatorProfile.find_or_create_by!(user: user) do |profile|
     profile.update!(schools: [school])
   end
   SchoolCohort.find_or_create_by!(cohort: Cohort.current, school: school, induction_programme_choice: "full_induction_programme")
+end
+
+User.find_or_create_by!(email: "cpd-pentest-5#{DOMAIN}") do |user|
+  user.update!(full_name: "Admin User1")
+  AdminProfile.find_or_create_by!(user: user)
+end
+
+User.find_or_create_by!(email: "cpd-pentest-6#{DOMAIN}") do |user|
+  user.update!(full_name: "Admin User2")
+  AdminProfile.find_or_create_by!(user: user)
+end
+
+User.find_or_create_by!(email: "cpd-pentest-7#{DOMAIN}") do |user|
+  user.update!(full_name: "Lead ProviderUser1")
+  LeadProviderProfile.find_or_create_by!(user: user, lead_provider: LeadProvider.first)
+end
+
+User.find_or_create_by!(email: "cpd-pentest-8#{DOMAIN}") do |user|
+  user.update!(full_name: "Lead ProviderUser2")
+  LeadProviderProfile.find_or_create_by!(user: user, lead_provider: LeadProvider.second)
 end
