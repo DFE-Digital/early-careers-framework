c4c9b1b04e90b69a5440d6e2d00a601814f86f9f
Alfred Mazimbe <alfred@404-digital.co.uk>
2021-04-13 23:58:18 +0100

    code I think we no longer need

--- config/routes.rb
+++ config/routes.rb
@@ -19,15 +19,9 @@ Rails.application.routes.draw do
   resource :cookies, only: %i[show update]
   resource :privacy_policy, only: %i[show]
   resource :dashboard, controller: :dashboard, only: :show
-  resource :supplier_dashboard, controller: :supplier_dashboard, only: :show
-  resource :school_invites, only: %i[show create]
 
   namespace :api do
     resources :school_search, only: %i[index]
-  end
-
-  scope path: "induction-programme", module: :induction_programme do
-    resource :estimates, only: %i[show create]
   end
 
   namespace :demo do
@@ -136,13 +130,10 @@ Rails.application.routes.draw do
         get "add_participants"
       end
     end
-    resources :estimate_participants, only: %i[edit update], path: "estimate-participants"
   end
 
   get "/403", to: "errors#forbidden", via: :all
   get "/404", to: "errors#not_found", via: :all
   get "/422", to: "errors#unprocessable_entity", via: :all
   get "/500", to: "errors#internal_server_error", via: :all
-
-  resource :school_search, only: %i[show create], path: "school-search", controller: :school_search
 end
