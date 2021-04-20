ba32ac4cd334a04767eb3bc9e5ecd2010a6384d7
Michal Kreft <michal.kreft@softwire.com>
2021-04-20 14:57:44 +0100

    ECF-RP-523: Fix rspec test

--- spec/requests/lead_providers/report_schools_spec.rb
+++ spec/requests/lead_providers/report_schools_spec.rb
@@ -3,8 +3,11 @@
 require "rails_helper"
 
 RSpec.describe "Report schools spec", type: :request do
+  let(:user) { create(:user, :lead_provider) }
+
   describe "GET /lead_providers/report-schools/start" do
-    it "should show the start page" do
+    it "should show the start page to a lead provider" do
+      sign_in user
       get start_lead_providers_report_schools_path
 
       expect(response).to render_template :start
