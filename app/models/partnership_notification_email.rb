e98c1043c7c676bf088db87e7597c337480b56fc
Ethan Mills <ethan.mills@softwire.com>
2021-04-20 17:29:06 +0100

    ECF-RP-480: Add expiry to partnership notification emails

--- app/models/partnership_notification_email.rb
+++ app/models/partnership_notification_email.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class PartnershipNotificationEmail < ApplicationRecord
+  TOKEN_LIFETIME = 14.days
+
   belongs_to :partnership
   has_one :nomination_email
   delegate :school, to: :partnership, allow_nil: false
@@ -12,4 +14,8 @@ class PartnershipNotificationEmail < ApplicationRecord
     induction_coordinator_email: "induction_coordinator_email",
     school_email: "school_email",
   }
+
+  def token_expired?
+    created_at + TOKEN_LIFETIME < Time.zone.now
+  end
 end
--- spec/models/partnership_notification_email_spec.rb
+++ spec/models/partnership_notification_email_spec.rb
@@ -11,4 +11,22 @@ RSpec.describe PartnershipNotificationEmail, type: :model do
     it { is_expected.to delegate_method(:delivery_partner).to(:partnership).allow_nil }
     it { is_expected.to delegate_method(:cohort).to(:partnership) }
   end
+
+  describe "#token_expired?" do
+    it "returns true when the token is more than 14 days old" do
+      email = create(:partnership_notification_email)
+
+      travel 15.days
+
+      expect(email.token_expired?).to eq true
+    end
+
+    it "returns false when the token is less than 14 days old" do
+      email = create(:partnership_notification_email)
+
+      travel 13.days
+
+      expect(email.token_expired?).to eq false
+    end
+  end
 end
