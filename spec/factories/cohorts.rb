9dbab994878d01ffea4c2fa52ed4f16ceb6aad44
Michal Kreft <michal.kreft@softwire.com>
2021-04-20 15:15:32 +0100

    Update factories to avoid spurious errors
    How likely is it that two unique dates from range of 80 years
    fall into the same year? Too likely.

--- spec/factories/cohorts.rb
+++ spec/factories/cohorts.rb
@@ -2,6 +2,6 @@
 
 FactoryBot.define do
   factory :cohort do
-    start_year { Faker::Date.unique.between(from: "2020-01-01", to: "2099-12-31").year }
+    start_year { 2020 + Faker::Number.unique.between(from: 1, to: 79) }
   end
 end
