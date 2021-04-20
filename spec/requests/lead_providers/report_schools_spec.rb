aaebdf8553f0246e2e3d1df750eeb43a2edc657e
Michal Kreft <michal.kreft@softwire.com>
2021-04-20 15:57:13 +0100

    Add a cohort in rspec

--- spec/requests/lead_providers/report_schools_spec.rb
+++ spec/requests/lead_providers/report_schools_spec.rb
@@ -5,6 +5,10 @@ require "rails_helper"
 RSpec.describe "Report schools spec", type: :request do
   let(:user) { create(:user, :lead_provider) }
 
+  before do
+    create(:cohort, start_year: 2021)
+  end
+
   describe "GET /lead-providers/report-schools/start" do
     it "should show the start page to a lead provider" do
       sign_in user
