a1cdf9bd2f8868ce6da140e4d75665e53110b8b8
Callum Macrae <callum.macrae@digital.education.gov.uk>
2021-04-21 15:39:56 +0100

    Removing missing bit of non-determinative data from percy tests

--- spec/cypress/app_commands/scenarios/admin/schools.rb
+++ spec/cypress/app_commands/scenarios/admin/schools.rb
@@ -3,7 +3,7 @@
 school = FactoryBot.create(:school, name: "Include this school", urn: 123_456)
 local_authority = FactoryBot.create(:local_authority)
 SchoolLocalAuthority.create!(school: school, local_authority: local_authority, start_year: 2021)
-FactoryBot.create(:user, :induction_coordinator, full_name: "Sarah Smith", schools: [school])
+FactoryBot.create(:user, :induction_coordinator, full_name: "Sarah Smith", email: "sarah.smith@example.com", schools: [school])
 
 Faker::UniqueGenerator.clear
 Faker::Config.random = Random.new(42)
