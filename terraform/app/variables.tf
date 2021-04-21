384708559acc5a94fe5369c2440c32e1e5b288a4
Michal Kreft <michal.kreft@softwire.com>
2021-04-21 10:06:16 +0100

    Bump cloudfoundry to 0.14.1, bring back blue-green deploys

--- terraform/app/variables.tf
+++ terraform/app/variables.tf
@@ -49,7 +49,7 @@ variable paas_user {
 }
 
 variable paas_web_app_deployment_strategy {
-  default = "standard"
+  default = "blue-green-v2"
 }
 
 variable paas_web_app_instances {
@@ -65,7 +65,7 @@ variable paas_web_app_start_command {
 }
 
 variable paas_worker_app_deployment_strategy {
-  default = "standard"
+  default = "blue-green-v2"
 }
 
 variable paas_worker_app_instances {
