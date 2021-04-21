b280184d279ca2541cb0b13308df17a321ff66e0
Michal Kreft <michal.kreft@softwire.com>
2021-04-21 16:17:09 +0100

    ECF-RP-548: Ensure session cookies are secure in deployed envs

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,5 @@
 # frozen_string_literal: true
 
-Rails.application.config.session_store :cookie_store, key: "_early_career_framework_session", expire_after: 2.weeks
+in_local_environment = Rails.env.development? || Rails.env.test?
+
+Rails.application.config.session_store :cookie_store, key: "_early_career_framework_session", secure: !in_local_environment, expire_after: 2.weeks
