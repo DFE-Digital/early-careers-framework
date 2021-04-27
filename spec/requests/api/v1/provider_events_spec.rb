65db54b36e28627f908b9045a2955e43f61de686
Pavel Lisovin <pavel.lisovin@digital.education.gov.uk>
2021-04-27 09:49:38 +0100

    Move spec test to correct api/v1 directory

--- spec/requests/api/v1/provider_events_spec.rb
+++ spec/requests/api/v1/provider_events_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe "API Search schools", type: :request do
   describe "provider_events" do
     let(:parsed_response) { JSON.parse(response.body) }
 
+<<<<<<< HEAD
     # todo: configure base path /api/v1/ with swagger helper
     it "returns 201-created status" do
       post "/api/v1/provider_events"
@@ -16,5 +17,12 @@ RSpec.describe "API Search schools", type: :request do
       post "/api/v1/provider_events"
       expect(response.status).to eq 422
     end
+=======
+    # TODO: configure base path /api/v1/ with swagger helper
+    it "returns 201 status" do
+      post "/api/v1/provider_events" # TODO: send some data... , params: { foo: "bar" }
+      expect(response.status).to eq 201
+    end
+>>>>>>> Move spec test to correct api/v1 directory
   end
 end
