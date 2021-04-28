62d14219ae5dcbf7afc04e11e601bad6ac689ed3
Stanislaw Klajn <sklajn@gmail.com>
2021-04-28 13:37:32 +0100

    No more schools to select in confirm-schools

--- spec/requests/lead_providers/confirm_schools_spec.rb
+++ spec/requests/lead_providers/confirm_schools_spec.rb
@@ -7,6 +7,8 @@ RSpec.describe "Lead Provider confirmation of schools", type: :request do
   let(:delivery_partner) { create :delivery_partner }
   let(:lead_provider) { create :user, :lead_provider }
 
+  subject { response }
+
   before do
     sign_in lead_provider
 
@@ -18,20 +20,34 @@ RSpec.describe "Lead Provider confirmation of schools", type: :request do
   end
 
   describe "GET /lead-providers/report-schools/confirm" do
-    it "renders show template" do
+    before do
       get "/lead-providers/report-schools/confirm"
+    end
 
-      expect(response).to render_template "lead_providers/confirm_schools/show"
+    context "with some pre-selected schools" do
+      it { is_expected.to render_template "lead_providers/confirm_schools/show" }
+    end
+
+    context "when the list of pre-selected schools is empty" do
+      let(:schools) { [] }
+
+      it { is_expected.to render_template "lead_providers/confirm_schools/no_schools" }
     end
   end
 
   describe "POST /lead-providers/report-schools/confirm/remove" do
     let(:school_to_remove) { schools.sample }
 
-    it "removes given school from the list" do
+    before do
       post "/lead-providers/report-schools/confirm/remove", params: { remove: { school_id: school_to_remove.id } }
+    end
 
+    it "removes given school from the list" do
       expect(session[:confirm_schools_form]["school_ids"]).not_to include school_to_remove.id
+    end
+
+    it "displayes appropriate flash message" do
+      expect(flash[:success]).to be_present
     end
   end
 end
