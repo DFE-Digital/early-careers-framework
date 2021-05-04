9d3a1d08f53127f4dd180a170b76b9003782f14c
maxmills <8balldigitalsolutions@gmail.com>
2021-05-04 12:04:02 +0100

    update csv aws vars

--- config/initializers/csv_bucket.rb
+++ config/initializers/csv_bucket.rb
@@ -2,32 +2,30 @@
 
 if Rails.application.config.x.vcap_services.present?
 
-  if ENV["BUCKET_NAME"].blank?
-    bucket_name = Rails.application.config.x.vcap_services.dig(
-      "aws-s3-bucket", 0, "credentials", "bucket_name"
-    )
-    ENV["BUCKET_NAME"] = bucket_name if bucket_name.present?
+  csv_bucket = Rails.application.config.x.vcap_services["aws-s3-bucket"].select { |bucket| bucket["name"].include?("csv") }.dig(0, "credentials")
+
+  if ENV["CSV_BUCKET_NAME"].blank?
+    bucket_name = csv_bucket.dig "bucket_name"
+
+    ENV["CSV_BUCKET_NAME"] = bucket_name if bucket_name.present?
   end
 
-  if ENV["AWS_ACCESS_KEY_ID"].blank?
-    aws_access_key_id = Rails.application.config.x.vcap_services.dig(
-      "aws-s3-bucket", 0, "credentials", "aws_access_key_id"
-    )
-    ENV["AWS_ACCESS_KEY_ID"] = aws_access_key_id if aws_access_key_id.present?
+  if ENV["CSV_AWS_ACCESS_KEY_ID"].blank?
+    aws_access_key_id = csv_bucket.dig "aws_access_key_id"
+
+    ENV["CSV_AWS_ACCESS_KEY_ID"] = aws_access_key_id if aws_access_key_id.present?
   end
 
-  if ENV["AWS_SECRET_ACCESS_KEY"].blank?
-    aws_secret_access_key = Rails.application.config.x.vcap_services.dig(
-      "aws-s3-bucket", 0, "credentials", "aws_secret_access_key"
-    )
-    ENV["AWS_SECRET_ACCESS_KEY"] = aws_secret_access_key if aws_secret_access_key.present?
+  if ENV["CSV_AWS_SECRET_ACCESS_KEY"].blank?
+    aws_secret_access_key = csv_bucket.dig "aws_secret_access_key"
+
+    ENV["CSV_AWS_SECRET_ACCESS_KEY"] = aws_secret_access_key if aws_secret_access_key.present?
   end
 
-  if ENV["AWS_REGION"].blank?
-    aws_region = Rails.application.config.x.vcap_services.dig(
-      "aws-s3-bucket", 0, "credentials", "aws_region"
-    )
-    ENV["AWS_REGION"] = aws_region if aws_region.present?
+  if ENV["CSV_AWS_REGION"].blank?
+    aws_region = csv_bucket.dig "aws_region"
+
+    ENV["CSV_AWS_REGION"] = aws_region if aws_region.present?
   end
 
 end
