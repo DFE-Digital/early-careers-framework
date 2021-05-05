8fd6005bfee06be17c14a7eb91f39c29979d4f26
maxmills <8balldigitalsolutions@gmail.com>
2021-05-05 10:19:08 +0100

    null false delivery_partner_id

--- db/migrate/20210504150201_add_delivery_partner_to_csv_upload.rb
+++ db/migrate/20210504150201_add_delivery_partner_to_csv_upload.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class AddDeliveryPartnerToCsvUpload < ActiveRecord::Migration[6.1]
+  # rubocop:disable Rails/NotNullColumn
   def change
-    add_reference :partnership_csv_uploads, :delivery_partner, index: true
+    add_reference :partnership_csv_uploads, :delivery_partner, index: true, null: false
   end
+  # rubocop:enable Rails/NotNullColumn
 end
