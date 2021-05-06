8f2db93f543942aaaafc126b618779041af1b40a
Ethan Mills <ethan.mills@softwire.com>
2021-05-06 13:56:30 +0100

    ECF-RP-545: CSV error page

--- app/controllers/lead_providers/partnership_csv_uploads_controller.rb
+++ app/controllers/lead_providers/partnership_csv_uploads_controller.rb
@@ -14,19 +14,31 @@ module LeadProviders
         render :new and return
       end
 
-      @partnership_csv_upload = PartnershipCsvUpload.new(upload_params.merge(
-                                                           lead_provider_id: current_user.lead_provider_profile.lead_provider.id,
-                                                           delivery_partner_id: session[:delivery_partner_id],
-                                                         ))
+      @partnership_csv_upload = PartnershipCsvUpload.new(
+        upload_params.merge(
+          lead_provider_id: current_user.lead_provider_profile.lead_provider.id,
+          delivery_partner_id: session[:delivery_partner_id],
+        ),
+      )
 
       if @partnership_csv_upload.save
-        redirect_to error_page_lead_providers_report_schools_partnership_csv_uploads_path
+        session[:partnership_csv_upload_id] = @partnership_csv_upload.id
+        if @partnership_csv_upload.invalid_schools.empty?
+          redirect_to lead_providers_report_schools_confirm_schools_path
+        else
+          redirect_to errors_lead_providers_report_schools_partnership_csv_uploads_path
+        end
       else
         render :new
       end
     end
 
-    def error_page; end
+    def errors
+      partnership_csv_upload = PartnershipCsvUpload.find(session[:partnership_csv_upload_id])
+      @urns = partnership_csv_upload.urns
+      @errors = partnership_csv_upload.invalid_schools
+      @valid_schools = partnership_csv_upload.valid_schools
+    end
 
   private
 
