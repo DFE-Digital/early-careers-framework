23c21d4de64342ad5daf9f92e2dc3c8371cddc70
Ethan Mills <ethan.mills@softwire.com>
2021-05-17 11:57:40 +0100

    PartnershipActivationJob to convert pending partnerships

---
+++ app/jobs/partnership_activation_job.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class PartnershipActivationJob < ApplicationJob
+  discard_on ActiveJob::DeserializationError
+
+  def perform(partnership)
+    return if partnership.challenged?
+    return unless partnership.pending
+
+    ActiveRecord::Base.transaction do
+      partnership.update!(pending: false)
+      school_cohort = SchoolCohort.find_by!(school_id: partnership.school_id, cohort_id: partnership.cohort_id)
+      school_cohort.update!(induction_programme_choice: "full_induction_programme", core_induction_programme: nil)
+    end
+  end
+end
