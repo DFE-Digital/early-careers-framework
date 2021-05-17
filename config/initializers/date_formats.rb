b04961a3dc7df5b600bc5fecc37934c42f3934e6
Stanislaw Klajn <sklajn@gmail.com>
2021-05-17 09:54:13 +0100

    Ecf rp 563 notification banner (#318)
    * wip
    
    * Added banner to more views
    
    * COrrect render? logic
    
    * Correct challenging link in the challenge banner
    
    * School cohort layout
    
    * Govuk time formats
    
    * Removed capybara. For now

--- config/initializers/date_formats.rb
+++ config/initializers/date_formats.rb
@@ -4,3 +4,6 @@
 Date::DATE_FORMATS[:govuk]        = "%-d %B %Y" # 2 January 1998
 Date::DATE_FORMATS[:govuk_short]  = "%-d %b %Y" # 2 Jan 1998
 Date::DATE_FORMATS[:govuk_approx] = "%B %Y"     # January 1998
+
+Time::DATE_FORMATS[:govuk] = "#{Date::DATE_FORMATS[:govuk]}, %l:%M %p"
+Time::DATE_FORMATS[:govuk_short] = "#{Date::DATE_FORMATS[:govuk_short]}, %l:%M %p"
