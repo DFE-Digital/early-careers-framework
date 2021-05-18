d2f531d7ce9bfd41f88ae93739f0d252553483f4
Michal Kreft <michal.kreft@softwire.com>
2021-05-18 12:30:35 +0100

    CPDEL-438: Allow api access in serious environments

--- config/routes.rb
+++ config/routes.rb
@@ -40,7 +40,7 @@ Rails.application.routes.draw do
 
     namespace :v1 do
       resources :early_career_teacher_participants, only: %i[create], path: "early-career-teacher-participants"
-      resources :users, only: :index unless %w[sandbox staging production].include?(Rails.env)
+      resources :users, only: :index
     end
   end
 
