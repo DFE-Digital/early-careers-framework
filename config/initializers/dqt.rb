703ddd2ffc18154187056d130085a2d14973e675
Ethan Mills <ethan.mills@softwire.com>
2021-05-20 10:48:37 +0100

    CPDRP-336: Set configuration values in development

--- config/initializers/dqt.rb
+++ config/initializers/dqt.rb
@@ -5,8 +5,7 @@ Dqt.configure do |config|
     JSON.parse(json.gsub("=>", ":"), symbolize_names: true)
   end
 
-  config.client.headers = json_to_hash(ENV["DQT_CLIENT_HEADERS"]) if ENV["DQT_CLIENT_HEADERS"]
-  config.client.host = ENV["DQT_CLIENT_HOST"] if ENV["DQT_CLIENT_HOST"]
-  config.client.params = json_to_hash(ENV["DQT_CLIENT_PARAMS"]) if ENV["DQT_CLIENT_PARAMS"]
-  config.client.port = ENV["DQT_CLIENT_PORT"].to_i if ENV["DQT_CLIENT_PORT"]
+  config.client.headers = { Authorization: Rails.application.config.dqt_client_api_key }
+  config.client.host = Rails.application.config.dqt_client_host
+  config.client.params = json_to_hash(Rails.application.config.dqt_client_params)
 end
