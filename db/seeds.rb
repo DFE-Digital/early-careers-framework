ab72ddc88a633e1826b0582c5cb4b31356f3695f
Tim Abell <tim@timwise.co.uk>
2021-05-21 14:55:47 +0100

    Run sandbox seeds in all of dev/test/sandbox environments
    To reduce complexity and make it easier to test that changes are working in all environments.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,9 @@
 # frozen_string_literal: true
 
-Dir[Rails.root.join("db/seeds/initial_seed.rb")].each { |seed| load seed } unless Rails.env.sandbox?
+Dir[Rails.root.join("db/seeds/initial_seed.rb")].each { |seed| load seed }
 
-if %w[development deployed_development test].include?(Rails.env)
+if %w[development deployed_development test sandbox].include?(Rails.env)
   Dir[Rails.root.join("db/seeds/test_data.rb")].each { |seed| load seed }
   Dir[Rails.root.join("db/seeds/dummy_structures.rb")].each { |seed| load seed }
-end
-
-if Rails.env.sandbox?
   Dir[Rails.root.join("db/seeds/sandbox_data.rb")].each { |seed| load seed }
 end
