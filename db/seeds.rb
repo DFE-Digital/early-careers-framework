b90b7bf540aef2a9333433e4bd76afdc5c2b2fe2
Tim Abell <tim@timwise.co.uk>
2021-05-21 16:38:45 +0100

    Add `.to_s` to seed loading code
    @kicferk1 informs me that not having this causes problems with the cypress integration tests even though it works without it locally.
    https://github.com/DFE-Digital/ecf-engage-and-learn/pull/78/commits/60d99ab2d87d6763d01d51b851566a06c9132d67

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-load Rails.root.join("db/seeds/initial_seed.rb")
+load Rails.root.join("db/seeds/initial_seed.rb").to_s
 
 if %w[development deployed_development test sandbox].include?(Rails.env)
   %w[test_data dummy_structures sandbox_data].each do |seed|
-    load Rails.root.join("db/seeds", "#{seed}.rb")
+    load Rails.root.join("db/seeds", "#{seed}.rb").to_s
   end
 end
