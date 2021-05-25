817b1279a717e91c6006fff218c0d1b0178857b3
Paul McKibbin <pmckibbin@gmail.com>
2021-05-25 19:19:23 +0100

    Post PR fixes
    - set-up_fees -> set_up_fee
    - move @combined_results into the test
    - change the contribution percentages (output payment 60% and service fee payment 40%) into methods to make the code clearer to scan.
    - remove comment block from models
    - make boundary calculations less obtuse

--- app/models/participant_band.rb
+++ app/models/participant_band.rb
@@ -4,23 +4,29 @@ class ParticipantBand < ApplicationRecord
   belongs_to :call_off_contract
 
   def number_of_participants_in_this_band(total_number_of_participants)
-    smaller_than_lower_boundary(total_number_of_participants) ||
-      between_boundaries(total_number_of_participants) ||
+    if smaller_than_lower_boundary(total_number_of_participants)
+      0
+    elsif upper_boundary.nil? || between_boundaries(total_number_of_participants)
+      total_number_of_participants - lower_boundary
+    else
       upper_boundary - lower_boundary
+    end
   end
 
 private
 
   def smaller_than_lower_boundary(total_number_of_participants)
-    0 if total_number_of_participants < lower_boundary
+    total_number_of_participants < lower_boundary
   end
 
   def between_boundaries(total_number_of_participants)
-    total_number_of_participants - lower_boundary if upper_boundary.nil? || (lower_boundary..upper_boundary).include?(total_number_of_participants)
+    (lower_boundary..upper_boundary).include?(total_number_of_participants)
   end
 
+  # This conversion is required because the range is specified without overlaps and starts from 0..x
+  # but then goes from x+1..y, y+1..z which is clear for layout but not so good for computational calculation.
   def lower_boundary
-    min.to_i.positive? && min - 1 || 0
+    [min.to_i - 1, 0].max
   end
 
   def upper_boundary
