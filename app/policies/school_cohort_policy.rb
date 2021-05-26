01ee866792ace1e6247c9567eef5475038b9c135
Tony Headford <tony@binarycircus.com>
2021-05-26 16:20:26 +0100

    Remove unused policy condition

--- app/policies/school_cohort_policy.rb
+++ app/policies/school_cohort_policy.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 class SchoolCohortPolicy < ApplicationPolicy
-  def index?
-    user.admin?
-  end
-
   def show?
     return true if user.admin?
     return false unless user.induction_coordinator_profile
