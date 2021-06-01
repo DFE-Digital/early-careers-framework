491213c146784f68b2f3d9d0a529ea38af2a25f0
Phil Lee <asmega@users.noreply.github.com>
2021-06-01 15:51:06 +0100

    prevent Dtq config from throwing if params blank
    - fixes the app from not able to boot when these values are not provided
    - which happens during rake tasks from lack of config

--- config/initializers/dqt.rb
+++ config/initializers/dqt.rb
@@ -9,5 +9,5 @@ Dqt.configure do |config|
 
   config.client.headers = { Authorization: Rails.application.config.dqt_client_api_key } if Rails.application.config.respond_to?(:dqt_client_api_key)
   config.client.host = Rails.application.config.dqt_client_host if Rails.application.config.respond_to?(:dqt_client_host)
-  config.client.params = json_to_hash(Rails.application.config.dqt_client_params) if Rails.application.config.respond_to?(:dqt_client_params)
+  config.client.params = json_to_hash(Rails.application.config.dqt_client_params) if Rails.application.config.respond_to?(:dqt_client_params) && Rails.application.config.dqt_client_params.present?
 end
