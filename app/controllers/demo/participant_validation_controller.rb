848fc22e09ecce18d45c26c3466d470caad914dc
Ethan Mills <ethan.mills@softwire.com>
2021-06-09 14:03:01 +0100

    Participant validation demo
    Adding basic auth still to do

---
+++ app/controllers/demo/participant_validation_controller.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Demo
+  class ParticipantValidationController < ApplicationController
+    def new
+      @form = DemoParticipantValidationForm.new
+    end
+
+    def create
+      @form = DemoParticipantValidationForm.new(params.require(:demo_participant_validation_form).permit(:full_name, :trn, :nino))
+      @form.dob = Date.new(params[:demo_participant_validation_form]["dob(1i)".to_sym].to_i, params[:demo_participant_validation_form]["dob(2i)".to_sym].to_i, params[:demo_participant_validation_form]["dob(3i)".to_sym].to_i)
+      render :new and return unless @form.valid?
+
+      @validation_result = ParticipantValidationService.new.validate(trn: @form.trn, nino: @form.nino, full_name: @form.full_name, dob: @form.dob)
+    end
+  end
+end
