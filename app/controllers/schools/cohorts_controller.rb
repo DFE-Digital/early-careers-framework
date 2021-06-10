f254d9eb0874a250047ae7884395affb354786f5
Stanislaw Klajn <sklajn@gmail.com>
2021-06-10 13:59:17 +0100

    Fixed clashing namespaces issues

--- app/controllers/schools/cohorts_controller.rb
+++ app/controllers/schools/cohorts_controller.rb
@@ -1,14 +1,16 @@
 # frozen_string_literal: true
 
-class Schools::CohortsController < Schools::BaseController
-  skip_after_action :verify_authorized
-  skip_after_action :verify_policy_scoped
+module Schools
+  class CohortsController < BaseController
+    skip_after_action :verify_authorized
+    skip_after_action :verify_policy_scoped
 
-  before_action :set_school_cohort
+    before_action :set_school_cohort
 
-  def show; end
+    def show; end
 
-  def add_participants
-    redirect_to schools_cohort_participants_path(@cohort.start_year) if FeatureFlag.active?(:induction_tutor_manage_participants)
+    def add_participants
+      redirect_to schools_cohort_participants_path(@cohort.start_year) if FeatureFlag.active?(:induction_tutor_manage_participants)
+    end
   end
 end
