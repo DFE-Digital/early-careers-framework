64cf9366fe48488a9d306aea5f62b6c48deec26a
Tony Headford <tony@binarycircus.com>
2021-06-14 16:22:30 +0100

    Remove pagination

--- app/controllers/api/v1/data_studio/school_rollout_controller.rb
+++ app/controllers/api/v1/data_studio/school_rollout_controller.rb
@@ -5,10 +5,9 @@ module Api
     module DataStudio
       class SchoolRolloutController < Api::ApiController
         include ApiTokenAuthenticatable
-        include Pagy::Backend
 
         def index
-          render json: ::DataStudio::SchoolRolloutSerializer.new(paginate(school_rollout_data))
+          render json: ::DataStudio::SchoolRolloutSerializer.new(school_rollout_data)
             .serializable_hash.to_json
         end
 
@@ -20,28 +19,6 @@ module Api
 
         def school_rollout_data
           ::DataStudio::FetchSchoolRolloutData.call
-        end
-
-        def paginate(scope)
-          _pagy, paginated_records = pagy(scope, items: per_page, page: page)
-
-          paginated_records
-        end
-
-        def per_page
-          [params.fetch(:per_page, default_per_page).to_i, max_per_page].min
-        end
-
-        def default_per_page
-          250
-        end
-
-        def max_per_page
-          250
-        end
-
-        def page
-          params.fetch(:page, 1).to_i
         end
       end
     end
