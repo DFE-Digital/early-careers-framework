0b87ad2285a2f39c4021af5448f696db04cd7c9b
Tim Abell <tim@timwise.co.uk>
2021-06-16 12:04:37 +0100

    wip
    making different uplift people

--- spec/services/participant_event_aggregator_service_spec.rb
+++ spec/services/participant_event_aggregator_service_spec.rb
@@ -7,6 +7,12 @@ RSpec.describe ParticipantEventAggregator do
 
   context "event declarations" do
     before do
+      sparse_school = create(:school, :sparsity_uplift)
+      pupil_premium_school = create(:school, :pupil_premium_uplift)
+      teacher_sparse = create(:early_career_teacher_profile, school: sparse_school)
+      teacher_pupil_premium = create(:early_career_teacher_profile, school: pupil_premium_school)
+      profile_sparse = create(:early_career_teacher_profile, teacher_sparse)
+
       # TODO: DRY
       6.times do
         participant_declaration = create(:participant_declaration, lead_provider: lead_provider)
@@ -15,7 +21,7 @@ RSpec.describe ParticipantEventAggregator do
         participant_record = create(:participation_record, lead_provider: lead_provider)
         participant_record.join!
       end
-      4.times do
+      2.times do
         early_career_teacher_profile = create(:early_career_teacher_profile, uplift: true)
         participant_declaration = create(:participant_declaration, lead_provider: lead_provider, early_career_teacher_profile: early_career_teacher_profile)
         create(:participant_declaration, early_career_teacher_profile: participant_declaration.early_career_teacher_profile, lead_provider: lead_provider)
