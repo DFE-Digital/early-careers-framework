e12407ec8ad519c3ef047efb2768e1066b75edf9
Phil Lee <asmega@users.noreply.github.com>
2021-06-17 13:23:20 +0100

    use enum for headteacher_status

--- app/models/npq_profile.rb
+++ app/models/npq_profile.rb
@@ -4,4 +4,10 @@ class NpqProfile < ApplicationRecord
   belongs_to :user
   belongs_to :npq_lead_provider
   belongs_to :npq_course
+
+  enum headteacher_status: {
+    no: "no",
+    yes_in_first_two_years: "yes_in_first_two_years",
+    yes_over_two_years: "yes_over_two_years",
+  }
 end
