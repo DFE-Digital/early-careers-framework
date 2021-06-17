a6b56505db1c2277eb5267fbf3d4513be9c05235
Tim Abell <tim@timwise.co.uk>
2021-06-17 12:51:53 +0100

    Allow calling without `new`

--- app/services/participant_event_aggregator.rb
+++ app/services/participant_event_aggregator.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
 class ActiveParticipantAggregator
-  # We could add this as a convenience to allow calling with default dependencies without `new`, but it's not too important
-  # class << self
-  #   def call(lead_provider:)
-  #     new.call(lead_provider: lead_provider)
-  #   end
-  # end
-  #
+  class << self
+    def call(lead_provider:)
+      new.call(lead_provider: lead_provider)
+    end
+  end
+
   def initialize(participant_declaration_class: ParticipantDeclaration)
     @participant_declaration_class = participant_declaration_class
   end
