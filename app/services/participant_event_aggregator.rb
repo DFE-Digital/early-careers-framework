052b801023bb3363620596a71aded88d87b0da1e
Tim Abell <tim@timwise.co.uk>
2021-06-17 12:49:33 +0100

    Refactor spike - hard-code scope, normal injection for class
    Rename the class to reflect the only thing it can do now.

--- app/services/participant_event_aggregator.rb
+++ app/services/participant_event_aggregator.rb
@@ -1,20 +1,18 @@
 # frozen_string_literal: true
 
-require "has_di_parameters"
-
-class ParticipantEventAggregator
-  include HasDIParameters
-
-  def call(event_type: :started, lead_provider:)
-    recorder.send(params[event_type], lead_provider)
+class ActiveParticipantAggregator
+  # We could add this as a convenience to allow calling with default dependencies without `new`, but it's not too important
+  # class << self
+  #   def call(lead_provider:)
+  #     new.call(lead_provider: lead_provider)
+  #   end
+  # end
+  #
+  def initialize(participant_declaration_class: ParticipantDeclaration)
+    @participant_declaration_class = participant_declaration_class
   end
 
-private
-
-  def default_params
-    {
-      recorder: ParticipantDeclaration,
-      started: :count_active_for_lead_provider,
-    }
+  def call(lead_provider:)
+    @participant_declaration_class.count_active_for_lead_provider(lead_provider)
   end
 end
