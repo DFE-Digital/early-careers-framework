1e8c456d7de8ecae7cca5b04bb253a982e2c1b57
Tim Abell <tim@timwise.co.uk>
2021-06-17 12:59:46 +0100

    Reduce to absurdity

--- app/services/participant_event_aggregator.rb
+++ app/services/participant_event_aggregator.rb
@@ -3,15 +3,7 @@
 class ActiveParticipantAggregator
   class << self
     def call(participant_declaration_class: ParticipantDeclaration, lead_provider:)
-      new(participant_declaration_class).call(lead_provider: lead_provider)
+      participant_declaration_class.count_active_for_lead_provider(lead_provider)
     end
-  end
-
-  def initialize(participant_declaration_class: ParticipantDeclaration)
-    @participant_declaration_class = participant_declaration_class
-  end
-
-  def call(lead_provider:)
-    @participant_declaration_class.count_active_for_lead_provider(lead_provider)
   end
 end
