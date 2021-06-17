c501e665507a3a9723533a249ec0729f48150f4e
Pavel Lisovin <pavel.lisovin@digital.education.gov.uk>
2021-06-17 10:59:54 +0100

    Fix feature test not to use Double as the method it's relied on is in the model

--- spec/lib/payment_calculator/ecf/contract/output_payment_calculations_spec.rb
+++ spec/lib/payment_calculator/ecf/contract/output_payment_calculations_spec.rb
@@ -6,6 +6,11 @@ class DummyClass
 end
 
 describe ::PaymentCalculator::Ecf::Contract::OutputPaymentCalculations do
+  let(:contract) { create(:call_off_contract) }
+  let(:band_a) { create(:participant_band, min: 0, max: 200, per_participant: 100, call_off_contract: contract) }
+  let(:band_b) { create(:participant_band, min: 201, max: 400, per_participant: 200, call_off_contract: contract) }
+  let(:band_c) { create(:participant_band, min: 401, max: 500, per_participant: 300, call_off_contract: contract) }
+
   it "performs calculations of the output payments" do
     band_a = double("Band Double", per_participant: 996.00)
     contract = double("Contract Double", band_a: band_a)
@@ -20,5 +25,17 @@ describe ::PaymentCalculator::Ecf::Contract::OutputPaymentCalculations do
     %i[retention_1 retention_2 retention_3 retention_4].each do |event_type|
       expect(call_off_contract.output_payment_per_participant_for_event(event_type: event_type, band: band_a).round(0)).to eq(90.00)
     end
+  end
+
+  it "calculates subtotal based on banding total participants" do
+    contract = double("Contract Double", band_a: band_a)
+    call_off_contract = DummyClass.new({ contract: contract, band_a: band_a, band_b: band_b, band_c: band_c })
+
+    expect(call_off_contract.output_payment_for_event(event_type: :started, total_participants: 500, band: band_a)).to eq(2400.00)
+    expect(call_off_contract.output_payment_for_event(event_type: :started, total_participants: 500, band: band_b)).to eq(4800.00)
+    expect(call_off_contract.output_payment_for_event(event_type: :started, total_participants: 500, band: band_c)).to eq(3600.00)
+
+    expect(call_off_contract.output_payment_for_event(event_type: :started, total_participants: 50, band: band_a)).to eq(600.00)
+    expect(call_off_contract.output_payment_for_event(event_type: :started, total_participants: 90, band: band_a)).to eq(1080.00)
   end
 end
