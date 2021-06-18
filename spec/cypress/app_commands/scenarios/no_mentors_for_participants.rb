1e8aa736d33d95324b9ad878fe7d47ece3d73d4d
Tony Headford <tony@binarycircus.com>
2021-06-18 16:45:33 +0100

    Added comments linking to govuk_link_to issue

--- spec/cypress/app_commands/scenarios/no_mentors_for_participants.rb
+++ spec/cypress/app_commands/scenarios/no_mentors_for_participants.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# expects "school_participants" to have been previously run
+#
 school = School.find_by(name: "Hogwarts Academy")
 if school.present?
   school.mentor_profiles.each do |profile|
