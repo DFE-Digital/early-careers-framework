9ff5f3559aa672ce7734acace9c72cbeec443c57
Pavel Lisovin <pavel.lisovin@digital.education.gov.uk>
2021-06-22 11:31:36 +0100

    Add more test for sparsity and pupil premium flags

--- spec/factories/early_career_teacher_profiles.rb
+++ spec/factories/early_career_teacher_profiles.rb
@@ -8,5 +8,14 @@ FactoryBot.define do
     trait :sparsity_uplift do
       sparsity_uplift { true }
     end
+
+    trait :pupil_premium_uplift do
+      pupil_premium_uplift { true }
+    end
+
+    trait :uplift_flags do
+      sparsity_uplift
+      pupil_premium_uplift
+    end
   end
 end
