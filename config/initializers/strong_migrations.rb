c469897c382208fd21cb70dd5a52274c183878fc
Paul McKibbin <pmckibbin@gmail.com>
2021-06-22 19:03:34 +0100

    Prevent rubocop from autocorrecting version numeric value
    Rubocop was enforcing a change to a numerically encoded version to
    make it more readable as a number, but less readable as a date.
    
    This is related to the database migration version, so clearer if
    laid out as a date format.
    
    Setting this manually and setting a single line disable for Rubocop
    prevents the autocorrect from changing it.

--- config/initializers/strong_migrations.rb
+++ config/initializers/strong_migrations.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Mark existing migrations as safe
-StrongMigrations.start_after = 20_210_622_170_835
+StrongMigrations.start_after = 2021_06_22_17_08_35 # rubocop:disable Style/NumericLiterals
 
 # Set timeouts for migrations
 # If you use PgBouncer in transaction mode, delete these lines and set timeouts on the database user
