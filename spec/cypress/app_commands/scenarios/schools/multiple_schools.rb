4a5ef470292ee9567564955fa2668d50f66ff3ba
Ethan Mills <ethan.mills@softwire.com>
2021-06-22 11:51:35 +0100

    CPDRP-345: cypress markups

--- spec/cypress/app_commands/scenarios/schools/multiple_schools.rb
+++ spec/cypress/app_commands/scenarios/schools/multiple_schools.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 cohort = FactoryBot.create(:cohort, :current)
 first_school = FactoryBot.create(:school, name: "Test School 1", slug: "111111-test-school-1")
 FactoryBot.create(:school_cohort, school: first_school, cohort: cohort)
 second_school = FactoryBot.create(:school, name: "Test School 2", slug: "111112-test-school-2")
 FactoryBot.create(:school_cohort, school: second_school, cohort: cohort)
+third_school = FactoryBot.create(:school, name: "Test School 3", slug: "111113-test-school-3")
+FactoryBot.create(:school_cohort, school: third_school, cohort: cohort)
 
 FactoryBot.create(:user, :induction_coordinator, email: "school-leader@example.com", school_ids: [first_school.id, second_school.id])
--- spec/cypress/integration/schools/MultipleSchools.feature
+++ spec/cypress/integration/schools/MultipleSchools.feature
@@ -8,8 +8,9 @@ Feature: School leaders should be able to manage multiple schools
     Given I am on "schools" page
     Then "page body" should contain "Test School 1"
     And "page body" should contain "Test School 2"
+    And "page body" should not contain "Test School 3"
     And the page should be accessible
-    And percy should be sent snapshot called "select school page"
+    And percy should be sent snapshot called "induction coordinator select school page"
 
     When I click on "link" containing "Test School 1"
     Then I should be on "school cohorts" page
