b42ccd7c2ea0d86c602cd66ae18d026162020657
Paul McKibbin <pmckibbin@gmail.com>
2021-06-23 15:36:23 +0100

    Set payment uplift flags on new EarlyCareerTeacherProfile records
    Added this in as a callback on the model, but would prefer to
    move it to a service wrapper once we have a refactor on the
    multistep, which currently leaves either this approach, or
    changing the form.save! as options. This was chosen to make
    it obvious that it's only for uplift payment.
    
    This is only required the first time a profile is created,
    since it shouldn't change even if the underlying school
    uplift payment scope changes.

--- app/models/early_career_teacher_profile.rb
+++ app/models/early_career_teacher_profile.rb
@@ -11,4 +11,16 @@ class EarlyCareerTeacherProfile < ApplicationRecord
   has_one :mentor, through: :mentor_profile, source: :user
   has_one :participation_record, dependent: :destroy
   has_many :participant_declarations
+
+  # Would like to move the following once we have a refactor on the multistep, which currently leaves either this
+  # approach, or changing the form.save! as options. This was chosen to make it obvious that it's separate from the
+  # R&P code, since it's only required for uplift payment.
+  # This is only required the first time a profile is created, since it shouldn't change even if the underlying
+  # school uplift payment scope changes.
+  before_save :set_uplift_payment_flags, if: :new_record?
+
+  def set_uplift_payment_flags
+    self.sparsity_uplift = school.sparsity_uplift?(cohort.start_year)
+    self.pupil_premium_uplift = school.pupil_premium_uplift?(cohort.start_year)
+  end
 end
