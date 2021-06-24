ce7f8dbb47e1d281c254a600e099bd35c10c6112
Tony Headford <tony@binarycircus.com>
2021-06-24 17:19:58 +0100

    CPDRP-443 notification banners for opted-out schools (#531)
    * Show notification banners for opted out schools
    
    * Fix Rubocop offence
    
    * Add pending state to specs

---
+++ spec/cypress/integration/schools/SeeChallengeBanners.feature
@@ -0,0 +1,19 @@
+Feature: Induction tutors seeing challenge banners
+  Induction tutors should be able to see a banner informing them that
+  they have ben recruited by a provider when they have chosen an opt-out
+  programme
+
+  Background:
+    Given cohort was created with start_year "2021"
+    And scenario "school_opted_out_but_was_recruited" has been run
+    And I am logged in as existing user with email "ted.tutor@example.com"
+    Then I should be on "schools" page
+    And the page should be accessible
+
+  Scenario: Viewing the next steps page
+    When I click on "link" containing "2021"
+    Then I should be on "2021 school cohorts" page
+    And the page should be accessible
+    And percy should be sent snapshot called "Opted out with banner"
+    And "page body" should contain "Important"
+    And "page body" should contain "Ranchero Partners, with Fab Provider, has confirmed your school"
--- spec/jobs/partnership_activation_job_spec.rb
+++ spec/jobs/partnership_activation_job_spec.rb
@@ -17,13 +17,28 @@ RSpec.describe PartnershipActivationJob do
 
     def execute
       subject.perform(partnership: partnership, report_id: report_id)
+      partnership.reload
+      school_cohort.reload
     end
 
     it "updates the partnership and school cohort" do
       execute
 
-      expect(partnership.reload.pending).to eql false
-      expect(school_cohort.reload.induction_programme_choice).to eql "full_induction_programme"
+      expect(partnership).not_to be_pending
+      expect(school_cohort).to be_full_induction_programme
+    end
+
+    context "when school had chosen to opt-out" do
+      before do
+        school_cohort.update!(induction_programme_choice: "no_early_career_teachers",
+                              opt_out_of_updates: true)
+      end
+
+      it "opts the school back in" do
+        execute
+
+        expect(school_cohort).not_to be_opt_out_of_updates
+      end
     end
 
     context "when partnership has been challenged" do
