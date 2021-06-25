307fc590a640a141d43bb3695a297013c329a621
Ethan Mills <ethan.mills@softwire.com>
2021-06-25 15:09:24 +0100

    Remove mock login

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -6,7 +6,6 @@ class Users::SessionsController < Devise::SessionsController
   TEST_USERS = %w[admin@example.com lead-provider@example.com school-leader@example.com early-career-teacher@example.com].freeze
 
   skip_before_action :check_privacy_policy_accepted
-  before_action :mock_login, only: :create, if: -> { Rails.env.development? || Rails.env.deployed_development? }
   before_action :redirect_to_dashboard, only: %i[sign_in_with_token redirect_from_magic_link]
   before_action :ensure_login_token_valid, only: %i[sign_in_with_token redirect_from_magic_link]
 
@@ -56,14 +55,5 @@ private
 
   def login_token_expired?
     Time.zone.now > @user.login_token_valid_until
-  end
-
-  def mock_login
-    email = params.dig(:user, :email)
-    return unless TEST_USERS.include?(email)
-
-    user = User.find_by_email(email)
-    sign_in(user, scope: :user)
-    redirect_to profile_dashboard_path(user)
   end
 end
