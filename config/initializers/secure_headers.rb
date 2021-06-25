faf6c1118b80d0e1ce3d695ee962d887723c29ef
matt <mattonfoot@gmail.com>
2021-06-25 07:22:37 +0100

    CSP rules added to allow display of API Docs and report only

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -11,21 +11,26 @@ SecureHeaders::Configuration.default do |config|
 
   google_analytics = %w[www.google-analytics.com ssl.google-analytics.com *.googletagmanager.com tagmanager.google.com *.googleusercontent.com *.gstatic.com]
 
+  config.csp = {
+    default_src: %w['self' http: https: data: 'unsafe-inline'],
+    script_src: %w['self' http: https: 'unsafe-inline'],
+  }
+
   config.csp_report_only = {
     default_src: %w['none'],
     base_uri: %w['self'],
     block_all_mixed_content: true, # see http://www.w3.org/TR/mixed-content/
     child_src: %w['self'],
     connect_src: %W['self'] + google_analytics,
-    font_src: %w['self' *.gov.uk fonts.gstatic.com fonts.googleapis.com],
+    font_src: %w['self' *.gov.uk fonts.gstatic.com],
     form_action: %w['self'],
     frame_ancestors: %w['self'],
     frame_src: %w['self'],
-    img_src: %W['self' *.gov.uk] + google_analytics,
+    img_src: %W['self' data: *.gov.uk] + google_analytics,
     manifest_src: %w['self'],
     media_src: %w['self'],
     script_src: %W['self' 'unsafe-inline' 'unsafe-eval' *.gov.uk] + google_analytics,
-    style_src: %w['self' 'unsafe-inline' *.gov.uk] + google_analytics,
+    style_src: %w['self' 'unsafe-inline' *.gov.uk fonts.googleapis.com] + google_analytics,
     worker_src: %w['self'],
     # upgrade_insecure_requests: !Rails.env.development?, # see https://www.w3.org/TR/upgrade-insecure-requests/
     report_uri: %w[/csp_reports],
