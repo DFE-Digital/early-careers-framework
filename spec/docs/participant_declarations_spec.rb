ea4ac6eaa1d965bd95e81ca8d12f2c7096760da1
matt <mattonfoot@gmail.com>
2021-06-29 10:14:08 +0100

    added spaces

--- spec/docs/participant_declarations_spec.rb
+++ spec/docs/participant_declarations_spec.rb
@@ -42,7 +42,7 @@ RSpec.describe "Participant Declarations", type: :request, swagger_doc: "v1/api_
 
         schema "$ref": "#/components/schemas/ParticipantDeclarationRecordedResponse"
 
-        #run_test!
+        # run_test!
       end
 
       response 204, "Successful" do
@@ -60,7 +60,7 @@ RSpec.describe "Participant Declarations", type: :request, swagger_doc: "v1/api_
 
         schema "$ref": "#/components/schemas/ParticipantDeclarationRecordedResponse"
 
-        #run_test!
+        # run_test!
       end
 
       response "422", "Bad or Missing parameter" do
@@ -68,7 +68,7 @@ RSpec.describe "Participant Declarations", type: :request, swagger_doc: "v1/api_
 
         schema "$ref": "#/components/schemas/BadOrParameterMissingParameterResponse"
 
-        #run_test!
+        # run_test!
       end
 
       response "401", "Unauthorized" do
@@ -76,7 +76,7 @@ RSpec.describe "Participant Declarations", type: :request, swagger_doc: "v1/api_
 
         schema "$ref": "#/components/schemas/UnauthorizedResponse"
 
-        #run_test!
+        # run_test!
       end
     end
   end
