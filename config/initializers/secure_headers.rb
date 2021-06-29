5eb109bd2f4bf86e6fb4b333d7e23ae58e34738f
Matt Smith <mattonfoot@gmail.com>
2021-06-29 21:27:02 +0100

    0 is valid value for XSS protection header

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -4,7 +4,7 @@
 SecureHeaders::Configuration.default do |config|
   config.x_frame_options = "DENY"
   config.x_content_type_options = "nosniff"
-  config.x_xss_protection = "0; mode=block"
+  config.x_xss_protection = "0"
   config.x_download_options = "noopen"
   config.x_permitted_cross_domain_policies = "none"
   config.referrer_policy = %w[origin-when-cross-origin strict-origin-when-cross-origin]
