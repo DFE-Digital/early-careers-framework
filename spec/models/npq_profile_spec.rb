95baec3b81144d9d3860e34811cb9e51c755fad8
James Rose <james@jbpr.net>
2021-07-01 08:45:39 +0100

    Expand valid NPQ profile headteacher statuses

---
+++ spec/models/npq_profile_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+RSpec.describe NpqProfile, type: :model do
+  it {
+    is_expected.to define_enum_for(:headteacher_status).with_values(
+      no: "no",
+      yes_when_course_starts: "yes_when_course_starts",
+      yes_in_first_two_years: "yes_in_first_two_years",
+      yes_over_two_years: "yes_over_two_years",
+    ).backed_by_column_of_type(:text)
+  }
+end
