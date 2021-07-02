ac37d75c218a341173ec48391dfa2d5d5047159d
Tony Headford <tony@binarycircus.com>
2021-07-02 10:40:40 +0100

    Reworked SQL query and data retrieval

--- app/controllers/api/v1/data_studio/school_rollout_controller.rb
+++ app/controllers/api/v1/data_studio/school_rollout_controller.rb
@@ -7,18 +7,66 @@ module Api
         include ApiTokenAuthenticatable
 
         def index
-          render json: ::DataStudio::SchoolRolloutSerializer.new(school_rollout_data)
-            .serializable_hash.to_json
+          render json: school_rollout_data
         end
 
       private
 
-        def access_scope
-          ApiToken.where(private_api_access: true)
+        def school_rollout_data
+          {
+            data: ActiveRecord::Base.connection.select_all(school_query).map do |row|
+              {
+                id: row["id"],
+                type: "school_rollout",
+                attributes: row.except("id"),
+              }
+            end
+          }
         end
 
-        def school_rollout_data
-          ::DataStudio::FetchSchoolRolloutData.call
+        def school_query
+          <<~SQL
+            SELECT
+              s.id,
+              s.name,
+              s.urn,
+              ne.sent_at,
+              ne.opened_at,
+              ne.notify_status,
+              (icp.id IS NOT NULL) AS induction_tutor_nominated,
+              icp.created_at AS tutor_nominated_time,
+              (u.current_sign_in_at IS NOT NULL) AS induction_tutor_signed_in,
+              sc.induction_programme_choice,
+              sc.created_at AS programme_chosen_time,
+              (p.id IS NOT NULL) AS in_partnership,
+              p.created_at AS partnership_time,
+              p.challenge_reason AS partnership_challenge_reason,
+              p.challenged_at AS partnership_challenge_time,
+              lp.name AS lead_provider,
+              dp.name AS delivery_partner,
+              cip.name AS chosen_cip,
+              sc.updated_at AS cip_chosen_time 
+            FROM
+              schools s
+              FULL OUTER JOIN nomination_emails ne ON s.id = ne.school_id
+              LEFT OUTER JOIN induction_coordinator_profiles_schools icps ON s.id = icps.school_id
+              LEFT OUTER JOIN induction_coordinator_profiles icp ON icps.induction_coordinator_profile_id = icp.id
+              LEFT OUTER JOIN users u on icp.user_id = u.id
+              LEFT OUTER JOIN school_cohorts sc on s.id = sc.school_id
+              LEFT OUTER JOIN partnerships p on s.id = p.school_id
+              LEFT OUTER JOIN core_induction_programmes cip on sc.core_induction_programme_id = cip.id
+              LEFT OUTER JOIN lead_providers lp on p.lead_provider_id = lp.id
+              LEFT OUTER JOIN delivery_partners dp on p.delivery_partner_id = dp.id 
+            WHERE
+              s.school_status_code IN (1, 3)
+              AND s.school_type_code IN (1, 2, 3, 5, 6, 7, 8, 12, 14, 15, 18, 28, 31, 32, 33, 34, 35, 36, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48)
+              AND s.administrative_district_code ILIKE 'E%'
+            ORDER BY s.urn
+          SQL
+        end
+
+        def access_scope
+          ApiToken.where(private_api_access: true)
         end
       end
     end
