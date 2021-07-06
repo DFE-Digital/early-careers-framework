29b03fe0d938e5c08dcfd44409251f83f02b6e77
Stanislaw Klajn <sklajn@gmail.com>
2021-07-06 10:03:41 +0100

    Fixing old migrations

--- db/migrate/20210630000110_backfill_mentor_profile_uplift_data.rb
+++ db/migrate/20210630000110_backfill_mentor_profile_uplift_data.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class BackfillMentorProfileUpliftData < ActiveRecord::Migration[6.1]
+  class MentorProfile < ApplicationRecord
+    belongs_to :school
+  end
+
   def up
     MentorProfile.find_each do |profile|
       profile.sparsity_uplift = profile.school.sparsity_uplift?(2021)
