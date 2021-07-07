8fa2ffb38b313db1cb33136a73f7c7608d8aa59b
Phil Lee <asmega@users.noreply.github.com>
2021-07-07 17:39:51 +0100

    add coverage for models

---
+++ spec/models/npq_lead_provider_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe NpqLeadProvider, type: :model do
+  it "can be created" do
+    expect {
+      described_class.create(name: "Test Lead Provider")
+    }.to change { described_class.count }.by(1)
+  end
+
+  describe "associations" do
+    it { is_expected.to belong_to(:cpd_lead_provider).required(false) }
+  end
+end
