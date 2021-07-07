9b32ff37122fd9f44b23d12978a32ba45e5e3971
Steven <steven.woods@digital.education.gov.uk>
2021-07-07 10:55:15 +0100

    add-induction-materials-link-to-IT-dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,6 +6,22 @@ class PagesController < ApplicationController
   end
 
   def induction_tutor_materials
-    render template: "pages/induction_tutor_materials/#{params[:provider].tr('-', '_')}/#{params[:year].tr('-', '_')}"
+    provider_name = params[:provider]
+    render template: "pages/induction_tutor_materials/#{folder_name(provider_name)}/#{params[:year].tr('-', '_')}"
+  end
+
+private
+
+  def folder_name(provider_name)
+    case provider_name
+    when "ambition-institute"
+      "ambition"
+    when "educational-development-trust"
+      "educational_development_trust"
+    when "teach-first"
+      "teach_first"
+    when "ucl-institute-of-education"
+      "ucl"
+    end
   end
 end
