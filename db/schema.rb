4c72576f5e6824f2693da87bf92633c12f3d522a
Phil Lee <asmega@users.noreply.github.com>
2021-07-07 13:24:49 +0100

    data migration to populate CpdLeadProvider table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_07_115324) do
+ActiveRecord::Schema.define(version: 2021_07_07_120216) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
