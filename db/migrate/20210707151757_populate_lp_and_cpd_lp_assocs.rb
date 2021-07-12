e4ca93e89f30fc26b41980f3e7d5f2d7d602ddca
Phil Lee <asmega@users.noreply.github.com>
2021-07-12 12:56:18 +0100

    change migrations to use throwaway classes

--- db/migrate/20210707151757_populate_lp_and_cpd_lp_assocs.rb
+++ db/migrate/20210707151757_populate_lp_and_cpd_lp_assocs.rb
@@ -1,18 +1,30 @@
 # frozen_string_literal: true
 
+class MigrationLeadProvider2 < ApplicationRecord
+  self.table_name = "lead_providers"
+end
+
+class MigrationNPQLeadProvider2 < ApplicationRecord
+  self.table_name = "npq_lead_providers"
+end
+
+class MigrationCpdLeadProvider2 < ApplicationRecord
+  self.table_name = "cpd_lead_providers"
+end
+
 class PopulateLpAndCpdLpAssocs < ActiveRecord::Migration[6.1]
   def up
-    LeadProvider.all.each do |lp|
-      lp.update!(cpd_lead_provider: CpdLeadProvider.find_by(name: lp.name))
+    MigrationLeadProvider2.all.each do |lp|
+      lp.update!(cpd_lead_provider: MigrationCpdLeadProvider2.find_by(name: lp.name))
     end
 
-    NPQLeadProvider.all.each do |lp|
-      lp.update!(cpd_lead_provider: CpdLeadProvider.find_by(name: lp.name))
+    MigrationNPQLeadProvider2.all.each do |lp|
+      lp.update!(cpd_lead_provider: MigrationCpdLeadProvider2.find_by(name: lp.name))
     end
   end
 
   def down
-    LeadProvider.update_all(cpd_lead_provider: nil)
-    NPQLeadProvider.update_all(cpd_lead_provider: nil)
+    MigrationLeadProvider2.update_all(cpd_lead_provider: nil)
+    MigrationNPQLeadProvider2.update_all(cpd_lead_provider: nil)
   end
 end
