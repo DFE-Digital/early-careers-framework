2cd3037318a015ceff3ee40a113ac160fc8c95f0
Tim Abell <tim@timwise.co.uk>
2021-07-12 17:37:22 +0100

    Simplify payment_calculation_spec
    Prep work for moving output_payment.feature to pure rspec tests

--- spec/lib/payment_calculator/ecf/payment_calculation_spec.rb
+++ spec/lib/payment_calculator/ecf/payment_calculation_spec.rb
@@ -3,14 +3,9 @@
 require "payment_calculator/ecf/payment_calculation"
 
 describe ::PaymentCalculator::Ecf::PaymentCalculation do
-  let(:contract) do
-    FactoryBot.build_stubbed(:call_off_contract)
-  end
+  let(:contract) { FactoryBot.build_stubbed(:call_off_contract) }
 
-  let(:start_event_name) { "Started" }
-  let(:participant_for_event) { 1900 }
-
-  let(:retained_event_aggregations) do
+  describe ".call" do
     {
       "Started" => 1900,
       "Retention 1" => 1700,
@@ -18,31 +13,21 @@ describe ::PaymentCalculator::Ecf::PaymentCalculation do
       "Retention 3" => 1000,
       "Retention 4" => 800,
       "Completion" => 500,
-    }
-  end
+    }.each do |event_name, retained_participants|
+      it "returns the expected types for '#{event_name}' event" do
+        result = described_class.call(contract: contract, event_type: event_name, total_participants: retained_participants)
 
-  let(:params) do
-    {
-      contract: contract,
-    }
-  end
+        result.dig(:service_fees).each do |service_fee|
+          expect(service_fee[:service_fee_per_participant]).to be_a(BigDecimal)
+          expect(service_fee[:service_fee_total]).to be_a(BigDecimal)
+          expect(service_fee[:service_fee_monthly]).to be_a(BigDecimal)
+        end
 
-  it "returns the expected types for all outputs" do
-    retained_event_aggregations.each do |key, value|
-      result = described_class.call(contract: contract, event_type: key, total_participants: value)
-
-      result.dig(:service_fees).each do |service_fee|
-        expect(service_fee[:service_fee_per_participant]).to be_a(BigDecimal)
-        expect(service_fee[:service_fee_total]).to be_a(BigDecimal)
-        expect(service_fee[:service_fee_monthly]).to be_a(BigDecimal)
+        result.dig(:output_payments).each do |output_payment|
+          expect(output_payment.dig(event_name, :retained_participants)).to be_an(Integer)
+          expect(output_payment.dig(event_name, :per_participant)).to be_an(BigDecimal)
+        end
       end
-    end
-
-    result = described_class.call(contract: contract, event_type: start_event_name, total_participants: participant_for_event)
-
-    result.dig(:output_payments).each do |output_payment|
-      expect(output_payment.dig(start_event_name, :retained_participants)).to be_an(Integer)
-      expect(output_payment.dig(start_event_name, :per_participant)).to be_an(BigDecimal)
     end
   end
 end
