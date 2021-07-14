f0c3675bd47ca22ea496c416e54f0e50b75c5ec9
Ethan Mills <ethan.mills@softwire.com>
2021-07-14 16:33:16 +0100

    CPDRP-585: Set cohorts in tests

--- spec/cypress/app_commands/scenarios/school_participants.rb
+++ spec/cypress/app_commands/scenarios/school_participants.rb
@@ -16,10 +16,9 @@ FactoryBot.create(
 coordinator = FactoryBot.create(:user, :induction_coordinator, email: "school-leader@example.com", full_name: "Ms School Leader")
 coordinator.induction_coordinator_profile.schools = [school]
 
-mentor = FactoryBot.create(:user, :mentor, full_name: "Abdul Mentor", id: "51223b41-a562-4d94-b50c-0ce59a8bb34d")
-mentor.mentor_profile.update!(school: school)
+mentor = FactoryBot.create(:user, :mentor, full_name: "Abdul Mentor", id: "51223b41-a562-4d94-b50c-0ce59a8bb34d", school: school, cohort: cohort)
 
-FactoryBot.create(:user, :mentor, full_name: "Unrelated user", email: "unrelated@example.com")
+FactoryBot.create(:user, :mentor, full_name: "Unrelated user", email: "unrelated@example.com", cohort: cohort)
 
-FactoryBot.create(:user, :early_career_teacher, full_name: "Joe Bloggs", email: "joe-bloggs@example.com", school: school, mentor: mentor)
-FactoryBot.create(:user, :early_career_teacher, full_name: "Dan Smith", email: "dan-smith@example.com", school: school)
+FactoryBot.create(:user, :early_career_teacher, full_name: "Joe Bloggs", email: "joe-bloggs@example.com", school: school, mentor: mentor, cohort: cohort)
+FactoryBot.create(:user, :early_career_teacher, full_name: "Dan Smith", email: "dan-smith@example.com", school: school, cohort: cohort)
