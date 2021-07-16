3da5d6a9e380a2a1b258a76b68b6d0e928f8ff3f
Tim Abell <tim@timwise.co.uk>
2021-07-16 16:48:09 +0100

    Skip mock login for unknown email instead of raising
    Resolves an exception seen on sandbox environment when entering in an
    unknown email address with a domain that is in the `TEST_DOMAINS` list
    into `/users/sign_in`.
    
    https://sentry.io/organizations/dfe-bat/issues/2509940522/
    
    Co-authored-by: Pavel Lisovin <pavel.lisovin@digital.education.gov.uk>

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -81,7 +81,8 @@ private
     email = params.dig(:user, :email)
     return unless TEST_DOMAINS.any? { |domain| email.include?(domain) }
 
-    user = User.find_by_email(email)
+    user = User.find_by_email(email) || return
+
     sign_in(user, scope: :user)
     redirect_to profile_dashboard_path(user)
   end
