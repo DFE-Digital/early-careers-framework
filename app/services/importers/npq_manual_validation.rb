73e795bc044153414a4cc71bf7e229a5111b0b45
Phil Lee <asmega@users.noreply.github.com>
2021-07-23 13:18:55 +0100

    service to import NPQ manual validation CSV data

---
+++ app/services/importers/npq_manual_validation.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+class Importers::NPQManualValidation
+  attr_reader :path_to_csv
+
+  def initialize(path_to_csv:)
+    @path_to_csv = path_to_csv
+  end
+
+  def call
+    check_headers
+
+    rows.each do |row|
+      data = NPQValidationData.find_by(id: row["application_ecf_id"])
+
+      puts "no NPQValidationData found for #{row['application_ecf_id']}" if data.nil?
+      next if data.nil?
+
+      puts "updating trn for NPQValidationData: #{row['application_ecf_id']} with trn: #{row['validated_trn']}"
+
+      data.update!(teacher_reference_number: row["validated_trn"], teacher_reference_number_verified: true)
+    end
+  end
+
+private
+
+  def check_headers
+    unless rows.headers == %w[application_ecf_id validated_trn]
+      raise NameError, "Invalid headers"
+    end
+  end
+
+  def rows
+    @rows ||= CSV.read(path_to_csv, headers: true)
+  end
+end
---
+++ spec/lib/importers/npq_manual_validation.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Importers::NPQManualValidation do
+  let(:profile) { create(:npq_validation_data) }
+
+  let(:file) { Tempfile.new("test.csv") }
+
+  around do |example|
+    original_stdout = $stdout
+    $stdout = File.open(File::NULL, "w")
+
+    example.run
+
+    $stdout = original_stdout
+  end
+
+  describe "#call" do
+    subject do
+      described_class.new(path_to_csv: file.path)
+    end
+
+    context "with well formed csv" do
+      before do
+        file.write("application_ecf_id,validated_trn")
+        file.write("\n")
+        file.write("123,7654321")
+        file.write("\n")
+        file.write("#{profile.id},7654321")
+        file.rewind
+      end
+
+      it "updates trn" do
+        expect {
+          subject.call
+        }.to change { profile.reload.teacher_reference_number }.to("7654321")
+      end
+
+      it "updates teacher_reference_number_verified to true" do
+        expect {
+          subject.call
+        }.to change { profile.reload.teacher_reference_number_verified }.to(true)
+      end
+    end
+
+    context "with malformed csv" do
+      before do
+        file.write("application_id,trn")
+        file.write("\n")
+        file.rewind
+      end
+
+      it "raises error" do
+        expect {
+          subject.call
+        }.to raise_error(NameError)
+      end
+    end
+  end
+end
