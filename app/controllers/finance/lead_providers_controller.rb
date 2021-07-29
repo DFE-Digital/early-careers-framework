eb9119e60ec6bc6bd656bdd2d21dbe00d90e113e
Tim Abell <tim@timwise.co.uk>
2021-07-29 16:50:59 +0100

    Remove redundant calculation code
    PR feedback

--- app/controllers/finance/lead_providers_controller.rb
+++ app/controllers/finance/lead_providers_controller.rb
@@ -14,16 +14,12 @@ class Finance::LeadProvidersController < Finance::BaseController
     @breakdown = CalculationOrchestrator.call(
       cpd_lead_provider: cpd_lead_provider,
       contract: @ecf_lead_provider.call_off_contract,
-      aggregator: ::ParticipantEventAggregator,
-      uplift_aggregator: ::ParticipantUpliftAggregator,
-      calculator: ::PaymentCalculator::Ecf::PaymentCalculation,
-      event_type: :started,
     )
 
     @total_ect = ParticipantEventAggregator.call({ cpd_lead_provider: cpd_lead_provider, started: :count_active_ects_for_lead_provider })
     @total_mentors = ParticipantEventAggregator.call({ cpd_lead_provider: cpd_lead_provider, started: :count_active_mentors_for_lead_provider })
-    @total_participants = ParticipantEventAggregator.call({ cpd_lead_provider: cpd_lead_provider, started: :count_active_for_lead_provider })
-    @uplift_participants = ParticipantUpliftAggregator.call({ cpd_lead_provider: cpd_lead_provider }).to_i
+    @total_participants = ParticipantEventAggregator.call({ cpd_lead_provider: cpd_lead_provider })
+    @uplift_participants = ParticipantUpliftAggregator.call({ cpd_lead_provider: cpd_lead_provider })
   end
 
 private
