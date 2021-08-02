81ea5fec79f9f70a97e37cc39b5fc962981553ae
Michal Kreft <michal.kreft@softwire.com>
2021-08-02 09:45:02 +0100

    Improve participant declaration validations

--- app/models/participant_declaration/ecf.rb
+++ app/models/participant_declaration/ecf.rb
@@ -2,6 +2,10 @@
 
 class ParticipantDeclaration::ECF < ParticipantDeclaration
   def valid_courses
-    %w[ecf-induction ecf-mentor]
+    valid_courses = []
+    valid_courses << "ecf-induction" if user.early_career_teacher?
+    valid_courses << "ecf-mentor" if user.mentor?
+
+    valid_courses
   end
 end
--- spec/models/participant_declaration_spec.rb
+++ spec/models/participant_declaration_spec.rb
@@ -3,6 +3,8 @@
 require "rails_helper"
 
 RSpec.describe ParticipantDeclaration, type: :model do
+  subject { described_class.new(user: create(:user)) }
+
   describe "associations" do
     it { is_expected.to belong_to(:cpd_lead_provider) }
     it { is_expected.to have_one(:profile_declaration) }
@@ -12,29 +14,25 @@ RSpec.describe ParticipantDeclaration, type: :model do
     let(:call_off_contract) { create(:call_off_contract) }
 
     context "when one profile" do
-      let(:profile_declaration) do
-        create(:participant_declaration,
-               :with_profile_type,
-               cpd_lead_provider: call_off_contract.lead_provider.cpd_lead_provider,
-               profile_type: profile_type,
-               course_identifier: course_identifier)
-      end
-
       context "for mentor was created" do
-        let(:profile_type) { :mentor_profile }
-        let(:course_identifier) { "ecf-mentor" }
+        let(:mentor_participant_declaration) do
+          create(:mentor_participant_declaration,
+                 cpd_lead_provider: call_off_contract.lead_provider.cpd_lead_provider)
+        end
 
         it "includes declaration with mentor profile" do
-          expect(ParticipantDeclaration.uplift).to include(profile_declaration)
+          expect(ParticipantDeclaration.uplift).to include(mentor_participant_declaration)
         end
       end
 
       context "for early career teacher was created" do
-        let(:profile_type) { :early_career_teacher_profile }
-        let(:course_identifier) { "ecf-induction" }
+        let(:ect_participant_declaration) do
+          create(:ect_participant_declaration,
+                 cpd_lead_provider: call_off_contract.lead_provider.cpd_lead_provider)
+        end
 
         it "includes declaration with mentor profile" do
-          expect(ParticipantDeclaration.uplift).to include(profile_declaration)
+          expect(ParticipantDeclaration.uplift).to include(ect_participant_declaration)
         end
       end
     end
