08e4e74d4606ede29201ddd4da1cfdf2a4e1b49e
Tony Headford <tony@binarycircus.com>
2021-08-05 13:53:58 +0100

    add spec for participant eligibility

---
+++ spec/models/ecf_participant_eligibility_spec.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe ECFParticipantEligibility, type: :model do
+  subject(:eligibility) { described_class.new(active_flags: false, previous_participation: false, previous_induction: false, qts: true) }
+
+  it { is_expected.to belong_to(:participant_profile) }
+  it {
+    is_expected.to define_enum_for(:status).with_values(
+      eligible: "eligible",
+      matched: "matched",
+      manual_check: "manual_check",
+    ).backed_by_column_of_type(:string).with_suffix
+  }
+
+  describe "#determine_status" do
+    context "when active_flags are true" do
+      it "sets the status to manual_check" do
+        eligibility.active_flags = true
+        eligibility.valid?
+        expect(eligibility).to be_manual_check_status
+      end
+    end
+
+    context "when previous_participation is true" do
+      it "sets the status to manual_check" do
+        eligibility.previous_participation = true
+        eligibility.valid?
+        expect(eligibility).to be_manual_check_status
+      end
+    end
+
+    context "when previous_induction is true" do
+      it "sets the status to manual_check" do
+        eligibility.previous_induction = true
+        eligibility.valid?
+        expect(eligibility).to be_manual_check_status
+      end
+    end
+
+    context "when QTS status is false and no other flags are set" do
+      it "sets the status to matched" do
+        eligibility.qts = false
+        eligibility.valid?
+        expect(eligibility).to be_matched_status
+      end
+    end
+
+    context "when QTS status is true and no other flags are set" do
+      # NOTE: this is the holding pen functionality to make all requests
+      # go via a manual validation until we have more supporting data
+      # This scenario should set the status to :eligible otherwise
+      it "sets the status to matched" do
+        eligibility.qts = true
+        eligibility.valid?
+        expect(eligibility).to be_matched_status
+      end
+    end
+  end
+end
