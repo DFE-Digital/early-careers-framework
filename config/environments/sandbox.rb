ab400ddac304d65136fc16b9616801ddc77db7d2
Pavel Lisovin <pavel.lisovin@digital.education.gov.uk>
2021-08-06 09:41:10 +0100

    Add time traveler to sandbox environment

--- config/environments/sandbox.rb
+++ config/environments/sandbox.rb
@@ -5,6 +5,8 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.middleware.use TimeTraveler
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
