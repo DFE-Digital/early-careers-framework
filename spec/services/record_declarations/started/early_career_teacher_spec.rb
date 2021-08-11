8d2bf14bb4a95069c0d29e0da2da4ecf0acb5e3d
Pavel Lisovin <pavel.lisovin@digital.education.gov.uk>
2021-08-11 09:09:42 +0100

    Fix rspec test output warning

--- spec/services/record_declarations/started/early_career_teacher_spec.rb
+++ spec/services/record_declarations/started/early_career_teacher_spec.rb
@@ -90,7 +90,7 @@ RSpec.describe RecordDeclarations::Started::EarlyCareerTeacher do
     it "does not raise ParameterMissing error" do
       params = ect_params.merge({ declaration_date: (Time.zone.now - 100.years).rfc3339(9) })
       params[:raw_event] = generate_raw_event(params)
-      expect { described_class.call(params) }.to_not raise_error(ActionController::ParameterMissing)
+      expect { described_class.call(params) }.to_not raise_error
     end
   end
 end
