b638adddcbca3b4b363b2e07c0a4dc7930fc4a01
Michal Kreft <michal.kreft@softwire.com>
2021-08-12 16:05:24 +0100

    Fixing middleware step three: Make auditor tests closer to real life

--- spec/middlewares/lead_provider_request_auditor_spec.rb
+++ spec/middlewares/lead_provider_request_auditor_spec.rb
@@ -1,19 +1,15 @@
 # frozen_string_literal: true
 
-describe LeadProviderRequestAuditor do
-  let(:app) { proc { [200, {}, Time.zone.now.to_s] } }
-  let(:middleware) { described_class.new(app) }
-  let(:request) { Rack::MockRequest.new(middleware) }
-
+describe LeadProviderRequestAuditor, type: :request do
   context "when sending a request to an audited path" do
     it "saves the request details" do
-      expect { request.post("/api/v1/participant-declarations.json") }.to change(ApiRequestAudit, :count).by(1)
+      expect { post("/api/v1/participant-declarations.json") }.to change(ApiRequestAudit, :count).by(1)
     end
   end
 
   context "when sending a request to non-audited path" do
     it "changes the current date to the date from the header" do
-      expect { request.get("/") }.not_to change(ApiRequestAudit, :count)
+      expect { get("/") }.not_to change(ApiRequestAudit, :count)
     end
   end
 end
