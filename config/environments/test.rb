d072888fcb7934e3fd0e36e010a0f7d5512d98d2
Michal Kreft <michal.kreft@softwire.com>
2021-08-12 16:04:15 +0100

    Fixing middleware step one: enable in test environment

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -12,6 +12,10 @@ Rails.application.configure do
 
   config.cache_classes = false
 
+  config.middleware.use TimeTraveler
+
+  config.middleware.use LeadProviderRequestAuditor
+
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that
   # preloads Rails for running tests, you may have to set it to true.
