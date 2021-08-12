4bfadab3465d169e37528b2866e7605fc2d02318
Michal Kreft <michal.kreft@softwire.com>
2021-08-12 16:54:37 +0100

    Bring back old middleware test using mocs

--- spec/middlewares/lead_provider_request_auditor_spec.rb
+++ spec/middlewares/lead_provider_request_auditor_spec.rb
@@ -1,15 +1,19 @@
 # frozen_string_literal: true
 
-describe LeadProviderRequestAuditor, type: :request do
+describe LeadProviderRequestAuditor do
+  let(:app) { proc { [200, {}, Time.zone.now.to_s] } }
+  let(:middleware) { described_class.new(app) }
+  let(:request) { Rack::MockRequest.new(middleware) }
+
   context "when sending a request to an audited path" do
     it "saves the request details" do
-      expect { post("/api/v1/participant-declarations.json") }.to change(ApiRequestAudit, :count).by(1)
+      expect { request.post("/api/v1/participant-declarations.json") }.to change(ApiRequestAudit, :count).by(1)
     end
   end
 
   context "when sending a request to non-audited path" do
     it "changes the current date to the date from the header" do
-      expect { get("/") }.not_to change(ApiRequestAudit, :count)
+      expect { request.get("/") }.not_to change(ApiRequestAudit, :count)
     end
   end
 end
