f8bbae4035ed05a8d8f4edff39d570f1523d4c8f
Paul McKibbin <pmckibbin@gmail.com>
2021-08-17 14:28:51 +0100

    Make the next method a little easier to follow

--- lib/tasks/school_urn_generator.rb
+++ lib/tasks/school_urn_generator.rb
@@ -5,10 +5,20 @@ ALL_URNS = (1..999_999).freeze unless defined?(ALL_URNS)
 class SchoolURNGenerator
   class << self
     def next
-      sprintf("%06d", taken.push(available.pop || (raise "URN available list exhausted"))[-1])
+      next_urn=next_from_available_stack
+      add_to_taken_stack(next_urn)
+      sprintf("%06d", next_urn)
     end
 
-  private
+    private
+
+    def add_to_taken_stack(next_urn)
+      taken.push(next_urn)
+    end
+
+    def next_from_available_stack
+      available.pop || (raise "URN available list exhausted")
+    end
 
     def available
       @available.presence || reseed
--- lib/tasks/trn_generator.rb
+++ lib/tasks/trn_generator.rb
@@ -5,10 +5,19 @@ ALL_TRNS = (1111..9_999_999).freeze unless defined?(ALL_TRNS)
 class TRNGenerator
   class << self
     def next
-      sprintf("%07d", taken.push(available.pop || (raise "TRN available list exhausted"))[-1])
+      next_trn=next_from_available_stack
+      add_to_taken_stack(next_trn)
+      sprintf("%07d", next_trn)
     end
 
   private
+    def add_to_taken_stack(next_trn)
+      taken.push(next_trn)
+    end
+
+    def next_from_available_stack
+      available.pop || (raise "TRN available list exhausted")
+    end
 
     def available
       @available.presence || reseed
