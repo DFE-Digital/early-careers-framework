d07c97bc4b7039ec0d363f96e752c1e16bfaa1c9
Paul McKibbin <pmckibbin@gmail.com>
2021-08-17 15:19:02 +0100

    Added sparsity uplift to the participant profile

--- lib/tasks/valid_test_data_generator.rb
+++ lib/tasks/valid_test_data_generator.rb
@@ -14,7 +14,9 @@ module ValidTestDataGenerator
     def call(total_schools: 10, participants_per_school: 100)
       generate_new_schools(count: total_schools - lead_provider.schools.count) if lead_provider.schools.count < total_schools
       lead_provider.schools.order("created_at desc").limit(total_schools).each do |school|
-        find_or_create_participants(school: school, number_of_participants: participants_per_school)
+        sparsity_uplift = weighted_choice(selection: [true, false], odds: [11, 89])
+        pupil_premium_uplift = weighted_choice(selection: [true, false], odds: [11, 39])
+        find_or_create_participants(school: school, number_of_participants: participants_per_school, sparsity_uplift: sparsity_uplift, pupil_premium_uplift: pupil_premium_uplift)
       end
     end
 
@@ -30,27 +32,35 @@ module ValidTestDataGenerator
       count.times { create_fip_school_with_cohort(urn: SchoolURNGenerator.next) }
     end
 
-    def find_or_create_participants(school:, number_of_participants:)
-      generate_new_participants(school: school, count: number_of_participants - school.ecf_participants.count) if school.ecf_participants.count < number_of_participants
-      school.ecf_participants.order("created_at desc").limit(number_of_participants).to_a
+    def find_or_create_participants(school:, number_of_participants:, sparsity_uplift:, pupil_premium_uplift: )
+      generate_new_participants(school: school, count: number_of_participants - school.ecf_participants.count, sparsity_uplift: sparsity_uplift, pupil_premium_uplift: pupil_premium_uplift) if school.ecf_participants.count < number_of_participants
     end
 
-    def generate_new_participants(school:, count:)
-      ((count + 1) / 2).times do
-        status = (%w[active] * 6 + %w[withdrawn]).sample
-        mentor = create_participant(school_cohort: school_cohort(school: school), profile_klass: ParticipantProfile::Mentor, status: status)
-        create_participant(school_cohort: school_cohort(school: school), profile_klass: ParticipantProfile::ECT, mentor_profile: mentor, status: status)
+    def generate_new_participants(school:, count:, sparsity_uplift:, pupil_premium_uplift:)
+      while count > 0
+        status = weighted_choice(selection: %w[active withdrawn], odds: [6,1])
+        profile_type = weighted_choice(selection: %i[mentor ect], odds: [9,1])
+        count-=1
+        if profile_type==:mentor
+          mentor = create_participant(school_cohort: school_cohort(school: school), profile_type: :mentor, status: status, sparsity_uplift: sparsity_uplift, pupil_premium_uplift: pupil_premium_uplift)
+          rand(0..3).times do
+            create_participant(school_cohort: school_cohort(school: school), profile_type: :ect, mentor_profile: mentor, status: status, sparsity_uplift: sparsity_uplift, pupil_premium_uplift: pupil_premium_uplift)
+            count-=1
+          end
+        else
+          create_participant(school_cohort: school_cohort(school: school), profile_type: :ect, status: status, sparsity_uplift: sparsity_uplift, pupil_premium_uplift: pupil_premium_uplift)
+        end
       end
     end
 
-    def create_participant(school_cohort:, profile_klass: ParticipantProfile::ECT, mentor_profile: nil, status: "active")
+    def create_participant(school_cohort:, profile_type: :ect, mentor_profile: nil, status: "active", sparsity_uplift: false, pupil_premium_uplift: false)
       name = Faker::Name.name
       user = User.create!(full_name: name, email: Faker::Internet.email(name: name))
       teacher_profile = TeacherProfile.create!(user: user, trn: random_or_nil_trn)
-      if profile_klass == ParticipantProfile::ECT
-        ParticipantProfile::ECT.create!(teacher_profile: teacher_profile, school_cohort: school_cohort, mentor_profile: mentor_profile, status: status)
+      if profile_type == :ect
+        ParticipantProfile::ECT.create!(teacher_profile: teacher_profile, school_cohort: school_cohort, mentor_profile: mentor_profile, status: status, sparsity_uplift: sparsity_uplift, pupil_premium_uplift: pupil_premium_uplift)
       else
-        ParticipantProfile::Mentor.create!(teacher_profile: teacher_profile, school_cohort: school_cohort, status: status)
+        ParticipantProfile::Mentor.create!(teacher_profile: teacher_profile, school_cohort: school_cohort, status: status, sparsity_uplift: sparsity_uplift, pupil_premium_uplift: pupil_premium_uplift)
       end
     end
 
@@ -63,8 +73,6 @@ module ValidTestDataGenerator
         s.name = Faker::Company.name
         s.address_line1 = Faker::Address.street_address
         s.postcode = Faker::Address.postcode
-        s.sparsity_uplift = ([true] * 11 + [false] * 89).sample
-        s.pupil_premium = ([true] * 11 + [false] * 39).sample
       end
       school_cohort(school: school)
       attach_partnership_to_school(school: school)
@@ -87,6 +95,12 @@ module ValidTestDataGenerator
 
     def random_or_nil_trn
       [true, false].sample ? nil : TRNGenerator.next
+    end
+
+    def weighted_choice(selection:, odds:)
+      selection.each_with_index.map do |item, index|
+        [item] * odds[index]
+      end.flatten.sample
     end
   end
 end
