027c29f4b6352873c9bba6120da380546f5404a2
Michal Kreft <michal.kreft@softwire.com>
2021-08-18 10:55:46 +0100

    See whether safety assured quells the strong migration wrath for postgres 11

--- db/migrate/20210817084817_validate_remove_null_from_participant_profile_schedules.rb
+++ db/migrate/20210817084817_validate_remove_null_from_participant_profile_schedules.rb
@@ -3,7 +3,7 @@
 class ValidateRemoveNullFromParticipantProfileSchedules < ActiveRecord::Migration[6.1]
   def change
     validate_check_constraint :participant_profiles, name: "participant_profiles_schedule_id_null"
-    change_column_null :participant_profiles, :schedule_id, false
+    safety_assured { change_column_null :participant_profiles, :schedule_id, false }
     remove_check_constraint :participant_profiles, name: "participant_profiles_schedule_id_null"
   end
 end
