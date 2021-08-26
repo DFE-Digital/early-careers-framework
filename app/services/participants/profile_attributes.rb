ed130989dea3563ed81a1de79fcfa4abee71667d
Paul McKibbin <pmckibbin@gmail.com>
2021-08-26 09:41:01 +0100

    Update profile attribute naming

--- app/services/participants/profile_attributes.rb
+++ app/services/participants/profile_attributes.rb
@@ -11,28 +11,31 @@ module Participants
       validates :participant_id, presence: { message: I18n.t(:invalid_participant) }
       validates :participant_id, format: /\A[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}\Z/, allow_blank: true
       validates :cpd_lead_provider, presence: { message: I18n.t(:missing_lead_provider) }
-      validate :valid_course_for_participant
-      validate :existing_profile
+      validate :course_valid_for_participant
+      validate :participant_has_user_profile
+    end
+
+    def course_valid_for_participant
+      return if errors.any?
+
+      errors.add(:course_identifier, I18n.t(:invalid_identifier)) unless valid_courses.include?(course_identifier)
+    end
+
+    def participant_has_user_profile
+      return if errors.any?
+
+      errors.add(:participant_id, I18n.t(:invalid_participant)) if user_profile.blank?
+    end
+
+    private
+
+    def user
+      @user ||= User.find_by(id: participant_id)
     end
 
     def valid_courses
       self.class.valid_courses
     end
 
-    def valid_course_for_participant
-      return if errors.any?
-
-      errors.add(:course_identifier, I18n.t(:invalid_identifier)) unless valid_courses.include?(course_identifier)
-    end
-
-    def user
-      @user ||= User.find_by(id: participant_id)
-    end
-
-    def existing_profile
-      return if errors.any?
-
-      errors.add(:participant_id, I18n.t(:invalid_participant)) if user_profile.blank?
-    end
   end
 end
