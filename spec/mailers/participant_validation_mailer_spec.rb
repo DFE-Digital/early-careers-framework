6e59f2a243e45cfdb0687e8784168650c5a0e851
Ethan Mills <ethan.mills@softwire.com>
2021-08-26 17:37:15 +0100

    Expand validation beta to all school types
    Extract logic into dedicated service and mailer

---
+++ spec/mailers/participant_validation_mailer_spec.rb
@@ -0,0 +1,163 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe ParticipantValidationMailer, type: :mailer do
+  let(:recipient) { Faker::Internet.email }
+  let(:school_name) { Faker::Company.name }
+  let(:start_url) { "https://www.example.com/participants/start-registration" }
+  let(:research_url) { "https://www.example.com/pages/user-research" }
+
+  describe "#ect_email" do
+    let(:participant_validation_ect_email) do
+      described_class.ect_email(
+        recipient: recipient,
+        school_name: school_name,
+        start_url: start_url,
+      )
+    end
+
+    it "renders the right headers" do
+      expect(participant_validation_ect_email.from).to match_array ["mail@example.com"]
+      expect(participant_validation_ect_email.to).to match_array [recipient]
+    end
+  end
+
+  describe "#ect_ur_email" do
+    let(:participant_validation_ect_email) do
+      described_class.ect_ur_email(
+        recipient: recipient,
+        school_name: school_name,
+        start_url: start_url,
+        user_research_url: research_url,
+      )
+    end
+
+    it "renders the right headers" do
+      expect(participant_validation_ect_email.from).to match_array ["mail@example.com"]
+      expect(participant_validation_ect_email.to).to match_array [recipient]
+    end
+  end
+
+  describe "#fip_mentor_email" do
+    let(:fip_mentor_email) do
+      described_class.fip_mentor_email(
+        recipient: recipient,
+        school_name: school_name,
+        start_url: start_url,
+      )
+    end
+
+    it "renders the right headers" do
+      expect(fip_mentor_email.from).to match_array ["mail@example.com"]
+      expect(fip_mentor_email.to).to match_array [recipient]
+    end
+  end
+
+  describe "#fip_mentor_ur_email" do
+    let(:fip_mentor_email) do
+      described_class.fip_mentor_ur_email(
+        recipient: recipient,
+        school_name: school_name,
+        start_url: start_url,
+        user_research_url: research_url,
+      )
+    end
+
+    it "renders the right headers" do
+      expect(fip_mentor_email.from).to match_array ["mail@example.com"]
+      expect(fip_mentor_email.to).to match_array [recipient]
+    end
+  end
+
+  describe "#cip_mentor_email" do
+    let(:cip_mentor_email) do
+      described_class.cip_mentor_email(
+        recipient: recipient,
+        school_name: school_name,
+        start_url: start_url,
+      )
+    end
+
+    it "renders the right headers" do
+      expect(cip_mentor_email.from).to match_array ["mail@example.com"]
+      expect(cip_mentor_email.to).to match_array [recipient]
+    end
+  end
+
+  describe "#engage_beta_mentor_email" do
+    let(:engage_beta_mentor_email) do
+      described_class.engage_beta_mentor_email(
+        recipient: recipient,
+        school_name: school_name,
+        start_url: start_url,
+      )
+    end
+
+    it "renders the right headers" do
+      expect(engage_beta_mentor_email.from).to match_array ["mail@example.com"]
+      expect(engage_beta_mentor_email.to).to match_array [recipient]
+    end
+  end
+
+  describe "#induction_coordinator_email" do
+    let(:induction_coordinator_email) do
+      described_class.induction_coordinator_email(
+        recipient: recipient,
+        school_name: school_name,
+        start_url: start_url,
+      )
+    end
+
+    it "renders the right headers" do
+      expect(induction_coordinator_email.from).to match_array ["mail@example.com"]
+      expect(induction_coordinator_email.to).to match_array [recipient]
+    end
+  end
+
+  describe "#induction_coordinator_ur_email" do
+    let(:induction_coordinator_email) do
+      described_class.induction_coordinator_ur_email(
+        recipient: recipient,
+        school_name: school_name,
+        start_url: start_url,
+      )
+    end
+
+    it "renders the right headers" do
+      expect(induction_coordinator_email.from).to match_array ["mail@example.com"]
+      expect(induction_coordinator_email.to).to match_array [recipient]
+    end
+  end
+
+  describe "#coordinator_and_mentor_email" do
+    let(:coordinator_and_mentor_email) do
+      described_class.coordinator_and_mentor_email(
+        recipient: recipient,
+        school_name: school_name,
+        start_url: start_url,
+      )
+    end
+
+    it "renders the right headers" do
+      expect(coordinator_and_mentor_email.from).to match_array ["mail@example.com"]
+      expect(coordinator_and_mentor_email.to).to match_array [recipient]
+    end
+  end
+
+  describe "#coordinator_and_mentor_ur_email" do
+    let(:coordinator_and_mentor_email) do
+      described_class.coordinator_and_mentor_ur_email(
+        recipient: recipient,
+        school_name: school_name,
+        start_url: start_url,
+        user_research_url: research_url,
+      )
+    end
+
+    it "renders the right headers" do
+      expect(coordinator_and_mentor_email.from).to match_array ["mail@example.com"]
+      expect(coordinator_and_mentor_email.to).to match_array [recipient]
+    end
+  end
+end
---
+++ spec/services/validation_beta_service_spec.rb
@@ -0,0 +1,217 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe ValidationBetaService do
+  subject(:validation_beta_service) { described_class.new }
+  let(:fip_school_1) { create(:school_cohort, induction_programme_choice: "full_induction_programme").school }
+  let(:fip_school_2) { create(:school_cohort, induction_programme_choice: "full_induction_programme").school }
+  let(:cip_school) { create(:school_cohort).school }
+  let!(:mentor_1) { create(:participant_profile, :mentor, school: fip_school_1) }
+  let!(:mentor_2) { create(:participant_profile, :mentor, school: fip_school_2) }
+  let!(:mentor_3) { create(:participant_profile, :mentor, school: cip_school) }
+  let!(:ect_1) { create(:participant_profile, :ect, school: fip_school_1) }
+  let!(:ect_2) { create(:participant_profile, :ect, school: fip_school_2) }
+  let!(:ect_3) { create(:participant_profile, :ect, school: cip_school) }
+  let!(:induction_coordinator) { create(:user, :induction_coordinator, school_ids: [fip_school_1.id]) }
+  let!(:induction_coordinator_2) { create(:user, :induction_coordinator, school_ids: [fip_school_2.id]) }
+  let!(:induction_coordinator_3) { create(:user, :induction_coordinator, school_ids: [cip_school.id]) }
+  let(:start_url) { "http://www.example.com/participants/start-registration?utm_campaign=participant-validation-beta&utm_medium=email&utm_source=participant-validation-beta" }
+  let(:research_url) { "http://www.example.com/pages/user-research?utm_campaign=participant-validation-research&utm_medium=email&utm_source=participant-validation-research" }
+  let(:mentor_research_url) { "http://www.example.com/pages/user-research?mentor=true&utm_campaign=participant-validation-research&utm_medium=email&utm_source=participant-validation-research" }
+  let(:coordinator_mentor_research_url) { "http://www.example.com/pages/sit-user-research?utm_campaign=participant-validation-research&utm_medium=email&utm_source=participant-validation-research" }
+  let(:induction_coordinator_start_url) { "http://www.example.com/?utm_campaign=participant-validation-sit-notification&utm_medium=email&utm_source=cpdservice" }
+  let(:schools) { [fip_school_1, fip_school_2, cip_school] }
+  let(:urns) { schools.map(&:urn) }
+
+  describe "#invite_schools" do
+    it "activates the participant validation feature flag for schools" do
+      validation_beta_service.invite_schools(array_of_urns: urns, participant_research: false, coordinator_research: false)
+
+      [fip_school_1, fip_school_2, cip_school].each do |school|
+        expect(FeatureFlag.active?(:participant_validation, for: school)).to be true
+      end
+    end
+
+    context "when not carrying out participant research" do
+      before do
+        validation_beta_service.invite_schools(array_of_urns: urns, participant_research: false, coordinator_research: false)
+      end
+
+      it "emails the induction coordinator" do
+        expect(ParticipantValidationMailer).to delay_email_delivery_of(:induction_coordinator_email)
+                                                 .with(hash_including(
+                                                         recipient: induction_coordinator.email,
+                                                         school_name: fip_school_1.name,
+                                                         start_url: induction_coordinator_start_url,
+                                                       ))
+      end
+
+      it "emails early career teachers" do
+        expect(ParticipantValidationMailer).to delay_email_delivery_of(:ect_email)
+                                                 .with(hash_including(
+                                                         recipient: ect_1.user.email,
+                                                         school_name: fip_school_1.name,
+                                                         start_url: start_url,
+                                                       ))
+        expect(ParticipantValidationMailer).to delay_email_delivery_of(:ect_email)
+                                                 .with(hash_including(
+                                                         recipient: ect_2.user.email,
+                                                         school_name: fip_school_2.name,
+                                                         start_url: start_url,
+                                                       ))
+        expect(ParticipantValidationMailer).to delay_email_delivery_of(:ect_email)
+                                                 .with(hash_including(
+                                                         recipient: ect_3.user.email,
+                                                         school_name: cip_school.name,
+                                                         start_url: start_url,
+                                                       ))
+      end
+
+      it "emails mentors" do
+        expect(ParticipantValidationMailer).to delay_email_delivery_of(:fip_mentor_email)
+                                                 .with(hash_including(
+                                                         recipient: mentor_1.user.email,
+                                                         school_name: fip_school_1.name,
+                                                         start_url: start_url,
+                                                       ))
+        expect(ParticipantValidationMailer).to delay_email_delivery_of(:fip_mentor_email)
+                                                 .with(hash_including(
+                                                         recipient: mentor_2.user.email,
+                                                         school_name: fip_school_2.name,
+                                                         start_url: start_url,
+                                                       ))
+        expect(ParticipantValidationMailer).to delay_email_delivery_of(:cip_mentor_email)
+                                                 .with(hash_including(
+                                                         recipient: mentor_3.user.email,
+                                                         school_name: cip_school.name,
+                                                         start_url: start_url,
+                                                       ))
+      end
+    end
+
+    context "when carrying out participant research" do
+      before do
+        validation_beta_service.invite_schools(array_of_urns: urns, participant_research: true, coordinator_research: false)
+      end
+
+      it "notifies the induction coordinator of UR" do
+        expect(ParticipantValidationMailer).to delay_email_delivery_of(:induction_coordinator_ur_email)
+                                                 .with(hash_including(
+                                                         recipient: induction_coordinator.email,
+                                                         school_name: fip_school_1.name,
+                                                         start_url: induction_coordinator_start_url,
+                                                       ))
+        expect(ParticipantValidationMailer).to delay_email_delivery_of(:induction_coordinator_ur_email)
+                                                 .with(hash_including(
+                                                         recipient: induction_coordinator_2.email,
+                                                         school_name: fip_school_2.name,
+                                                         start_url: induction_coordinator_start_url,
+                                                       ))
+        expect(ParticipantValidationMailer).to delay_email_delivery_of(:induction_coordinator_ur_email)
+                                                 .with(hash_including(
+                                                         recipient: induction_coordinator_3.email,
+                                                         school_name: cip_school.name,
+                                                         start_url: induction_coordinator_start_url,
+                                                       ))
+      end
+
+      it "emails early career teachers for UR" do
+        expect(ParticipantValidationMailer).to delay_email_delivery_of(:ect_ur_email)
+                                                 .with(hash_including(
+                                                         recipient: ect_1.user.email,
+                                                         school_name: fip_school_1.name,
+                                                         start_url: start_url,
+                                                         user_research_url: research_url,
+                                                       ))
+        expect(ParticipantValidationMailer).to delay_email_delivery_of(:ect_ur_email)
+                                                 .with(hash_including(
+                                                         recipient: ect_2.user.email,
+                                                         school_name: fip_school_2.name,
+                                                         start_url: start_url,
+                                                         user_research_url: research_url,
+                                                       ))
+        expect(ParticipantValidationMailer).to delay_email_delivery_of(:ect_email)
+                                                 .with(hash_including(
+                                                         recipient: ect_3.user.email,
+                                                         school_name: cip_school.name,
+                                                         start_url: start_url,
+                                                       ))
+      end
+
+      it "emails mentors for UR" do
+        expect(ParticipantValidationMailer).to delay_email_delivery_of(:fip_mentor_ur_email)
+                                                 .with(hash_including(
+                                                         recipient: mentor_1.user.email,
+                                                         school_name: fip_school_1.name,
+                                                         start_url: start_url,
+                                                         user_research_url: mentor_research_url,
+                                                       ))
+        expect(ParticipantValidationMailer).to delay_email_delivery_of(:fip_mentor_ur_email)
+                                                 .with(hash_including(
+                                                         recipient: mentor_2.user.email,
+                                                         school_name: fip_school_2.name,
+                                                         start_url: start_url,
+                                                         user_research_url: mentor_research_url,
+                                                       ))
+        expect(ParticipantValidationMailer).to delay_email_delivery_of(:cip_mentor_email)
+                                                 .with(hash_including(
+                                                         recipient: mentor_3.user.email,
+                                                         school_name: cip_school.name,
+                                                         start_url: start_url,
+                                                       ))
+      end
+    end
+
+    context "when the coordinator is also a mentor" do
+      before do
+        create(:participant_profile, :mentor, user: induction_coordinator, school_cohort: fip_school_1.school_cohorts.first)
+      end
+
+      it "emails the induction coordinator a notification" do
+        validation_beta_service.invite_schools(array_of_urns: urns, participant_research: false, coordinator_research: false)
+        expect(ParticipantValidationMailer).to delay_email_delivery_of(:induction_coordinator_email)
+                                                 .with(hash_including(
+                                                         recipient: induction_coordinator.email,
+                                                         school_name: fip_school_1.name,
+                                                         start_url: induction_coordinator_start_url,
+                                                       ))
+      end
+
+      it "emails the induction coordinator to enter their details" do
+        validation_beta_service.invite_schools(array_of_urns: urns, participant_research: false, coordinator_research: false)
+        expect(ParticipantValidationMailer).to delay_email_delivery_of(:coordinator_and_mentor_email)
+                                                 .with(hash_including(
+                                                         recipient: induction_coordinator.email,
+                                                         school_name: fip_school_1.name,
+                                                         start_url: start_url,
+                                                       ))
+      end
+
+      it "emails the induction coordinator to enter their details" do
+        validation_beta_service.invite_schools(array_of_urns: urns, participant_research: false, coordinator_research: true)
+        expect(ParticipantValidationMailer).to delay_email_delivery_of(:coordinator_and_mentor_ur_email)
+                                                 .with(hash_including(
+                                                         recipient: induction_coordinator.email,
+                                                         school_name: fip_school_1.name,
+                                                         start_url: start_url,
+                                                         user_research_url: coordinator_mentor_research_url,
+                                                       ))
+      end
+    end
+
+    it "sends the correct emails to mentors in the engage beta" do
+      user = create(:user, id: "36049a75-5c7c-4a64-849d-972bac8187ea")
+      create(:participant_profile, :mentor, user: user, school_cohort: cip_school.school_cohorts.first)
+
+      validation_beta_service.invite_schools(array_of_urns: urns, participant_research: false, coordinator_research: false)
+
+      expect(ParticipantValidationMailer).to delay_email_delivery_of(:engage_beta_mentor_email)
+                                               .with(hash_including(
+                                                       recipient: user.email,
+                                                       school_name: cip_school.name,
+                                                       start_url: start_url,
+                                                     ))
+    end
+  end
+end
