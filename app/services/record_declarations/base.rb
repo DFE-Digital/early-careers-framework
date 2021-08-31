78054ccdc304ca19e8c55ae6d86f42b4f693ae15
Pavel Lisovin <pavel.lisovin@digital.education.gov.uk>
2021-08-31 15:17:20 +0100

    Removing using additional variable

--- app/services/record_declarations/base.rb
+++ app/services/record_declarations/base.rb
@@ -43,9 +43,7 @@ module RecordDeclarations
       declaration.refresh_payability!
       declaration_attempt.update!(participant_declaration: declaration)
 
-      participant_declarations_hash = ParticipantDeclarationSerializer.new(declaration).serializable_hash
-
-      participant_declarations_hash.to_json
+      ParticipantDeclarationSerializer.new(declaration).serializable_hash.to_json
     end
 
   private
