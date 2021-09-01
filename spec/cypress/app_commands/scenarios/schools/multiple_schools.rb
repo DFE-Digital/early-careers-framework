3a16e291816660f90fe5dbad8e8d5f3a29d2c8a7
Michal Kreft <michal.kreft@softwire.com>
2021-09-01 16:13:40 +0100

    Remove some percy randomness

--- spec/cypress/app_commands/scenarios/schools/multiple_schools.rb
+++ spec/cypress/app_commands/scenarios/schools/multiple_schools.rb
@@ -2,10 +2,10 @@
 
 cohort = FactoryBot.create(:cohort, :current)
 first_school = FactoryBot.create(:school, name: "Test School 1", slug: "111111-test-school-1", urn: "111111")
-FactoryBot.create(:school_cohort, school: first_school, cohort: cohort)
+FactoryBot.create(:school_cohort, school: first_school, cohort: cohort, induction_programme_choice: "core_induction_programme")
 second_school = FactoryBot.create(:school, name: "Test School 2", slug: "111112-test-school-2", urn: "111112")
-FactoryBot.create(:school_cohort, school: second_school, cohort: cohort)
+FactoryBot.create(:school_cohort, school: second_school, cohort: cohort, induction_programme_choice: "core_induction_programme")
 third_school = FactoryBot.create(:school, name: "Test School 3", slug: "111113-test-school-3", urn: "111113")
-FactoryBot.create(:school_cohort, school: third_school, cohort: cohort)
+FactoryBot.create(:school_cohort, school: third_school, cohort: cohort, induction_programme_choice: "core_induction_programme")
 
 FactoryBot.create(:user, :induction_coordinator, email: "school-leader@example.com", school_ids: [first_school.id, second_school.id])
