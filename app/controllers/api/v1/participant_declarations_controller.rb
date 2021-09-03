b3eabf08bb7af251e638fcc3e6a43bbbf567e296
Michal Kreft <michal.kreft@softwire.com>
2021-09-03 09:23:00 +0100

    Add csv version of participant declarations endpoint

--- app/controllers/api/v1/participant_declarations_controller.rb
+++ app/controllers/api/v1/participant_declarations_controller.rb
@@ -6,6 +6,7 @@ module Api
       include ApiAuditable
       include ApiTokenAuthenticatable
       include ApiPagination
+      include ApiCsv
 
       def create
         params = HashWithIndifferentAccess.new({ cpd_lead_provider: cpd_lead_provider }).merge(permitted_params["attributes"] || {})
@@ -13,15 +14,26 @@ module Api
       end
 
       def index
-        participant_declarations = ParticipantDeclaration.for_lead_provider(cpd_lead_provider)
-        participant_declarations_hash = ParticipantDeclarationSerializer.new(paginate(participant_declarations)).serializable_hash
-        render json: participant_declarations_hash.to_json
+        respond_to do |format|
+          format.json do
+            participant_declarations_hash = ParticipantDeclarationSerializer.new(paginate(participant_declarations)).serializable_hash
+            render json: participant_declarations_hash.to_json
+          end
+          format.csv do
+            participant_declarations_hash = ParticipantDeclarationSerializer.new(participant_declarations).serializable_hash
+            render body: to_csv(participant_declarations_hash)
+          end
+        end
       end
 
     private
 
       def cpd_lead_provider
         current_user
+      end
+
+      def participant_declarations
+        ParticipantDeclaration.for_lead_provider(cpd_lead_provider)
       end
 
       def permitted_params
--- app/controllers/api/v1/participants_controller.rb
+++ app/controllers/api/v1/participants_controller.rb
@@ -7,6 +7,7 @@ module Api
     class ParticipantsController < Api::ApiController
       include ApiTokenAuthenticatable
       include ApiPagination
+      include ApiCsv
 
       def index
         respond_to do |format|
@@ -37,21 +38,6 @@ module Api
 
       def access_scope
         LeadProviderApiToken.joins(cpd_lead_provider: [:lead_provider])
-      end
-
-      def to_csv(hash)
-        return "" if hash[:data].empty?
-
-        headers = %w[id]
-        attributes = hash[:data].first[:attributes].keys
-        headers.concat(attributes.map(&:to_s))
-        CSV.generate(headers: headers, write_headers: true) do |csv|
-          hash[:data].each do |item|
-            row = [item[:id]]
-            row.concat(attributes.map { |attribute| item[:attributes][attribute].to_s })
-            csv << row
-          end
-        end
       end
 
       def updated_since
