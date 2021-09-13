adb5e04de8a9668915fac0c11582480c28949e0f
Erica Porter <ericaporter@users.noreply.github.com>
2021-09-13 17:22:56 +0100

    Fix spec

--- spec/features/schools/dashboard/manage_design_our_own_training_spec.rb
+++ spec/features/schools/dashboard/manage_design_our_own_training_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe "Manage Design Our Own training", js: true, with_feature_flags: {
   scenario "Design Our Own Induction Coordinator" do
     given_there_is_a_school_that_has_chosen_design_our_own_for_2021
     and_i_am_signed_in_as_an_induction_coordinator
-    then_i_should_see_the_no_ect_induction_dashboard
+    then_i_should_see_the_design_our_own_induction_dashboard
     when_i_select_view_details
     then_i_am_taken_to_no_ect_training_info_page
   end
