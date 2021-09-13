1bbe5aecbc7a50fbc15c20808b793917479cfeb4
Ethan Mills <ethan.mills@softwire.com>
2021-09-13 17:30:10 +0100

    CPDRP-800: Only use analytics database in configured environments
    Zeitwerk eager loading fails when a non-existent database is specified
    for a model

--- app/models/analytics/base_record.rb
+++ app/models/analytics/base_record.rb
@@ -4,6 +4,6 @@ module Analytics
   class BaseRecord < ApplicationRecord
     self.abstract_class = true
 
-    connects_to database: { writing: :analytics }
+    connects_to database: { writing: :analytics } if %w[test development staging production].include? Rails.env
   end
 end
