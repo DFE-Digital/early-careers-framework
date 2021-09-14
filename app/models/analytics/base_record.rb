dc743e38fbb860c365f3e30a7cc7c579ce107078
Ethan Mills <ethan.mills@softwire.com>
2021-09-14 16:34:20 +0100

    Remove the analytics database from staging
    Now that we know the approach works, remove the database for costsavings

--- app/models/analytics/base_record.rb
+++ app/models/analytics/base_record.rb
@@ -4,6 +4,6 @@ module Analytics
   class BaseRecord < ApplicationRecord
     self.abstract_class = true
 
-    connects_to database: { writing: :analytics } if %w[test development staging production].include? Rails.env
+    connects_to database: { writing: :analytics } if %w[test development production].include? Rails.env
   end
 end
--- app/services/analytics/ecf_validation_service.rb
+++ app/services/analytics/ecf_validation_service.rb
@@ -4,7 +4,7 @@ module Analytics
   class ECFValidationService
     class << self
       def upsert_record(participant_profile)
-        return unless %w[development staging production].include? Rails.env
+        return unless %w[test development production].include? Rails.env
 
         record = Analytics::ECFParticipant.find_or_initialize_by(participant_profile_id: participant_profile.id)
         record.user_id = participant_profile.user.id
@@ -26,7 +26,7 @@ module Analytics
       handle_asynchronously :upsert_record
 
       def record_validation(participant_profile:, real_time_attempts:, real_time_success:, nino_entered:)
-        return unless %w[development staging production].include? Rails.env
+        return unless %w[test development production].include? Rails.env
 
         record = Analytics::ECFParticipant.find_or_initialize_by(participant_profile_id: participant_profile.id)
         record.real_time_attempts = real_time_attempts
