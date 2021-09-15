5b4b56d42596471329e4ec7565800cea0a7ab0fd
BroiSatse <sklajn@gmail.com>
2021-09-15 15:04:26 +0100

    Using different email templataes for new participant and aprticipant reminder emails

--- app/mailers/participant_mailer.rb
+++ app/mailers/participant_mailer.rb
@@ -3,9 +3,21 @@
 class ParticipantMailer < ApplicationMailer
   PARTICIPANT_REMOVED_BY_STI = "ab8fb8b1-9f44-4d27-8e80-01d5d70d22f6"
 
+  PARTICIPANT_ADDED_TEMPLATES = {
+    ect: "50ee41e5-06b9-41cf-9afd-d5bc4db356c4",
+    mentor: "e0198213-c09d-41aa-8197-b167e495e49d",
+    sit_mentor: "7e7d3fdb-41f5-4e04-a4ae-acf92e8fefe6",
+  }.freeze
+
+  DETAIL_REMINDER_TEMPLATES = {
+    ect: "0bf633c3-54c9-4150-b1fb-57748376aed1",
+    mentor: "691eb49b-f23f-49dd-b799-f8efdd5e010f",
+    sit_mentor: "fcf85c00-6d96-48a6-bf27-ea09c61f0eee",
+  }.freeze
+
   def participant_added(participant_profile:)
     template_mail(
-      "50ee41e5-06b9-41cf-9afd-d5bc4db356c4",
+      PARTICIPANT_ADDED_TEMPLATES[template_type_for participant_profile],
       to: participant_profile.user.email,
       rails_mailer: mailer_name,
       rails_mail_template: action_name,
@@ -35,7 +47,7 @@ class ParticipantMailer < ApplicationMailer
 
   def add_details_reminder(participant_profile:)
     template_mail(
-      "0bf633c3-54c9-4150-b1fb-57748376aed1",
+      DETAIL_REMINDER_TEMPLATES[template_type_for participant_profile],
       to: participant_profile.user.email,
       rails_mailer: mailer_name,
       rails_mail_template: action_name,
@@ -46,5 +58,13 @@ class ParticipantMailer < ApplicationMailer
         participant_start: new_user_session_url,
       },
     )
+  end
+
+private
+
+  def template_type_for(profile)
+    type = profile.participant_type
+    type = :sit_mentor if type == :mentor && profile.user.induction_coordinator?
+    type
   end
 end
