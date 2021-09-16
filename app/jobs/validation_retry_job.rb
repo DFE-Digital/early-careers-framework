d43ea03e9597e81066e722eb06384cb611dfa7fb
Tony Headford <tony@binarycircus.com>
2021-09-16 12:13:23 +0100

    Add first name check flag to validation call

--- app/jobs/validation_retry_job.rb
+++ app/jobs/validation_retry_job.rb
@@ -7,7 +7,8 @@ class ValidationRetryJob < CronJob
 
   def perform
     ECFParticipantValidationData.where(api_failure: true).find_each do |validation_data|
-      ValidateParticipant.call(participant_profile: validation_data.participant_profile)
+      ValidateParticipant.call(participant_profile: validation_data.participant_profile,
+                               config: { check_first_name_only: true })
     rescue StandardError => e
       Rails.logger.error("Problem with DQT API on retry: " + e.message)
       Sentry.capture_message("Problem with DQT API on retry: " + e.message)
