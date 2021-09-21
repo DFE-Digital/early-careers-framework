a53d94a4015315521036a0b6993dafd88a4486b1
Paul McKibbin <pmckibbin@gmail.com>
2021-09-21 17:27:26 +0100

    Add specific tests for uplift and the new call_off_contract public methods

---
+++ spec/lib/payment_calculator/ecf/contract/uplift_payment_calculations_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe PaymentCalculator::ECF::Contract::UpliftPaymentCalculations do
+  let(:dummy_class) { Class.new { include PaymentCalculator::ECF::Contract::UpliftPaymentCalculations } }
+  let(:call_off_contract) { create(:call_off_contract) }
+  let(:calculator) { dummy_class.new({ contract: call_off_contract }) }
+
+  context "when condition" do
+    it "returns the uplift_payment_per_participant" do
+      expect(calculator.uplift_payment_per_participant).to eq(100)
+    end
+
+    it "returns the expected uplift_payment_per_participant for a each event type" do
+      expect(calculator.uplift_payment_per_participant_for_event(event_type: :started)).to eq(100)
+      expect(calculator.uplift_payment_per_participant_for_event(event_type: :retained_1)).to eq(0)
+      expect(calculator.uplift_payment_per_participant_for_event(event_type: :retained_2)).to eq(0)
+      expect(calculator.uplift_payment_per_participant_for_event(event_type: :retained_3)).to eq(0)
+      expect(calculator.uplift_payment_per_participant_for_event(event_type: :retained_4)).to eq(0)
+      expect(calculator.uplift_payment_per_participant_for_event(event_type: :completed)).to eq(0)
+    end
+
+    it "returns a capped uplift_payment_for_events when the total exceeds 5% of the total contract value" do
+      expect(calculator.uplift_payment_for_event(uplift_participants: 10_000, event_type: :started)).to eq(99_500)
+    end
+  end
+end
--- spec/models/call_off_contract_spec.rb
+++ spec/models/call_off_contract_spec.rb
@@ -8,8 +8,16 @@ RSpec.describe CallOffContract, type: :model do
   describe "associations" do
     it { is_expected.to have_many(:participant_bands) }
 
-    it "is expected to have band_a with nil as the lowest min value" do
-      expect(call_off_contract.band_a.min).to eq(nil)
+    it "is expected to have band_a with the lowest minimum value" do
+      expect(call_off_contract.band_a.min.to_i).to eq(0)
+    end
+
+    it "is expected to have a total contract value" do
+      expect(call_off_contract.total_contract_value).to eq(2000 * 995) # recruitment_target * per_participant
+    end
+
+    it "is expected to have an uplift cap of 5% of the total contract value" do
+      expect(call_off_contract.uplift_cap).to eq(99_500.0)
     end
   end
 end
