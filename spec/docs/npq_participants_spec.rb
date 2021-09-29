935eda9e336835578f2a2bbd2738c5358c17258d
Pavel Lisovin <pavel.lisovin@digital.education.gov.uk>
2021-09-29 18:03:32 +0100

    Add filter and rename trn to teacher_reference_number in the swagger doc

--- spec/docs/npq_participants_spec.rb
+++ spec/docs/npq_participants_spec.rb
@@ -16,6 +16,18 @@ describe "API", type: :request, swagger_doc: "v1/api_spec.json" do
       tags "NPQ participants"
       security [bearerAuth: []]
 
+      parameter name: :filter,
+                in: :query,
+                schema: {
+                  "$ref": "#/components/schemas/ListFilter",
+                },
+                type: :object,
+                style: :deepObject,
+                explode: true,
+                required: false,
+                description: "Refine NPQ participants to return.",
+                example: { updated_since: "2020-11-13T11:21:55Z" }
+
       parameter name: :page,
                 in: :query,
                 schema: {
