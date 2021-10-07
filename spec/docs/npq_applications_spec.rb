d026a23f6adb1085abcdfb5a5df8a1f8d680df39
Michal Kreft <michal.kreft@softwire.com>
2021-10-07 14:11:02 +0100

    Looks like removing content_type from schemas makes rswag validate them correctly

--- spec/docs/npq_applications_spec.rb
+++ spec/docs/npq_applications_spec.rb
@@ -8,7 +8,8 @@ describe "API", type: :request, swagger_doc: "v1/api_spec.json" do
   let(:npq_lead_provider) { create(:npq_lead_provider) }
   let(:token) { LeadProviderApiToken.create_with_random_token!(cpd_lead_provider: cpd_lead_provider) }
   let(:Authorization) { "Bearer #{token}" }
-  let!(:npq_application) { create(:npq_validation_data, npq_lead_provider: npq_lead_provider) }
+  let(:npq_course) { create(:npq_course, identifier: "npq-leading-teaching") }
+  let!(:npq_application) { create(:npq_validation_data, npq_lead_provider: npq_lead_provider, npq_course: npq_course) }
 
   path "/api/v1/npq-applications" do
     get "Retrieve multiple NPQ applications" do
--- spec/docs/npq_participants_spec.rb
+++ spec/docs/npq_participants_spec.rb
@@ -3,12 +3,11 @@
 require "swagger_helper"
 
 describe "API", type: :request, swagger_doc: "v1/api_spec.json" do
-  let!(:default_schedule) { create(:schedule, name: "ECF September standard 2021") }
   let(:cpd_lead_provider) { create(:cpd_lead_provider, npq_lead_provider: npq_lead_provider) }
   let(:npq_lead_provider) { create(:npq_lead_provider) }
   let(:token) { LeadProviderApiToken.create_with_random_token!(cpd_lead_provider: cpd_lead_provider) }
   let(:Authorization) { "Bearer #{token}" }
-  let(:npq_application) { create(:npq_validation_data, npq_lead_provider: npq_lead_provider) }
+  let!(:npq_application) { create(:npq_validation_data, npq_lead_provider: npq_lead_provider) }
 
   path "/api/v1/participants/npq" do
     get "Retrieve multiple NPQ participants" do
@@ -41,10 +40,6 @@ describe "API", type: :request, swagger_doc: "v1/api_spec.json" do
                 description: "Pagination options to navigate through the list of NPQ participants."
 
       response "200", "A list of NPQ participants" do
-        before do
-          npq_application
-        end
-
         schema({ "$ref": "#/components/schemas/MultipleNPQParticipantsResponse" })
 
         run_test!
