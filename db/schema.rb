b61777408ac51107e39354565722e7fa921a81f3
Pavel Lisovin <pavel.lisovin@digital.education.gov.uk>
2021-10-08 16:51:06 +0100

    Fix db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2021_10_07_131604) do
-=======
-ActiveRecord::Schema.define(version: 2021_10_05_162156) do
->>>>>>> Add missing updated schema
+ActiveRecord::Schema.define(version: 2021_10_08_081430) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
