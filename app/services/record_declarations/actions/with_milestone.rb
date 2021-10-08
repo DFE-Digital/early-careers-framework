38e05bb530b483f3d4f4ce95c688b765967ef03a
Paul McKibbin <pmckibbin@gmail.com>
2021-10-08 06:19:44 +0100

    Remove all links and associations to profile_declarations and update tests

---
+++ app/services/record_declarations/actions/with_milestone.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module RecordDeclarations
+  module Actions
+    module WithMilestone
+      extend ActiveSupport::Concern
+
+      included do
+        extend WithMilestoneClassMethods
+      end
+
+      module WithMilestoneClassMethods
+        def call(milestone:)
+          new(milestone: milestone).call
+        end
+      end
+
+    private
+
+      attr_reader :milestone
+
+      def initialize(milestone:)
+        @milestone = milestone
+      end
+    end
+  end
+end
