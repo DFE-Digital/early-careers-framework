6bd5d9df8d79d71cc44d1941dfe55da2846f2f24
Pavel Lisovin <pavel.lisovin@digital.education.gov.uk>
2021-10-08 16:47:08 +0100

    Add an additional rspec test for lead provider with vat chargeable set to false

--- spec/helpers/finance_helper_spec.rb
+++ spec/helpers/finance_helper_spec.rb
@@ -14,12 +14,22 @@ describe FinanceHelper do
       )
     end
 
-    it "returns the total payment for the breakddown" do
-      expect(helper.total_payment(breakdown).to_i).to eq(22_287)
+    context "when lead provider is vat chargeable" do
+      it "returns the total payment for the breakddown" do
+        expect(helper.total_payment(breakdown).to_i).to eq(22_287)
+      end
+
+      it "returns the total VAT for the breakddown" do
+        expect(helper.total_vat(breakdown, lead_provider).to_i).to eq(4_457)
+      end
     end
 
-    it "returns the total VAT for the breakddown" do
-      expect(helper.total_vat(breakdown, lead_provider).to_i).to eq(4_457)
+    context "when lead provider is not vat chargeable" do
+      let!(:lead_provider) { create(:lead_provider, vat_chargeable: false) }
+
+      it "returns the total VAT for the breakddown" do
+        expect(helper.total_vat(breakdown, lead_provider).to_i).to eq(0)
+      end
     end
   end
 end
