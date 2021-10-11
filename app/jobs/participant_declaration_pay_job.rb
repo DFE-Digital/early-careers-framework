6dcb9a488f68ff1dd339862456c265ef9d167607
Paul McKibbin <pmckibbin@gmail.com>
2021-10-11 17:38:43 +0100

    Create job to mark participant declarations payable at 10am on the 1st November.
    Update rake task to take the state to calculate the payments against.

---
+++ app/jobs/participant_declaration_pay_job.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class ParticipantDeclarationPayJob < ApplicationJob
+  queue_as :default
+
+  class << self
+    def schedule
+      set(wait_until: payment_time + 10.hours)
+    end
+
+    def payment_time
+      Time.local(2021,12,1).to_s(:db)
+    end
+  end
+
+  def perform(*)
+    ParticipantDeclaration.payable.each(&:make_paid!)
+  end
+end
