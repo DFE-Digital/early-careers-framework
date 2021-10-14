cf8037252efbcd962d0ab04ae25491257650e29a
Ethan Mills <ethan.mills@softwire.com>
2021-10-14 10:43:34 +0100

    CPDRP-1046: Simplify training status

--- analytics_queries/participants.sql
+++ analytics_queries/participants.sql
@@ -10,12 +10,7 @@ SELECT u.id                                                    as participant_id
                  JOIN users u2 on tp2.user_id = u2.id
         WHERE pp2.id = pp.mentor_profile_id)                   as mentor_id,
        c.start_year                                            as cohort,
-       pp.status                                               as status,
-       (SELECT state
-        FROM participant_profile_states
-        WHERE participant_profile_id = pp.id
-        ORDER BY participant_profile_states.created_at DESC
-        LIMIT 1)                                               as training_status,
+       pp.training_status                                      as training_status,
        s.name                                                  as schedule,
        epvd.created_at                                         as trn_provided_at,
        (epe.status IN ('eligible', 'matched'))                 AS trn_validated,
