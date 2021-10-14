dd41e35d6fdb393deca69b3bbdfa1315b7c2d7c1
Michal Kreft <michal.kreft@softwire.com>
2021-10-14 10:04:34 +0100

    Prioritise 2021 profiles over 2020 profiles in fetching ecf profile

--- app/models/teacher_profile.rb
+++ app/models/teacher_profile.rb
@@ -9,7 +9,7 @@ class TeacherProfile < ApplicationRecord
   # TODO: Legacy associations, to be removed
   has_one :early_career_teacher_profile, -> { active_record }, class_name: "ParticipantProfile::ECT"
   has_one :mentor_profile, -> { active_record }, class_name: "ParticipantProfile::Mentor"
-  has_one :ecf_profile, -> { active_record }, class_name: "ParticipantProfile::ECF"
+  has_one :ecf_profile, -> { active_record.includes(:cohort).order("cohort.start_year DESC") }, class_name: "ParticipantProfile::ECF"
 
   has_many :npq_profiles, class_name: "ParticipantProfile::NPQ"
   # end: TODO
