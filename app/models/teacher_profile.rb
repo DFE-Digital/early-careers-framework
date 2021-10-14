1fa0c8a36313d4abb867ccc4bf4eacbada70abcc
Michal Kreft <michal.kreft@softwire.com>
2021-10-14 11:01:10 +0100

    Fix annoying cohort sql error

--- app/models/teacher_profile.rb
+++ app/models/teacher_profile.rb
@@ -10,8 +10,8 @@ class TeacherProfile < ApplicationRecord
   has_one :early_career_teacher_profile, -> { active_record }, class_name: "ParticipantProfile::ECT"
   has_one :mentor_profile, -> { active_record }, class_name: "ParticipantProfile::Mentor"
 
-  has_many :ecf_profiles, -> { active_record.includes(:cohort).order("cohort.start_year DESC") }, class_name: "ParticipantProfile::ECF"
-  has_one :current_ecf_profile, -> { active_record.includes(:cohort).where(cohort: Cohort.current) }, class_name: "ParticipantProfile::ECF"
+  has_many :ecf_profiles, -> { active_record.includes(school_cohort: :cohort).joins(school_cohort: :cohort).order("cohort.start_year DESC") }, class_name: "ParticipantProfile::ECF"
+  has_one :current_ecf_profile, -> { active_record.includes(:school_cohort).where(school_cohort: { cohort: Cohort.current }) }, class_name: "ParticipantProfile::ECF"
 
   has_many :npq_profiles, class_name: "ParticipantProfile::NPQ"
   # end: TODO
