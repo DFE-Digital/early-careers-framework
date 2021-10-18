84e37f086f3a2cc9b217bfa74ba2c6e78366b5af
BroiSatse <sklajn@gmail.com>
2021-10-18 12:49:43 +0100

    Replacing blank with blank_allowed in mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -3,7 +3,7 @@
 class ApplicationMailer < Mail::Notify::Mailer
 private
 
-  def blank
-    Mail::Notify::Personalisation::BLANK
+  def blank_allowed(value)
+    value.presence || Mail::Notify::Personalisation::BLANK
   end
 end
