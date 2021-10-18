f13a28df007e79c914efe5364f3173ce9d260e11
Michal Kreft <michal.kreft@softwire.com>
2021-10-18 09:28:30 +0100

    Markups

--- app/jobs/create_new_fake_sandbox_data_job.rb
+++ app/jobs/create_new_fake_sandbox_data_job.rb
@@ -3,22 +3,16 @@
 class CreateNewFakeSandboxDataJob < CronJob
   self.cron_expression = "0 2 * * *"
 
+  EDT_NAME = "Education Development Trust"
   def perform
-    return unless Rails.env.sandbox? || Rails.env.test?
+    return if Rails.env.production?
 
-    provider_name = "Education Development Trust"
-
-    edt_ecf_lead_provider = LeadProvider.find_by(name: provider_name)
-    edt_npq_lead_provider = NPQLeadProvider.find_by(name: provider_name)
-
-    school = edt_ecf_lead_provider.schools.first
-    school_cohort = SchoolCohort.find_by(school: school, cohort: Cohort.current)
     10.times do
       name = Faker::Name.name
       EarlyCareerTeachers::Create.call(
         full_name: name,
         email: Faker::Internet.email(name: name),
-        school_cohort: school_cohort,
+        school_cohort: random_edt_school_cohort,
         mentor_profile_id: nil,
         year_2020: false,
       )
@@ -34,7 +28,7 @@ class CreateNewFakeSandboxDataJob < CronJob
         funding_choice: "",
         headteacher_status: "",
         nino: "",
-        school_urn: school.urn,
+        school_urn: random_edt_school.urn,
         teacher_reference_number: TRNGenerator.next,
         teacher_reference_number_verified: true,
         npq_course: NPQCourse.all.sample,
@@ -42,5 +36,23 @@ class CreateNewFakeSandboxDataJob < CronJob
         user: user,
       )
     end
+  end
+
+private
+
+  def edt_ecf_lead_provider
+    @edt_ecf_lead_provider ||= LeadProvider.find_by(name: EDT_NAME)
+  end
+
+  def edt_npq_lead_provider
+    @edt_npq_lead_provider ||= NPQLeadProvider.find_by(name: EDT_NAME)
+  end
+
+  def random_edt_school
+    @random_edt_school ||= edt_ecf_lead_provider.schools.sample
+  end
+
+  def random_edt_school_cohort
+    @random_edt_school_cohort ||= SchoolCohort.find_by(school: random_edt_school, cohort: Cohort.current)
   end
 end
