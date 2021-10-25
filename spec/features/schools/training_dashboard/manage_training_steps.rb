06e7d181e63eb35f0fb6f44bd741b57d0baa442d
Erica Porter <ericaporter@users.noreply.github.com>
2021-10-25 11:08:10 +0100

    Update manage training steps

--- spec/features/schools/training_dashboard/manage_training_steps.rb
+++ spec/features/schools/training_dashboard/manage_training_steps.rb
@@ -69,6 +69,24 @@ module ManageTrainingSteps
     expect(page).to have_text("An induction tutor should only assign themself as a mentor in exceptional circumstances")
   end
 
+  def given_i_am_on_the_cip_induction_dashboard
+    expect(page).to have_selector("h1", text: "Manage your training")
+    expect(page).not_to have_text("Programme materials")
+  end
+
+  def given_i_can_view_the_fip_induction_dashboard_without_partnership_details
+    expect(page).to have_selector("h1", text: "Manage your training")
+    expect(page).not_to have_text("Delivery partner")
+  end
+
+  def given_i_am_taken_to_fip_induction_dashboard
+    expect(page).to have_selector("h1", text: "Manage your training")
+    expect(page).to have_text("Training provider")
+    expect(page).to have_text(@school_cohort.lead_provider.name)
+    expect(page).to have_text("Delivery partner")
+    expect(page).to have_text(@school_cohort.delivery_partner.name)
+  end
+
   # And_steps
 
   def and_i_am_signed_in_as_an_induction_coordinator
@@ -81,46 +99,53 @@ module ManageTrainingSteps
   end
 
   def and_i_have_added_an_ect
-    @participant_profile_ect = create(:participant_profile, :ect, user: create(:user, full_name: "Sally Teacher"), school_cohort: @school_cohort)
+    @ect = create(:participant_profile, :ect, user: create(:user, full_name: "Sally ECT"), school_cohort: @school_cohort)
   end
 
   def and_i_have_added_a_mentor
-    @participant_profile_mentor = create(:participant_profile, :mentor, user: create(:user, full_name: "Billy Mentor"), school_cohort: @school_cohort)
+    @mentor = create(:participant_profile, :mentor, :ecf_participant_eligibility, :ecf_participant_validation_data, user: create(:user, full_name: "Billy Mentor"), school_cohort: @school_cohort)
   end
 
-  def and_i_have_added_an_eligible_ect
-    @eligible_ect = create(:participant_profile, :ect, :ecf_participant_eligibility, :ecf_participant_validation_data, school_cohort: @school_cohort)
+  def and_i_have_added_an_eligible_ect_with_mentor
+    @eligible_ect_with_mentor = create(:participant_profile, :ect, :ecf_participant_eligibility, :ecf_participant_validation_data, user: create(:user, full_name: "Eligible With-mentor"), mentor_profile_id: @contacted_for_info_mentor.id, school_cohort: @school_cohort)
   end
 
-  def and_i_have_added_an_ineligible_ect
-    @ineligible_ect = create(:participant_profile, :ect, :ecf_participant_eligibility, :ecf_participant_validation_data, school_cohort: @school_cohort)
-    @ineligible_ect.ecf_participant_eligibility.update!(status: "manual_check", reason: "active_flags")
+  def and_i_have_added_an_eligible_ect_without_mentor
+    @eligible_ect_without_mentor = create(:participant_profile, :ect, :ecf_participant_eligibility, :ecf_participant_validation_data, user: create(:user, full_name: "Eligible Without-mentor"), school_cohort: @school_cohort)
   end
 
   def and_i_have_added_an_eligible_mentor
-    @eligible_mentor = create(:participant_profile, :mentor, :ecf_participant_eligibility, :ecf_participant_validation_data, school_cohort: @school_cohort)
+    @eligible_mentor = create(:participant_profile, :mentor, :ecf_participant_eligibility, :ecf_participant_validation_data, user: create(:user, full_name: "Eligible Mentor"), school_cohort: @school_cohort)
+  end
+
+  def and_i_have_added_an_ineligible_ect_with_mentor
+    @ineligible_ect_with_mentor = create(:participant_profile, :ect, :ecf_participant_eligibility, :ecf_participant_validation_data, user: create(:user, full_name: "Ineligible With-mentor"), mentor_profile_id: @contacted_for_info_mentor.id, school_cohort: @school_cohort)
+    @ineligible_ect_with_mentor.ecf_participant_eligibility.update!(status: "ineligible", reason: "active_flags")
+  end
+
+  def and_i_have_added_an_ineligible_ect_without_mentor
+    @ineligible_ect_without_mentor = create(:participant_profile, :ect, :ecf_participant_eligibility, :ecf_participant_validation_data, user: create(:user, full_name: "Ineligible Without-mentor"), school_cohort: @school_cohort)
+    @ineligible_ect_without_mentor.ecf_participant_eligibility.update!(status: "ineligible", reason: "active_flags")
   end
 
   def and_i_have_added_an_ineligible_mentor
-    @ineligible_mentor = create(:participant_profile, :mentor, :ecf_participant_eligibility, :ecf_participant_validation_data, school_cohort: @school_cohort)
-    @ineligible_mentor.ecf_participant_eligibility.update!(status: "manual_check", reason: "previous_induction")
+    @ineligible_mentor = create(:participant_profile, :mentor, :ecf_participant_eligibility, :ecf_participant_validation_data, user: create(:user, full_name: "Ineligible Mentor"), school_cohort: @school_cohort)
+    @ineligible_mentor.ecf_participant_eligibility.update!(status: "ineligible", reason: "active_flags")
   end
 
   def and_i_have_added_an_ero_mentor
-    @ero_mentor = create(:participant_profile, :ecf_participant_eligibility, :ecf_participant_validation_data, :mentor, school_cohort: @school_cohort)
-    @ero_mentor.ecf_participant_eligibility.update!(status: "manual_check", reason: "previous_participation")
+    @ero_mentor = create(:participant_profile, :mentor, :ecf_participant_eligibility, :ecf_participant_validation_data, user: create(:user, full_name: "ERO Mentor"), school_cohort: @school_cohort)
+    @ero_mentor.ecf_participant_eligibility.update!(status: "ineligible", reason: "previous_participation")
   end
 
-  def and_i_have_added_an_ect_contacted_for_info
-    @contacted_for_info_ect = create(:participant_profile, :ect, request_for_details_sent_at: 5.days.ago, school_cohort: @school_cohort)
+  def and_i_have_added_a_contacted_for_info_ect_with_mentor
+    @contacted_for_info_ect_with_mentor = create(:participant_profile, :ect, :email_sent, request_for_details_sent_at: 5.days.ago, user: create(:user, full_name: "CFI With-mentor"), mentor_profile_id: @mentor.id, school_cohort: @school_cohort)
   end
 
-  def and_i_have_added_an_ect_whose_details_are_being_checked
-    @details_being_checked_ect = create(:participant_profile, :ecf_participant_eligibility, :ecf_participant_validation_data, :ect, school_cohort: @school_cohort)
-    @details_being_checked_ect.ecf_participant_eligibility.update!(status: "manual_check", reason: "no_qts")
+  def and_i_have_added_a_contacted_for_info_ect_without_mentor
+    @contacted_for_info_ect_without_mentor = create(:participant_profile, :ect, :email_bounced, request_for_details_sent_at: 5.days.ago, user: create(:user, full_name: "CFI Without-mentor"), school_cohort: @school_cohort)
   end
 
-<<<<<<< HEAD
   def then_i_am_on_schools_page
     visit "/schools"
   end
@@ -215,6 +240,10 @@ module ManageTrainingSteps
     and_i_have_added_an_ect_whose_details_are_being_checked
   end
 
+  def and_i_have_added_a_contacted_for_info_mentor
+    @contacted_for_info_mentor = create(:participant_profile, :mentor, :email_sent, request_for_details_sent_at: 5.days.ago, user: create(:user, full_name: "CFI Mentor"), school_cohort: @school_cohort)
+  end
+
   def and_i_am_signed_in_as_an_induction_coordinator_for_multiple_schools
     induction_coordinator = User.find_by(email: "school-leader@example.com")
     sign_in_as induction_coordinator
@@ -237,7 +266,26 @@ module ManageTrainingSteps
     click_on "Confirm"
   end
 
+  def and_i_have_added_a_details_being_checked_ect_with_mentor
+    @details_being_checked_ect_with_mentor = create(:participant_profile, :ect, :ecf_participant_eligibility, :ecf_participant_validation_data, user: create(:user, full_name: "DBC With-Mentor"), mentor_profile_id: @contacted_for_info_mentor.id, school_cohort: @school_cohort)
+    @details_being_checked_ect_with_mentor.ecf_participant_eligibility.update!(status: "manual_check", reason: "no_qts")
+  end
+
+  def and_i_have_added_a_details_being_checked_ect_without_mentor
+    @details_being_checked_ect_without_mentor = create(:participant_profile, :ect, :ecf_participant_eligibility, :ecf_participant_validation_data, user: create(:user, full_name: "DBC Without-Mentor"), school_cohort: @school_cohort)
+    @details_being_checked_ect_without_mentor.ecf_participant_eligibility.update!(status: "manual_check", reason: "no_qts")
+  end
+
+  def and_i_have_added_a_details_being_checked_mentor
+    @details_being_checked_mentor = create(:participant_profile, :mentor, :ecf_participant_eligibility, :ecf_participant_validation_data, user: create(:user, full_name: "DBC Mentor"), school_cohort: @school_cohort)
+    @details_being_checked_mentor.ecf_participant_eligibility.update!(status: "manual_check", reason: "no_qts")
+  end
+
   # When_steps
+
+  def when_i_visit_manage_training_dashboard
+    visit schools_dashboard_path(@school)
+  end
 
   def when_i_click_on_back
     click_on("Back")
@@ -312,7 +360,7 @@ module ManageTrainingSteps
   end
 
   def when_i_choose_a_mentor
-    choose(@participant_profile_mentor.user.full_name.to_s, allow_label_click: true)
+    choose(@mentor.user.full_name.to_s, allow_label_click: true)
   end
 
   def when_i_add_ect_or_mentor_name
@@ -324,7 +372,7 @@ module ManageTrainingSteps
   end
 
   def when_i_add_ect_or_mentor_email_that_already_exists
-    fill_in "Email", with: @participant_profile_ect.user.email
+    fill_in "Email", with: @ect.user.email
   end
 
   def when_i_choose_assign_mentor_later
@@ -347,6 +395,57 @@ module ManageTrainingSteps
     visit schools_dashboard_path(@school)
   end
 
+  def when_i_click_on_details_within_eligible
+    within(".eligible") do
+      click_on("Details")
+    end
+  end
+
+  def when_i_click_on_check_within_eligible
+    within(".eligible") do
+      click_on("Check")
+    end
+  end
+
+  def when_i_click_on_check_within_ineligible
+    within(".ineligible") do
+      click_on("Check")
+    end
+  end
+
+  def when_i_click_on_check_within_checked
+    within(".checked") do
+      click_on("Check")
+    end
+  end
+
+  def when_i_click_on_details_within_checked
+    within(".checked") do
+      click_on("Details")
+    end
+  end
+
+  def when_i_click_on_details_within_details
+    within(".details") do
+      click_on("Details")
+    end
+  end
+
+  def when_i_click_on_check
+    click_on("Check")
+  end
+
+  def when_i_click_on_details
+    click_on("Details")
+  end
+
+  def when_i_navigate_to_participants_dashboard
+    when_i_click_on_add_your_early_career_teacher_and_mentor_details
+    then_i_am_taken_to_roles_page
+    when_i_click_on_continue
+    then_i_am_taken_to_your_ect_and_mentors_page
+  end
+
   # Then_steps
 
   def then_i_am_taken_to_roles_page
@@ -357,19 +456,6 @@ module ManageTrainingSteps
   def then_i_am_taken_to_add_mentor_page
     expect(page).to have_selector("h1", text: "Who will mentor")
     expect(page).to have_text("You can tell us later if you’re not sure")
-  end
-
-  def then_i_am_taken_to_fip_induction_dashboard
-    expect(page).to have_selector("h1", text: "Manage your training")
-    expect(page).to have_text("Training provider")
-    expect(page).to have_text(@school_cohort.lead_provider.name)
-    expect(page).to have_text("Delivery partner")
-    expect(page).to have_text(@school_cohort.delivery_partner.name)
-  end
-
-  def then_i_am_taken_to_cip_induction_dashboard
-    expect(page).to have_selector("h1", text: "Manage your training")
-    expect(page).not_to have_text("Programme materials")
   end
 
   def then_i_am_taken_to_your_ect_and_mentors_page
@@ -474,7 +560,7 @@ module ManageTrainingSteps
 
   def then_i_can_view_mentor_name
     expect(page).to have_selector("h1", text: "Check your answers")
-    expect(page).to have_text(@participant_profile_mentor.user.full_name.to_s)
+    expect(page).to have_text(@mentor.user.full_name.to_s)
   end
 
   def then_i_can_view_updated_name
@@ -491,11 +577,6 @@ module ManageTrainingSteps
     expect(page).to have_selector("h1", text: "Manage your training")
     expect(page).to have_text("Materials")
     expect(page).to have_text(@cip.name)
-  end
-
-  def then_i_can_view_the_fip_induction_dashboard_without_partnership_details
-    expect(page).to have_selector("h1", text: "Manage your training")
-    expect(page).not_to have_text("Delivery partner")
   end
 
   def then_i_receive_a_missing_name_error_message
@@ -515,7 +596,99 @@ module ManageTrainingSteps
     click_on "Change induction programme choice"
   end
 
-  def then_i_should_see_the_fip_induction_dashboard
+  def then_i_am_taken_to_view_details_page
+    expect(page).to have_text("Participant details")
+  end
+
+  def then_i_can_view_ineligible_participant_status
+    expect(page).to have_text("This person is not eligible for this programme.")
+  end
+
+  def then_i_can_view_eligible_fip_partnered_ect_status
+    expect(page).to have_text("We’ve confirmed this person is eligible for this programme. Your training provider will contact them directly.")
+  end
+
+  def then_i_can_view_eligible_fip_unpartnered_status
+    expect(page).to have_text("We’ve confirmed this person is eligible for this programme. Once you choose a training provider, they’ll contact this person directly.")
+  end
+
+  def then_i_can_view_contacted_for_info_status
+    expect(page).to have_text("We’ve asked this person to use our service to provide some information. We need this to check their eligibility with the Teaching Regulation Agency.")
+  end
+
+  def then_i_can_view_contacted_for_info_bounced_email_status
+    expect(page).to have_text("We could not send an email to this address. Please check it’s correct.")
+  end
+
+  def then_i_can_view_details_being_checked_status
+    expect(page).to have_text("Our checks show this person does not have qualified teacher status (QTS). Their status should be up to date if they completed their ITT in 2021.")
+  end
+
+  def then_i_can_view_details_being_checked_mentor_status
+    expect(page).to have_text("We’re checking this person’s details with the Teaching Regulation Agency. We’ll confirm if they’re eligible for this programme soon.")
+  end
+
+  def then_i_can_view_eligible_cip_status
+    expect(page).to have_text("We’ve confirmed this person is eligible for this programme. They have access to their materials.")
+  end
+
+  def then_i_am_taken_to_cip_induction_dashboard
+    expect(page).to have_selector("h1", text: "Manage your training")
+    expect(page).not_to have_text("Programme materials")
+  end
+
+  def then_i_can_view_ineligible_participants
+    expect(page).to have_text("Not eligible for funded training")
+    expect(page).to have_text("We’ve checked these people's details and found they're not eligible for this programme.")
+  end
+
+  def then_the_action_required_is_none
+    expect(page).to have_text("None")
+  end
+
+  def then_the_action_required_is_assign_mentor
+    expect(page).to have_text("Assign mentor")
+  end
+
+  def then_the_action_required_is_check_email_address
+    expect(page).to have_text("Check email address")
+  end
+
+  def then_the_action_required_is_remind_them
+    expect(page).to have_text("Remind them")
+  end
+
+  def then_i_can_view_fip_unpartnered_eligible_participants
+    expect(page).to have_text("Eligible to start")
+    expect(page).to have_text("We’ve confirmed these people are eligible for this programme. Once you choose a training provider, they'll contact your ECTs and mentors directly.")
+  end
+
+  def then_i_can_view_details_being_checked_participants
+    expect(page).to have_text("DfE checking eligibility")
+    expect(page).to have_text("We’re checking these people's details with the Teaching Regulation Agency. We'll confirm if they're eligible for this programme soon.")
+  end
+
+  def then_i_can_view_eligible_participants
+    expect(page).to have_text("Eligible to start")
+    expect(page).to have_text("We’ve confirmed these people are eligible for this programme. Your training provider will contact them directly.")
+  end
+
+  def then_i_can_view_cip_eligible_participants
+    expect(page).to have_text("Eligible to start")
+    expect(page).to have_text("We’ve confirmed these people are eligible for this programme. They have access to their materials.")
+  end
+
+  def then_i_can_view_contacted_for_info_participants
+    expect(page).to have_text("Contacted for information")
+    expect(page).to have_text("We need this to check their eligibility with the Teaching Regulation Agency.")
+  end
+
+  def then_i_can_view_the_fip_induction_dashboard_without_partnership_details
+    expect(page).to have_selector("h1", text: "Manage your training")
+    expect(page).not_to have_text("Delivery partner")
+  end
+
+  def then_i_am_taken_to_fip_induction_dashboard
     expect(page).to have_selector("h1", text: "Manage your training")
     expect(page).to have_text("Training provider")
     expect(page).to have_text(@school_cohort.lead_provider.name)
