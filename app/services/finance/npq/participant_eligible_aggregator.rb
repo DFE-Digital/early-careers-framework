9105c8cf3535ef1953046283085a2c33440dcbfa
Paul McKibbin <pmckibbin@gmail.com>
2021-10-27 08:15:38 +0100

    Update the aggregators to support not_paid, not_payable, etc.

--- app/services/finance/npq/participant_eligible_aggregator.rb
+++ app/services/finance/npq/participant_eligible_aggregator.rb
@@ -11,7 +11,7 @@ module Finance
         def aggregation_types
           {
             started: {
-              not_paid: :submitted_for_lead_provider,
+              not_eligible: :not_eligible_for_lead_provider,
               all: :eligible_for_lead_provider,
             },
           }
