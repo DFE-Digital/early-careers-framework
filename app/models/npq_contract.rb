53950ff8af96adc916121acf399f7d59a3cce7c3
Paul McKibbin <pmckibbin@gmail.com>
2021-10-27 16:23:43 +0100

    Remove unnecessary scope and set limits around the numeric values allowed

--- app/models/npq_contract.rb
+++ app/models/npq_contract.rb
@@ -2,5 +2,7 @@
 
 class NPQContract < ApplicationRecord
   belongs_to :npq_lead_provider
-  validates_numericality_of :number_of_payment_periods, :output_payment_percentage, :per_participant, :recruitment_target, :service_fee_installments, :service_fee_percentage
+  validates_numericality_of :number_of_payment_periods, :output_payment_percentage, :service_fee_installments, :service_fee_percentage, only_integer: true, greater_than_or_equal_to: 0
+  validates_numericality_of :per_participant, greater_than: 0
+  validates_numericality_of :recruitment_target, only_integer: true, greater_than: 0
 end
