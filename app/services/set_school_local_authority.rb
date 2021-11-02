0934140f9c13d2d1adc5cbe8ee45072c71d83858
Tony Headford <tony@binarycircus.com>
2021-11-02 11:45:54 +0000

    Spec for set local authority service

--- app/services/set_school_local_authority.rb
+++ app/services/set_school_local_authority.rb
@@ -8,7 +8,6 @@ class SetSchoolLocalAuthority < BaseService
         SchoolLocalAuthority.create!(school: school,
                                      local_authority: LocalAuthority.find_by(code: la_code),
                                      start_year: start_year)
-        update_pupil_premiums!
       end
     end
   end
@@ -21,17 +20,5 @@ private
     @school = school
     @la_code = la_code
     @start_year = start_year
-  end
-
-  def update_pupil_premiums!
-    # pupil premiums are added via an import process so if this school
-    # is new it may not have had any associated with it yet
-    unless school.partnerships.any?
-      uplift = school.pupil_premium_uplift?(start_year)
-
-      school.school_cohorts.for_year(start_year).first&.ecf_participant_profiles&.each do |profile|
-        profile.update!(pupil_premium_uplift: uplift)
-      end
-    end
   end
 end
