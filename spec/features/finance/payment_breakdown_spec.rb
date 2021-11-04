6d744d3f95d8d3c77461b3e63d74713c447af4ed
Phil Lee <asmega@users.noreply.github.com>
2021-11-04 16:00:36 +0000

    roll payment breakdown tests together

--- spec/features/finance/payment_breakdown_spec.rb
+++ spec/features/finance/payment_breakdown_spec.rb
@@ -28,19 +28,22 @@ RSpec.feature "Finance users payment breakdowns", type: :feature, js: true do
     when_i_click_on_view_contract_link
     then_the_page_should_be_accessible
     and_percy_should_be_sent_a_snapshot_named("Contract breakdown for an ECF provider")
-  end
 
-  scenario "Can get to ECF payment breakdown page for payable declarations" do
-    given_i_am_logged_in_as_a_finance_user
-    and_there_is_ecf_provider_with_contract
-    and_there_is_a_schedule
-    visit "/finance/ecf/payment_breakdowns/cffd2237-c368-4044-8451-68e4a4f73369/payable"
-    and_i_click_open_all_button
+    when_i_click_on("Back")
+    and_i_click_on("Payable")
     then_the_page_should_be_accessible
     and_percy_should_be_sent_a_snapshot_named("Payment breakdown for an ECF provider (payable)")
   end
 
 private
+
+  def when_i_click_on(string)
+    page.click_link(string)
+  end
+
+  def and_i_click_on(string)
+    when_i_click_on(string)
+  end
 
   def and_there_is_a_schedule
     create(:schedule)
