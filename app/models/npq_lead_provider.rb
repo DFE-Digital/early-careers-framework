46164e72d6f7af194331fc1d2ea2128c9843f753
StupidCodeFactory <ymarquet@gmail.com>
2021-11-18 17:44:22 +0100

    [CPDTP-378] - fix some specs

--- app/models/npq_lead_provider.rb
+++ app/models/npq_lead_provider.rb
@@ -6,6 +6,5 @@ class NPQLeadProvider < ApplicationRecord
   has_many :npq_applications
   has_many :npq_participant_profiles, through: :npq_applications, source: :profile
   has_many :npq_participants, through: :npq_participant_profiles, source: :user
-
   has_many :npq_contracts
 end
