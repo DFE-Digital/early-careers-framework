2d326a37c2c8f63e665a85f5971981c45e1e3706
StupidCodeFactory <ymarquet@gmail.com>
2021-11-19 15:21:35 +0100

    [CPDTP-378] - remove debug from github workflow

--- app/models/participant_declaration/npq.rb
+++ app/models/participant_declaration/npq.rb
@@ -5,6 +5,5 @@ class ParticipantDeclaration::NPQ < ParticipantDeclaration
 
   scope :for_course, ->(course_identifier) { where(course_identifier: course_identifier) }
   scope :for_lead_provider_and_course, ->(cpd_lead_provider, course_identifier) { for_lead_provider(cpd_lead_provider).for_course(course_identifier) }
-  scope :submitted_for_lead_provider_and_course, ->(cpd_lead_provider, course_identifier) { for_lead_provider(cpd_lead_provider).for_course(course_identifier).unique_id.submitted }
   scope :eligible_or_payable, -> { eligible.or(payable) }
 end
