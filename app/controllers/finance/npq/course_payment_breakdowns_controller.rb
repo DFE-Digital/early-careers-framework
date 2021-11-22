2bd1ccc93ba8efad7ed27acab0934726d0421124
StupidCodeFactory <ymarquet@gmail.com>
2021-11-22 18:46:22 +0100

    [CPDTP-378] - fix cutoff date

--- app/controllers/finance/npq/course_payment_breakdowns_controller.rb
+++ app/controllers/finance/npq/course_payment_breakdowns_controller.rb
@@ -4,16 +4,15 @@ module Finance
   module NPQ
     class CoursePaymentBreakdownsController < BaseController
       def show
+        @invoice           = Finance::Invoice.find_by_name("current")
         @npq_lead_provider = lead_provider_scope.find(params[:lead_provider_id])
         @npq_course        = NPQCourse.find_by!(identifier: params[:id])
         @breakdown         = Finance::NPQ::CalculationOrchestrator.call(
           aggregator: CurrentMilestoneParticipantDeclarationAggregator,
           calculator: PaymentCalculator::NPQ::PaymentCalculation,
-          interval: Finance::Invoice.find_by_name("current").interval,
+          interval: @invoice.interval,
           npq_contract: @npq_lead_provider.npq_contracts.find_by!(course_identifier: params[:id]),
         )
-
-        @cutoff_date = "On #{helpers.cutoff_date}"
       end
 
     private
--- app/helpers/finance_helper.rb
+++ app/helpers/finance_helper.rb
@@ -1,20 +1,6 @@
 # frozen_string_literal: true
 
 module FinanceHelper
-  MILESTONE_DATES = [
-    "30.09.2021",
-    "31.10.2021",
-    "31.01.2022",
-    "30.04.2022",
-    "30.09.2022",
-    "31.01.2023",
-    "30.04.2023",
-    "31.10.2023",
-    "31.01.2024",
-    "30.04.2024",
-    "30.09.2024",
-  ].freeze
-
   def number_to_pounds(number)
     number_to_currency number, precision: 2, unit: "£"
   end
@@ -46,14 +32,5 @@ module FinanceHelper
 
   def total_vat(breakdown, lead_provider)
     total_payment(breakdown) * (lead_provider.vat_chargeable ? 0.2 : 0.0)
-  end
-
-  def payment_period
-    index = MILESTONE_DATES.each_index.detect { |date| Time.zone.today.before?(Date.parse(MILESTONE_DATES[date])) }
-    index < 1 ? ["1.09.2021", MILESTONE_DATES[0]] : MILESTONE_DATES.slice(index - 1, 2)
-  end
-
-  def cutoff_date
-    Date.parse(payment_period.length < 2 ? MILESTONE_DATES[0] : payment_period[1]).to_s(:govuk)
   end
 end
