dec6405e4a1464aa055a6409a92f3cf18519c748
Yann Marquet <yann.marquet@digital.education.gov.uk>
2021-11-22 12:26:02 +0100

    [CPDTP-378] - fix cypress test seeds

--- spec/cypress/app_commands/scenarios/admin/school_participants.rb
+++ spec/cypress/app_commands/scenarios/admin/school_participants.rb
@@ -4,27 +4,23 @@ school = FactoryBot.create :school, name: "Test School", slug: "test-school", ur
 cohort = FactoryBot.create :cohort, start_year: 2021
 school_cohort = FactoryBot.create(:school_cohort, :cip, school: school, cohort: cohort)
 
-mentor_1 = FactoryBot.create :participant_profile,
-                             :mentor,
+mentor_1 = FactoryBot.create :mentor_participant_profile,
                              user: FactoryBot.create(:user, full_name: "Mentor User 1"),
                              school_cohort: school_cohort,
                              created_at: Date.parse("20/03/2020")
 
-FactoryBot.create :participant_profile,
-                  :mentor,
+FactoryBot.create :mentor_participant_profile,
                   user: FactoryBot.create(:user, full_name: "Mentor User 2"),
                   school_cohort: school_cohort,
                   created_at: Date.parse("06/05/2020")
 
-FactoryBot.create :participant_profile,
-                  :ect,
+FactoryBot.create :ect_participant_profile,
                   user: FactoryBot.create(:user, full_name: "ECT User 1", email: "young_prosacco@crist.net"),
                   mentor_profile: mentor_1,
                   school_cohort: school_cohort,
                   created_at: Date.parse("01/07/2020")
 
-FactoryBot.create :participant_profile,
-                  :ect,
+FactoryBot.create :ect_participant_profile,
                   user: FactoryBot.create(:user, full_name: "ECT User 2"),
                   mentor_profile: mentor_1,
                   school_cohort: school_cohort,
@@ -33,14 +29,12 @@ FactoryBot.create :participant_profile,
 another_school = FactoryBot.create(:school, name: "Some other school", urn: "222222")
 another_school_cohort = FactoryBot.create(:school_cohort, :cip, school: another_school, cohort: cohort)
 
-FactoryBot.create :participant_profile,
-                  :mentor,
+FactoryBot.create :mentor_participant_profile,
                   user: FactoryBot.create(:user, full_name: "Unrelated mentor"),
                   school_cohort: another_school_cohort,
                   created_at: Date.parse("10/11/2020")
 
-FactoryBot.create :participant_profile,
-                  :ect,
+FactoryBot.create :ect_participant_profile,
                   user: FactoryBot.create(:user, full_name: "Unrelated ect"),
                   school_cohort: another_school_cohort,
                   created_at: Date.parse("29/12/2020")
