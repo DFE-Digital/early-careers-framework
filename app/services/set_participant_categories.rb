fd31485ef5416618619788639f7d1604a3f96422
Steven <steven.woods@digital.education.gov.uk>
2021-11-25 09:06:23 +0000

    Move withdrawn ppts to schools participants index view

--- app/services/set_participant_categories.rb
+++ app/services/set_participant_categories.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class SetParticipantCategories < BaseService
-  ParticipantCategories = Struct.new(:eligible, :ineligible, :contacted_for_info, :details_being_checked)
+  ParticipantCategories = Struct.new(:eligible, :ineligible, :withdrawn, :contacted_for_info, :details_being_checked)
 
   def call
     set_participant_categories
@@ -27,11 +27,11 @@ private
   end
 
   def fip_participant_categories_feature_flag_active
-    ParticipantCategories.new(eligible_participants, fip_flag_active_ineligible_participants, contacted_for_info_participants, details_being_checked_participants)
+    ParticipantCategories.new(eligible_participants, fip_flag_active_ineligible_participants, fip_flag_active_withdrawn_participants, contacted_for_info_participants, details_being_checked_participants)
   end
 
   def fip_participant_categories_feature_flag_inactive
-    ParticipantCategories.new([], [], contacted_for_info_participants, fip_flag_inactive_details_being_checked_participants)
+    ParticipantCategories.new([], [], withdrawn_participants, contacted_for_info_participants, fip_flag_inactive_details_being_checked_participants)
   end
 
   def cip_participant_categories
@@ -50,8 +50,13 @@ private
     active_participant_profiles.eligible_status.includes(:user).order("users.full_name")
   end
 
+  def withdrawn_participants
+    active_participant_profiles.training_status_withdrawn.includes(:user).order("users.full_name")
+  end
+
   def contacted_for_info_participants
-    active_participant_profiles.contacted_for_info.includes(:user).order("users.full_name")
+    contacted_for_info = active_participant_profiles.contacted_for_info.includes(:user).order("users.full_name")
+    contacted_for_info - withdrawn_participants
   end
 
   def details_being_checked_participants
@@ -62,11 +67,15 @@ private
     ineligible_participants - eligible_participants
   end
 
+  def fip_flag_active_withdrawn_participants
+    withdrawn_participants - fip_flag_active_ineligible_participants
+  end
+
   def cip_eligible_participants
-    [*eligible_participants, *ineligible_participants, *details_being_checked_participants].uniq
+    [*eligible_participants, *ineligible_participants, *withdrawn_participants, *details_being_checked_participants].uniq
   end
 
   def fip_flag_inactive_details_being_checked_participants
-    [*details_being_checked_participants, *ineligible_participants, *eligible_participants].uniq
+    [*details_being_checked_participants, *ineligible_participants, *eligible_participants, *withdrawn_participants].uniq
   end
 end
