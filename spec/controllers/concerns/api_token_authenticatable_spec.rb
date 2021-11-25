9cf9e87418fa1eb87c0f9f4c6df1a1a812383dc6
Phil Lee <asmega@users.noreply.github.com>
2021-11-25 14:57:29 +0000

    refactor tests for ApiTokenAuthenticatable

---
+++ spec/controllers/concerns/api_token_authenticatable_spec.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe ApiTokenAuthenticatable, type: :controller do
+  controller(ApplicationController) do
+    include ApiTokenAuthenticatable
+
+    def index
+      head :ok
+    end
+  end
+
+  describe "#authenticate" do
+    context "when authorization header not present" do
+      let(:bearer_token) { "Bearer invalid" }
+
+      it "returns 401" do
+        get :index
+        expect(response.status).to eq(401)
+      end
+    end
+
+    context "when authorization header invalid" do
+      before do
+        request.headers["Authorization"] = bearer_token
+      end
+
+      let(:bearer_token) { "Bearer invalid" }
+
+      it "returns 401" do
+        get :index
+        expect(response.status).to eq(401)
+      end
+    end
+
+    context "when authorization header is provided" do
+      let(:lead_provider) { create(:lead_provider) }
+      let(:token) { LeadProviderApiToken.create_with_random_token!(lead_provider: lead_provider) }
+      let(:bearer_token) { "Bearer #{token}" }
+
+      before do
+        request.headers["Authorization"] = bearer_token
+      end
+
+      it "passes through" do
+        get :index
+        expect(response.status).to eq(200)
+      end
+    end
+  end
+end
