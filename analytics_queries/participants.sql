3a770ac582753ce1611bc43751882d768f6df9de
Nicholas Wieland <ngw@nofeed.org>
2021-11-26 13:20:21 +0000

    Add status to participants query and metor_id to schools query

--- analytics_queries/participants.sql
+++ analytics_queries/participants.sql
@@ -10,6 +10,7 @@ SELECT u.id                                                    as participant_id
                  JOIN users u2 on tp2.user_id = u2.id
         WHERE pp2.id = pp.mentor_profile_id)                   as mentor_id,
        c.start_year                                            as cohort,
+       pp.status                                               as status,
        pp.training_status                                      as training_status,
        s.name                                                  as schedule,
        tp.trn                                                  as trn,
