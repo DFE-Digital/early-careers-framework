4366becf4483dbf03d5443389dd2fa1e25bfee12
Phil Lee <asmega@users.noreply.github.com>
2021-12-03 13:39:54 +0000

    use bang method in test

--- spec/factories/finance/schedules.rb
+++ spec/factories/finance/schedules.rb
@@ -21,7 +21,7 @@ FactoryBot.define do
       end
     end
 
-    cohort { Cohort.find_or_create_by(start_year: 2021) }
+    cohort { Cohort.find_or_create_by!(start_year: 2021) }
 
     factory :ecf_schedule, class: "Finance::Schedule::ECF", parent: :schedule do
       name { "ECF September standard 2021" }
