715bf4a23d0e157b6ba7e27e053fb69355e4ee0e
Tony Headford <tony@binarycircus.com>
2021-12-08 15:11:28 +0000

    Add profiles to identity when adding identity

--- app/services/identity/create.rb
+++ app/services/identity/create.rb
@@ -3,7 +3,24 @@
 module Identity
   class Create < BaseService
     def call
-      ParticipantIdentity.find_or_create_by!(email: user.email) do |identity|
+      ActiveRecord::Base.transaction do
+        find_or_create_identity
+        add_participant_profiles_to_identity
+      end
+      identity
+    end
+
+  private
+
+    attr_accessor :user, :origin, :identity
+
+    def initialize(user:, origin: :ecf)
+      @user = user
+      @origin = origin
+    end
+
+    def find_or_create_identity
+      @identity = ParticipantIdentity.find_or_create_by!(email: user.email) do |identity|
         identity.user = user
         identity.external_identifier = user.id
         identity.email = user.email
@@ -11,13 +28,12 @@ module Identity
       end
     end
 
-  private
+    def add_participant_profiles_to_identity
+      return unless user.teacher_profile&.participant_profiles&.any?
 
-    attr_accessor :user, :origin
-
-    def initialize(user:, origin: :ecf)
-      @user = user
-      @origin = origin
+      user.teacher_profile.participant_profiles.where(participant_identity: nil).each do |profile|
+        profile.update!(participant_identity: identity)
+      end
     end
   end
 end
--- spec/services/identity/create_spec.rb
+++ spec/services/identity/create_spec.rb
@@ -33,6 +33,33 @@ RSpec.describe Identity::Create do
       end
     end
 
+    context "when the user has existing participant_profiles" do
+      let(:teacher_profile) { create(:teacher_profile, user: user) }
+      let!(:mentor_profile) { create(:mentor_participant_profile, teacher_profile: teacher_profile) }
+      let!(:npq_profile) { create(:npq_participant_profile, teacher_profile: teacher_profile) }
+
+      it "adds the profiles to the new identity" do
+        identity = service.call(user: user)
+        expect(identity.participant_profiles).to match_array [mentor_profile, npq_profile]
+      end
+
+      context "when the existing profiles belong to another identity" do
+        let(:id1) { create(:participant_identity) }
+
+        before do
+          id1.update!(user: user)
+          mentor_profile.update!(participant_identity: id1)
+          npq_profile.update!(participant_identity: id1)
+        end
+
+        it "does not add the profiles to the new identity" do
+          identity = service.call(user: user)
+          expect(identity.participant_profiles).to be_empty
+          expect(id1.participant_profiles).to match_array [mentor_profile, npq_profile]
+        end
+      end
+    end
+
     context "when a profile for the users email address already exists" do
       let!(:identity) { create(:participant_identity, user: user) }
 
