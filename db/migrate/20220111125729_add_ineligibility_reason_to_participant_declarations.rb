2a7de448f65db26f25e721fa0643b6c7089e1381
StupidCodeFactory <ymarquet@gmail.com>
2022-01-11 18:34:56 +0100

    [CPDLP-757] - fix rubocop offense

--- db/migrate/20220111125729_add_ineligibility_reason_to_participant_declarations.rb
+++ db/migrate/20220111125729_add_ineligibility_reason_to_participant_declarations.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddIneligibilityReasonToParticipantDeclarations < ActiveRecord::Migration[6.1]
   def change
     create_enum :state_reason_type, %w[duplicate]
