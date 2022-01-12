e1121c47f000bc00a4f48126a5303c12b7fb9fa4
StupidCodeFactory <ymarquet@gmail.com>
2022-01-12 13:42:29 +0100

    [CPDLP-757] - re-add cpd_lead_provider when generating a lead_provider

--- spec/factories/lead_provider.rb
+++ spec/factories/lead_provider.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
   factory :lead_provider do
     name  { "Lead Provider" }
     cohorts { Cohort.all }
+    cpd_lead_provider
 
     trait :contract do
       before(:create) do |lead_provider|
