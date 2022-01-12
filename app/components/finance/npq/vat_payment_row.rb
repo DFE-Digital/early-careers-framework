a665607c7b2ecac3e235a8f4e111294349c8fb1f
Erica Porter <ericaporter@users.noreply.github.com>
2022-01-12 11:02:27 +0000

    Create vat_payment row

---
+++ app/components/finance/npq/vat_payment_row.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Finance
+  module NPQ
+    class VatPaymentRow < BaseComponent
+      include FinanceHelper
+
+      def initialize(breakdown, lead_provider)
+        self.breakdown = breakdown
+        self.lead_provider = lead_provider
+      end
+
+    private
+
+      attr_accessor :breakdown, :lead_provider
+    end
+  end
+end
