2ade1feffc6b019e3a5b1a016a3a5c6f2f509f14
Erica Porter <ericaporter@users.noreply.github.com>
2022-01-12 11:05:50 +0000

    Update course rows to show vat and udpate methods for collection

--- app/components/finance/npq/payment_overviews/payment_courses_row.rb
+++ app/components/finance/npq/payment_overviews/payment_courses_row.rb
@@ -9,22 +9,22 @@ module Finance
 
         with_collection_parameter :breakdown
 
-        def total
-          output_payments_subtotal + service_fees_monthly
+        def initialize(statement:, breakdown:, npq_lead_provider:)
+          self.statement                = statement
+          self.course_identifier        = breakdown.dig(:breakdown_summary, :course_identifier)
+          self.monthly_service_fees     = breakdown.dig(:service_fees, :monthly)
+          self.output_payments_subtotal = breakdown.dig(:output_payments, :subtotal)
+          self.npq_lead_provider        = npq_lead_provider
+        end
+
+        def npq_total_payment
+          monthly_service_fees + output_payments_subtotal
         end
 
       private
 
-        attr_accessor :output_payments_subtotal, :service_fees_monthly, :statement
+        attr_accessor :output_payments_subtotal, :monthly_service_fees, :statement
         attr_writer :course_identifier, :npq_lead_provider
-
-        def initialize(statement:, breakdown:, npq_lead_provider:)
-          self.statement                = statement
-          self.course_identifier        = breakdown.dig(:breakdown_summary, :course_identifier)
-          self.service_fees_monthly     = breakdown.dig(:service_fees, :monthly)
-          self.output_payments_subtotal = breakdown.dig(:output_payments, :subtotal)
-          self.npq_lead_provider        = npq_lead_provider
-        end
       end
     end
   end
