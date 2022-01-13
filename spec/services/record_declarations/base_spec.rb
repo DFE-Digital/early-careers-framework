eabfb09ed9a3c805af06433707634418dffe468d
Yann Marquet <yann.marquet@digital.education.gov.uk>
2022-01-13 14:38:36 +0100

    [CPDLP-892] - allow to resubmitting a declaration after one has been voided

--- spec/services/record_declarations/base_spec.rb
+++ spec/services/record_declarations/base_spec.rb
@@ -3,36 +3,69 @@
 require "rails_helper"
 
 RSpec.describe RecordDeclarations::Base do
-  let(:cpd_lead_provider) { build(:cpd_lead_provider) }
-  let(:user) { create(:user) }
-  let(:teacher_profile) { create(:teacher_profile, user: user) }
-  let!(:ecf_profile) { create(:ecf_participant_profile, teacher_profile: teacher_profile) }
+  let(:cpd_lead_provider)       { create(:cpd_lead_provider, :with_lead_provider) }
+  let(:cohort)                  { create(:cohort, start_year: Time.zone.today.year) }
+  let(:school)                  { create(:school) }
+  let(:school_cohort)           { create(:school_cohort, school: school, cohort: cohort) }
+  let(:declaration_date)        { Time.zone.parse("2021-11-02").rfc3339 }
+  let(:declaration_type)        { "started" }
+  let(:user)                    { create(:user) }
+  let(:teacher_profile)         { create(:teacher_profile, user: user) }
+  let!(:ect_participant_profile) { create(:ect_participant_profile, school_cohort: school_cohort, teacher_profile: teacher_profile) }
+  let(:params) do
+    {
+      participant_id: ect_participant_profile.user_id,
+      course_identifier: "ecf-induction",
+      cpd_lead_provider: cpd_lead_provider,
+      declaration_date: declaration_date,
+      declaration_type: declaration_type,
+    }
+  end
 
-  let(:klass) do
-    Class.new(described_class) do
-      def self.valid_declaration_types
-        %w[started completed retained-1 retained-2 retained-3 retained-4]
-      end
+  before do
+    create(:partnership, lead_provider: cpd_lead_provider.lead_provider, cohort: cohort, school: school)
+  end
 
-      def self.valid_courses
-        %w[ecf-induction]
-      end
+  context "when a similar declaration has been voided" do
+    subject(:record_declaration) { RecordDeclarations::Started::EarlyCareerTeacher.call(params: params) }
+    let(:void_declaration) do
+      VoidParticipantDeclaration.new(
+        cpd_lead_provider: cpd_lead_provider,
+        id: JSON.parse(record_declaration).dig("data", "id"),
+      ).call
+    end
 
-      def self.model_name
-        ActiveModel::Name.new(self, nil, "temp")
-      end
+    before { pp void_declaration }
 
-      def user_profile
-        user.participant_profiles[0]
-      end
-
-      def matches_lead_provider?
-        true
-      end
+    it "allows to re-send a new declaration" do
+      pp record_declaration
     end
   end
 
   context "when milestone has null milestone_date" do
+    let(:klass) do
+      Class.new(described_class) do
+        def self.valid_declaration_types
+          %w[started completed retained-1 retained-2 retained-3 retained-4]
+        end
+
+        def self.valid_courses
+          %w[ecf-induction]
+        end
+
+        def self.model_name
+          ActiveModel::Name.new(self, nil, "temp")
+        end
+
+        def user_profile
+          user.participant_profiles[0]
+        end
+
+        def matches_lead_provider?
+          true
+        end
+      end
+    end
     subject do
       klass.new(
         params: {
