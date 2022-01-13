3de0bf302176461c702b1cacf5cd5d902c53be0e
Phil Lee <asmega@users.noreply.github.com>
2022-01-13 17:31:01 +0000

    use let instead in feature test

--- spec/features/finance/schedules_spec.rb
+++ spec/features/finance/schedules_spec.rb
@@ -3,33 +3,35 @@
 require "rails_helper"
 
 RSpec.feature "Finance users payment breakdowns", type: :feature do
-  fscenario "viewing a schedule" do
+  let(:schedule) { create(:ecf_schedule) }
+
+  scenario "viewing a schedule" do
     given_i_am_logged_in_as_a_finance_user
     and_there_is_a_schedule
     when_i_click("Schedules")
     then_i_see("Schedules")
-    and_see_table_with_schedule(@schedule)
+    and_see_table_with_schedule
 
-    when_i_click(@schedule.schedule_identifier)
+    when_i_click(schedule.schedule_identifier)
     then_i_see("Milestones")
-    and_see_table_of_milestones_for_schedule(@schedule)
+    and_see_table_of_milestones_for_schedule
   end
 
   def and_there_is_a_schedule
-    @schedule = create(:ecf_schedule)
+    schedule
   end
 
   def then_i_see(string)
     expect(page).to have_content(string)
   end
 
-  def and_see_table_with_schedule(schedule)
+  def and_see_table_with_schedule
     within("table") do
       expect(page).to have_content(schedule.schedule_identifier)
     end
   end
 
-  def and_see_table_of_milestones_for_schedule(schedule)
+  def and_see_table_of_milestones_for_schedule
     within("table") do
       schedule.milestones.each do |milestone|
         expect(page).to have_content(milestone.name)
