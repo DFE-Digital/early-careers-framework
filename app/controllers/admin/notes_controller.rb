e3694d2737f2a7aaf9163b2a53ab51427c0d2983
Steven <steven.woods@digital.education.gov.uk>
2022-01-19 11:21:11 +0000

    Add notes controller in admin
    Update notes controller
    
    refactor notes controller
    
    Change require params in controller

---
+++ app/controllers/admin/notes_controller.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module Admin
+  class NotesController < Admin::BaseController
+    skip_after_action :verify_authorized
+    skip_after_action :verify_policy_scoped
+
+    before_action :load_participant
+
+    def edit; end
+
+    def update
+      @participant_profile.assign_attributes(note_params)
+
+      if @participant_profile.save
+        redirect_to admin_participant_path
+      else
+        render action: "edit"
+      end
+    end
+
+  private
+
+    def load_participant
+      @participant_profile = ParticipantProfile.find(params[:id])
+      authorize @participant_profile, policy_class: @participant_profile.policy_class
+    end
+
+    def note_params
+      params.require(:participant_profile).permit(:notes)
+    end
+  end
+end
