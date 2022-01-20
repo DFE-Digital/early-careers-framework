81c08d537c38a459c549d8e7bde4ab951247edbb
Yann Marquet <yann.marquet@digital.education.gov.uk>
2022-01-20 10:58:40 +0100

    [CPDLP-825] - Change current NPQ payment breakdown
    Changed it to the current milestone milestone_date
    ```ruby
    f = Finance::Schedule.find_by(name: "NPQ Specialist November 2021")
    f.milestones.order(milestone_date: :asc).where("milestone_date > ?", Date.today
    ).first.milestone_date # => Sat, 25 Jun 2022
    ```

--- app/models/finance/statement/npq.rb
+++ app/models/finance/statement/npq.rb
@@ -8,7 +8,7 @@ class Finance::Statement::NPQ < Finance::Statement::Base
       payment_date: Date.new(2022, 1, 31),
     },
     {
-      interval: Time.zone.parse("2021-12-26T00:00:00+00:00")..Time.zone.parse("2022-06-01T23:59:59+00:00"),
+      interval: Time.zone.parse("2021-12-26T00:00:00+00:00")..Time.zone.parse("2022-06-25T23:59:59+00:00"),
       name: "current",
       payment_date: Date.new(2022, 3, 31),
     },
