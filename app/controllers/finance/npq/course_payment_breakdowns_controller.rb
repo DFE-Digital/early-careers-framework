665ad37c858dcfa8ec537cc1702f69600a5620df
Phil Lee <asmega@users.noreply.github.com>
2022-01-27 12:05:18 +0000

    NPQ course PB is statement aware

--- app/controllers/finance/npq/course_payment_breakdowns_controller.rb
+++ app/controllers/finance/npq/course_payment_breakdowns_controller.rb
@@ -4,16 +4,32 @@ module Finance
   module NPQ
     class CoursePaymentBreakdownsController < BaseController
       def show
-        @statement         = Finance::Statement::NPQ.find_by_name(params[:statement_id])
         @npq_lead_provider = lead_provider_scope.find(params[:lead_provider_id])
+        @cpd_lead_provider = @npq_lead_provider.cpd_lead_provider
+
+        @statement = Finance::Statement::NPQ.find_by(
+          name: params[:statement_id],
+          cpd_lead_provider: @cpd_lead_provider,
+        )
+
         @npq_course        = NPQCourse.find_by!(identifier: params[:id])
         @breakdown         = Finance::NPQ::CalculationOrchestrator.call(
-          cpd_lead_provider: @npq_lead_provider.cpd_lead_provider,
+          cpd_lead_provider: @cpd_lead_provider,
           contract: @npq_lead_provider.npq_contracts.find_by!(course_identifier: params[:id]),
+          aggregator: aggregator_with_statement(statement: @statement),
         )
       end
 
     private
+
+      def aggregator_with_statement(statement:)
+        Class.new(Finance::NPQ::ParticipantEligibleAndPayableAggregator) do
+          define_singleton_method(:call) do |cpd_lead_provider: nil, interval: nil, recorder: ParticipantDeclaration::NPQ.where(statement: statement), event_type: :started, course_identifier: nil|
+            new(cpd_lead_provider: cpd_lead_provider, recorder: recorder, course_identifier: course_identifier)
+              .call(event_type: event_type, interval: interval)
+          end
+        end
+      end
 
       def lead_provider_scope
         policy_scope(NPQLeadProvider, policy_scope_class: FinanceProfilePolicy::Scope)
