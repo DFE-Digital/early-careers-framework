f4d4aef0cb7ba4d4e673a5c379144a042536af7f
Phil Lee <asmega@users.noreply.github.com>
2022-01-27 11:32:14 +0000

    NPQ overview statements are statement aware

--- app/controllers/finance/npq/statements_controller.rb
+++ app/controllers/finance/npq/statements_controller.rb
@@ -6,26 +6,34 @@ module Finance
   module NPQ
     class StatementsController < BaseController
       def show
-        @statement         = temporary_statements.find { |statement| statement.name == params[:id] }
         @npq_lead_provider = lead_provider_scope.find(params[:lead_provider_id])
-        @breakdowns        = Finance::NPQ::CalculationOverviewOrchestrator.call(
-          cpd_lead_provider: @npq_lead_provider.cpd_lead_provider,
+        @cpd_lead_provider = @npq_lead_provider.cpd_lead_provider
+
+        @statement = Finance::Statement::NPQ.find_by(
+          name: params[:id],
+          cpd_lead_provider: @cpd_lead_provider,
+        )
+
+        @breakdowns = Finance::NPQ::CalculationOverviewOrchestrator.call(
+          cpd_lead_provider: @cpd_lead_provider,
           event_type: :started,
+          aggregator: aggregator_with_statement(statement: @statement),
         )
       end
 
     private
 
-      def lead_provider_scope
-        policy_scope(NPQLeadProvider, policy_scope_class: FinanceProfilePolicy::Scope)
+      def aggregator_with_statement(statement:)
+        Class.new(Finance::NPQ::ParticipantEligibleAndPayableAggregator) do
+          define_singleton_method(:call) do |cpd_lead_provider: nil, interval: nil, recorder: ParticipantDeclaration::NPQ.where(statement: statement), event_type: :started, course_identifier: nil|
+            new(cpd_lead_provider: cpd_lead_provider, recorder: recorder, course_identifier: course_identifier)
+              .call(event_type: event_type, interval: interval)
+          end
+        end
       end
 
-      helper_method :temporary_statements
-      def temporary_statements
-        [
-          { name: "December 2021", deadline_date: Date.new(2021, 12, 25), payment_date: Date.new(2022, 1, 31) },
-          { name: "January 2022", deadline_date: Date.new(2022, 1, 31), payment_date: Date.new(2022, 2, 28) },
-        ].map { |hash| Finance::Statement::NPQ.new(hash) }
+      def lead_provider_scope
+        policy_scope(NPQLeadProvider, policy_scope_class: FinanceProfilePolicy::Scope)
       end
     end
   end
--- app/services/finance/npq/calculation_overview_orchestrator.rb
+++ app/services/finance/npq/calculation_overview_orchestrator.rb
@@ -7,12 +7,13 @@ module Finance
     class CalculationOverviewOrchestrator
       class << self
         def call(
+          aggregator:,
           cpd_lead_provider:,
           calculation_orchestrator: ::Finance::NPQ::CalculationOrchestrator,
           event_type: :started,
           interval: nil
         )
-          new(cpd_lead_provider: cpd_lead_provider, calculation_orchestrator: calculation_orchestrator, interval: interval)
+          new(cpd_lead_provider: cpd_lead_provider, calculation_orchestrator: calculation_orchestrator, interval: interval, aggregator: aggregator)
             .call(event_type: event_type)
         end
       end
@@ -24,18 +25,20 @@ module Finance
             cpd_lead_provider: cpd_lead_provider,
             contract: contract,
             interval: interval,
+            aggregator: aggregator,
           )
         end
       end
 
     private
 
-      attr_accessor :cpd_lead_provider, :calculation_orchestrator, :interval
+      attr_accessor :cpd_lead_provider, :calculation_orchestrator, :interval, :aggregator
 
-      def initialize(cpd_lead_provider:, calculation_orchestrator:, interval:)
+      def initialize(cpd_lead_provider:, calculation_orchestrator:, interval:, aggregator:)
         self.cpd_lead_provider        = cpd_lead_provider
         self.calculation_orchestrator = calculation_orchestrator
         self.interval                 = interval
+        self.aggregator               = aggregator
       end
     end
   end
