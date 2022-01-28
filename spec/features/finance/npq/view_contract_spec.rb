63c5d1294812913b3d2c2e90d874c66919129827
Jack Bracewell <jack@reasonable.engineering>
2022-01-28 10:50:28 +0000

    Fix remaining feature specs

--- spec/features/finance/npq/view_contract_spec.rb
+++ spec/features/finance/npq/view_contract_spec.rb
@@ -20,6 +20,13 @@ RSpec.feature "NPQ view contract" do
     @npq_lt = create(:npq_contract, :npq_leading_teaching)
     @npq_lead_provider = @npq_lt.npq_lead_provider
     @npq_lbc = create(:npq_contract, :npq_leading_behaviour_culture, npq_lead_provider: @npq_lead_provider)
+
+    Finance::Statement::NPQ.create!(
+      name: "January 2022",
+      deadline_date: Date.new(2022, 1, 31),
+      payment_date: Date.new(2022, 2, 16),
+      cpd_lead_provider: @npq_lead_provider.cpd_lead_provider,
+    )
   end
 
   def when_i_visit_the_payment_breakdown_page
