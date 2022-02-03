d8bab6258c87db9e8e03719c1aa43fcdb7b6ad7c
Yann Marquet <yann.marquet@digital.education.gov.uk>
2022-02-03 11:57:36 +0100

    [CPDLP-847] - increase db connection pool size and wrap seed in transaction

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,22 @@
 # frozen_string_literal: true
 
 seed_path = %w[db seeds]
+ActiveRecord::Base.transaction do
+  load Rails.root.join(*seed_path, "initial_seed.rb").to_s
+  load Rails.root.join(*seed_path, "schedules.rb").to_s
 
-load Rails.root.join(*seed_path, "initial_seed.rb").to_s
-load Rails.root.join(*seed_path, "schedules.rb").to_s
-
-if %w[development deployed_development test sandbox].include?(Rails.env)
-  %w[test_data dummy_structures].each do |seed|
-    load Rails.root.join(*seed_path, "#{seed}.rb").to_s
+  if %w[development deployed_development test sandbox].include?(Rails.env)
+    %w[test_data dummy_structures].each do |seed|
+      load Rails.root.join(*seed_path, "#{seed}.rb").to_s
+    end
   end
-end
 
-if Rails.env.development? || Rails.env.deployed_development?
-  FeatureFlag::PERMANENT_SETTINGS.each do |feature|
-    FeatureFlag.activate(feature)
-  end
-  FeatureFlag::TEMPORARY_FEATURE_FLAGS.each do |feature|
-    FeatureFlag.activate(feature)
+  if Rails.env.development? || Rails.env.deployed_development?
+    FeatureFlag::PERMANENT_SETTINGS.each do |feature|
+      FeatureFlag.activate(feature)
+    end
+    FeatureFlag::TEMPORARY_FEATURE_FLAGS.each do |feature|
+      FeatureFlag.activate(feature)
+    end
   end
 end
