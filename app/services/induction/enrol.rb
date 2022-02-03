23a6ebf65aaa0ef910d08c6b158352eae2f6532d
Tony Headford <tony@binarycircus.com>
2022-02-03 14:09:20 +0000

    Fix linting again and make attrs private

--- app/services/induction/enrol.rb
+++ app/services/induction/enrol.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class Induction::Enrol < BaseService
-  attr_reader :participant_profile, :induction_programme, :start_date
-
   def call
     # TODO: The participant_profile could already have an induction record already in play
     # that we could update with an end_date / status
@@ -13,7 +11,9 @@ class Induction::Enrol < BaseService
 
 private
 
-def initialize(participant_profile:, induction_programme: nil, start_date: Time.zone.now)
+  attr_reader :participant_profile, :induction_programme, :start_date
+
+  def initialize(participant_profile:, induction_programme: nil, start_date: Time.zone.now)
     @participant_profile = participant_profile
     @induction_programme = induction_programme || default_induction_programme
     @start_date = start_date
