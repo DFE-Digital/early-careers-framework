22cc071fce27fe98aa7c3aac39e851c5f06ac2f7
Tony Headford <tony@binarycircus.com>
2022-02-08 10:08:47 +0000

    update data in migration

--- db/migrate/20220207133656_change_default_start_term_on_participant_profile.rb
+++ db/migrate/20220207133656_change_default_start_term_on_participant_profile.rb
@@ -1,7 +1,19 @@
 # frozen_string_literal: true
 
 class ChangeDefaultStartTermOnParticipantProfile < ActiveRecord::Migration[6.1]
-  def change
-    change_column_default :participant_profiles, :start_term, from: "Autumn 2021", to: "autumn_2021"
+  def up
+    change_column_default :participant_profiles, :start_term, "autumn_2021"
+
+    ParticipantProfile.where(start_term: "Autumn 2021").update_all(start_term: "autumn_2021")
+    ParticipantProfile.where(start_term: "Spring 2022").update_all(start_term: "spring_2022")
+    ParticipantProfile.where(start_term: "Summer 2022").update_all(start_term: "summer_2022")
+  end
+
+  def down
+    change_column_default :participant_profiles, :start_term, "Autumn 2021"
+
+    ParticipantProfile.where(start_term: "autumn_2021").update_all(start_term: "Autumn 2021")
+    ParticipantProfile.where(start_term: "spring_2022").update_all(start_term: "Spring 2022")
+    ParticipantProfile.where(start_term: "summer_2022").update_all(start_term: "Summer 2022")
   end
 end
