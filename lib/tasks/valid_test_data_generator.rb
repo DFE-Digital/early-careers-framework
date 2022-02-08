c9abb6085040df39f1a06aa2f20db20f27b6e9cf
Yann Marquet <yann.marquet@digital.education.gov.uk>
2022-02-08 14:00:37 +0100

    reduce amount of participants in seeds

--- lib/tasks/valid_test_data_generator.rb
+++ lib/tasks/valid_test_data_generator.rb
@@ -6,24 +6,22 @@ require "tasks/trn_generator"
 module ValidTestDataGenerator
   class LeadProviderPopulater
     class << self
-      def call(name:, total_schools: 10, participants_per_school: 200)
+      def call(name:, total_schools: 10, participants_per_school: 50)
         Importers::SeedStatements.new.call
         new(name: name).call(total_schools: total_schools, participants_per_school: participants_per_school)
       end
     end
 
     def call(total_schools: 10, participants_per_school: 100)
-      ActiveRecord::Base.transaction do
-        generate_new_schools(count: total_schools - lead_provider.schools.count) if lead_provider.schools.count < total_schools
-        lead_provider.schools.order("created_at desc").limit(total_schools).each do |school|
-          sparsity_uplift = weighted_choice(selection: [true, false], odds: [11, 89])
-          pupil_premium_uplift = weighted_choice(selection: [true, false], odds: [11, 39])
-          find_or_create_participants(school: school, number_of_participants: participants_per_school, sparsity_uplift: sparsity_uplift, pupil_premium_uplift: pupil_premium_uplift)
-        end
+      generate_new_schools(count: total_schools - lead_provider.schools.count) if lead_provider.schools.count < total_schools
+      lead_provider.schools.order("created_at desc").limit(total_schools).each do |school|
+        sparsity_uplift = weighted_choice(selection: [true, false], odds: [11, 89])
+        pupil_premium_uplift = weighted_choice(selection: [true, false], odds: [11, 39])
+        find_or_create_participants(school: school, number_of_participants: participants_per_school, sparsity_uplift: sparsity_uplift, pupil_premium_uplift: pupil_premium_uplift)
       end
     end
 
-  private
+    private
 
     attr_reader :lead_provider
 
