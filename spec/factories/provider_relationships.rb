7ab87857001d41bbe23a9a912ab1fde45a94ce48
matt <mattonfoot@gmail.com>
2022-02-28 15:52:28 +0000

    use factorybot to get the current cohort

--- spec/factories/provider_relationships.rb
+++ spec/factories/provider_relationships.rb
@@ -2,7 +2,7 @@
 
 FactoryBot.define do
   factory :provider_relationship do
-    cohort { Cohort.find_or_create_by!(start_year: 2021) }
+    cohort { create :cohort, :current }
     lead_provider
     delivery_partner { create :delivery_partner }
   end
