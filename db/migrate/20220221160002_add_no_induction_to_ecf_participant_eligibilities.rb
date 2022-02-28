8ede1c648670062240540c4472979e32ee985c1e
Jack Bracewell <jack@reasonable.engineering>
2022-02-28 15:58:33 +0000

    Run CheckNoInductionParticipants job at 7am monday morning

--- db/migrate/20220221160002_add_no_induction_to_ecf_participant_eligibilities.rb
+++ db/migrate/20220221160002_add_no_induction_to_ecf_participant_eligibilities.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddNoInductionToECFParticipantEligibilities < ActiveRecord::Migration[6.1]
   def change
     add_column :ecf_participant_eligibilities, :no_induction, :boolean
