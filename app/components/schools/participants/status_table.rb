1bd8a3ee0fb30d6b4e42eb30db48a8c7fb03ebde
Tony Headford <tony@binarycircus.com>
2022-03-14 14:25:25 +0000

    Remove transferred stuff from status table

--- app/components/schools/participants/status_table.rb
+++ app/components/schools/participants/status_table.rb
@@ -12,34 +12,6 @@ module Schools
         participant_profiles.all? { |pp| pp.ecf_participant_eligibility&.ineligible_status? }
       end
 
-      def transferring_out_participants?
-        school_cohort.transferring_out_induction_records.where(participant_profile: participant_profiles).any?
-      end
-
-      def transferring_in_participants?
-        school_cohort.transferring_in_induction_records.where(participant_profile: participant_profiles).any?
-      end
-
-      def transferred_participants?
-        school_cohort.transferred_induction_records.where(participant_profile: participant_profiles).any?
-      end
-
-      def date_column_heading
-        if FeatureFlag.active?(:change_of_circumstances)
-          if transferring_out_participants?
-            "Leaving"
-          elsif transferring_in_participants?
-            "Joining"
-          elsif transferred_participants?
-            "Transferred"
-          else
-            "Induction start"
-          end
-        else
-          "Induction start"
-        end
-      end
-
       attr_reader :participant_profiles, :school_cohort
     end
   end
