97d81f2bf060fc25bd06e6a7ab819a3600880137
Steven <steven.woods@digital.education.gov.uk>
2022-03-17 23:18:32 +0000

    Add redirect in add participants to transferring participant

--- spec/requests/schools/add_participants_spec.rb
+++ spec/requests/schools/add_participants_spec.rb
@@ -4,6 +4,7 @@ require "rails_helper"
 
 RSpec.describe "Schools::AddParticipant", type: :request do
   let(:user) { create(:user, :induction_coordinator) }
+  let(:school_cohort) { create(:school_cohort, :fip, school: school) }
   let(:school) { user.induction_coordinator_profile.schools.sample }
   let(:cohort) { create(:cohort, :current) }
 
@@ -16,11 +17,42 @@ RSpec.describe "Schools::AddParticipant", type: :request do
 
   describe "GET /schools/:school_id/cohorts/:cohort_id/participants/add" do
     before do
-      get "/schools/#{school.slug}/cohorts/#{cohort.start_year}/participants/add"
+      get "/schools/#{school.slug}/cohorts/#{cohort.start_year}/participants/add", params: { type: :joining }
     end
 
     it "sets up the form in the session" do
       expect(session[:schools_add_participant_form]).to include("school_cohort_id" => school_cohort.id)
+    end
+  end
+
+  describe "GET /schools/:school_id/cohorts/:cohort_id/participants/add/who" do
+    context "when session has not been set up with the form" do
+      before do
+        get "/schools/#{school.slug}/cohorts/#{cohort.start_year}/participants/add/who"
+      end
+
+      it { is_expected.to redirect_to schools_cohort_path(school_id: school.slug, cohort_id: cohort.start_year) }
+    end
+
+    context "when form has been set up in the session" do
+      before do
+        set_session(:schools_add_participant_form, {
+          type: :teacher,
+          full_name: Faker::Name.name,
+          email: Faker::Internet.email,
+          mentor_id: "later",
+          school_cohort_id: school_cohort.id,
+          current_user_id: user.id,
+          start_term: "Autumn 2050",
+        })
+        get "/schools/#{school.slug}/cohorts/#{cohort.start_year}/participants/add/who", params: { type: :joining }
+      end
+
+      it "renders the expected page" do
+        expect(subject).to render_template(:who)
+        expect(response.body).to include("A teacher transferring from another school")
+        expect(response.body).to include("A new ECT")
+      end
     end
   end
 
