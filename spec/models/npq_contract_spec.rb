372b1d257c907d7582cf8b75748c1f2e1f151f03
Yann Marquet <yann.marquet@digital.education.gov.uk>
2022-03-23 17:51:37 +0100

    [CPDLP-1149] - add backfill cohort rake tasks

--- spec/models/npq_contract_spec.rb
+++ spec/models/npq_contract_spec.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 require "rails_helper"
 
 RSpec.describe NPQContract do
-  it { is_expected.to belong_to :cohort }
+  it { is_expected.to belong_to(:cohort).optional(true) }
 end
