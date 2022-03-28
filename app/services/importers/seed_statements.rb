432160c492f8356c7c511e53be9e0a51cbad6a5c
Yann Marquet <yann.marquet@digital.education.gov.uk>
2022-03-28 10:31:37 +0200

    [CPDLP-1150] - Current statement applies to output fee + service fee months

--- app/services/importers/seed_statements.rb
+++ app/services/importers/seed_statements.rb
@@ -49,22 +49,29 @@ private
 
   def ecf_statements
     [
-      { name: "November 2021", deadline_date: Date.new(2021, 11, 30), payment_date: Date.new(2021, 11, 30), contract_version: "0.0.1" },
-      { name: "January 2022", deadline_date: Date.new(2021, 12, 31), payment_date: Date.new(2022, 1, 25), contract_version: "0.0.1" },
-      { name: "February 2022", deadline_date: Date.new(2022, 1, 31), payment_date: Date.new(2022, 2, 25), contract_version: "0.0.1" },
+      { name: "November 2021",  deadline_date: Date.new(2021, 11, 30), payment_date: Date.new(2021, 11, 30), contract_version: "0.0.1" },
+      { name: "January 2022",   deadline_date: Date.new(2021, 12, 31), payment_date: Date.new(2022, 1, 25),  contract_version: "0.0.1" },
+      { name: "February 2022",  deadline_date: Date.new(2022, 1, 31),  payment_date: Date.new(2022, 2, 25),  contract_version: "0.0.1" },
+      { name: "March 2022",     deadline_date: Date.new(2022, 2, 28),  payment_date: Date.new(2022, 3, 25),  contract_version: "0.0.1" },
+      { name: "April 2022",     deadline_date: Date.new(2022, 3, 31),  payment_date: Date.new(2022, 4, 25),  contract_version: "0.0.1" },
+      { name: "May 2022",       deadline_date: Date.new(2022, 4, 30),  payment_date: Date.new(2022, 5, 25),  contract_version: "0.0.1" },
+      { name: "June 2022",      deadline_date: Date.new(2022, 5, 31),  payment_date: Date.new(2022, 6, 25),  contract_version: "0.0.1" },
+      { name: "July 2022",      deadline_date: Date.new(2022, 6, 30),  payment_date: Date.new(2022, 7, 25),  contract_version: "0.0.1" },
+      { name: "August 2022",    deadline_date: Date.new(2022, 7, 31),  payment_date: Date.new(2022, 8, 25),  contract_version: "0.0.1" },
+      { name: "September 2022", deadline_date: Date.new(2022, 8, 31),  payment_date: Date.new(2022, 9, 25),  contract_version: "0.0.1" },
     ].map { |hash| OpenStruct.new(hash) }
   end
 
   def npq_statements
     [
-      { name: "January 2022", deadline_date: Date.new(2021, 12, 25), payment_date: Date.new(2022, 1, 25), contract_version: "0.0.1" },
-      { name: "February 2022", deadline_date: Date.new(2022, 1, 25), payment_date: Date.new(2022, 2, 25), contract_version: "0.0.1" },
-      { name: "March 2022", deadline_date: Date.new(2022, 2, 25), payment_date: Date.new(2022, 3, 25), contract_version: "0.0.2" },
-      { name: "April 2022", deadline_date: Date.new(2022, 3, 25), payment_date: Date.new(2022, 4, 25), contract_version: "0.0.1" },
-      { name: "May 2022", deadline_date: Date.new(2022, 4, 25), payment_date: Date.new(2022, 5, 25), contract_version: "0.0.1" },
-      { name: "June 2022", deadline_date: Date.new(2022, 5, 25), payment_date: Date.new(2022, 6, 25), contract_version: "0.0.1" },
-      { name: "July 2022", deadline_date: Date.new(2022, 6, 25), payment_date: Date.new(2022, 7, 25), contract_version: "0.0.1" },
-      { name: "August 2022", deadline_date: Date.new(2022, 7, 25), payment_date: Date.new(2022, 8, 25), contract_version: "0.0.1" },
+      { name: "January 2022", deadline_date: Date.new(2021, 12, 25), payment_date: Date.new(2022, 1, 25),  contract_version: "0.0.1" },
+      { name: "February 2022", deadline_date: Date.new(2022, 1, 25), payment_date: Date.new(2022, 2, 25),  contract_version: "0.0.1" },
+      { name: "March 2022", deadline_date: Date.new(2022, 2, 25), payment_date: Date.new(2022, 3, 25),     contract_version: "0.0.2" },
+      { name: "April 2022", deadline_date: Date.new(2022, 3, 25), payment_date: Date.new(2022, 4, 25),     contract_version: "0.0.1" },
+      { name: "May 2022", deadline_date: Date.new(2022, 4, 25), payment_date: Date.new(2022, 5, 25),       contract_version: "0.0.1" },
+      { name: "June 2022", deadline_date: Date.new(2022, 5, 25), payment_date: Date.new(2022, 6, 25),      contract_version: "0.0.1" },
+      { name: "July 2022", deadline_date: Date.new(2022, 6, 25), payment_date: Date.new(2022, 7, 25),      contract_version: "0.0.1" },
+      { name: "August 2022", deadline_date: Date.new(2022, 7, 25), payment_date: Date.new(2022, 8, 25),    contract_version: "0.0.1" },
       { name: "September 2022", deadline_date: Date.new(2022, 8, 25), payment_date: Date.new(2022, 9, 25), contract_version: "0.0.1" },
     ].map { |hash| OpenStruct.new(hash) }
   end
