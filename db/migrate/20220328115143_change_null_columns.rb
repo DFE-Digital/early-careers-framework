cdaae0b6f8c3bc64f34a5771085b8b2e8606214b
Yann Marquet <yann.marquet@digital.education.gov.uk>
2022-03-28 14:37:58 +0200

    [CPDLP-1149] - fix migrations

--- db/migrate/20220328115143_change_null_columns.rb
+++ db/migrate/20220328115143_change_null_columns.rb
@@ -2,13 +2,15 @@
 
 class ChangeNullColumns < ActiveRecord::Migration[6.1]
   def change
-    change_column_null :statements, :cohort_id, false
-    remove_check_constraint :statements, name: "statements_cohort_id_null"
+    safety_assured do
+      change_column_null :statements, :cohort_id, false
+      remove_check_constraint :statements, name: "statements_cohort_id_null"
 
-    change_column_null :call_off_contracts, :cohort_id, false
-    remove_check_constraint :call_off_contracts, name: "call_off_contracts_cohort_id_null"
+      change_column_null :call_off_contracts, :cohort_id, false
+      remove_check_constraint :call_off_contracts, name: "call_off_contracts_cohort_id_null"
 
-    change_column_null :npq_contracts, :cohort_id, false
-    remove_check_constraint :npq_contracts, name: "npq_contracts_cohort_id_null"
+      change_column_null :npq_contracts, :cohort_id, false
+      remove_check_constraint :npq_contracts, name: "npq_contracts_cohort_id_null"
+    end
   end
 end
