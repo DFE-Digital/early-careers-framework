8b059bd73b47606d2823b3afe06cdacdcd97609f
Yann Marquet <yann.marquet@digital.education.gov.uk>
2022-03-28 13:34:30 +0200

    [CPDLP-1149] - fix migration

--- db/migrate/20220323173651_add_cohort_foreign_key_to_statements_and_contracts.rb
+++ db/migrate/20220323173651_add_cohort_foreign_key_to_statements_and_contracts.rb
@@ -4,12 +4,12 @@ class AddCohortForeignKeyToStatementsAndContracts < ActiveRecord::Migration[6.1]
   disable_ddl_transaction!
 
   def change
-    add_foreign_key :statements,         :cohorts, validate: false
-    add_foreign_key :call_off_contracts, :cohorts, validate: false
-    add_foreign_key :npq_contracts,      :cohorts, validate: false
-
     add_check_constraint :statements, "cohort_id IS NOT NULL",         name: "statements_cohort_id_null",         validate: false
     add_check_constraint :call_off_contracts, "cohort_id IS NOT NULL", name: "call_off_contracts_cohort_id_null", validate: false
     add_check_constraint :npq_contracts, "cohort_id IS NOT NULL",      name: "npq_contracts_cohort_id_null",      validate: false
+
+    add_foreign_key :statements,         :cohorts, validate: false
+    add_foreign_key :call_off_contracts, :cohorts, validate: false
+    add_foreign_key :npq_contracts,      :cohorts, validate: false
   end
 end
