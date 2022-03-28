a055374c47c999bde99e095ca15fc94c6775621e
Phil Lee <asmega@users.noreply.github.com>
2022-03-28 16:34:26 +0100

    refactor resume participant tests

--- spec/support/shared_examples/participant_actions_resume_support.rb
+++ spec/support/shared_examples/participant_actions_resume_support.rb
@@ -1,25 +1,5 @@
 # frozen_string_literal: true
 
-RSpec.shared_examples "a participant resume action service" do
-  it_behaves_like "a participant action service"
-
-  it "creates an active state and makes the profile active" do
-    expect { described_class.call(params: given_params) }.to change { ParticipantProfileState.count }.by(1)
-    expect(user_profile.participant_profile_state).to be_active
-    expect(user_profile).to be_training_status_active
-  end
-
-  it "fails when the participant is already active" do
-    ParticipantProfileState.create!(participant_profile: user_profile, state: "active")
-    expect { described_class.call(params: given_params) }.to raise_error(ActiveRecord::RecordInvalid)
-  end
-
-  it "fails when the participant is already withdrawn" do
-    ParticipantProfileState.create!(participant_profile: user_profile, state: "withdrawn")
-    expect { described_class.call(params: given_params) }.to raise_error(ActiveRecord::RecordInvalid)
-  end
-end
-
 RSpec.shared_examples "JSON Participant Resume endpoint" do |serialiser_type|
   let(:parsed_response) { JSON.parse(response.body) }
 
