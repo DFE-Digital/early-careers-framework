b544a760d75e9ff0c9aa1603817a023137dcd88e
Tony Headford <tony@binarycircus.com>
2022-04-01 13:43:32 +0100

    Add induction_programme during school_cohort choice

---
+++ spec/services/induction/set_cohort_induction_programme_spec.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+RSpec.describe Induction::SetCohortInductionProgramme do
+  describe "#call" do
+    let(:school_cohort) { create :school_cohort, induction_programme_choice: "no_early_career_teachers", opt_out_of_updates: true }
+
+    subject(:service) { described_class }
+
+    it "sets the programme choice" do
+      service.call(school_cohort: school_cohort, programme_choice: "full_induction_programme")
+      expect(school_cohort).to be_full_induction_programme
+    end
+
+    it "sets the opt out flag correctly" do
+      service.call(school_cohort: school_cohort, programme_choice: "full_induction_programme")
+      expect(school_cohort).not_to be_opt_out_of_updates
+    end
+
+    it "creates a new induction programme" do
+      expect {
+        service.call(school_cohort: school_cohort, programme_choice: "full_induction_programme")
+      }.to change { InductionProgramme.count }.by 1
+    end
+
+    it "sets the default induction programme" do
+      service.call(school_cohort: school_cohort, programme_choice: "full_induction_programme")
+      expect(school_cohort.default_induction_programme).to be_full_induction_programme
+    end
+
+    context "when a non-training programme is chosen" do
+      let(:induction_programme) { create(:induction_programme, :fip, school_cohort: school_cohort) }
+
+      before do
+        school_cohort.update!(default_induction_programme: induction_programme, opt_out_of_updates: false)
+      end
+
+      it "does not add an induction programme" do
+        expect {
+          service.call(school_cohort: school_cohort, programme_choice: "no_early_career_teachers")
+        }.not_to change { InductionProgramme.count }
+      end
+
+      it "removes any default induction programme association" do
+        service.call(school_cohort: school_cohort, programme_choice: "no_early_career_teachers")
+        expect(school_cohort.default_induction_programme).to be_blank
+      end
+
+      it "sets the opt out flag correctly" do
+        service.call(school_cohort: school_cohort, programme_choice: "no_early_career_teachers", opt_out_of_updates: true)
+        expect(school_cohort).to be_opt_out_of_updates
+      end
+    end
+
+    context "when a default induction programme already exists" do
+      let(:induction_programme) { create(:induction_programme, :fip, school_cohort: school_cohort) }
+
+      before do
+        school_cohort.update!(default_induction_programme: induction_programme)
+      end
+
+      it "replaces the default induction programme" do
+        service.call(school_cohort: school_cohort, programme_choice: "core_induction_programme")
+        expect(school_cohort.default_induction_programme).to be_core_induction_programme
+      end
+    end
+  end
+end
