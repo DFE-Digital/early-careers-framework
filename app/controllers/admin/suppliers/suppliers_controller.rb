7b0b43cc1aaedbd5f33fbea269383a28fe2b1b23
Fabio Volpe <volpefabio@outlook.com>
2022-04-01 17:32:10 +0100

    replace Kaminati with Pagy for Suppliers admin

--- app/controllers/admin/suppliers/suppliers_controller.rb
+++ app/controllers/admin/suppliers/suppliers_controller.rb
@@ -9,9 +9,12 @@ module Admin
         lead_providers = policy_scope(LeadProvider).sort_by(&:name)
         delivery_partners = policy_scope(DeliveryPartner).sort_by(&:name)
         sorted_suppliers = (lead_providers + delivery_partners)
-        @suppliers = Kaminari.paginate_array(sorted_suppliers).page(params[:page]).per(20)
+        @suppliers = Kaminari.paginate_array(sorted_suppliers).page(params[:page]).per(5)
         @page = @suppliers.current_page
         @total_pages = @suppliers.total_pages
+        @pagy, @suppliers2 = pagy_array(sorted_suppliers, page: params[:page], items: 5)
+        # @page = @pagy.page
+        # @total_pages = @pagy.pages
       end
     end
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  include Pagy::Frontend
+
   def profile_dashboard_path(user)
     if user.admin?
       admin_schools_path
