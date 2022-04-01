ec331bb20f1d56e5758de0650ecef68ea481d2f0
Tony Headford <tony@binarycircus.com>
2022-04-01 13:51:31 +0100

    Add missing spec for CIP

--- spec/services/induction/set_cohort_induction_programme_spec.rb
+++ spec/services/induction/set_cohort_induction_programme_spec.rb
@@ -63,5 +63,14 @@ RSpec.describe Induction::SetCohortInductionProgramme do
         expect(school_cohort.default_induction_programme).to be_core_induction_programme
       end
     end
+    
+    context "when a CIP is chosen and the programme specified" do
+      let(:cip) { create(:core_induction_programme) }
+
+      it "sets the core_induction_programme" do
+        service.call(school_cohort: school_cohort, programme_choice: "core_induction_programme", core_induction_programme: cip)
+        expect(school_cohort.default_induction_programme.core_induction_programme).to eq cip
+      end
+    end
   end
 end
