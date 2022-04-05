21a6c707b9280d6064fd2704aa788ec76043f7ac
Tony Headford <tony@binarycircus.com>
2022-04-05 14:26:12 +0100

    Add job to enrol school cohorts

---
+++ app/jobs/enrol_school_cohorts_job.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+class EnrolSchoolCohortsJob < ApplicationJob
+  def perform
+    SchoolCohort
+      .where(induction_programme_choice: InductionProgramme.training_programmes.keys)
+      .left_joins(:induction_programmes)
+      .where(induction_programmes: { id: nil })
+      .find_each do |sc|
+        choice = sc.induction_programme_choice
+        programme = InductionProgramme.new(school_cohort: sc,
+                                           training_programme: choice)
+        case choice
+        when "full_induction_programme"
+          programme.partnership = sc.school.partnerships.active.where(cohort: sc.cohort).first
+        when "core_induction_programme"
+          programme.core_induction_programme = sc.core_induction_programme
+        end
+        programme.save!
+
+        sc.ecf_participant_profiles.each do |profile|
+          induction_record = Induction::Enrol.call(participant_profile: profile, induction_programme: programme)
+          induction_record.update!(induction_status: profile.status,
+                                   training_status: profile.training_status,
+                                   mentor_profile_id: profile.mentor_profile_id)
+        end
+        sc.update!(default_induction_programme: programme)
+
+        Rails.logger.info "Added #{choice} to #{sc.school.urn}"
+      end
+  end
+end
