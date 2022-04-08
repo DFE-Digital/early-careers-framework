6db404871c22c7cf467f056ce6b117a15803f2c2
Fabio Volpe <volpefabio@outlook.com>
2022-04-08 14:10:40 +0100

    test fix

--- spec/components/lead_providers/your_schools/table_spec.rb
+++ spec/components/lead_providers/your_schools/table_spec.rb
@@ -3,10 +3,12 @@
 RSpec.describe LeadProviders::YourSchools::Table, type: :view_component do
   include Pagy::Backend
 
+  ITEMS = 10
+
   let(:partnerships) { Array.new(rand(21..30)) { |i| double "Partnership #{i}" } }
   let(:page) { rand(1..2) }
   let(:paginated_partnerships) do
-    _, p = pagy_array(partnerships, page: page, items: 10)
+    _, p = pagy_array(partnerships, page: page, items: ITEMS)
     p
   end
 
@@ -16,7 +18,7 @@ RSpec.describe LeadProviders::YourSchools::Table, type: :view_component do
   stub_component LeadProviders::YourSchools::TableRow
 
   it "renders table row for each school" do
-    expected_partnerships = partnerships.each_slice(10).to_a[page - 1]
+    expected_partnerships = partnerships.each_slice(ITEMS).to_a[page - 1]
 
     expected_partnerships.each do |partnership|
       expect(rendered).to have_rendered(LeadProviders::YourSchools::TableRow).with(partnership: partnership)
