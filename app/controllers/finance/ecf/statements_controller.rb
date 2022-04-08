a72de4cadaff074a2eeab2d01a43fd1f4763b542
Yann Marquet <yann.marquet@digital.education.gov.uk>
2022-04-08 17:59:09 +0200

    [CPDLP-1150] - fix statements displaying far too many future statements

--- app/controllers/finance/ecf/statements_controller.rb
+++ app/controllers/finance/ecf/statements_controller.rb
@@ -9,8 +9,8 @@ module Finance
         @ecf_lead_provider = lead_provider_scope.find(params[:payment_breakdown_id])
         @cpd_lead_provider = @ecf_lead_provider.cpd_lead_provider
 
-        @statements = @ecf_lead_provider.statements.upto_current.order(payment_date: :desc)
-
+        current_statement = @ecf_lead_provider.statements.current
+        @statements = @ecf_lead_provider.statements.upto(current_statement).order(deadline_date: :desc)
         @statement = @ecf_lead_provider.statements.find_by(name: identifier_to_name)
 
         aggregator = ParticipantAggregator.new(
