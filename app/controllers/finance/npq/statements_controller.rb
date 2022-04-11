17595d3114e5f13dc6f7b260d38cf88910f85136
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2022-04-11 16:45:27 +0100

    [CPDLP-1148] Version contract reference data

--- app/controllers/finance/npq/statements_controller.rb
+++ app/controllers/finance/npq/statements_controller.rb
@@ -10,7 +10,7 @@ module Finance
         @cpd_lead_provider = @npq_lead_provider.cpd_lead_provider
         @statement         = @cpd_lead_provider.npq_lead_provider.statements.find_by(name: identifier_to_name)
         @statements        = @npq_lead_provider.statements.upto_current.order(payment_date: :desc)
-        @npq_contracts     = @npq_lead_provider.npq_contracts.order(course_identifier: :asc)
+        @npq_contracts     = @npq_lead_provider.npq_contracts.where(version: @statement.contract_version).order(course_identifier: :asc)
       end
 
       def voided
