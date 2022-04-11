13b0d4ece2efea74234f13596c5baa5982e21f03
Leonidas Apostolidis <leoapost@gmail.com>
2022-04-11 14:20:13 +0100

    Set the cohort for confirming more schools to 2022

--- spec/requests/lead_providers/report_schools/base_spec.rb
+++ spec/requests/lead_providers/report_schools/base_spec.rb
@@ -5,6 +5,7 @@ require "rails_helper"
 RSpec.describe "Lead Provider school reporting", type: :request do
   let(:user) { create(:user, :lead_provider) }
   let!(:cohort) { create(:cohort, :current) }
+  let!(:cohort_2022) { create(:cohort, :next) }
 
   before do
     sign_in user
