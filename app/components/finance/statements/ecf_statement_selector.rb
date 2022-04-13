e273949c93d01020585cc5911c3b32aa9e72ab9c
Phil Lee <asmega@users.noreply.github.com>
2022-04-13 10:37:00 +0100

    extract submit button copy to i18n locale

--- app/components/finance/statements/ecf_statement_selector.rb
+++ app/components/finance/statements/ecf_statement_selector.rb
@@ -24,6 +24,16 @@ module Finance
             )
           end
       end
+
+      def t(key)
+        I18n.t key, scope: i18n_scope
+      end
+
+    private
+
+      def i18n_scope
+        %i[components finance statements ecf_statement_selector]
+      end
     end
   end
 end
