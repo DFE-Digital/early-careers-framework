5d26283816c33f0c0476674f86c1fcba40324424
Fabio Volpe <volpefabio@outlook.com>
2022-04-22 14:00:04 +0100

    DIY CIP no expected ects page

--- app/controllers/schools/setup_school_cohort_controller.rb
+++ app/controllers/schools/setup_school_cohort_controller.rb
@@ -3,8 +3,9 @@
 module Schools
   class SetupSchoolCohortController < ::Schools::BaseController
     before_action :load_form
+    before_action :latest_induction_record, only: %i[email choose_mentor teachers_current_programme schools_current_programme check_answers complete]
     before_action :school
-    before_action :validate_request_or_render, except: %i[training_confirmation no_expected_ects]
+    before_action :validate_request_or_render, except: %i[what_we_need]
 
     skip_after_action :verify_authorized
     skip_after_action :verify_policy_scoped
@@ -17,33 +18,17 @@ module Schools
         store_form_redirect_to_next_step(:change_provider)
       elsif previous_cohort.core_induction_programme?
         # DIY CIP
-        if no_ects_expected
-          store_form_redirect_to_next_step :no_expected_ects
-        elsif ects_expected
-          store_form_redirect_to_next_step :how_will_you_run_training
+        if setup_school_cohort_form_params[:expect_any_ects_choice] == "no"
+          redirect_to no_expected_ects_schools_setup_school_cohort_path
         end
       end
     end
 
     def no_expected_ects
-      unless school_cohort.persisted?
-        school_cohort.induction_programme_choice = :no_early_career_teachers
-        school_cohort.save!
-      end
-
-      reset_form_data
     end
 
     # cip
-    def how_will_you_run_training
-      store_form_redirect_to_next_step :programme_confirmation
-    end
-
-    def programme_confirmation; end
-
-    def training_confirmation
-      reset_form_data
-    end
+    def how_will_you_run_training; end
 
     def change_provider; end
 
@@ -53,21 +38,7 @@ module Schools
 
     def complete; end
 
-    def save_programme
-      save_school_choice!
-
-      redirect_to training_confirmation_schools_setup_school_cohort_path
-    end
-
   private
-
-    def ects_expected
-      setup_school_cohort_form_params[:expect_any_ects_choice] == "yes"
-    end
-
-    def no_ects_expected
-      setup_school_cohort_form_params[:expect_any_ects_choice] == "no"
-    end
 
     def load_form
       @setup_school_cohort_form = SetupSchoolCohortForm.new(session[:setup_school_cohort_form])
@@ -79,7 +50,6 @@ module Schools
 
       params.require(:schools_setup_school_cohort_form)
             .permit(:expect_any_ects_choice,
-                    :how_will_you_run_training_choice,
                     :trn,
                     :date_of_birth,
                     :start_date,
@@ -90,7 +60,7 @@ module Schools
     end
 
     def validate_request_or_render
-      render unless (request.post? || request.put?) && step_valid?
+      render unless request.put? && step_valid?
     end
 
     def store_form_redirect_to_next_step(step)
@@ -108,19 +78,6 @@ module Schools
 
     def school
       @school ||= active_school
-    end
-
-    def save_school_choice!
-      Induction::SetCohortInductionProgramme.call(school_cohort: school_cohort,
-                                                  programme_choice: @setup_school_cohort_form.attributes[:how_will_you_run_training_choice])
-    end
-
-    def school_cohort
-      @school_cohort ||= school.school_cohorts.find_or_initialize_by(cohort: cohort)
-    end
-
-    def cohort
-      @cohort ||= Cohort.find_by(start_year: params[:cohort_id])
     end
   end
 end
