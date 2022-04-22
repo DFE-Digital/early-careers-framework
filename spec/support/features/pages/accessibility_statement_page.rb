2ec5ab95ea2becc345a439258627be82d65c491f
matt <mattonfoot@gmail.com>
2022-04-22 13:43:21 +0100

    add back page object for accessibility page

---
+++ spec/support/features/pages/accessibility_statement_page.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "./base"
+
+module Pages
+  class AccessibilityStatementPage < ::Pages::Base
+    set_url "/accessibility-statement"
+    set_primary_heading "Accessibility statement for Manage training for early career teachers service"
+  end
+end
