ccaa1fedfa89e4d92362dc33bbf28d3a000721a8
Steven <steven.woods@digital.education.gov.uk>
2022-04-25 18:13:06 +0100

    Send participant an email when they have been added and validated by a SIT

--- spec/forms/schools/add_participant_form_spec.rb
+++ spec/forms/schools/add_participant_form_spec.rb
@@ -267,10 +267,6 @@ RSpec.describe Schools::AddParticipantForm, type: :model do
       end
     end
 
-    it "creates new validation data" do
-      expect { form.save! }.to change(ECFParticipantValidationData, :count).by 1
-    end
-
     context "no dqt record is present" do
       it "does not create ecf validation data" do
         form.dqt_record = nil
