5e0bc28f840e374372c9f6b5709d7010394a2513
Leonidas Apostolidis <leoapost@gmail.com>
2022-04-26 13:48:44 +0100

    Add the registration and academic year dates in the cohort factory

--- spec/factories/cohorts.rb
+++ spec/factories/cohorts.rb
@@ -3,6 +3,8 @@
 FactoryBot.define do
   factory :cohort do
     start_year { Faker::Number.unique.between(from: 2022, to: 2100) }
+    registration_start_date { Date.new(start_year.to_i, 5, 10) }
+    academic_year_start_date { Date.new(start_year.to_i, 9, 1) }
 
     trait :current do
       start_year { 2021 }
