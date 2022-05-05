2c568e73f09bdcc8913c6a4a4cd49f307da1a9b1
James Rose <james@jbpr.net>
2022-05-05 14:23:44 +0100

    Add mailer to invite existing schools to choose programme for 22/23

---
+++ spec/services/invite_ects_spec.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe InviteEcts do
+  subject(:invite_ects) { described_class.new }
+  let!(:cohort) { create(:cohort, :next) }
+
+  let!(:school) { create(:school) }
+  let!(:school_cohort) { create(:school_cohort, school: school) }
+
+  let!(:induction_coordinator_profile) { create(:induction_coordinator_profile, schools: [school]) }
+
+  before(:all) do
+    FeatureFlag.activate(:multiple_cohorts)
+    RSpec::Mocks.configuration.verify_partial_doubles = false
+  end
+
+  before(:each) do
+    allow_any_instance_of(Mail::TestMailer).to receive_message_chain(:response, :id) { "notify_id" }
+  end
+
+  after(:all) do
+    FeatureFlag.deactivate(:multiple_cohorts)
+    RSpec::Mocks.configuration.verify_partial_doubles = true
+  end
+
+  describe "#preterm_reminder" do
+    it "sends the nomination email" do
+      expect(ParticipantMailer).to receive(:preterm_reminder).with(
+        hash_including(induction_coordinator_profile: induction_coordinator_profile),
+      ).and_call_original
+
+      invite_ects.preterm_reminder
+    end
+
+    context "with an induction profile that has already received the email" do
+      before do
+        create(:email, associated_with: [induction_coordinator_profile], tags: %w[preterm_reminder])
+      end
+
+      it "does not send the email again" do
+        expect(ParticipantMailer).to receive(:preterm_reminder).never
+
+        invite_ects.preterm_reminder
+      end
+    end
+
+    context "where the school is a childrens centre" do
+      before do
+        school.update!(school_type_code: GiasTypes::NO_INVITATIONS_TYPE_CODES.sample)
+      end
+
+      it "does not send the email again" do
+        expect(ParticipantMailer).to receive(:preterm_reminder).never
+
+        invite_ects.preterm_reminder
+      end
+    end
+
+    context "where the school has already chosen a programme" do
+      before { create :school_cohort, school: school, cohort: cohort }
+
+      it "does not send the email again" do
+        expect(ParticipantMailer).to receive(:preterm_reminder).never
+
+        invite_ects.preterm_reminder
+      end
+    end
+  end
+end
