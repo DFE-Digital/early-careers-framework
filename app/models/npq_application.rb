534644710c96ac82efde32fb43c153a27af90004
Phil Lee <asmega@users.noreply.github.com>
2022-05-09 11:37:22 +0100

    add new possible values for NPQ headteacher status

--- app/models/npq_application.rb
+++ app/models/npq_application.rb
@@ -17,6 +17,8 @@ class NPQApplication < ApplicationRecord
     yes_when_course_starts: "yes_when_course_starts",
     yes_in_first_two_years: "yes_in_first_two_years",
     yes_over_two_years: "yes_over_two_years",
+    yes_in_first_five_years: "yes_in_first_five_years",
+    yes_over_five_years: "yes_over_five_years",
   }
 
   enum funding_choice: {
--- spec/models/npq_application_spec.rb
+++ spec/models/npq_application_spec.rb
@@ -9,6 +9,8 @@ RSpec.describe NPQApplication, type: :model do
       yes_when_course_starts: "yes_when_course_starts",
       yes_in_first_two_years: "yes_in_first_two_years",
       yes_over_two_years: "yes_over_two_years",
+      yes_in_first_five_years: "yes_in_first_five_years",
+      yes_over_five_years: "yes_over_five_years",
     ).backed_by_column_of_type(:text)
   }
 
