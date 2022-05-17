a14c4e6898f4c4b722bafb0775434abad3c7d5e8
Phil Lee <asmega@users.noreply.github.com>
2022-05-17 16:12:17 +0100

    hookup controller to npq funding service

---
+++ spec/requests/api/v1/npq_funding_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "NPQ Funding API", type: :request do
+  let(:token) { NPQRegistrationApiToken.create_with_random_token!(private_api_access: true) }
+  let(:bearer_token) { "Bearer #{token}" }
+  let(:parsed_response) { JSON.parse(response.body) }
+
+  describe "GET /api/v1/npq-funding" do
+    context "when authorized" do
+      before do
+        default_headers[:Authorization] = bearer_token
+      end
+
+      it "returns correct response" do
+        get "/api/v1/npq-funding?trn=1234567"
+        expect(parsed_response["previously_funded"]).to be(false)
+      end
+    end
+
+    context "when unauthorized" do
+      it "returns 401 for invalid bearer token" do
+        default_headers[:Authorization] = "Bearer ugLPicDrpGZdD_w7hhCL"
+        get "/api/v1/npq-funding?trn=1234567"
+        expect(response.status).to eq 401
+      end
+    end
+  end
+end
