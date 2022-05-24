aedb4026e06843afbc9a0a544ad6c2ea0d5e3014
matt <mattonfoot@gmail.com>
2022-05-24 21:18:49 +0100

    initial page object based features with docs and runner scripts

---
+++ spec/features/accessibility_statement_page_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.feature "Accessibility statement page", type: :feature, js: true, rutabaga: false do
+  scenario "Accessibility statement is accessible" do
+    given_i_am_on_the_accessibility_statement_page
+    then_the_page_is_accessible
+    and_percy_is_sent_a_snapshot_named "Accessibility statement page"
+  end
+
+  scenario "Reading the accessibility statement" do
+    given_i_am_on_the_start_page
+    when_i_view_accessibility_statement_from_the_start_page
+    then_i_am_on_the_accessibility_statement_page
+  end
+end
