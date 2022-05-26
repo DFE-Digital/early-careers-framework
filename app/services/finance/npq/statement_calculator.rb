f2d7edeba4f54c31a0a767f8005d7da6df87deb5
Phil Lee <asmega@users.noreply.github.com>
2022-05-26 12:11:02 +0100

    refactor NPQ components to single calculator class

---
+++ app/services/finance/npq/statement_calculator.rb
@@ -0,0 +1,99 @@
+# frozen_string_literal: true
+
+module Finance
+  module NPQ
+    class StatementCalculator
+      attr_reader :statement
+
+      def initialize(statement:)
+        @statement = statement
+      end
+
+      def summary_overall_total
+        total_overview_payment + overall_vat
+      end
+
+      def total_output_payment_subtotal
+        output_payment.sum { |output_payment| output_payment[:subtotal] }
+      end
+
+      def total_service_fees
+        service_fees.sum { |service_fee| service_fee[:monthly] }
+      end
+
+      def overall_vat
+        total_payment * (npq_lead_provider.vat_chargeable ? 0.2 : 0.0)
+      end
+
+      def total_payment
+        total_service_fees + total_output_payment_subtotal
+      end
+
+      def total_starts
+        statement_declarations.where(declaration_type: "started").count
+      end
+
+      def total_retained
+        statement_declarations.where(declaration_type: %w[retained-1 retained-2]).count
+      end
+
+      def total_completed
+        statement_declarations.where(declaration_type: "completed").count
+      end
+
+      def total_voided
+        voided_declarations.count
+      end
+
+    private
+
+      def voided_declarations
+        statement.voided_participant_declarations.unique_id
+      end
+
+      def statement_declarations_per_contract(contract)
+        statement
+          .participant_declarations
+          .for_course_identifier(contract.course_identifier)
+          .unique_id
+          .count
+      end
+
+      def statement_declarations
+        statement.participant_declarations
+      end
+
+      def output_payment
+        contracts.map do |contract|
+          PaymentCalculator::NPQ::OutputPayment.call(
+            contract: contract,
+            total_participants: statement_declarations_per_contract(contract),
+          )
+        end
+      end
+
+      def total_overview_payment
+        total_service_fees + total_output_payment_subtotal
+      end
+
+      def service_fees
+        contracts.map { |contract| PaymentCalculator::NPQ::ServiceFees.call(contract: contract) }.compact
+      end
+
+      def contracts
+        npq_lead_provider
+          .npq_contracts
+          .where(version: statement.contract_version)
+          .order(course_identifier: :asc)
+      end
+
+      def cpd_lead_provider
+        statement.cpd_lead_provider
+      end
+
+      def npq_lead_provider
+        cpd_lead_provider.npq_lead_provider
+      end
+    end
+  end
+end
