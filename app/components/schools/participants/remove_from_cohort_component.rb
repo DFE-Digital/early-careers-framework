ec94ef718ae926de648fbc64aa851d97d44fda00
Tony Headford <tony@binarycircus.com>
2022-06-09 16:38:47 +0100

    Use induction records instead of participant profile

--- app/components/schools/participants/remove_from_cohort_component.rb
+++ app/components/schools/participants/remove_from_cohort_component.rb
@@ -3,33 +3,33 @@
 module Schools
   module Participants
     class RemoveFromCohortComponent < BaseComponent
-      def initialize(profile:, current_user:)
-        @profile = profile
+      def initialize(induction_record:, current_user:)
+        @induction_record = induction_record
         @current_user = current_user
       end
 
     private
 
-      attr_reader :current_user, :profile
+      attr_reader :current_user, :induction_record
 
       def manual_removal_possible?
-        ParticipantProfile::ECFPolicy.new(current_user, profile).withdraw_record?
+        ParticipantProfile::ECFPolicy.new(current_user, induction_record.participant_profile).withdraw_record?
       end
 
       def fip?
-        profile.school_cohort.full_induction_programme?
+        induction_record.enrolled_in_fip?
       end
 
       def cip?
-        profile.school_cohort.core_induction_programme?
+        induction_record.enrolled_in_cip?
       end
 
       def name
-        profile.user.full_name
+        induction_record.user.full_name
       end
 
       def lead_provider
-        profile.school_cohort.lead_provider
+        induction_record.induction_programme.lead_provider
       end
     end
   end
