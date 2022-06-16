3071a4ec34785008dfd68eadb79d7bcb9bd02824
James Rose <james@jbpr.net>
2022-06-16 09:25:01 +0100

    Inject data files as kwargs into UpdateStagedSchools

--- app/jobs/import_gias_data_job.rb
+++ app/jobs/import_gias_data_job.rb
@@ -3,6 +3,6 @@
 class ImportGiasDataJob < ApplicationJob
   def perform
     Rails.logger.info "Importing GIAS data..."
-    DataStage::FetchGiasDataFiles.call { |files| DataStage::UpdateStagedSchools.call(files) }
+    DataStage::FetchGiasDataFiles.call { |files| DataStage::UpdateStagedSchools.call(**files) }
   end
 end
