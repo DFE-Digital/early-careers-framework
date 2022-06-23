424be2d87ef65dfb9534d0cf07db0b7b2194f81c
Fabio Volpe <volpefabio@outlook.com>
2022-06-23 11:13:20 +0100

    factory and spec

--- spec/factories/appropriate_bodies.rb
+++ spec/factories/appropriate_bodies.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :appropriate_body do
-    # TODO: appropriate body factory
+    factory :ab_local_authority do
+      sequence :name do |n|
+        "Local authority #{n}"
+      end
+      # name { "la" }
+      body_type { "local_authority" }
+    end
+
+    factory :ab_teaching_school_hub do
+      sequence :name do |n|
+        "Teaching school hub #{n}"
+      end
+      body_type { "teaching_school_hub" }
+    end
   end
 end
--- spec/models/appropriate_body_spec.rb
+++ spec/models/appropriate_body_spec.rb
@@ -2,5 +2,14 @@ require 'rails_helper'
 
 # TODO: appropriate body spec
 RSpec.describe AppropriateBody, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with name and body type" do
+    expect(create(:ab_local_authority)).to be_valid
+    expect(create(:ab_teaching_school_hub)).to be_valid
+  end
+
+  it "has name unique per body type" do
+    create(:ab_local_authority, name: "not unique one")
+    not_valid_ab = build(:ab_local_authority, name: "not unique one")
+    expect(not_valid_ab).to_not be_valid
+  end
 end
