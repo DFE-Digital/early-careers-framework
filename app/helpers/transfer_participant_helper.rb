a39882f052b7df89bfcb12781c9f3eb6da813f98
Steven <steven.woods@digital.education.gov.uk>
2022-06-30 14:03:12 +0100

    Bug-fix prevent a SIT from transferring a user without a previous cohort
    There's a bug (https://sentry.io/organizations/dfe-teacher-services/issues/3353853344/?project=5748989&referrer=issue_alert-slack) where if a SIT can try to transfer a ppt from to the 2021 to their school, even if they dont have a school cohort for that year. This blocks them from doing it. We need them, at the minute, to create a full induction programme before they're able to do so. This is a quick fix, as agreed with Mili, Brian and Jo, that tells the user to go to support.

---
+++ app/helpers/transfer_participant_helper.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module TransferParticipantHelper
+  def transfer_participant_in_cohort(cohort, school)
+    school_cohort = SchoolCohort.find_by(school:, cohort:)
+
+    if doing_fip?(school_cohort)
+      what_we_need_schools_transferring_participant_path(cohort_id: cohort)
+    else
+      contact_support_schools_transferring_participant_path
+    end
+  end
+
+private
+
+  def doing_fip?(school_cohort)
+    school_cohort.induction_programmes.any?(&:full_induction_programme?)
+  end
+end
