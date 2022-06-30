79f58d48281077399c604966dd6b23761bc7b81b
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2022-06-30 17:28:56 +0100

    [CPDLP-1471] ECF GET participant endpoint response can be inaccurate

--- app/serializers/api/v1/participant_from_induction_record_serializer.rb
+++ app/serializers/api/v1/participant_from_induction_record_serializer.rb
@@ -48,10 +48,8 @@ module Api
       end
 
       active_participant_attribute :email do |induction_record|
-        # NOTE: using this will retain the original email exposed to provider
-        induction_record.participant_profile.participant_identity.email
-        # NOTE: use this instead to use new (de-duped) email
-        # induction_record.participant_profile.user.email
+        induction_record.preferred_identity&.email ||
+          induction_record.participant_profile.user.email
       end
 
       attribute :full_name do |induction_record|
