d9824dcb17781e4f16094983762ef43c269a6d47
Leonidas Apostolidis <leoapost@gmail.com>
2022-07-01 14:41:36 +0100

    Assign cohort to the npq test data

--- app/jobs/create_new_fake_sandbox_data_job.rb
+++ app/jobs/create_new_fake_sandbox_data_job.rb
@@ -41,6 +41,7 @@ class CreateNewFakeSandboxDataJob < ApplicationJob
           npq_course: NPQCourse.all.sample,
           npq_lead_provider:,
           participant_identity: identity,
+          cohort: random_school_cohort.cohort,
         )
       end
     end
