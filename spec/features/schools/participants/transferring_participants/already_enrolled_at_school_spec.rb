c9b7c93ca88dd446c16d06cb6bfad7fcc449475b
Steven <steven.woods@digital.education.gov.uk>
2022-07-04 11:07:13 +0100

    Fix transferring test after default tab select change

--- spec/features/schools/participants/transferring_participants/already_enrolled_at_school_spec.rb
+++ spec/features/schools/participants/transferring_participants/already_enrolled_at_school_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe "Transferring participants", with_feature_flags: { change_of_circ
         given_a_school_has_chosen_fip_for_2021_and_partnered
         and_they_have_already_added_this_ect
         and_i_am_signed_in_as_an_induction_coordinator
+        and_i_have_selected_my_cohort_tab
         when_i_click_to_view_ects_and_mentors
         then_i_am_taken_to_your_ect_and_mentors_page
       end
@@ -48,6 +49,7 @@ RSpec.describe "Transferring participants", with_feature_flags: { change_of_circ
           given_a_school_has_chosen_fip_for_2021_and_partnered
           and_they_have_already_added_this_ect
           and_i_am_signed_in_as_an_induction_coordinator
+          and_i_have_selected_my_cohort_tab
           when_i_click_to_view_ects_and_mentors
           then_i_am_taken_to_your_ect_and_mentors_page
         end
@@ -57,6 +59,9 @@ RSpec.describe "Transferring participants", with_feature_flags: { change_of_circ
           then_i_should_be_on_the_who_to_add_page
 
           when_i_select "A new ECT"
+          click_on "Continue"
+          then_i_should_be_on_what_we_need_for_adding_participant_page
+
           click_on "Continue"
           then_i_am_taken_to_add_ect_name_page
 
@@ -155,6 +160,11 @@ RSpec.describe "Transferring participants", with_feature_flags: { change_of_circ
     expect(page).to have_text("To do this, you need to tell us their")
   end
 
+  def then_i_should_be_on_what_we_need_for_adding_participant_page
+    expect(page).to have_selector("h1", text: "What we need from you")
+    expect(page).to have_text("You can now choose to enter their teacher reference number ")
+  end
+
   def then_i_am_taken_to_add_ect_name_page
     expect(page).to have_selector("h1", text: "What’s this person’s full name?")
   end
@@ -198,6 +208,10 @@ RSpec.describe "Transferring participants", with_feature_flags: { change_of_circ
     @participant_profile_ect = create(:ect_participant_profile, user: create(:user, full_name: "Sally Teacher"), school_cohort: @school_cohort_one)
     create(:ecf_participant_validation_data, participant_profile: @participant_profile_ect, full_name: "Sally Teacher", trn: "1001000", date_of_birth: Date.new(1990, 10, 24))
     Induction::Enrol.call(participant_profile: @participant_profile_ect, induction_programme: @induction_programme_one)
+  end
+
+  def and_i_have_selected_my_cohort_tab
+    click_on @cohort.description
   end
 
   # data setup
