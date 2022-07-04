5b201123fcc7c344d55fb06cee8d740838640a50
Phil Lee <asmega@users.noreply.github.com>
2022-07-04 11:36:00 +0100

    refactor hard coded query to SQL LIKE clause

--- app/services/finance/npq/course_statement_calculator.rb
+++ app/services/finance/npq/course_statement_calculator.rb
@@ -42,17 +42,19 @@ module Finance
       end
 
       def billable_declarations_count_for_declaration_type(declaration_type)
-        if declaration_type == "retained"
-          declaration_type = %w[retained-1 retained-2]
-        end
-
-        statement
+        scope = statement
           .billable_statement_line_items
           .joins(:participant_declaration)
           .where(participant_declarations: { course_identifier: course.identifier })
-          .where(participant_declarations: { declaration_type: })
           .merge(ParticipantDeclaration.select("DISTINCT (user_id, declaration_type)"))
-          .count
+
+        scope = if declaration_type == "retained"
+                  scope.where("participant_declarations.declaration_type LIKE ?", "retained-%")
+                else
+                  scope.where(participant_declarations: { declaration_type: })
+                end
+
+        scope.count
       end
 
       def clawback_payment
