d636294ee3105b6ae9528802d473a0da5a8a7bc2
xswoodsx <69353998+xswoodsx@users.noreply.github.com>
2022-07-05 15:41:18 +0100

    Update app/helpers/transfer_participant_helper.rb
    Co-authored-by: Tony Headford <tony@binarycircus.com>

--- app/helpers/transfer_participant_helper.rb
+++ app/helpers/transfer_participant_helper.rb
@@ -4,7 +4,7 @@ module TransferParticipantHelper
   def transfer_participant_in_cohort(cohort, school)
     school_cohort = SchoolCohort.find_by(school:, cohort:)
 
-    if doing_fip?(school_cohort)
+    if school_cohort&.full_induction_programme?
       what_we_need_schools_transferring_participant_path(cohort_id: cohort)
     else
       contact_support_schools_transferring_participant_path
