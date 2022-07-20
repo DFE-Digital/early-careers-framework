9f637b0a0e3d41ce939bedd02b8ef7df6b2658c8
Yann Marquet <yann.marquet@digital.education.gov.uk>
2022-07-20 18:23:49 +0200

    [CPDLP-1540] - re-write index query

--- app/controllers/api/v1/ecf_participants_controller.rb
+++ app/controllers/api/v1/ecf_participants_controller.rb
@@ -14,12 +14,12 @@ module Api
       def index
         respond_to do |format|
           format.json do
-            participant_hash = ParticipantFromInductionRecordSerializer.new(paginate(induction_records)).serializable_hash
+            participant_hash = ParticipantSerializer.new(paginate(participant_profiles)).serializable_hash
             render json: participant_hash.to_json
           end
 
           format.csv do
-            participant_hash = ParticipantFromInductionRecordSerializer.new(induction_records).serializable_hash
+            participant_hash = ParticipantSerializer.new(participant_profiles).serializable_hash
             render body: to_csv(participant_hash)
           end
         end
@@ -27,8 +27,24 @@ module Api
 
     private
 
-      def induction_records
-        @induction_records ||= ECFParticipants::Index.new(cpd_lead_provider: current_user, params:).induction_records
+      def participant_profiles
+        join = InductionRecord
+          .select("induction_records.id, induction_records.participant_profile_id, MAX(induction_records.created_at), lead_providers.id AS lead_provider_id, schedules.cohort_id")
+          .joins(:schedule, induction_programme: { partnership: :lead_provider })
+          .group("induction_records.id, induction_records.participant_profile_id, lead_providers.id, schedules.cohort_id")
+
+        query = ParticipantProfile
+          .joins("JOIN (#{join.to_sql}) AS ir ON ir.participant_profile_id = participant_profiles.id")
+          .where("ir.lead_provider_id = ? AND ir.cohort_id IN (?)", lead_provider.id, with_cohorts.map(&:id))
+        query.order
+      end
+
+      def serialized_response(profile)
+        relevant_induction_record = profile.relevant_induction_record(lead_provider:)
+
+        ParticipantFromInductionRecordSerializer
+          .new(relevant_induction_record)
+          .serializable_hash.to_json
       end
 
       def access_scope
