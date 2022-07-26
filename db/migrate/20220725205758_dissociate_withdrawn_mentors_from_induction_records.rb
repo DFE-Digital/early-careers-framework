add5349b236041f95e89f80f8ce203dd558b4a4d
Lorenzo Tello <ltello8a@gmail.com>
2022-07-26 08:55:35 +0200

    CST-857: nullify existing induction records mentor_profile_id when the mentor profile has been withdrown

---
+++ db/migrate/20220725205758_dissociate_withdrawn_mentors_from_induction_records.rb
@@ -0,0 +1,7 @@
+class DissociateWithdrawnMentorsFromInductionRecords < ActiveRecord::Migration[6.1]
+  def change
+    InductionRecord.joins(:mentor_profile)
+                   .where(participant_profiles: { status: :withdrawn })
+                   .update_all(mentor_profile_id: nil)
+  end
+end
