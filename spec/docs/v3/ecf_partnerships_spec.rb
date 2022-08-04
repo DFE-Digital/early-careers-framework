7b1bce1866b41fb8019ad2fc55dcadd4114be443
Phil Lee <asmega@users.noreply.github.com>
2022-08-04 16:07:37 +0100

    add GET partnerships v3 api docs

---
+++ spec/docs/v3/ecf_partnerships_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require "swagger_helper"
+
+RSpec.describe "API", type: :request, swagger_doc: "v3/api_spec.json" do
+  path "/api/v3/partnerships/ecf" do
+    get "Retrieve multiple ECF partnerships" do
+      operationId :partnerships_ecf_get
+      tags "ECF partnerships"
+      security [bearerAuth: []]
+
+      response "200", "A list of ECF partnerships" do
+        schema({ "$ref": "#/components/schemas/MultipleECFPartnershipsResponse" })
+
+        run_test!
+      end
+
+      response "401", "Unauthorized" do
+        let(:Authorization) { "Bearer invalid" }
+
+        schema({ "$ref": "#/components/schemas/UnauthorisedResponse" })
+
+        run_test!
+      end
+    end
+  end
+end
