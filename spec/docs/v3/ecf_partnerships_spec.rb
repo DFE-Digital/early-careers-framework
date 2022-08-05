8b9793700570bbc9accf3feaaed388ac348b41a2
Phil Lee <asmega@users.noreply.github.com>
2022-08-05 16:13:14 +0100

    docs for api v3 create partnership

--- spec/docs/v3/ecf_partnerships_spec.rb
+++ spec/docs/v3/ecf_partnerships_spec.rb
@@ -24,4 +24,34 @@ RSpec.describe "API", type: :request, swagger_doc: "v3/api_spec.json" do
       end
     end
   end
+
+  path "/api/v3/partnerships/ecf" do
+    post "Create an ECF partnership with a school and delivery partner" do
+      operationId :partnerships_ecf_post
+      tags "ECF partnerships"
+      security [bearerAuth: []]
+
+      request_body content: {
+        "application/json": {
+          "schema": {
+            "$ref": "#/components/schemas/ECFPartnershipRequest",
+          },
+        },
+      }
+
+      response "200", "Create an ECF partnership" do
+        schema({ "$ref": "#/components/schemas/ECFPartnershipResponse" })
+
+        run_test!
+      end
+
+      response "401", "Unauthorized" do
+        let(:Authorization) { "Bearer invalid" }
+
+        schema({ "$ref": "#/components/schemas/UnauthorisedResponse" })
+
+        run_test!
+      end
+    end
+  end
 end
