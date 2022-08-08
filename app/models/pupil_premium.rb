1331eb6df80410404b879d654cf78848da76f909
Peter Yates <peter.yates@graphia.co.uk>
2022-08-08 16:08:12 +0100

    Make PupilPremium scopes composable
    Take a complex SQL scope and split it into smaller chunks that can be
    combined to form the orignal. This lets us test the parts individually
    and potentially reuse scopes elsewhere.

--- app/models/pupil_premium.rb
+++ app/models/pupil_premium.rb
@@ -9,10 +9,21 @@ class PupilPremium < ApplicationRecord
     percentage_eligible >= THRESHOLD_PERCENTAGE
   end
 
-  scope :only_with_uplift, lambda { |start_year|
-    where(start_year:)
-      .where("total_pupils > 0 AND (CAST(eligible_pupils AS float) / total_pupils) * 100 >= ?", THRESHOLD_PERCENTAGE)
-  }
+  scope :with_pupils, -> { where(arel_table[:total_pupils].gt(0)) }
+  scope :with_start_year, ->(start_year) { where(start_year:) }
+  scope :only_with_uplift, ->(start_year) { with_pupils.merge(with_start_year(start_year)).merge(exceeding_percentage) }
+
+  def self.exceeding_percentage(threshold: THRESHOLD_PERCENTAGE)
+    eligible_pupils = Arel::Nodes::NamedFunction.new("CAST", [arel_table[:eligible_pupils].as("FLOAT")])
+    total_pupils = arel_table[:total_pupils]
+
+    where(
+      Arel::Nodes::Multiplication.new(
+        Arel::Nodes::Division.new(eligible_pupils, total_pupils),
+        100,
+      ).gteq(threshold),
+    )
+  end
 
 private
 
