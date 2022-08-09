a2e1691750943f9f44198cfa82873f77ed947887
Phil Lee <asmega@users.noreply.github.com>
2022-08-09 14:19:20 +0100

    api v3 docs: NPQ outcomes now filterable

--- spec/docs/v3/npq_outcomes_spec.rb
+++ spec/docs/v3/npq_outcomes_spec.rb
@@ -3,11 +3,27 @@
 require "swagger_helper"
 
 RSpec.describe "API", type: :request, swagger_doc: "v3/api_spec.json" do
-  path "/api/v3/outcomes/npq/{id}" do
+  path "/api/v3/outcomes/npq" do
     get "Retrieve NPQ outcomes for the specified participant" do
       operationId :npq_outcome_get
       tags "outcomes"
       security [bearerAuth: []]
+
+      parameter name: :filter,
+                in: :query,
+                schema: {
+                  "$ref": "#/components/schemas/NPQOutcomesFilter",
+                },
+                type: :object,
+                style: :deepObject,
+                explode: true,
+                required: true,
+                description: "Refine NPQ outcomes to return.",
+                example: CGI.unescape({
+                  filter: {
+                    participant_id: "dcb52af5-e2c9-4f30-8138-60e7dfb54e36",
+                  },
+                }.to_param)
 
       response "200", "Successfully return NPQ outcomes" do
         schema({ "$ref": "#/components/schemas/NPQOutcomesResponse" })
