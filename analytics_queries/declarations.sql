efb91926b67da3d74072d1814692a51a25b56f48
Peter Yates <peter.yates@graphia.co.uk>
2022-08-09 16:41:17 +0100

    Add Makefile to ease analytics exporting
    We're also now moving the `\copy` statement into the individual SQL
    files so they can simply be processed by make into a useable format
    (`\copy` doesn't work with SQL files that contain newlines)

--- analytics_queries/declarations.sql
+++ analytics_queries/declarations.sql
@@ -1,11 +1,13 @@
-SELECT pd.id,
-       pd.user_id,
-       pd.declaration_type,
-       pd.declaration_date,
-       (pd.state = 'voided') as voided,
-       pd.evidence_held,
-       pd.state,
-       pds.state_reason
-FROM participant_declarations pd
-LEFT JOIN declaration_states pds on pd.id = pds.participant_declaration_id and pd.state = pds.state
-WHERE course_identifier in ('ecf-induction', 'ecf-mentor');
+\copy (
+    SELECT pd.id,
+          pd.user_id,
+          pd.declaration_type,
+          pd.declaration_date,
+          (pd.state = 'voided') as voided,
+          pd.evidence_held,
+          pd.state,
+          pds.state_reason
+    FROM participant_declarations pd
+    LEFT JOIN declaration_states pds on pd.id = pds.participant_declaration_id and pd.state = pds.state
+    WHERE course_identifier in ('ecf-induction', 'ecf-mentor')
+) to '/tmp/exports/declarations.csv' with csv header;
--- analytics_queries/participants.sql
+++ analytics_queries/participants.sql
@@ -1,42 +1,44 @@
-SELECT u.id                                                    as participant_id,
-       pi.external_identifier                                  as external_id,
-       pp.created_at                                           as added_at,
-       sch.urn                                                 as school_urn,
-       u.full_name                                             as name,
-       u.email                                                 as email,
-       SPLIT_PART(pp.type, '::', 2)                            as type,
-       (SELECT u2.id
-        FROM participant_profiles pp2
-                 JOIN teacher_profiles tp2 on pp2.teacher_profile_id = tp2.id
-                 JOIN users u2 on tp2.user_id = u2.id
-        WHERE pp2.id = pp.mentor_profile_id)                   as mentor_id,
-       c.start_year                                            as cohort,
-       pp.status                                               as status,
-       pp.training_status                                      as training_status,
-       pps.reason                                              as training_status_reason,
-       s.name                                                  as schedule,
-       s.schedule_identifier                                   as schedule_identifier,
-       tp.trn                                                  as trn,
-       epvd.created_at                                         as trn_provided_at,
-       (epe.status IN ('eligible', 'matched'))                 as trn_validated,
-       epe.reason                                              as trn_validated_reason,
-       (epe.manually_validated OR epe.status = 'manual_check') as manual_validation_required,
-       (CASE
-            WHEN epe.status = 'eligible' THEN true
-            WHEN epe.status = 'ineligible' THEN false
-           END)                                                as eligible_for_funding
-FROM participant_profiles pp
-         JOIN school_cohorts sc on pp.school_cohort_id = sc.id
-         JOIN cohorts c on sc.cohort_id = c.id
-         JOIN schools sch on sc.school_id = sch.id
-         JOIN schedules s on pp.schedule_id = s.id
-         JOIN teacher_profiles tp on pp.teacher_profile_id = tp.id
-         JOIN users u on tp.user_id = u.id
-         JOIN participant_identities pi on pp.participant_identity_id = pi.id
-         LEFT OUTER JOIN ecf_participant_validation_data epvd on pp.id = epvd.participant_profile_id
-         LEFT OUTER JOIN ecf_participant_eligibilities epe on pp.id = epe.participant_profile_id
-         LEFT OUTER JOIN participant_profile_states pps on pp.id = pps.participant_profile_id AND pps.id = (
-          SELECT id from participant_profile_states _pps WHERE _pps.participant_profile_id = pp.id AND _pps.state = pp.training_status ORDER BY created_at desc LIMIT 1
-         )
-WHERE pp.type IN ('ParticipantProfile::ECT', 'ParticipantProfile::Mentor')
-  AND c.start_year > 2020;
+\copy (
+    SELECT u.id                                                    as participant_id,
+          pi.external_identifier                                  as external_id,
+          pp.created_at                                           as added_at,
+          sch.urn                                                 as school_urn,
+          u.full_name                                             as name,
+          u.email                                                 as email,
+          SPLIT_PART(pp.type, '::', 2)                            as type,
+          (SELECT u2.id
+            FROM participant_profiles pp2
+                    JOIN teacher_profiles tp2 on pp2.teacher_profile_id = tp2.id
+                    JOIN users u2 on tp2.user_id = u2.id
+            WHERE pp2.id = pp.mentor_profile_id)                   as mentor_id,
+          c.start_year                                            as cohort,
+          pp.status                                               as status,
+          pp.training_status                                      as training_status,
+          pps.reason                                              as training_status_reason,
+          s.name                                                  as schedule,
+          s.schedule_identifier                                   as schedule_identifier,
+          tp.trn                                                  as trn,
+          epvd.created_at                                         as trn_provided_at,
+          (epe.status IN ('eligible', 'matched'))                 as trn_validated,
+          epe.reason                                              as trn_validated_reason,
+          (epe.manually_validated OR epe.status = 'manual_check') as manual_validation_required,
+          (CASE
+                WHEN epe.status = 'eligible' THEN true
+                WHEN epe.status = 'ineligible' THEN false
+              END)                                                as eligible_for_funding
+    FROM participant_profiles pp
+            JOIN school_cohorts sc on pp.school_cohort_id = sc.id
+            JOIN cohorts c on sc.cohort_id = c.id
+            JOIN schools sch on sc.school_id = sch.id
+            JOIN schedules s on pp.schedule_id = s.id
+            JOIN teacher_profiles tp on pp.teacher_profile_id = tp.id
+            JOIN users u on tp.user_id = u.id
+            JOIN participant_identities pi on pp.participant_identity_id = pi.id
+            LEFT OUTER JOIN ecf_participant_validation_data epvd on pp.id = epvd.participant_profile_id
+            LEFT OUTER JOIN ecf_participant_eligibilities epe on pp.id = epe.participant_profile_id
+            LEFT OUTER JOIN participant_profile_states pps on pp.id = pps.participant_profile_id AND pps.id = (
+              SELECT id from participant_profile_states _pps WHERE _pps.participant_profile_id = pp.id AND _pps.state = pp.training_status ORDER BY created_at desc LIMIT 1
+            )
+    WHERE pp.type IN ('ParticipantProfile::ECT', 'ParticipantProfile::Mentor')
+      AND c.start_year > 2020
+) to '/tmp/exports/participants.csv' with csv header;
