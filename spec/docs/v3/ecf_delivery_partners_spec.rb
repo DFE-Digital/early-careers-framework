be2f19497ed3458b449e70e59fb1acaa21c50457
Phil Lee <asmega@users.noreply.github.com>
2022-08-10 12:53:29 +0100

    api v3 docs: filter delivery partners by cohort

--- spec/docs/v3/ecf_delivery_partners_spec.rb
+++ spec/docs/v3/ecf_delivery_partners_spec.rb
@@ -9,6 +9,22 @@ RSpec.describe "API", type: :request, swagger_doc: "v3/api_spec.json" do
       tags "delivery partners"
       security [bearerAuth: []]
 
+      parameter name: :filter,
+                in: :query,
+                schema: {
+                  "$ref": "#/components/schemas/DeliveryPartnersFilter",
+                },
+                type: :object,
+                style: :deepObject,
+                explode: true,
+                required: false,
+                description: "Refine delivery partners to return.",
+                example: CGI.unescape({
+                  filter: {
+                    cohort: "2021",
+                  },
+                }.to_param)
+
       response "200", "Successfully return a list of delivery partners" do
         schema({ "$ref": "#/components/schemas/DeliveryPartnersResponse" })
 
