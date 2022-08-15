475b4d0c9d2da6740e1673f14268b0849e8634eb
Phil Lee <asmega@users.noreply.github.com>
2022-08-15 11:59:27 +0100

    api v3 docs: remove shared examples

--- spec/docs/v3/participants_ecf_spec.rb
+++ spec/docs/v3/participants_ecf_spec.rb
@@ -2,11 +2,7 @@
 
 require "swagger_helper"
 
-require_relative "../../shared/context/lead_provider_profiles_and_courses"
-
 describe "API", type: :request, swagger_doc: "v3/api_spec.json" do
-  include_context "lead provider profiles and courses"
-
   let(:token) { LeadProviderApiToken.create_with_random_token!(cpd_lead_provider:) }
   let(:bearer_token) { "Bearer #{token}" }
   let(:Authorization) { bearer_token }
@@ -335,18 +331,56 @@ describe "API", type: :request, swagger_doc: "v3/api_spec.json" do
     end
   end
 
-  it_behaves_like "JSON Participant Change schedule documentation",
-                  "/api/v3/participants/ecf/{id}/change-schedule",
-                  "#/components/schemas/ECFParticipantChangeScheduleRequest",
-                  "#/components/schemas/ECFParticipantResponse",
-                  "ECF Participant",
-                  :with_default_schedules do
-    let(:participant) { mentor_profile }
-    let(:attributes) do
-      {
-        schedule_identifier: "ecf-standard-september",
-        course_identifier: "ecf-mentor",
+  path "/api/v3/participants/ecf/{id}/change-schedule" do
+    put "Notify that an ECF Participant is changing training schedule" do
+      operationId "ecf_participant_change_schedule"
+      tags "ECF Participant"
+      security [bearerAuth: []]
+      consumes "application/json"
+
+      request_body content: {
+        "application/json": {
+          "schema": {
+            "$ref": "#/components/schemas/ECFParticipantChangeScheduleRequest",
+          },
+        },
       }
+
+      parameter name: :id,
+                in: :path,
+                type: :string,
+                required: true,
+                example: "28c461ee-ffc0-4e56-96bd-788579a0ed75",
+                description: "The ID of the participant"
+
+      parameter name: :params,
+                in: :body,
+                type: :object,
+                style: :deepObject,
+                required: true,
+                schema: {
+                  "$ref": "#/components/schemas/ECFParticipantChangeScheduleRequest",
+                }
+
+      response "200", "The ECF Participant changing schedule" do
+        let(:participant) { mentor_profile }
+        let(:id) { participant.user.id }
+        let(:params) do
+          {
+            "data": {
+              "type": "participant",
+              "attributes": {
+                schedule_identifier: "ecf-standard-september",
+                course_identifier: "ecf-mentor",
+              },
+            },
+          }
+        end
+
+        schema({ "$ref": "#/components/schemas/ECFParticipantResponse" })
+
+        run_test!
+      end
     end
   end
 end
