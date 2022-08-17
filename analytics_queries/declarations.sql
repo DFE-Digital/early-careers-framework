0abd12357d27fa81a2a90e045f95ab8f581d3b01
Peter Yates <peter.yates@graphia.co.uk>
2022-08-17 11:45:34 +0100

    Add cpd_lead_provider_name as per Nathan's request

--- analytics_queries/declarations.sql
+++ analytics_queries/declarations.sql
@@ -6,8 +6,10 @@
           (pd.state = 'voided') as voided,
           pd.evidence_held,
           pd.state,
-          pds.state_reason
+          pds.state_reason,
+          clp.name as cpd_lead_provider_name
     FROM participant_declarations pd
     LEFT JOIN declaration_states pds on pd.id = pds.participant_declaration_id and pd.state = pds.state
+    LEFT JOIN cpd_lead_providers clp on pd.cpd_lead_provider_id = clp.id
     WHERE course_identifier in ('ecf-induction', 'ecf-mentor')
 ) to '/tmp/exports/declarations.csv' with csv header;
