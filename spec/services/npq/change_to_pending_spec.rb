da62f9078519416a636cd37284c193fc5817afa9
Yann Marquet <yann.marquet@digital.education.gov.uk>
2022-08-17 17:19:43 +0200

    [CPDLP-1405] - fix spec

--- spec/services/npq/change_to_pending_spec.rb
+++ spec/services/npq/change_to_pending_spec.rb
@@ -2,45 +2,24 @@
 
 require "rails_helper"
 
-RSpec.describe NPQ::ChangeToPending do
-  let!(:cohort) { create :cohort }
-  let!(:schedule) { create :npq_leadership_schedule, cohort: }
-  let(:npq_course) { create :npq_course, identifier: "npq-senior-leadership" }
-  let(:npq_lead_provider) { create :npq_lead_provider }
+RSpec.describe NPQ::ChangeToPending, :with_default_schedules do
+  let(:cpd_lead_provider) { create(:cpd_lead_provider, :with_npq_lead_provider) }
+  let(:npq_lead_provider) { cpd_lead_provider.npq_lead_provider }
 
-  let!(:npq_application) do
-    create(
-      :npq_application,
-      application_status,
-      npq_lead_provider:,
-      npq_course:,
-      cohort:,
-    )
-  end
+  let(:npq_application) { create(:npq_application, :eligible_for_funding, application_status, npq_lead_provider:) }
+  let(:participant_profile) { npq_application.profile }
 
-  let!(:participant_profile) { npq_application.profile }
+  let(:participant_declaration) { create(:npq_participant_declaration, participant_profile:) }
 
-  let!(:participant_declaration) do
-    create(
-      :npq_participant_declaration,
-      declaration_type: "started",
-      user: npq_application.user,
-      participant_profile:,
-      course_identifier: npq_course.identifier,
-      state: "submitted",
-    )
-  end
-
-  subject do
-    described_class.new(npq_application:)
-  end
+  subject { described_class.new(npq_application:) }
 
   describe "#call" do
     context "when application has already been accepted" do
       let(:application_status) { :accepted }
 
-      it "changes to pending" do
+      it "changes to pending", :aggregate_failures do
         subject.call
+
         npq_application.reload
         expect(npq_application).to be_pending
         expect(npq_application.profile).to be_nil
@@ -48,11 +27,13 @@ RSpec.describe NPQ::ChangeToPending do
     end
 
     context "when application has already been rejected" do
-      let(:application_status) { :rejected }
-      let!(:participant_profile) { create(:npq_participant_profile, npq_application:) }
+      let(:application_status) { :accepted }
+
+      before { described_class.new(npq_application:).call }
 
       it "changes to pending" do
         subject.call
+
         npq_application.reload
         expect(npq_application).to be_pending
         expect(npq_application.profile).to be_nil
@@ -63,19 +44,11 @@ RSpec.describe NPQ::ChangeToPending do
     %w[eligible payable paid awaiting_clawback].each do |dec_state|
       context "accepted application with #{dec_state} declaration" do
         let(:application_status) { :accepted }
-        let!(:participant_declaration) do
-          create(
-            :npq_participant_declaration,
-            declaration_type: "started",
-            user: npq_application.user,
-            participant_profile:,
-            course_identifier: npq_course.identifier,
-            state: dec_state,
-          )
-        end
+        let!(:participant_declaration) { create(:npq_participant_declaration, dec_state, participant_profile:) }
 
-        it "returns error" do
+        it "returns error", :aggregate_failures do
           subject.call
+
           npq_application.reload
           expect(npq_application).to be_accepted
           expect(npq_application.errors[:lead_provider_approval_status]).to include("There are already declarations for this participant on this course, please ask provider to void and/or clawback any declarations they have made before attempting to reset the application.")
@@ -87,19 +60,11 @@ RSpec.describe NPQ::ChangeToPending do
     %w[submitted voided ineligible].each do |dec_state|
       context "accepted application with #{dec_state} declaration" do
         let(:application_status) { :accepted }
-        let!(:participant_declaration) do
-          create(
-            :npq_participant_declaration,
-            declaration_type: "started",
-            user: npq_application.user,
-            participant_profile:,
-            course_identifier: npq_course.identifier,
-            state: dec_state,
-          )
-        end
+        let!(:participant_declaration) { create(:npq_participant_declaration, dec_state, participant_profile:) }
 
-        it "changes to pending" do
+        it "changes to pending", :aggregate_failures do
           subject.call
+
           npq_application.reload
           expect(npq_application).to be_pending
           expect(npq_application.errors[:lead_provider_approval_status]).to_not be_present
