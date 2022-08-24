29fe681b7f71c294a5c18cf543b74118caf2aa19
Peter Yates <peter.yates@graphia.co.uk>
2022-08-24 11:49:15 +0100

    Add participant transfer notification service
    The Schools::TransferringParticipantsController is a bit heavy and
    contains lots of business logic.
    
    Here I've moved the email-specific code to its own class that can be
    easily tested.

---
+++ spec/services/induction/send_notification_emails_spec.rb
@@ -0,0 +1,129 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe(Induction::SendNotificationEmails) do
+  subject { Induction::SendNotificationEmails.new(**kwargs) }
+
+  let(:provider_mailer) { double(ActionMailer::MessageDelivery, deliver_later: true) }
+  let(:participant_mailer) { double(ActionMailer::MessageDelivery, deliver_later: true) }
+
+  before do
+    allow(ParticipantTransferMailer).to receive(template).and_return(provider_mailer)
+    allow(ParticipantTransferMailer).to receive(:participant_transfer_in_notification).and_return(participant_mailer)
+  end
+
+  shared_examples "notifying the participant" do
+    it "sends a 'particicpant transfter in notification' email to the participant" do
+      expect(ParticipantTransferMailer).to(have_received(:participant_transfer_in_notification).with(induction_record:))
+    end
+  end
+
+  # defaults
+  let(:template) { :provider_transfer_in_notification }
+  let(:induction_record) { create(:induction_record) }
+  let(:was_withdrawn_participant) { false }
+  let(:same_delivery_partner) { false }
+  let(:same_provider) { false }
+  let(:switch_to_schools_programme) { false }
+  let(:current_lead_provider_profiles) { create_list(:lead_provider_profile, 3) }
+  let(:target_lead_provider_profiles) { create_list(:lead_provider_profile, 2) }
+
+  let(:kwargs) do
+    {
+      induction_record:,
+      was_withdrawn_participant:,
+      same_delivery_partner:,
+      same_provider:,
+      switch_to_schools_programme:,
+      current_lead_provider_profiles:,
+      target_lead_provider_profiles:,
+    }
+  end
+
+  before { subject.call }
+
+  %i[
+    induction_record
+    was_withdrawn_participant
+    same_delivery_partner
+    same_provider
+    switch_to_schools_programme
+    current_lead_provider_profiles
+    target_lead_provider_profiles
+    was_withdrawn_participant?
+    same_delivery_partner?
+    same_provider?
+    switch_to_schools_programme?
+  ].each { |a| it { is_expected.to respond_to(a) } }
+
+  context "when the participant has been withdrawn" do
+    let(:template) { :provider_transfer_in_notification }
+    let(:was_withdrawn_participant) { true }
+
+    it "sends 'provider transfer in notification' emails to the current lead provider profiles " do
+      current_lead_provider_profiles.each do |lead_provider_profile|
+        expect(ParticipantTransferMailer).to(have_received(template).with(induction_record:, lead_provider_profile:))
+      end
+
+      expect(provider_mailer).to have_received(:deliver_later).exactly(current_lead_provider_profiles.size).times
+    end
+  end
+
+  context "when the lead provider and delivery partner match" do
+    let(:template) { :provider_existing_school_transfer_notification }
+    let(:same_delivery_partner) { true }
+    let(:same_provider) { true }
+
+    it "sends 'provider transfer in notification' emails to the current lead provider profiles " do
+      current_lead_provider_profiles.each do |lead_provider_profile|
+        expect(ParticipantTransferMailer).to(have_received(template).with(induction_record:, lead_provider_profile:))
+      end
+
+      expect(provider_mailer).to have_received(:deliver_later).exactly(current_lead_provider_profiles.size).times
+    end
+
+    include_examples "notifying the participant"
+  end
+
+  context "when it's a switch to schools programme" do
+    let(:template) { :provider_existing_school_transfer_notification }
+    let(:switch_to_schools_programme) { true }
+
+    describe "in" do
+      let(:template) { :provider_transfer_in_notification }
+
+      it "sends 'provider transfer in notification' emails to the current lead provider profiles " do
+        current_lead_provider_profiles.each do |lead_provider_profile|
+          expect(ParticipantTransferMailer).to(have_received(template).with(induction_record:, lead_provider_profile:))
+        end
+      end
+    end
+
+    describe "out" do
+      let(:template) { :provider_transfer_out_notification }
+
+      it "sends 'provider transfer out notification' emails to the target lead provider profiles " do
+        target_lead_provider_profiles.each do |lead_provider_profile|
+          expect(ParticipantTransferMailer).to(have_received(template).with(induction_record:, lead_provider_profile:))
+        end
+      end
+    end
+
+    include_examples "notifying the participant"
+  end
+
+  context "when it's a new school transfer" do
+    let(:template) { :provider_new_school_transfer_notification }
+
+    it "sends 'provider transfer in notification' emails to the current lead provider profiles " do
+      target_lead_provider_profiles.each do |lead_provider_profile|
+        expect(ParticipantTransferMailer).to(have_received(template).with(induction_record:, lead_provider_profile:))
+      end
+
+      expect(provider_mailer).to have_received(:deliver_later).exactly(target_lead_provider_profiles.size).times
+    end
+
+    include_examples "notifying the participant"
+  end
+end
