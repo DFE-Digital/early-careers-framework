466b22d49315b8bc35783cbadd8f409267af75aa
Phil Lee <asmega@users.noreply.github.com>
2022-08-24 12:32:42 +0100

    statement selector supports multi cohort

--- app/controllers/finance/ecf/participant_declarations/voided_controller.rb
+++ app/controllers/finance/ecf/participant_declarations/voided_controller.rb
@@ -7,7 +7,7 @@ module Finance
         def show
           @ecf_lead_provider = LeadProvider.find(params[:payment_breakdown_id])
           @cpd_lead_provider = @ecf_lead_provider.cpd_lead_provider
-          @statement = @ecf_lead_provider.statements.find_by_humanised_name(params[:statement_id])
+          @statement = @ecf_lead_provider.statements.find(params[:statement_id])
           @voided_declarations = @statement.participant_declarations.voided
         end
       end
