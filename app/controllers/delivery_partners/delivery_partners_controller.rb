99d80e05ea59764ffc5c44944efd81af6c616152
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2022-09-23 15:38:50 +0100

    [CPDLP-1580] Access management - Switch between deliver partners

--- app/controllers/delivery_partners/delivery_partners_controller.rb
+++ app/controllers/delivery_partners/delivery_partners_controller.rb
@@ -4,19 +4,17 @@ module DeliveryPartners
   class DeliveryPartnersController < BaseController
     layout "application"
 
-    before_action :set_choose_organisation_form
-
     def index
-      if @choose_organisation_form.only_one
-        redirect_to delivery_partner_participants_path(@choose_organisation_form.delivery_partner)
+      if choose_organisation_form.only_one
+        redirect_to delivery_partner_participants_path(choose_organisation_form.delivery_partner)
       end
     end
 
     def create
-      @choose_organisation_form.assign_attributes(choose_organisation_form_params)
+      choose_organisation_form.assign_attributes(choose_organisation_form_params)
 
-      if @choose_organisation_form.valid?
-        redirect_to delivery_partner_participants_path(@choose_organisation_form.delivery_partner)
+      if choose_organisation_form.valid?
+        redirect_to delivery_partner_participants_path(choose_organisation_form.delivery_partner)
       else
         render :index
       end
@@ -24,8 +22,8 @@ module DeliveryPartners
 
   private
 
-    def set_choose_organisation_form
-      @choose_organisation_form = ChooseOrganisationForm.new(user: current_user)
+    def choose_organisation_form
+      @choose_organisation_form ||= ChooseOrganisationForm.new(user: current_user)
     end
 
     def choose_organisation_form_params
--- spec/features/delivery_partners/delivery_partners_spec.rb
+++ spec/features/delivery_partners/delivery_partners_spec.rb
@@ -3,6 +3,11 @@
 require "rails_helper"
 
 RSpec.feature "Delivery partner choose organisations page", type: :feature do
+  let(:delivery_partner_user) { create(:user) }
+  let(:delivery_partner_profile1) { create(:delivery_partner_profile, user: delivery_partner_user) }
+  let(:delivery_partner_profile2) { create(:delivery_partner_profile, user: delivery_partner_user) }
+  let(:delivery_partner_profile3) { create(:delivery_partner_profile, user: delivery_partner_user) }
+
   scenario "User with one delivery partner" do
     given_user_with_one_delivery_partner_exists
     and_i_am_logged_in_as_delivery_partner_user
@@ -10,7 +15,7 @@ RSpec.feature "Delivery partner choose organisations page", type: :feature do
     when_i_visit_the_delivery_partners_page
 
     then_i_see("Participants")
-    and_i_see(@delivery_partner_profile1.delivery_partner.name)
+    and_i_see(delivery_partner_profile1.delivery_partner.name)
   end
 
   scenario "User with multiple delivery partners" do
@@ -22,27 +27,25 @@ RSpec.feature "Delivery partner choose organisations page", type: :feature do
     then_i_see("What organisation do you want to view?")
     and_i_see_multiple_delivery_partners
 
-    when_i_choose(@delivery_partner_profile2.delivery_partner.name)
+    when_i_choose(delivery_partner_profile2.delivery_partner.name)
     and_i_click_on("Continue")
 
     then_i_see("Participants")
-    and_i_see(@delivery_partner_profile2.delivery_partner.name)
+    and_i_see(delivery_partner_profile2.delivery_partner.name)
   end
 
   def given_user_with_one_delivery_partner_exists
-    @delivery_partner_user = create(:user)
-    @delivery_partner_profile1 = create(:delivery_partner_profile, user: @delivery_partner_user)
+    delivery_partner_profile1
   end
 
   def given_user_with_multiple_delivery_partner_exists
-    @delivery_partner_user = create(:user)
-    @delivery_partner_profile1 = create(:delivery_partner_profile, user: @delivery_partner_user)
-    @delivery_partner_profile2 = create(:delivery_partner_profile, user: @delivery_partner_user)
-    @delivery_partner_profile3 = create(:delivery_partner_profile, user: @delivery_partner_user)
+    delivery_partner_profile1
+    delivery_partner_profile2
+    delivery_partner_profile3
   end
 
   def and_i_am_logged_in_as_delivery_partner_user
-    sign_in_as(@delivery_partner_user)
+    sign_in_as(delivery_partner_user)
   end
 
   def when_i_visit_the_delivery_partners_page
@@ -56,9 +59,9 @@ RSpec.feature "Delivery partner choose organisations page", type: :feature do
   alias_method :and_i_see, :then_i_see
 
   def and_i_see_multiple_delivery_partners
-    expect(page).to have_content(@delivery_partner_profile1.delivery_partner.name)
-    expect(page).to have_content(@delivery_partner_profile2.delivery_partner.name)
-    expect(page).to have_content(@delivery_partner_profile3.delivery_partner.name)
+    expect(page).to have_css(".govuk-radios__item", text: delivery_partner_profile1.delivery_partner.name)
+    expect(page).to have_css(".govuk-radios__item", text: delivery_partner_profile2.delivery_partner.name)
+    expect(page).to have_css(".govuk-radios__item", text: delivery_partner_profile3.delivery_partner.name)
   end
 
   def when_i_choose(string)
