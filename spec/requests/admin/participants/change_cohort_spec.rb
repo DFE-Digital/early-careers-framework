a8a8bc75d4ce56113edde1a1e3ab6ce07d462fff
Peter Yates <peter.yates@graphia.co.uk>
2022-09-23 11:07:07 +0100

    Add some more tests covering change cohort page
    Nothing in depth here, just ensuring that #edit renders the form
    and #update interacts with the service object.

--- spec/requests/admin/participants/change_cohort_spec.rb
+++ spec/requests/admin/participants/change_cohort_spec.rb
@@ -15,10 +15,16 @@ RSpec.describe "Admin::Participants", :with_default_schedules, type: :request do
 
     it "renders the edit form for a participant's cohort" do
       expect(response).to render_template("admin/participants/change_cohort/edit")
+      expect(response.body).to match(%r{<form.*action="/admin/participants/#{mentor_profile.id}/change_cohort"})
     end
 
     it "has the correct heading" do
-      expect(response.body).to match(/Select #{mentor_profile.user.full_name}.* new cohort/)
+      expect(response.body).to match(/Change #{mentor_profile.user.full_name}.* cohort/)
+    end
+
+    it "has a form with a select element and button" do
+      expect(response.body).to match(/<select.*"induction_amend_participant_cohort\[target_cohort_start_year\]/)
+      expect(response.body).to match(/<button.*class="govuk-button"/)
     end
   end
 
@@ -26,6 +32,7 @@ RSpec.describe "Admin::Participants", :with_default_schedules, type: :request do
     let(:params) { { induction_amend_participant_cohort: { target_cohort_start_year: 2022 } } }
 
     it "initializes an Induction::AmendParticipantCohort" do
+      expect_any_instance_of(Induction::AmendParticipantCohort).to receive(:save).and_return(true)
       allow(Induction::AmendParticipantCohort).to receive(:new).and_call_original
 
       put("/admin/participants/#{mentor_profile.id}/change_cohort", params:)
@@ -35,10 +42,18 @@ RSpec.describe "Admin::Participants", :with_default_schedules, type: :request do
         target_cohort_start_year: "2022", # string because this one is passed in from the form
         email: mentor_profile.user.email,
       )
+
+      expect(response).to redirect_to(admin_participant_path(mentor_profile))
     end
 
     context "when there is an error" do
-      it "displays the error summary and re-renders :edit"
+      let(:params) { { induction_amend_participant_cohort: { target_cohort_start_year: "a bad value" } } }
+
+      it "displays the error summary and re-renders :edit" do
+        put("/admin/participants/#{mentor_profile.id}/change_cohort", params:)
+
+        expect(response.body).to include("Must be an integer")
+      end
     end
   end
 end
