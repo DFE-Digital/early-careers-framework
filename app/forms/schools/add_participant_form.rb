e148a1dd16aabaf0576da59181832de1515e5f7b
Fabio Volpe <volpefabio@outlook.com>
2022-09-28 16:11:33 +0100

    cannot add participant message differs between participant from same school or different school

--- app/forms/schools/add_participant_form.rb
+++ app/forms/schools/add_participant_form.rb
@@ -358,5 +358,9 @@ module Schools
     rescue ActiveRecord::RecordNotFound
       nil
     end
+
+    def existing_participant_profile_same_school?
+      existing_participant_profile&.school == school_cohort.school
+    end
   end
 end
