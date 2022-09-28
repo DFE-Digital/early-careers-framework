b072b115c6d3165a6915c04980241f5c7c1e276f
Lorenzo Tello <ltello8a@gmail.com>
2022-09-28 17:03:55 +0200

    CST-1062: add tab to display participant declarations to admins

--- app/controllers/admin/participants_controller.rb
+++ app/controllers/admin/participants_controller.rb
@@ -8,6 +8,7 @@ module Admin
     before_action :load_participant, except: :index
     before_action :historical_induction_records, only: :show, unless: -> { @participant_profile.npq? }
     before_action :latest_induction_record, only: :show, unless: -> { @participant_profile.npq? }
+    before_action :participant_declarations, only: :show, unless: -> { @participant_profile.npq? }
 
     def show; end
 
@@ -107,6 +108,12 @@ module Admin
 
     def latest_induction_record
       @latest_induction_record ||= induction_records.first
+    end
+
+    def participant_declarations
+      @participant_declarations ||= @participant_profile.participant_declarations
+                                                        .includes(:cpd_lead_provider, :delivery_partner)
+                                                        .order(created_at: :desc)
     end
   end
 end
