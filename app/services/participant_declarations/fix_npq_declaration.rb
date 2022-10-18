eaf98873ae8f5b977e2ebb757943e15889e7b630
Yann Marquet <yann.marquet@digital.education.gov.uk>
2022-10-18 19:04:22 +0200

    [CPDLP-1869] - Add service class to fix prod data

--- app/services/participant_declarations/fix_npq_declaration.rb
+++ app/services/participant_declarations/fix_npq_declaration.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module ParticipantDeclarations
   class FixNPQDeclaration
     def initialize(ecf_statement, npq_statement)
@@ -7,7 +9,7 @@ module ParticipantDeclarations
 
     def call(participant_declaration)
       ParticipantDeclaration.transaction do
-        participant_declaration.statement_line_items.first.update!(statement: npq_statement)
+        participant_declaration.statement_line_items.find_by(statement: ecf_statement).update!(statement: npq_statement)
         participant_declaration.update!(type: "ParticipantDeclaration::NPQ")
       end
     end
--- spec/services/participant_declarations/fix_npq_declaration_spec.rb
+++ spec/services/participant_declarations/fix_npq_declaration_spec.rb
@@ -1,29 +1,23 @@
+# frozen_string_literal: true
+
 require "rails_helper"
 
 RSpec.describe ParticipantDeclarations::FixNPQDeclaration, :with_default_schedules do
-  let(:cpd_lead_provider)       { create(:cpd_lead_provider, :with_lead_provider, :with_npq_lead_provider) }
-  let(:ecf_statement)           { create(:ecf_statement, cpd_lead_provider:) }
-  let(:npq_statement)           { create(:npq_statement, cpd_lead_provider:) }
-  let(:participant_declaration) { create(:ect_participant_declaration, :eligible, cpd_lead_provider:) }
+  let(:cpd_lead_provider)        { create(:cpd_lead_provider, :with_lead_provider, :with_npq_lead_provider) }
+  let(:ecf_statement)            { participant_declaration.statements.first }
+  let(:npq_statement)            { create(:npq_statement, :output_fee, cpd_lead_provider:) }
+  let!(:participant_declaration) { create(:ect_participant_declaration, :payable, cpd_lead_provider:) }
 
   before do
     travel_to ecf_statement.deadline_date { participant_declaration }
-
-    Statements::MarkAsPayable.new(ecf_statement).call
   end
 
   describe "#call" do
     it "moves the declaration to the NPQ statement and flip the type to NPQ" do
-      byebug
-      pp ecf_statement
-      pp npq_statement
-      pp participant_declaration.statement_line_items.first.statement.reload
-      expect do
+      expect {
         described_class.new(ecf_statement, npq_statement).call(participant_declaration)
-        pp Finance::StatementLineItem.all
-      end
-        .to change(participant_declaration.reload.statement_line_items.first, :statement_id).from(ecf_statement.id).to(npq_statement.id)
-        .and change(participant_declaration, :type).from("ParticipantDeclaration::ECF").to("ParticipantDeclaration::NPQ")
+      }.to change(ecf_statement.participant_declarations, :count).from(1).to(0)
+       .and change(npq_statement.participant_declarations, :count).from(0).to(1)
     end
   end
 end
