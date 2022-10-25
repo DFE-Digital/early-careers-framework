a036075d5f5ecfdbab480824f62630688317c952
Leonidas Apostolidis <leoapost@gmail.com>
2022-10-25 09:33:54 +0100

    Add model specs

--- spec/models/participant_identity_spec.rb
+++ spec/models/participant_identity_spec.rb
@@ -5,6 +5,13 @@ require "rails_helper"
 RSpec.describe ParticipantIdentity, type: :model do
   subject(:participant_identity) { create(:participant_identity) }
 
+  let(:user) { participant_identity.user }
+  let(:additional_identity) { create(:participant_identity, :secondary, user:, email: Faker::Internet.email) }
+  let(:identity_of_duplicate_user) { create(:ecf_participant_profile).participant_identity }
+  let(:transferred_identity) do
+    identity_of_duplicate_user.tap { |identity| identity.update!(user:) }
+  end
+
   it { is_expected.to belong_to(:user) }
   it { is_expected.to have_many(:participant_profiles) }
   it { is_expected.to have_many(:npq_applications) }
@@ -33,6 +40,39 @@ RSpec.describe ParticipantIdentity, type: :model do
         # address column is citext
         expect(described_class.email_matches("xyz").to_sql).to include("participant_identities.email like '%xyz%'")
       end
+    end
+
+    describe "#secondary" do
+      it "returns all the transferred and additional identities" do
+        expect(described_class.secondary).to include(additional_identity, transferred_identity)
+      end
+    end
+  end
+
+  describe "#original_identity?" do
+    it "returns true for the first identity of the participant" do
+      expect(participant_identity.original_identity?).to be true
+    end
+  end
+
+  describe "#secondary_identity?" do
+    it "returns true for any of the second identities" do
+      expect(additional_identity.secondary_identity?).to be true
+      expect(transferred_identity.secondary_identity?).to be true
+    end
+  end
+
+  describe "#additional_identity?" do
+    it "returns true when the identity has no participant profiles" do
+      expect(additional_identity.participant_profiles).to be_empty
+      expect(additional_identity.additional_identity?).to be true
+    end
+  end
+
+  describe "#transferred_identity?" do
+    it "returns true when the identity has participant profiles and the external_identifier does not matches the user id" do
+      expect(transferred_identity.participant_profiles).not_to be_empty
+      expect(transferred_identity.transferred_identity?).to be true
     end
   end
 end
