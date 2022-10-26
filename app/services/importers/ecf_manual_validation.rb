5e54054f191c04e4beb1d6248edb9950f9500bba
Tony Headford <tony@binarycircus.com>
2022-10-26 13:52:48 +0100

    Refactor validation importer

--- app/services/importers/ecf_manual_validation.rb
+++ app/services/importers/ecf_manual_validation.rb
@@ -2,53 +2,82 @@
 
 class Importers::ECFManualValidation < BaseService
   def call
-    check_headers
+    check_headers!
 
     rows.each do |row|
-      user = Identity.find_user_by(id: row["id"])
+      participant_profile = get_profile(row["id"]) || next
 
-      if user.nil?
-        puts "No profile found for #{row['id']}. Skipping"
-        next
-      end
-
-      participant_profile = user.teacher_profile.current_ecf_profile
-      if participant_profile.nil?
-        puts "No profile found for #{row['id']}. Skipping"
-        next
-      end
+      prepare_profile(participant_profile)
 
       Participants::ParticipantValidationForm.call(
         participant_profile,
-        save_validation_data_without_match: false,
+        save_validation_data_without_match: true,
         data: {
           trn: row["trn"],
           nino: row["nino"],
-          dob: Date.parse(row["dob"]),
+          date_of_birth: safe_parse(row["dob"]),
           full_name: row["name"],
         },
       )
 
-      if participant_profile.ecf_participant_eligibility.nil?
-        puts "No match found #{row['id']}"
-        next
-      end
-
-      unless participant_profile.ecf_participant_eligibility.eligible_status?
-        puts "#{participant_profile.ecf_participant_eligibility.reason} #{row['id']}"
+      if participant_profile.reload.ecf_participant_eligibility.nil?
+        logger.warn "No match found #{row['id']}"
+      elsif !participant_profile.ecf_participant_eligibility.eligible_status?
+        logger.warn "#{participant_profile.ecf_participant_eligibility.reason} #{row['id']}"
       end
     end
   end
 
 private
 
-  attr_reader :path_to_csv
+  attr_reader :path_to_csv, :logger
 
-  def initialize(path_to_csv:)
+  def initialize(path_to_csv:, logger: Rails.logger)
     @path_to_csv = path_to_csv
+    @logger = logger
   end
 
-  def check_headers
+  def get_profile(id)
+    user = Identity.find_user_by(id:)
+    if user.nil?
+      logger.warn "No profile found for #{id}. Skipping"
+      return
+    end
+
+    ecf_profiles = user.teacher_profile.ecf_profiles
+
+    if ecf_profiles.empty?
+      logger.warn "No profile found for #{id}. Skipping"
+      return
+    elsif ecf_profiles.count > 1
+      logger.warn "Multiple profiles found for #{id}. Skipping"
+      return
+    end
+
+    ecf_profiles.first
+  end
+
+  def prepare_profile(participant_profile)
+    # reset TRN if present on teacher profile to avoid "different trn" errors
+    # unless already set via NPQ
+    teacher_profile = participant_profile.teacher_profile
+    if teacher_profile.trn.present?
+      teacher_profile.update!(trn: nil) unless teacher_profile.participant_profiles.npqs.any?
+    end
+
+    # remove any existing eligibilty
+    participant_profile.ecf_participant_eligibility&.destroy
+  end
+
+  def safe_parse(date)
+    return if date.blank?
+    Date.parse(date)
+  rescue Date::Error
+    logger.warn "Error parsing date"
+    nil
+  end
+
+  def check_headers!
     unless %w[id name trn dob nino].all? { |header| rows.headers.include?(header) }
       raise NameError, "Invalid headers"
     end
---
+++ spec/services/importers/ecf_manual_validation_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+require "csv"
+
+RSpec.describe Importers::ECFManualValidation do
+  let(:example_csv_file) { file_fixture "example_ecf_manual_validation.csv" }
+
+  subject(:importer) { described_class }
+
+  describe ".call" do
+    context "when participants exist" do
+      before do
+        users = []
+        CSV.read(example_csv_file, headers: true).each { |row| users << create(:user, id: row["id"], full_name: row["name"]) }
+        users.each { |user| tp = create(:teacher_profile, user:); create(:ect_participant_profile, teacher_profile: tp) }
+      end
+
+      it "calls the validation service for each participant" do
+        expect(Participants::ParticipantValidationForm).to receive(:call).exactly(6).times
+        importer.call(path_to_csv: example_csv_file)
+      end
+
+      it "does handles bad or empty dates" do
+        expect(Participants::ParticipantValidationForm).to receive(:call).exactly(6).times
+        expect { 
+          importer.call(path_to_csv: example_csv_file)
+        }.not_to raise_error
+      end
+    end
+
+    context "when matching participants do not exist" do
+      it "does not call the validation service" do
+        expect(Participants::ParticipantValidationForm).not_to receive(:call)
+        importer.call(path_to_csv: example_csv_file)
+      end
+    end
+  end
+end
