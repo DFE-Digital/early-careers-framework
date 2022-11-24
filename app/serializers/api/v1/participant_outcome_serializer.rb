8878810ff8be2073bc7bf758aef16ed7d120cdf4
Pete Goddard <pete@artofsoftware.co.uk>
2022-11-24 14:47:09 +0000

    Introduce serialiser for outcome

---
+++ app/serializers/api/v1/participant_outcome_serializer.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require "jsonapi/serializer/instrumentation"
+
+module Api
+  module V1
+    class ParticipantOutcomeSerializer
+      include JSONAPI::Serializer
+      include JSONAPI::Serializer::Instrumentation
+
+      set_id :id
+      set_type :'participant-outcome'
+
+      attribute :completion_date do |outcome|
+        outcome.completion_date.rfc3339
+      end
+      attribute :course_identifier do |outcome|
+        outcome.participant_declaration.course_identifier
+      end
+
+      attribute :participant_id do |outcome|
+        outcome.participant_declaration.participant_profile.npq_application.participant_identity.user_id
+      end
+    end
+  end
+end
---
+++ spec/serializers/api/v1/participant_outcome_serializer_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Api::V1::ParticipantOutcomeSerializer, :with_default_schedules do
+  describe "#serializable_hash" do
+    let(:provider) { create :cpd_lead_provider, :with_npq_lead_provider }
+    let(:npq_application) { create :npq_application, :accepted, npq_lead_provider: provider.npq_lead_provider }
+    let(:declaration) { create :npq_participant_declaration, participant_profile: npq_application.profile, cpd_lead_provider: provider }
+    subject(:outcome) { create :participant_outcome, participant_declaration: declaration }
+
+    it "serialises to the correct structure" do
+      result = described_class.new(outcome).serializable_hash
+      expected = {
+        data: {
+          id: outcome.id,
+          type: :"participant-outcome",
+          attributes: {
+            completion_date: outcome.completion_date.rfc3339,
+            participant_id: npq_application.participant_identity.user_id,
+            course_identifier: declaration.course_identifier,
+          },
+        },
+      }
+      expect(result).to eql(expected)
+    end
+  end
+end
