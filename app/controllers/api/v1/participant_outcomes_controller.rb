4f9daac082eb1bb176f62b88380fda186f1c8a9b
Pete Goddard <pete@artofsoftware.co.uk>
2022-12-05 11:42:03 +0000

    Add endpoint

--- app/controllers/api/v1/participant_outcomes_controller.rb
+++ app/controllers/api/v1/participant_outcomes_controller.rb
@@ -11,6 +11,11 @@ module Api
         render json: participant_declarations_hash.to_json
       end
 
+      def show
+        participant_declarations_hash = serializer_class.new(query_scope).serializable_hash
+        render json: participant_declarations_hash.to_json
+      end
+
     private
 
       def serializer_class
@@ -20,11 +25,16 @@ module Api
       def query_scope
         ParticipantOutcomesQuery.new(
           cpd_lead_provider:,
+          participant_external_id:,
         ).scope
       end
 
       def cpd_lead_provider
         current_user
+      end
+
+      def participant_external_id
+        params[:id]
       end
     end
   end
