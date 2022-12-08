ac17fd0966f48b2df51b727e881609e3092d6ef0
Leandro Alemão <leandro.costantini@gmail.com>
2022-12-08 13:47:43 +0000

    [CPDLP-1686] Add specs for the service class

--- app/services/npq/create_participant_outcome.rb
+++ app/services/npq/create_participant_outcome.rb
@@ -11,8 +11,8 @@ module NPQ
     attribute :state
     attribute :completion_date, :date
 
-    validates :cpd_lead_provider, :completion_date, :participant_external_id, :course_identifier, presence: true
-    validates :completion_date, future_date: true
+    validates :cpd_lead_provider, :participant_external_id, presence: true
+    validates :completion_date, future_date: true, presence: { message: I18n.t(:missing_completion_date) }
     validates :course_identifier, course: true, presence: { message: I18n.t(:missing_course_identifier) }
     validates :state,
               presence: { message: I18n.t(:missing_state) },
