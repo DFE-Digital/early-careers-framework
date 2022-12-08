0b9e0d6f5fcc01182a99b10747016380b0752924
Leandro Alemão <leandro.costantini@gmail.com>
2022-12-08 00:57:00 +0000

    [CPDLP-1686] Add API endpoint to create an outcome for a participant

--- app/controllers/api/v1/participant_outcomes_controller.rb
+++ app/controllers/api/v1/participant_outcomes_controller.rb
@@ -10,6 +10,17 @@ module Api
         render json: participant_declarations_hash.to_json
       end
 
+      # Creates a new NPQ participant outcome
+      # Providers can create a NPQ participant declaration outcome for a NPQ participant via this endpoint
+      #
+      # POST /api/v1/participants/npq/:participant_id/outcomes
+      #
+      def create
+        service = NPQ::CreateParticipantOutcome.new(action_params)
+
+        render_from_service(service, serializer_class)
+      end
+
     private
 
       def serializer_class
@@ -28,7 +39,24 @@ module Api
       end
 
       def participant_external_id
-        params[:id]
+        params[:participant_id]
+      end
+
+      def action_params
+        HashWithIndifferentAccess.new(
+          cpd_lead_provider:,
+          participant_external_id:,
+        ).merge(permitted_params["attributes"] || {})
+      end
+
+      def permitted_params
+        params.require(:data).permit(:type, attributes: %i[course_identifier state completion_date])
+      rescue ActionController::ParameterMissing => e
+        if e.param == :data
+          raise ActionController::BadRequest, I18n.t(:invalid_data_structure)
+        else
+          raise
+        end
       end
     end
   end
