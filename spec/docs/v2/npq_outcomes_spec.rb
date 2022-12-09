01b690a9266be012b48b81b0747cbf6b1c304ca3
Leandro Alemão <leandro.costantini@gmail.com>
2022-12-09 12:34:16 +0000

    [CPDLP-1686] Update api v1/v2/v3 spec docs

--- spec/docs/v2/npq_outcomes_spec.rb
+++ spec/docs/v2/npq_outcomes_spec.rb
@@ -2,7 +2,7 @@
 
 require "swagger_helper"
 
-describe "API", :with_default_schedules, type: :request, swagger_doc: "v3/api_spec.json", with_feature_flags: { api_v3: "active" } do
+describe "API", :with_default_schedules, type: :request, swagger_doc: "v2/api_spec.json" do
   let(:cpd_lead_provider) { create(:cpd_lead_provider, :with_npq_lead_provider) }
   let(:token) { LeadProviderApiToken.create_with_random_token!(cpd_lead_provider:) }
   let(:Authorization) { "Bearer #{token}" }
@@ -10,40 +10,7 @@ describe "API", :with_default_schedules, type: :request, swagger_doc: "v3/api_sp
   let(:npq_declaration) { create(:npq_participant_declaration, npq_course:, cpd_lead_provider:) }
   let(:participant_profile) { npq_declaration.participant_profile }
 
-  path "/api/v3/participants/npq/{participant_id}/outcomes" do
-    get "Retrieve NPQ outcomes for the specified participant" do
-      operationId :npq_outcome_get
-      tags "NPQ outcomes"
-      security [bearerAuth: []]
-
-      parameter name: :participant_id,
-                description: "The unique ID of the participant",
-                in: :path,
-                required: true,
-                schema: {
-                  type: :string,
-                  format: :uuid,
-                },
-                example: "70885c85-f52b-45fe-b969-e09a93ffc6ee"
-
-      response "200", "Successfully return NPQ outcomes" do
-        let(:participant_id) { participant_profile.participant_identity.external_identifier }
-
-        schema({ "$ref": "#/components/schemas/NPQOutcomesResponse" })
-
-        run_test!
-      end
-
-      response "401", "Unauthorized" do
-        let(:Authorization) { "Bearer invalid" }
-        let(:participant_id) { participant_profile.participant_identity.external_identifier }
-
-        schema({ "$ref": "#/components/schemas/UnauthorisedResponse" })
-
-        run_test!
-      end
-    end
-
+  path "/api/v2/participants/npq/{participant_id}/outcomes" do
     post "Submit a NPQ outcome" do
       operationId :npq_outcome_post
       tags "NPQ outcomes"
