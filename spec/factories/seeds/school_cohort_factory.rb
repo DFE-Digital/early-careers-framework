5e0ef6cff545d2ca2c008a05f325e18a2f1febf1
Peter Yates <peter.yates@graphia.co.uk>
2022-12-12 16:01:42 +0000

    Split participant profile factories up
    Friends don't let friends use STI in Rails.

--- spec/factories/seeds/school_cohort_factory.rb
+++ spec/factories/seeds/school_cohort_factory.rb
@@ -2,14 +2,24 @@
 
 FactoryBot.define do
   factory(:seed_school_cohort, class: "SchoolCohort") do
-    school { association(:seed_school) }
-    cohort { association(:seed_cohort) }
     induction_programme_choice { "full_induction_programme" }
+
+    trait(:with_school) { school { association(:seed_school) } }
+    trait(:with_cohort) { cohort { association(:seed_cohort) } }
 
     trait(:cip) { induction_programme_choice { "core_induction_programme" } }
 
+    trait(:valid) do
+      with_school
+      with_cohort
+    end
+
     after(:build) do |sc|
-      Rails.logger.debug("seeded school cohort #{sc.cohort.start_year} for #{sc.school.name}")
+      if sc.cohort.present? && sc.school.present?
+        Rails.logger.debug("seeded school cohort #{sc.cohort.start_year} for #{sc.school.name}")
+      else
+        Rails.logger.debug("seeded incomplete school cohort")
+      end
     end
   end
 end
