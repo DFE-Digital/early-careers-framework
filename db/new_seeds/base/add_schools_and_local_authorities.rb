c4afaa2bfb011670b0eaae82515c00fdff2f6683
Peter Yates <peter.yates@graphia.co.uk>
2022-12-20 15:21:41 +0000

    Generate some school cohorts for generic schools

--- db/new_seeds/base/add_schools_and_local_authorities.rb
+++ db/new_seeds/base/add_schools_and_local_authorities.rb
@@ -3,11 +3,13 @@
 # create some local authorities
 local_authorities = FactoryBot.create_list(:local_authority, 10)
 
+cohorts = Cohort.where(start_year: [2021, 2022])
+
 # create some generic schools with:
 # * [x] a local authority
 # * [x] an induction tutor
-# * [ ] some school cohorts
-# * [ ] partnership
+# * [x] some school cohorts
+# * [ ] partnerships
 # * [ ] some ects
 # * [ ] some ects with eligibility
 # * [ ] some mentors
@@ -17,6 +19,10 @@ local_authorities.each do |local_authority|
     FactoryBot.create(:seed_school_local_authority, school:, local_authority:)
     FactoryBot.create(:seed_induction_coordinator_profile, :with_user).tap do |induction_coordinator_profile|
       FactoryBot.create(:seed_induction_coordinator_profiles_school, induction_coordinator_profile:, school:)
+
+      cohorts.sample(Random.rand(1..cohorts.length)).each do |cohort|
+        FactoryBot.create(:seed_school_cohort, school:, cohort:)
+      end
     end
   end
 end
