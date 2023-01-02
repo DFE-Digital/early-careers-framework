0f0546cb389c9db51f6060ac86d6eb73a03ee17c
Peter Yates <peter.yates@graphia.co.uk>
2023-01-02 13:33:33 +0000

    Simplify run.rb a little

--- db/new_seeds/run.rb
+++ db/new_seeds/run.rb
@@ -7,29 +7,26 @@ Dir.glob(Rails.root.join("db/new_seeds/scenarios/**/*.rb")).each do |scenario|
 end
 
 def load_base_file(file)
-  base_file = Rails.root.join(*(%w[db new_seeds base] << file))
+  base_file = Rails.root.join("db", "new_seeds", "base", file)
 
   load(base_file)
 end
 
 Rails.logger.info("Seeding database")
 
-# base files are simply ruby scripts we run, they contain static data and
-# generic records
-
-{
-  "cohorts" => "add_cohorts.rb",
-  "schedules" => "add_schedules.rb",
-  "privacy policy 1.0" => "add_privacy_policy.rb",
-  "core induction programmes and lead providers" => "add_lead_providers_and_cips.rb",
-  "seed statements" => "add_seed_statements.rb",
-  "users" => "add_users.rb",
-  "appropriate bodies" => "add_appropriate_bodies.rb",
-  "schools and local authorities" => "add_schools_and_local_authorities.rb",
-  "npq registrations" => "add_npq_registrations.rb",
-  "transfer scenarios" => "add_transfer_scenarios.rb",
-  "mentor scenarios" => "add_mentor_scenarios.rb",
-}.each do |msg, file|
-  Rails.logger.info("adding #{msg}")
+[
+  "add_cohorts.rb",
+  "add_schedules.rb",
+  "add_privacy_policy.rb",
+  "add_lead_providers_and_cips.rb",
+  "add_seed_statements.rb",
+  "add_users.rb",
+  "add_appropriate_bodies.rb",
+  "add_schools_and_local_authorities.rb",
+  "add_npq_registrations.rb",
+  "add_transfer_scenarios.rb",
+  "add_mentor_scenarios.rb",
+].each do |file|
+  Rails.logger.info("seeding #{file}")
   load_base_file(file)
 end
