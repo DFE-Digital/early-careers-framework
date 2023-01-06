5ba07bc7cd46d929ab0f2d0489e0910b9428f3fe
cwrw <abeer.salameh@gmail.com>
2023-01-06 10:34:47 +0300

    CPDLP-1924 Add method which calculates the correct EHCO schedule
    For each cohort, we need to return the correct EHCO schedule depending on the time the NPQ application is accepted.
    Calculate the correct schedule depending on the time range. If the schedule does not exist or is outside the time
    range raise on error.
    
    Current data in production is not the same for each cohort. For cohorts 2022 onwards we have all the schedules present. For 2021 we only have one. Default to that schedule if the cohort passed in is less than 2022

--- app/models/finance/schedule/npq_ehco.rb
+++ app/models/finance/schedule/npq_ehco.rb
@@ -12,6 +12,27 @@ module Finance
       def self.default
         find_by(cohort: Cohort.find_by!(start_year: 2022), schedule_identifier: "npq-ehco-december")
       end
+
+      def self.schedule_for(cohort: Cohort.current)
+        return Finance::Schedule::NPQEhco.find_by!(cohort:) unless cohort_with_multiple_schedules?(cohort)
+
+        case Date.current
+        when Date.new(cohort.start_year, 9, 1)..Date.new(cohort.start_year, 11, -1)
+          Finance::Schedule::NPQEhco.find_by!(cohort:, schedule_identifier: "npq-ehco-november")
+        when Date.new(cohort.start_year, 12, 1)..Date.new(cohort.start_year + 1, 2, -1)
+          Finance::Schedule::NPQEhco.find_by!(cohort:, schedule_identifier: "npq-ehco-december")
+        when Date.new(cohort.start_year + 1, 3, 1)..Date.new(cohort.start_year + 1, 5, -1)
+          Finance::Schedule::NPQEhco.find_by!(cohort:, schedule_identifier: "npq-ehco-march")
+        when Date.new(cohort.start_year + 1, 6, 1)..Date.new(cohort.start_year + 1, 9, -1)
+          Finance::Schedule::NPQEhco.find_by!(cohort:, schedule_identifier: "npq-ehco-june")
+        else
+          raise ArgumentError, "Invalid cohort for NPQEhco schedule"
+        end
+      end
+
+      def self.cohort_with_multiple_schedules?(cohort)
+        Cohort.where(start_year: 2022..).include?(cohort)
+      end
     end
   end
 end
--- spec/models/finance/schedule/npq_ehco_spec.rb
+++ spec/models/finance/schedule/npq_ehco_spec.rb
@@ -19,4 +19,75 @@ RSpec.describe Finance::Schedule::NPQEhco, type: :model do
       expect(described_class.default).to eql(expected_schedule)
     end
   end
+
+  describe ".schedule_for" do
+    let(:cohort)            { Cohort.find_by!(start_year: 2022) }
+    let(:cohort_start_year) { cohort.start_year }
+
+    context "when date is between September and November of cohort start year" do
+      it "returns NPQ EHCO November schedule" do
+        expected_schedule = described_class.find_by(cohort:, schedule_identifier: "npq-ehco-november")
+
+        travel_to Date.new(cohort_start_year, 9, 1) do
+          expect(described_class.schedule_for(cohort:)).to eq(expected_schedule)
+        end
+      end
+    end
+
+    context "when date is between December of cohort start year and February of the next year" do
+      it "returns NPQ EHCO December schedule" do
+        expected_schedule = described_class.find_by(cohort:, schedule_identifier: "npq-ehco-december")
+
+        travel_to Date.new(cohort_start_year, 12, 1) do
+          expect(described_class.schedule_for(cohort:)).to eq(expected_schedule)
+        end
+      end
+    end
+
+    context "when date is between March and May of the next year" do
+      it "returns NPQ EHCO March schedule" do
+        expected_schedule = described_class.find_by(cohort:, schedule_identifier: "npq-ehco-march")
+
+        travel_to Date.new(cohort_start_year + 1, 3, 1) do
+          expect(described_class.schedule_for(cohort:)).to eq(expected_schedule)
+        end
+      end
+    end
+
+    context "when date is between June and September of the next year" do
+      it "returns NPQ EHCO June schedule" do
+        expected_schedule = described_class.find_by(cohort:, schedule_identifier: "npq-ehco-june")
+
+        travel_to Date.new(cohort_start_year + 1, 6, 1) do
+          expect(described_class.schedule_for(cohort:)).to eq(expected_schedule)
+        end
+      end
+    end
+
+    context "when date range exceeds the current cohort" do
+      it "raises an error" do
+        travel_to Date.new(cohort_start_year + 1, 10, 1) do
+          expect { described_class.schedule_for(cohort:) }.to raise_error(ArgumentError, "Invalid cohort for NPQEhco schedule")
+        end
+      end
+    end
+
+    context "when no schedule exists for the cohort" do
+      let(:cohort) { create(:cohort, start_year: 2020) }
+
+      it "raises an error" do
+        expect { described_class.schedule_for(cohort:) }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+
+    context "when selected cohort is before multiple schedules existed for EHCO" do
+      let(:cohort) { Cohort.find_by!(start_year: 2021) }
+
+      it "returns NPQ EHCO June schedule" do
+        expected_schedule = described_class.find_by(cohort:, schedule_identifier: "npq-ehco-june")
+
+        expect(described_class.schedule_for(cohort:)).to eq(expected_schedule)
+      end
+    end
+  end
 end
