e11661ed216509fa0eae9696fbe0c2a383bb8086
cwrw <abeer.salameh@gmail.com>
2023-01-09 18:19:28 +0300

    CPDLP-1924 Use variables for date ranges to improve readability

--- app/models/finance/schedule/npq_ehco.rb
+++ app/models/finance/schedule/npq_ehco.rb
@@ -17,13 +17,13 @@ module Finance
         return Finance::Schedule::NPQEhco.find_by!(cohort:) unless cohort_with_multiple_schedules?(cohort)
 
         case Date.current
-        when Date.new(cohort.start_year, 9, 1)..Date.new(cohort.start_year, 11, -1)
+        when first_day_of_september_current_year(cohort.start_year)..last_day_of_november_current_year(cohort.start_year)
           Finance::Schedule::NPQEhco.find_by!(cohort:, schedule_identifier: "npq-ehco-november")
-        when Date.new(cohort.start_year, 12, 1)..Date.new(cohort.start_year + 1, 2, -1)
+        when first_day_of_december_current_year(cohort.start_year)..last_day_of_february_next_year(cohort.start_year)
           Finance::Schedule::NPQEhco.find_by!(cohort:, schedule_identifier: "npq-ehco-december")
-        when Date.new(cohort.start_year + 1, 3, 1)..Date.new(cohort.start_year + 1, 5, -1)
+        when first_day_of_march_next_year(cohort.start_year)..last_day_of_may_next_year(cohort.start_year)
           Finance::Schedule::NPQEhco.find_by!(cohort:, schedule_identifier: "npq-ehco-march")
-        when Date.new(cohort.start_year + 1, 6, 1)..Date.new(cohort.start_year + 1, 9, -1)
+        when first_day_of_june_next_year(cohort.start_year)..last_day_of_september_next_year(cohort.start_year)
           Finance::Schedule::NPQEhco.find_by!(cohort:, schedule_identifier: "npq-ehco-june")
         else
           default_for(cohort:)
@@ -32,6 +32,38 @@ module Finance
 
       def self.cohort_with_multiple_schedules?(cohort)
         Cohort.where(start_year: 2022..).include?(cohort)
+      end
+
+      def self.first_day_of_september_current_year(cohort_start_year)
+        Date.new(cohort_start_year, 9, 1)
+      end
+
+      def self.last_day_of_november_current_year(cohort_start_year)
+        Date.new(cohort_start_year, 11, -1)
+      end
+
+      def self.first_day_of_december_current_year(cohort_start_year)
+        Date.new(cohort_start_year, 12, 1)
+      end
+
+      def self.last_day_of_february_next_year(cohort_start_year)
+        Date.new(cohort_start_year + 1, 2, -1)
+      end
+
+      def self.first_day_of_march_next_year(cohort_start_year)
+        Date.new(cohort_start_year + 1, 3, 1)
+      end
+
+      def self.last_day_of_may_next_year(cohort_start_year)
+        Date.new(cohort_start_year + 1, 5, -1)
+      end
+
+      def self.first_day_of_june_next_year(cohort_start_year)
+        Date.new(cohort_start_year + 1, 6, 1)
+      end
+
+      def self.last_day_of_september_next_year(cohort_start_year)
+        Date.new(cohort_start_year + 1, 9, -1)
       end
     end
   end
