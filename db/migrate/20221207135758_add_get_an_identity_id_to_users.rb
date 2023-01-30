ebc97b86c14a7ba6813a862f5af9860d7e4ffd0a
Naomi Lockhart <naomi@lockyy.com>
2023-01-30 15:27:32 +0000

    Correct migration class name

--- db/migrate/20221207135758_add_get_an_identity_id_to_users.rb
+++ db/migrate/20221207135758_add_get_an_identity_id_to_users.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddUidAndProviderToUsers < ActiveRecord::Migration[6.1]
+class AddGetAnIdentityIdToUsers < ActiveRecord::Migration[6.1]
   def change
     add_column :users, :get_an_identity_id, :string
   end
