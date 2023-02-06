14a48aaeb595db7f829f2881a4bc83d73f8fc9ff
Naomi Lockhart <naomi@lockyy.com>
2023-02-06 14:10:52 +0000

    Add npq/users#update API for setting GAI IDs
    This is for use syncing historical GAI IDs from the NPQ app through to the ECF app

--- app/controllers/api/v1/npq/users_controller.rb
+++ app/controllers/api/v1/npq/users_controller.rb
@@ -8,30 +8,53 @@ module Api
           creation_response = ::NPQ::Users::FindOrCreateBy.new(params: find_or_create_params).call
 
           if creation_response.success
-            hash = Api::V1::NPQ::UserSerializer.new(creation_response.user).serializable_hash
-            status_code = creation_response.new_user ? :created : :ok
-
-            render json: hash, status: status_code
+            render_user(
+              user: creation_response.user,
+              status: creation_response.new_user ? :created : :ok,
+            )
           else
+            render_errors(errors: creation_response.errors)
+          end
+        end
 
-            hash = {
-              errors: creation_response.errors.map do |error|
-                {
-                  status: "400",
-                  title: error.attribute,
-                  detail: error.message,
-                }
-              end,
-            }
+        def update
+          @user = Identity.find_user_by(id: params[:id])
 
-            render json: hash, status: :bad_request
+          if @user.update(update_params)
+            render_user(user: @user, status: :ok)
+          else
+            render_errors(errors: @user.errors)
           end
         end
 
       private
 
+        def render_user(user:, status: :ok)
+          serialized_user = Api::V1::NPQ::UserSerializer.new(user).serializable_hash
+
+          render json: serialized_user, status:
+        end
+
+        def render_errors(errors:)
+          hash = {
+            errors: errors.map do |error|
+              {
+                status: "400",
+                title: error.attribute,
+                detail: error.message,
+              }
+            end,
+          }
+
+          render json: hash, status: :bad_request
+        end
+
         def find_or_create_params
           params.require(:data).require(:attributes).permit(:email, :get_an_identity_id, :full_name)
+        end
+
+        def update_params
+          params.require(:data).require(:attributes).permit(:get_an_identity_id)
         end
       end
     end
---
+++ spec/requests/api/v1/npq/users/update_spec.rb
@@ -0,0 +1,187 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "API Users", :with_default_schedules, type: :request do
+  let(:parsed_response) { JSON.parse(response.body) }
+  let(:token)           { NPQRegistrationApiToken.create_with_random_token! }
+  let(:bearer_token)    { "Bearer #{token}" }
+  let(:authorization_header) { bearer_token }
+
+  describe "#update" do
+    let(:url) { "/api/v1/npq/users/#{user_id}.json" }
+
+    let(:user) { create(:user) }
+    let(:user_id) { user.id }
+    let(:get_an_identity_id) { SecureRandom.uuid }
+
+    let(:request_body) do
+      {
+        data: {
+          attributes: {
+            get_an_identity_id:,
+          },
+        },
+      }
+    end
+
+    def send_request
+      patch url, params: request_body.to_json, headers: { "Content-Type" => "application/json" }
+    end
+
+    def slice_relevant_user_data(user)
+      user.reload.slice(
+        :email,
+        :get_an_identity_id,
+        :full_name,
+      ).symbolize_keys
+    end
+
+    shared_examples_for "correct response check" do
+      let(:expected_response_body) { raise NotImplementedError }
+      let(:expected_response_code) { raise NotImplementedError }
+
+      it "responds correctly", :aggregate_failures do
+        send_request
+        expect(JSON.parse(response.body)).to eql(expected_response_body)
+        expect(response).to have_http_status(expected_response_code)
+      end
+    end
+
+    before do
+      default_headers["Content-Type"] = "application/vnd.api+json"
+      default_headers[:Authorization] = authorization_header
+    end
+
+    context "when authorized" do
+      let(:authorization_header) { bearer_token }
+
+      context "when the user has a get an identity ID already" do
+        before do
+          user.update(get_an_identity_id: SecureRandom.uuid)
+        end
+
+        include_examples "correct response check" do
+          let(:expected_response_code) { 400 }
+          let(:expected_response_body) do
+            {
+              "errors" => [
+                {
+                  "status" => "400",
+                  "title" => "get_an_identity_id",
+                  "detail" => "cannot be changed once set",
+                },
+              ],
+            }
+          end
+        end
+
+        it "does not update the user" do
+          expect {
+            send_request
+          }.to_not change(user, :as_json)
+        end
+      end
+
+      context "when the user has not got a get an identity ID" do
+        before do
+          user.update!(get_an_identity_id: nil)
+        end
+
+        context "when the get an identity ID is in use" do
+          before do
+            create(:user, get_an_identity_id:)
+          end
+
+          include_examples "correct response check" do
+            let(:expected_response_code) { 400 }
+            let(:expected_response_body) do
+              {
+                "errors" => [
+                  {
+                    "status" => "400",
+                    "title" => "get_an_identity_id",
+                    "detail" => "has already been taken",
+                  },
+                ],
+              }
+            end
+          end
+
+          it "does not update the user" do
+            expect {
+              send_request
+            }.to_not change(user, :as_json)
+          end
+        end
+
+        context "when the get an identity ID is not in use" do
+          include_examples "correct response check" do
+            let(:expected_response_code) { 200 }
+            let(:expected_response_body) do
+              {
+                "data" => {
+                  "id" => user.id.to_s,
+                  "type" => "user",
+                  "attributes" => {
+                    "email" => user.email,
+                    "full_name" => user.full_name,
+                    "get_an_identity_id" => get_an_identity_id,
+                  },
+                },
+              }
+            end
+          end
+
+          it "updates the user" do
+            expect {
+              send_request
+            }.to change {
+              user.reload.get_an_identity_id
+            }.from(nil).to(get_an_identity_id)
+          end
+        end
+      end
+    end
+
+    context "when not authorized" do
+      context "due to providing a non-NPQ API token" do
+        let(:token) { EngageAndLearnApiToken.create_with_random_token! }
+
+        include_examples "correct response check" do
+          let(:expected_response_code) { 401 }
+          let(:expected_response_body) do
+            {
+              "error" => "HTTP Token: Access denied",
+            }
+          end
+        end
+
+        it "does not update the user" do
+          expect {
+            send_request
+          }.to_not change(user, :as_json)
+        end
+      end
+
+      context "due to providing no API token" do
+        let(:authorization_header) { nil }
+
+        include_examples "correct response check" do
+          let(:expected_response_code) { 401 }
+          let(:expected_response_body) do
+            {
+              "error" => "HTTP Token: Access denied",
+            }
+          end
+        end
+
+        it "does not update the user" do
+          expect {
+            send_request
+          }.to_not change(user, :as_json)
+        end
+      end
+    end
+  end
+end
