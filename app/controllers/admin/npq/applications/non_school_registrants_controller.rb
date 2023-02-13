7b4ac2199a442eefe8116f099cab7b632b620d85
Peter Yates <peter.yates@graphia.co.uk>
2023-02-13 19:20:14 +0000

    Add applicant details to the show template

--- app/controllers/admin/npq/applications/non_school_registrants_controller.rb
+++ app/controllers/admin/npq/applications/non_school_registrants_controller.rb
@@ -14,14 +14,16 @@ module Admin
               .does_not_work_in_childcare
               .not_eligible_for_funding
               .no_institution
-              .eager_load(:npq_course, participant_identity: :user)
+              .eager_load(:npq_course, participant_identity: :user),
           ).all
         end
 
         def show
           authorize NPQApplication
 
-          @npq_application = NPQApplication.find(params[:id])
+          @npq_application = NPQApplication
+            .eager_load(:profile, participant_identity: :user)
+            .find(params[:id])
         end
       end
     end
