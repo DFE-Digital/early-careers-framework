7ee65fce25a06ce63ee39cc6433237f0a93ff6ef
Peter Yates <peter.yates@graphia.co.uk>
2023-02-14 12:14:34 +0000

    Switch funding form around so status code selected
    This gives us a better audit trail for _why_ a decision was made and the
    code is determined based on the reason (assuming 'funded' is the only
    poisitive outcome for the moment but that may change)

--- app/controllers/admin/npq/applications/non_school_registrants_controller.rb
+++ app/controllers/admin/npq/applications/non_school_registrants_controller.rb
@@ -4,7 +4,7 @@ module Admin
   module NPQ
     module Applications
       class NonSchoolRegistrantsController < Admin::BaseController
-        BooleanOption = Struct.new(:value, :label)
+        StatusOption = Struct.new(:value, :label)
 
         skip_after_action :verify_authorized, only: :index
         skip_after_action :verify_policy_scoped, except: :index
@@ -25,27 +25,25 @@ module Admin
         def show; end
 
         def edit
-          @boolean_options = [BooleanOption.new(false, "No"), BooleanOption.new(true, "Yes")]
+          @status_options =
+            Admin::NPQApplications::EligibilityImport::ApplicationUpdater::VALID_FUNDING_ELIGIBILITY_STATUS_CODES
+              .map { |code| StatusOption.new(code, code.humanize) }
         end
 
         def update
           @npq_application.assign_attributes(npq_application_params)
 
           if @npq_application.valid?
-            new_code = if @npq_application.eligible_for_funding?
-                         "marked_funded_by_policy"
-                       else
-                         "marked_ineligible_by_policy"
-                       end
+            new_status = @npq_application.funding_eligiblity_status_code == "funded"
 
-            @npq_application.funding_eligiblity_status_code = new_code
+            @npq_application.eligible_for_funding = new_status
             @npq_application.save!
 
             name = @npq_application.participant_identity.user.full_name
 
             flash[:success] = {
               title: "#{name} updated",
-              content: "#{name} has been marked #{new_code.humanize}",
+              content: "#{name} has been marked '#{@npq_application.funding_eligiblity_status_code.humanize.downcase}' and is #{new_status ? 'eligible' : 'not eligible'} for funding",
             }
 
             redirect_to admin_npq_applications_non_school_registrants_path
@@ -65,7 +63,7 @@ module Admin
         end
 
         def npq_application_params
-          params.require(:npq_application).permit(:eligible_for_funding)
+          params.require(:npq_application).permit(:funding_eligiblity_status_code)
         end
       end
     end
