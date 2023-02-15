84bed72bb4d91ef2a85f097075f2fb772934358f
Fabio Volpe <volpefabio@outlook.com>
2023-02-15 16:58:00 +0000

    email decorator fix

--- app/decorators/email_decorator.rb
+++ app/decorators/email_decorator.rb
@@ -14,13 +14,15 @@ class EmailDecorator < SimpleDelegator
 private
 
   def partial_email
-    name, domain = @email.split("@")
-    if name.length <= 2
-      "#{'*' * name.length}@#{domain}"
-    elsif name.length == 3
-      "#{name[0]}**@#{domain}"
-    else
-      "#{name[0]}#{'*' * (name.length - 2)}#{name[-1]}@#{domain}"
+    unless @email.blank?
+      name, domain = @email.split("@")
+      if name.length <= 2
+        "#{'*' * name.length}@#{domain}"
+      elsif name.length == 3
+        "#{name[0]}**@#{domain}"
+      else
+        "#{name[0]}#{'*' * (name.length - 2)}#{name[-1]}@#{domain}"
+      end
     end
   end
 end
