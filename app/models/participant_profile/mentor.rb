a121883155c57141d2a29bbb479cc84b1fa323cd
Peter Yates <peter.yates@graphia.co.uk>
2023-02-15 17:12:11 +0000

    Unnest the ParticipantProfile subclasses

--- app/models/participant_profile/mentor.rb
+++ app/models/participant_profile/mentor.rb
@@ -1,30 +1,28 @@
 # frozen_string_literal: true
 
-class ParticipantProfile < ApplicationRecord
-  class Mentor < ECF
-    self.ignored_columns = %i[mentor_profile_id school_id]
+class ParticipantProfile::Mentor < ParticipantProfile::ECF
+  self.ignored_columns = %i[mentor_profile_id school_id]
 
-    COURSE_IDENTIFIERS = %w[ecf-mentor].freeze
+  COURSE_IDENTIFIERS = %w[ecf-mentor].freeze
 
-    has_many :mentee_profiles,
-             class_name: "ParticipantProfile::ECT",
-             foreign_key: :mentor_profile_id,
-             dependent: :nullify
-    has_many :mentees, through: :mentee_profiles, source: :user
+  has_many :mentee_profiles,
+            class_name: "ParticipantProfile::ECT",
+            foreign_key: :mentor_profile_id,
+            dependent: :nullify
+  has_many :mentees, through: :mentee_profiles, source: :user
 
-    has_many :school_mentors, dependent: :destroy, foreign_key: :participant_profile_id
-    has_many :schools, through: :school_mentors
+  has_many :school_mentors, dependent: :destroy, foreign_key: :participant_profile_id
+  has_many :schools, through: :school_mentors
 
-    def mentor?
-      true
-    end
+  def mentor?
+    true
+  end
 
-    def participant_type
-      :mentor
-    end
+  def participant_type
+    :mentor
+  end
 
-    def role
-      "Mentor"
-    end
+  def role
+    "Mentor"
   end
 end
--- app/models/participant_profile/npq.rb
+++ app/models/participant_profile/npq.rb
@@ -1,111 +1,109 @@
 # frozen_string_literal: true
 
-class ParticipantProfile < ApplicationRecord
-  class NPQ < ParticipantProfile
-    COURSE_IDENTIFIERS = %w[
-      npq-executive-leadership
-      npq-headship
-      npq-senior-leadership
-      npq-early-years-leadership
-      npq-additional-support-offer
-      npq-leading-teaching-development
-      npq-leading-teaching
-      npq-leading-behaviour-culture
-      npq-early-headship-coaching-offer
-      npq-leading-literacy
-    ].freeze
+class ParticipantProfile::NPQ < ParticipantProfile
+  COURSE_IDENTIFIERS = %w[
+    npq-executive-leadership
+    npq-headship
+    npq-senior-leadership
+    npq-early-years-leadership
+    npq-additional-support-offer
+    npq-leading-teaching-development
+    npq-leading-teaching
+    npq-leading-behaviour-culture
+    npq-early-headship-coaching-offer
+    npq-leading-literacy
+  ].freeze
 
-    VALID_EVIDENCE_HELD = %w[training-event-attended self-study-material-completed other].freeze
+  VALID_EVIDENCE_HELD = %w[training-event-attended self-study-material-completed other].freeze
 
-    WITHDRAW_REASONS = %w[
-      insufficient-capacity-to-undertake-programme
-      personal-reason-health-or-pregnancy-related
-      personal-reason-moving-school
-      personal-reason-other
-      insufficient-capacity
-      change-in-developmental-or-personal-priorities
-      change-in-school-circumstances
-      change-in-school-leadership
-      quality-of-programme-structure-not-suitable.
-      quality-of-programme-content-not-suitable
-      quality-of-programme-facilitation-not-effective
-      quality-of-programme-accessibility
-      quality-of-programme-other
-      programme-not-appropriate-for-role-and-cpd-needs
-      started-in-error
-      other
-    ].freeze
+  WITHDRAW_REASONS = %w[
+    insufficient-capacity-to-undertake-programme
+    personal-reason-health-or-pregnancy-related
+    personal-reason-moving-school
+    personal-reason-other
+    insufficient-capacity
+    change-in-developmental-or-personal-priorities
+    change-in-school-circumstances
+    change-in-school-leadership
+    quality-of-programme-structure-not-suitable.
+    quality-of-programme-content-not-suitable
+    quality-of-programme-facilitation-not-effective
+    quality-of-programme-accessibility
+    quality-of-programme-other
+    programme-not-appropriate-for-role-and-cpd-needs
+    started-in-error
+    other
+  ].freeze
 
-    self.ignored_columns = %i[mentor_profile_id school_cohort_id]
-    belongs_to :cohort, optional: true
-    belongs_to :school, optional: true
-    belongs_to :npq_course, optional: true
+  self.ignored_columns = %i[mentor_profile_id school_cohort_id]
+  belongs_to :cohort, optional: true
+  belongs_to :school, optional: true
+  belongs_to :npq_course, optional: true
 
-    has_one :npq_application, foreign_key: :id
+  has_one :npq_application, foreign_key: :id
 
-    has_many :participant_declarations, class_name: "ParticipantDeclaration::NPQ", foreign_key: :participant_profile_id
+  has_many :participant_declarations, class_name: "ParticipantDeclaration::NPQ", foreign_key: :participant_profile_id
 
-    after_commit :push_profile_to_big_query
+  after_commit :push_profile_to_big_query
 
-    self.validation_steps = %i[identity decision].freeze
+  self.validation_steps = %i[identity decision].freeze
 
-    def npq?
-      true
-    end
+  def npq?
+    true
+  end
 
-    def ecf?
-      false
-    end
+  def ecf?
+    false
+  end
 
-    def approved?
-      validation_decision(:decision).approved?
-    end
+  def approved?
+    validation_decision(:decision).approved?
+  end
 
-    def rejected?
-      decision = validation_decision(:decision)
-      decision.persisted? && !decision.approved?
-    end
+  def rejected?
+    decision = validation_decision(:decision)
+    decision.persisted? && !decision.approved?
+  end
 
-    def participant_type
-      :npq
-    end
+  def participant_type
+    :npq
+  end
 
-    def fundable?
-      npq_application&.eligible_for_dfe_funding
-    end
+  def fundable?
+    npq_application&.eligible_for_dfe_funding
+  end
 
-    def schedule_for(*)
-      schedule
-    end
+  def schedule_for(*)
+    schedule
+  end
 
-    def withdrawn_for?(*)
-      training_status_withdrawn?
-    end
+  def withdrawn_for?(*)
+    training_status_withdrawn?
+  end
 
-    def deferred_for?(*)
-      training_status_deferred?
-    end
+  def deferred_for?(*)
+    training_status_deferred?
+  end
 
-    def active_for?(*)
-      training_status_active?
-    end
+  def active_for?(*)
+    training_status_active?
+  end
 
-    def record_to_serialize_for(*)
-      user
-    end
+  def record_to_serialize_for(*)
+    user
+  end
 
-    def latest_induction_record_for(*)
-      nil
-    end
+  def latest_induction_record_for(*)
+    nil
+  end
 
-    def policy_class
-      ParticipantProfile::NPQPolicy
-    end
+  def policy_class
+    ParticipantProfile::NPQPolicy
+  end
 
-  private
+private
 
-    def push_profile_to_big_query
-      ::NPQ::StreamBigQueryProfileJob.perform_later(profile_id: id)
-    end
+  def push_profile_to_big_query
+    ::NPQ::StreamBigQueryProfileJob.perform_later(profile_id: id)
   end
 end
