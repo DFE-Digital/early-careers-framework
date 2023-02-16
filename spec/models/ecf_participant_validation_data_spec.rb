36c8a8056f86f9a060fd473b4ca6de81e1e95dac
Peter Yates <peter.yates@graphia.co.uk>
2023-02-16 09:56:01 +0000

    Make validation data link to participant profile
    Instead of belonging to an 'abstract model', make the belongs_to
    relationship between ecf_participant_validation_data and
    participant_profile and rely on validation to ensure we don't try and
    link the wrong kind of profile.

--- spec/models/ecf_participant_validation_data_spec.rb
+++ spec/models/ecf_participant_validation_data_spec.rb
@@ -14,4 +14,37 @@ RSpec.describe ECFParticipantValidationData, type: :model do
     validation_data.touch
     expect(user.reload.updated_at).to be_within(1.second).of Time.zone.now
   end
+
+  describe "validating the associated participant profile type" do
+    subject { FactoryBot.build(:seed_ecf_participant_validation_data, participant_profile:) }
+    before { subject.valid? }
+
+    context "when linked to a Mentor participant profile" do
+      let(:participant_profile) { FactoryBot.create(:seed_mentor_participant_profile, :valid) }
+
+      it "passes validation" do
+        expect(subject).to be_valid
+      end
+    end
+
+    context "when linked to a ECT participant profile" do
+      let(:participant_profile) { FactoryBot.create(:seed_ect_participant_profile, :valid) }
+
+      it "passes validation" do
+        expect(subject).to be_valid
+      end
+    end
+
+    context "when linked to a non-ECF participant profile" do
+      let(:participant_profile) { FactoryBot.create(:seed_npq_participant_profile, :valid) }
+
+      it "fails validation" do
+        expect(subject).not_to be_valid
+      end
+
+      it "has an informative error message" do
+        expect(subject.errors.messages[:participant_profile_id]).to include(/not an ECT or Mentor/)
+      end
+    end
+  end
 end
