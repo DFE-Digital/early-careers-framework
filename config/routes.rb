d27ab9b8417c9615eb8930784b943e3a597974cf
Fabio Volpe <volpefabio@outlook.com>
2023-02-17 16:54:49 +0000

    SIT already nominated page removed

--- config/routes.rb
+++ config/routes.rb
@@ -176,7 +176,6 @@ Rails.application.routes.draw do
         get "success", action: :success
         get "not-eligible", action: :not_eligible
         get "limit-reached", action: :limit_reached
-        get "already-nominated", action: :already_nominated
       end
     end
 
