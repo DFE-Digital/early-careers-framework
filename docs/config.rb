88c5124485ef1541876c95407906848b32eef964
Peter Yates <peter.yates@graphia.co.uk>
2023-02-22 21:45:36 +0000

    Add after build step to allow local preview
    When running middleman locally the hardcoded paths for JavaScript and
    Stylesheets make previewing changes difficult.
    
    Now, instead of hardcoding a api-reference prefix we'll just use
    javascripts and stylesheet directories in source and when building
    (using `middleman build`) run an additional step which copies the
    directories to api-reference, so the site will render correctly when
    served from within Rails too.

--- docs/config.rb
+++ docs/config.rb
@@ -12,8 +12,17 @@ end
 
 activate :open_api
 
-set :css_dir, "api-reference/stylesheets"
-set :js_dir, "api-reference/javascripts"
-set :images_dir, "api-reference/javascripts"
+set :css_dir, "stylesheets"
+set :js_dir, "javascripts"
+
+after_build do |build|
+  Middleman::Cli::Build.source_root(File.dirname(__FILE__))
+
+  build.thor.source_paths << "#{File.dirname(__FILE__)}/source"
+
+  %w[stylesheets javascripts].each do |type|
+    build.thor.directory(File.join(type), File.join(config[:build_dir], "api-reference", type))
+  end
+end
 
 set :relative_links, true
