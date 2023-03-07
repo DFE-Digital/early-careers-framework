aa2df8d9cd15fe4109f171f3912a47d6aac3e5bb
Sam Johnson <sh.johnson@me.com>
2023-03-07 10:11:03 +0000

    Fix tests

--- spec/requests/admin/npq/applications_controller_spec.rb
+++ spec/requests/admin/npq/applications_controller_spec.rb
@@ -3,47 +3,40 @@
 require "rails_helper"
 
 RSpec.describe "Admin::NPQ::ApplicationsController", :with_default_schedules, type: :request do
-  let!(:application) { create :npq_application }
+  before(:all) do
+    21.times do
+      create(:npq_application)
+    end
+  end
+
+  let(:application) { NPQApplication.first }
   let(:admin_user) { create :user, :admin }
-  let(:parsed_response) { JSON.parse(response.body) }
-  let(:params) { {} }
 
   before do
     sign_in admin_user
   end
 
   describe "GET (Index) /admin/npq/applications/applications" do
-    let(:index_request) { get("/admin/npq/applications/applications", params:) }
-
     it "renders the index template for applications" do
-      index_request
+      get("/admin/npq/applications/applications")
+
       expect(response).to render_template "admin/npq/applications/index"
     end
 
-    context "with users split over two pages" do
-      let(:params) { { per_page: 2, page: } }
-      let(:page) { 2 }
+    it "only returns first 20 results on page 1", :aggregate_failures do
+      get("/admin/npq/applications/applications", params: { page: 1 })
 
-      before do
-        25.times do
-          create(:npq_application)
-        end
-      end
+      response_body = response.parsed_body
 
-      it "can return paginated data" do
-        index_request
-
-        # TOO FIX
-        # expect(parsed_response["data"].size).to eql(2)
-
-        # expect(response).to render_template "admin/npq/applications/index"
-      end
+      expect(response_body.include?("NPQ Course 20")).to eq(true)
+      expect(response_body.include?("NPQ Course 21")).to eq(false)
     end
   end
 
   describe "GET (SHOW) /admin/npq/applications/applications/#application_id" do
     it "renders the show template for the application" do
       get "/admin/npq/applications/applications/#{application.id}"
+
       expect(response).to render_template "admin/npq/applications/show"
     end
   end
