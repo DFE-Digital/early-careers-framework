e02aa8ddc072d2bbab80ac9a5c27213243f7c8e7
Leandro Alemão <leandro.costantini@gmail.com>
2023-03-09 12:27:05 +0000

    [CPDLP-2037] Revert changes made on manual task Transfers::NPQParticipants

--- app/services/transfers/npq_participants.rb
+++ app/services/transfers/npq_participants.rb
@@ -2,17 +2,16 @@
 
 module Transfers
   class NPQParticipants
-    attr_reader :external_identifier, :current_npq_lead_provider_id, :new_npq_lead_provider_id, :course_identifier
+    attr_reader :external_identifier, :new_npq_lead_provider_id, :course_identifier
 
-    def initialize(external_identifier:, current_npq_lead_provider_id:, new_npq_lead_provider_id:, course_identifier:)
+    def initialize(external_identifier:, new_npq_lead_provider_id:, course_identifier:)
       @external_identifier = external_identifier
-      @current_npq_lead_provider_id = current_npq_lead_provider_id
       @new_npq_lead_provider_id = new_npq_lead_provider_id
       @course_identifier = course_identifier
     end
 
     def call
-      return unless new_npq_lead_provider && npq_application
+      return unless npq_lead_provider && participant_identity
 
       transfer_participant
     end
@@ -20,18 +19,14 @@ module Transfers
   private
 
     def transfer_participant
-      return unless npq_application
+      return unless participant_profile && npq_application
 
-      npq_application.update!(npq_lead_provider: new_npq_lead_provider)
-      Rails.logger.info "Participant (#{external_identifier}) was transferred to (#{new_npq_lead_provider.name}) successfully"
+      npq_application.update!(npq_lead_provider:)
+      Rails.logger.info "Participant (#{external_identifier}) was transferred to (#{npq_lead_provider.name}) successfully"
     end
 
-    def new_npq_lead_provider
-      @new_npq_lead_provider ||= NPQLeadProvider.find_by!(id: new_npq_lead_provider_id)
-    end
-
-    def current_npq_lead_provider
-      @current_npq_lead_provider ||= NPQLeadProvider.find_by!(id: current_npq_lead_provider_id)
+    def npq_lead_provider
+      @npq_lead_provider ||= NPQLeadProvider.find_by!(id: new_npq_lead_provider_id)
     end
 
     def participant_profile
@@ -43,12 +38,7 @@ module Transfers
     end
 
     def participant_identity
-      @participant_identity ||= ParticipantIdentityResolver
-                                  .call(
-                                    user_id: external_identifier,
-                                    course_identifier:,
-                                    cpd_lead_provider: current_npq_lead_provider.cpd_lead_provider,
-                                  )
+      @participant_identity ||= ParticipantIdentity.find_by!(external_identifier:)
     end
 
     def npq_application
