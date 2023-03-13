a0017442541921d6acb5c5014e488a2422c77db2
Lorenzo Tello <ltello8a@gmail.com>
2023-03-13 18:30:17 +0100

    partipant new statuses. First approach

--- app/components/participant_status_tag_component.rb
+++ app/components/participant_status_tag_component.rb
@@ -14,36 +14,134 @@ class ParticipantStatusTagComponent < BaseComponent
     end
   end
 
-private
+  private
 
   attr_reader :profile, :induction_record
 
   def tag_attributes
-    return { text: "Withdrawn by provider", colour: "red" } if training_status_withdrawn?
-    return { text: "Eligible to start", colour: "green" } if eligible? && profile.single_profile?
-    return { text: "Eligible: Mentor at main school", colour: "green" } if eligible? && profile.primary_profile?
-    return { text: "Eligible: Mentor at additional school", colour: "green" } if ineligible? && mentor_with_duplicate_profile?
+    # withdrawn trumps everything
+    return { text: "No longer being trained", colour: "grey" } if participant_withdrawn?
+    return { text: "No longer being trained", colour: "grey" } if participant_withdrawn_from_training?
 
-    return { text: "Not eligible: No QTS", colour: "red" } if participant_has_no_qts?
-    return { text: "DfE checking eligibility", colour: "orange" } if profile.manual_check_needed?
-    return { text: "Not eligible: NQT+1", colour: "red" } if nqt_plus_one? && ineligible?
-    return { text: "Eligible to start: ERO", colour: "green" } if ineligible? && mentor_was_in_early_rollout? && on_fip?
-    return { text: "Eligible to start", colour: "green" } if ineligible? && mentor_was_in_early_rollout?
-    return { text: "Not eligible", colour: "red" } if ineligible?
-    return { text: "Contacted for information", colour: "grey" } if latest_email&.delivered?
-    return { text: "Check email address", colour: "grey" } if latest_email&.failed?
+    # pre-registration checks for manual validation
+    return { text: "Check email address", colour: "blue" } if email_address_not_deliverable?
+    return { text: "Contacted for information", colour: "grey" } if awaiting_contact_from_participant?
 
-    { text: "Contacting for information", colour: "grey" }
+    # Initial TRA record checks
+    return { text: "No TRN provided", colour: "blue" } if has_no_trn?
+    return { text: "Pending", colour: "blue" } if active_flags_need_checking?
+    return { text: "Pending", colour: "blue" } if has_different_trn?
+
+    # Provisional eligibility
+    return { text: "Waiting for QTS", colour: "grey" } if waiting_for_qts?
+    return { text: "No induction start date", colour: "blue" } if has_no_induction?
+
+    # Not eligible TODO: if the induction is not complete then show "Joining your school" , colour: "blue"
+    return { text: "Statutory induction completed", colour: "grey" } if previous_induction_or_participation?
+    return { text: "Exempt", colour: "grey" } if exempt_from_induction?
+    return { text: "Duplicate profile", colour: "blue" } if duplicate_profile?
+    return { text: "No longer being trained", colour: "grey" } if active_flags_verified?
+    return { text: "Not qualified", colour: "grey" } if ect_not_qualified?
+
+    # Eligible and no longer participating // skip for mentors
+    return { text: "Training deferred", colour: "grey" } if training_status_deferred?
+    return { text: "Training completed", colour: "grey" } if participant_completed?
+
+
+    # return { text: "Leaving your school", colour: "grey" } if participant_leaving? # check date on IR
+    return { text: "No longer being trained", colour: "grey" } if participant_leaving?
+    # return { text: "Joining your school", colour: "grey" } if participant_leaving? # check date on IR
+
+    # TODO: if number of ECTs < 1 then "Not Mentoring", colour: "blue"
+    return { text: "Mentoring", colour: "green" } if profile&.mentor?
+
+    { text: "Training", colour: "green" }
   end
 
-  def training_status_withdrawn?
-    (induction_record || profile).training_status_withdrawn?
+  def participant_withdrawn?
+    induction_record&.withdrawn_induction_status? || profile&.withdrawn_record?
   end
 
-  def latest_email
+  def participant_leaving?
+    induction_record&.leaving_induction_status?
+  end
+
+  def participant_completed?
+    induction_record&.completed_induction_status?
+  end
+
+  def participant_withdrawn_from_training?
+    (induction_record || profile)&.training_status_withdrawn?
+  end
+
+  def training_status_deferred?
+    (induction_record || profile)&.training_status_deferred?
+  end
+
+  def email_address_not_deliverable?
+    request_for_details_email&.failed?
+  end
+
+  def awaiting_contact_from_participant?
+    request_for_details_email&.delivered? && profile&.ecf_participant_validation_data.nil?
+  end
+
+  def request_for_details_email
     return @latest_email if defined?(@latest_email)
 
     @latest_email = Email.associated_with(profile).tagged_with(:request_for_details).latest
+  end
+
+  def has_no_trn?
+    profile&.teacher_profile&.trn == nil
+  end
+
+  def active_flags_need_checking?
+    eligibility = profile&.ecf_participant_eligibility
+    eligibility&.manual_check_status? && eligibility.active_flags_reason?
+  end
+
+  def active_flags_verified?
+    eligibility = profile&.ecf_participant_eligibility
+    eligibility&.ineligible_status? && eligibility.active_flags_reason?
+  end
+
+  def has_different_trn?
+    eligibility = profile&.ecf_participant_eligibility
+    eligibility&.manual_check_status? && eligibility.different_trn_reason?
+  end
+
+  def waiting_for_qts?
+    eligibility = profile&.ecf_participant_eligibility
+    eligibility&.manual_check_status? && eligibility.no_qts_reason?
+  end
+
+  def ect_not_qualified?
+    eligibility = profile&.ecf_participant_eligibility
+    profile&.ect? && eligibility&.ineligible_status? && eligibility.no_qts_reason?
+  end
+
+  def has_no_qts?
+    profile&.no_qts?
+  end
+
+  def has_no_induction?
+    eligibility = profile&.ecf_participant_eligibility
+    eligibility&.manual_check_status? && eligibility.no_induction_reason?
+  end
+
+  def previous_induction_or_participation?
+    eligibility = profile&.ecf_participant_eligibility
+    eligibility&.ineligible_status? && (eligibility.previous_induction_reason? || eligibility.previous_participation_reason?)
+  end
+
+  def exempt_from_induction?
+    eligibility = profile&.ecf_participant_eligibility
+    eligibility&.ineligible_status? && eligibility.exempt_from_induction_reason?
+  end
+
+  def duplicate_profile?
+    profile.duplicate?
   end
 
   def eligible?
@@ -72,9 +170,5 @@ private
 
   def nqt_plus_one?
     profile.previous_induction?
-  end
-
-  def participant_has_no_qts?
-    profile.no_qts?
   end
 end
