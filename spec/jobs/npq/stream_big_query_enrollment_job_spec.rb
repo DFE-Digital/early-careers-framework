85a8b4f4c32ef413839a5f205c6c12e5a8e91704
Steve Lorek <steve@stevelorek.com>
2023-03-13 18:43:22 +0000

    Change data sent to BigQuery from cohort_id to cohort start year

--- spec/jobs/npq/stream_big_query_enrollment_job_spec.rb
+++ spec/jobs/npq/stream_big_query_enrollment_job_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe NPQ::StreamBigQueryEnrollmentJob do
 
       expect(table).to have_received(:insert).with([{
         "application_ecf_id" => npq_application.id,
-        "cohort_id" => npq_application.cohort_id,
+        "cohort" => npq_application.cohort.start_year,
         "status" => "pending",
         "updated_at" => npq_application.updated_at,
         "employer_name" => employer_name,
