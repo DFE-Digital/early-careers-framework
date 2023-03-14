8bd8cc96630940a3bbe43acb21da17109a38dae8
Lorenzo Tello <ltello8a@gmail.com>
2023-03-14 01:04:01 +0100

    refactoring

--- app/components/participant_status_tag_component.rb
+++ app/components/participant_status_tag_component.rb
@@ -19,160 +19,18 @@ class ParticipantStatusTagComponent < BaseComponent
 
   attr_reader :profile, :induction_record, :has_mentees
 
+  def status
+    @status ||= ::Participants::StatusAtSchool.new(induction_record:, has_mentees:, profile:).call
+  end
+
   def tag_attributes
-    # withdrawn trumps everything
-    return { text: "No longer being trained", colour: "grey" } if participant_withdrawn?
-    return { text: "No longer being trained", colour: "grey" } if participant_withdrawn_from_training?
-
-    # pre-registration checks for manual validation
-    return { text: "Check email address", colour: "blue" } if email_address_not_deliverable?
-    return { text: "Contacted for information", colour: "grey" } if awaiting_contact_from_participant?
-
-    # Initial TRA record checks
-    return { text: "No TRN provided", colour: "blue" } if has_no_trn?
-    return { text: "Pending", colour: "blue" } if active_flags_need_checking?
-    return { text: "Pending", colour: "blue" } if has_different_trn?
-
-    # Provisional eligibility
-    return { text: "Waiting for QTS", colour: "grey" } if waiting_for_qts?
-    return { text: "No induction start date", colour: "blue" } if has_no_induction?
-
-    # Not eligible TODO: if the induction is not complete then show "Joining your school" , colour: "blue"
-    return { text: "Statutory induction completed", colour: "grey" } if previous_induction_or_participation?
-    return { text: "Exempt", colour: "grey" } if exempt_from_induction?
-    return { text: "Duplicate profile", colour: "blue" } if duplicate_profile?
-    return { text: "No longer being trained", colour: "grey" } if active_flags_verified?
-    return { text: "Not qualified", colour: "grey" } if ect_not_qualified?
-
-    # Eligible and no longer participating // skip for mentors
-    return { text: "Training deferred", colour: "grey" } if training_status_deferred?
-    return { text: "Training completed", colour: "grey" } if participant_completed?
-
-    # return { text: "Leaving your school", colour: "grey" } if participant_leaving? # check date on IR
-    return { text: "No longer being trained", colour: "grey" } if participant_leaving?
-    # return { text: "Joining your school", colour: "grey" } if participant_leaving? # check date on IR
-
-    return { text: "Not Mentoring", colour: "blue" } if participant_not_mentoring?
-    return { text: "Mentoring", colour: "green" } if profile&.mentor?
-
-    { text: "Training", colour: "green" }
+    {
+      text: t(:header, scope: translation_scope),
+      colour: t(:colour, scope: translation_scope),
+    }
   end
 
-  def participant_withdrawn?
-    induction_record&.withdrawn_induction_status? || profile&.withdrawn_record?
-  end
-
-  def participant_leaving?
-    induction_record&.leaving_induction_status?
-  end
-
-  def participant_completed?
-    induction_record&.completed_induction_status?
-  end
-
-  def participant_not_mentoring?
-    profile&.mentor? && !has_mentees
-  end
-
-  def participant_withdrawn_from_training?
-    (induction_record || profile)&.training_status_withdrawn?
-  end
-
-  def training_status_deferred?
-    (induction_record || profile)&.training_status_deferred?
-  end
-
-  def email_address_not_deliverable?
-    request_for_details_email&.failed?
-  end
-
-  def awaiting_contact_from_participant?
-    request_for_details_email&.delivered? && profile&.ecf_participant_validation_data.nil?
-  end
-
-  def request_for_details_email
-    return @latest_email if defined?(@latest_email)
-
-    @latest_email = Email.associated_with(profile).tagged_with(:request_for_details).latest
-  end
-
-  def has_no_trn?
-    profile&.teacher_profile&.trn.blank?
-  end
-
-  def active_flags_need_checking?
-    eligibility = profile&.ecf_participant_eligibility
-    eligibility&.manual_check_status? && eligibility.active_flags_reason?
-  end
-
-  def active_flags_verified?
-    eligibility = profile&.ecf_participant_eligibility
-    eligibility&.ineligible_status? && eligibility.active_flags_reason?
-  end
-
-  def has_different_trn?
-    eligibility = profile&.ecf_participant_eligibility
-    eligibility&.manual_check_status? && eligibility.different_trn_reason?
-  end
-
-  def waiting_for_qts?
-    eligibility = profile&.ecf_participant_eligibility
-    eligibility&.manual_check_status? && eligibility.no_qts_reason?
-  end
-
-  def ect_not_qualified?
-    eligibility = profile&.ecf_participant_eligibility
-    profile&.ect? && eligibility&.ineligible_status? && eligibility.no_qts_reason?
-  end
-
-  def has_no_qts?
-    profile&.no_qts?
-  end
-
-  def has_no_induction?
-    eligibility = profile&.ecf_participant_eligibility
-    eligibility&.manual_check_status? && eligibility.no_induction_reason?
-  end
-
-  def previous_induction_or_participation?
-    eligibility = profile&.ecf_participant_eligibility
-    eligibility&.ineligible_status? && (eligibility.previous_induction_reason? || eligibility.previous_participation_reason?)
-  end
-
-  def exempt_from_induction?
-    eligibility = profile&.ecf_participant_eligibility
-    eligibility&.ineligible_status? && eligibility.exempt_from_induction_reason?
-  end
-
-  def duplicate_profile?
-    profile.duplicate?
-  end
-
-  def eligible?
-    profile.eligible?
-  end
-
-  def ineligible?
-    profile&.ineligible?
-  end
-
-  def mentor_was_in_early_rollout?
-    return unless profile.mentor?
-
-    profile.previous_participation?
-  end
-
-  def mentor_with_duplicate_profile?
-    return unless profile.mentor?
-
-    profile.duplicate?
-  end
-
-  def on_fip?
-    induction_record&.enrolled_in_fip? || profile&.school_cohort&.full_induction_programme?
-  end
-
-  def nqt_plus_one?
-    profile.previous_induction?
+  def translation_scope
+    @translation_scope ||= "schools.participants.status.#{status}"
   end
 end
--- app/components/schools/participants/status.rb
+++ app/components/schools/participants/status.rb
@@ -12,13 +12,6 @@ module Schools
 
       attr_reader :induction_record, :has_mentees
 
-      delegate :participant_profile, to: :induction_record
-      delegate :delivery_partner, to: :induction_record
-      delegate :school_cohort, to: :induction_record
-      delegate :ecf_participant_eligibility, to: :participant_profile
-      delegate :ecf_participant_validation_data, to: :participant_profile
-      delegate :core_induction_programme?, to: :school_cohort
-
       def content
         Array.wrap(t(:content, scope: translation_scope,
                      contact_us: render(MailToSupportComponent.new("contact us")),
@@ -32,122 +25,16 @@ module Schools
                   colour: t(:colour, scope: translation_scope))
       end
 
-      def profile_status
-        return :no_longer_being_trained_sit if participant_withdrawn?
-        return :no_longer_being_trained_provider if participant_withdrawn_from_training?
-        return :check_email_address if email_address_not_deliverable?
-        return :contacted_for_information if awaiting_contact_from_participant?
-        return :no_trn_provided if has_no_trn?
-        return :pending if active_flags_need_checking?
-        return :pending if has_different_trn?
-        return :waiting_for_qts if waiting_for_qts?
-        return :no_induction_start_date if has_no_induction?
-        return :statutory_induction_completed if previous_induction_or_participation?
-        return :exempt if exempt_from_induction?
-        return :duplicate_profile if duplicate_profile?
-        return :no_longer_being_trained_provider if active_flags_verified?
-        return :not_qualified if ect_not_qualified?
-        return :training_deferred if training_status_deferred?
-        return :training_completed if participant_completed?
-        return :leaving_your_school if participant_leaving?
-        return :joining_your_school if participant_joining?
-        return :not_mentoring if participant_not_mentoring?
-        return :mentoring if participant_profile.mentor?
+      def profile
+        @profile ||= induction_record.participant_profile
+      end
 
-        :training
+      def status
+        @status ||= ::Participants::StatusAtSchool.new(induction_record:, has_mentees:, profile:).call
       end
 
       def translation_scope
-        @translation_scope ||= "schools.participants.status.#{profile_status}"
-      end
-
-      # Status checks
-      def active_flags_need_checking?
-        ecf_participant_eligibility&.manual_check_status? && ecf_participant_eligibility.active_flags_reason?
-      end
-
-      def active_flags_verified?
-        ecf_participant_eligibility&.ineligible_status? && ecf_participant_eligibility.active_flags_reason?
-      end
-
-      def awaiting_contact_from_participant?
-        request_for_details_email&.delivered? && ecf_participant_validation_data.nil?
-      end
-
-      def duplicate_profile?
-        participant_profile.duplicate?
-      end
-
-      def ect_not_qualified?
-        participant_profile&.ect? &&
-          ecf_participant_eligibility&.ineligible_status? &&
-          ecf_participant_eligibility.no_qts_reason?
-      end
-
-      def email_address_not_deliverable?
-        request_for_details_email&.failed?
-      end
-
-      def exempt_from_induction?
-        ecf_participant_eligibility&.ineligible_status? && ecf_participant_eligibility.exempt_from_induction_reason?
-      end
-
-      def has_different_trn?
-        ecf_participant_eligibility&.manual_check_status? && ecf_participant_eligibility.different_trn_reason?
-      end
-
-      def has_no_induction?
-        ecf_participant_eligibility&.manual_check_status? && ecf_participant_eligibility.no_induction_reason?
-      end
-
-      def has_no_trn?
-        induction_record.trn.blank?
-      end
-
-      def participant_completed?
-        induction_record.completed_induction_status?
-      end
-
-      def participant_joining?
-        induction_record.active_induction_status? &&
-          induction_record.start_date.future? &&
-          induction_record.school_transfer?
-      end
-
-      def participant_leaving?
-        induction_record&.leaving_induction_status?
-      end
-
-      def participant_not_mentoring?
-        induction_record.mentor? && !has_mentees
-      end
-
-      def participant_withdrawn?
-        induction_record&.withdrawn_induction_status?
-      end
-
-      def participant_withdrawn_from_training?
-        induction_record&.training_status_withdrawn?
-      end
-
-      def previous_induction_or_participation?
-        ecf_participant_eligibility&.ineligible_status? &&
-          (ecf_participant_eligibility.previous_induction_reason? ||
-            ecf_participant_eligibility.previous_participation_reason?)
-      end
-
-      def request_for_details_email
-        return @latest_email if defined?(@latest_email)
-
-        @latest_email = Email.associated_with(participant_profile).tagged_with(:request_for_details).latest
-      end
-
-      def training_status_deferred?
-        induction_record&.training_status_deferred?
-      end
-
-      def waiting_for_qts?
-        ecf_participant_eligibility&.manual_check_status? && ecf_participant_eligibility.no_qts_reason?
+        @translation_scope ||= "schools.participants.status.#{status}"
       end
     end
   end
