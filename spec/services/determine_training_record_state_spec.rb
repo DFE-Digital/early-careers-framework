f0aba5e47a4581f665e663975ddf8dfe441c08d1
matt <mattonfoot@gmail.com>
2023-03-20 11:07:41 +0000

    adds deferred training state

--- spec/services/determine_training_record_state_spec.rb
+++ spec/services/determine_training_record_state_spec.rb
@@ -7,34 +7,44 @@ RSpec.describe DetermineTrainingRecordState, :with_training_record_state_example
     context "when the training record is for an ECT doing ECF core induction training" do
       context "who is currently training" do
         subject { described_class.call(participant_profile: ect_on_cip_being_trained) }
-        it { is_expected.to eql :training }
+        it { is_expected.to eql :is_training }
       end
 
       context "who has been withdrawn by their last lead provider" do
         subject { described_class.call(participant_profile: ect_on_cip_withdrawn_from_training) }
-        it { is_expected.to eql :withdrawn_from_training }
+        it { is_expected.to eql :has_withdrawn_from_training }
+      end
+
+      context "who has been deferred by their last lead provider" do
+        subject { described_class.call(participant_profile: ect_on_cip_having_deferred_their_training) }
+        it { is_expected.to eql :has_deferred_their_training }
       end
 
       context "who has been withdrawn by their last school" do
         subject { described_class.call(participant_profile: ect_on_cip_withdrawn_from_programme) }
-        it { is_expected.to eql :withdrawn_from_programme }
+        it { is_expected.to eql :has_withdrawn_from_programme }
       end
     end
 
     context "when the training record is for an ECT doing ECF full induction training" do
       context "who is currently training" do
         subject { described_class.call(participant_profile: ect_on_fip_being_trained) }
-        it { is_expected.to eql :training }
+        it { is_expected.to eql :is_training }
       end
 
       context "who has been withdrawn by their last lead provider" do
         subject { described_class.call(participant_profile: ect_on_fip_withdrawn_from_training) }
-        it { is_expected.to eql :withdrawn_from_training }
+        it { is_expected.to eql :has_withdrawn_from_training }
+      end
+
+      context "who has been deferred by their last lead provider" do
+        subject { described_class.call(participant_profile: ect_on_fip_having_deferred_their_training) }
+        it { is_expected.to eql :has_deferred_their_training }
       end
 
       context "who has been withdrawn by their last school" do
         subject { described_class.call(participant_profile: ect_on_fip_withdrawn_from_programme) }
-        it { is_expected.to eql :withdrawn_from_programme }
+        it { is_expected.to eql :has_withdrawn_from_programme }
       end
     end
   end
--- spec/support/with_training_record_state_examples.rb
+++ spec/support/with_training_record_state_examples.rb
@@ -60,6 +60,26 @@ RSpec.shared_context "with Training Record state examples", shared_context: :met
       .participant_profile
   end
 
+  let(:ect_on_cip_having_deferred_their_training) do
+    NewSeeds::Scenarios::Participants::Ects::Ect
+      .new(school_cohort: cip_school.school_cohort)
+      .build
+      .with_validation_data
+      .with_eligibility
+      .with_induction_record(induction_programme: cip_school.school_cohort.default_induction_programme, training_status: "deferred")
+      .participant_profile
+  end
+
+  let(:ect_on_fip_having_deferred_their_training) do
+    NewSeeds::Scenarios::Participants::Ects::Ect
+      .new(school_cohort: fip_school.school_cohort)
+      .build
+      .with_validation_data
+      .with_eligibility
+      .with_induction_record(induction_programme: fip_school.school_cohort.default_induction_programme, training_status: "deferred")
+      .participant_profile
+  end
+
   let(:ect_on_cip_withdrawn_from_programme) do
     NewSeeds::Scenarios::Participants::Ects::Ect
       .new(school_cohort: cip_school.school_cohort)
