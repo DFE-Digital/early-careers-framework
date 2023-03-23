05a350cbffd3410055ab9a64178ba8298fb08de4
matt <mattonfoot@gmail.com>
2023-03-23 13:14:05 +0000

    pull school status tag logic into service object

--- app/components/schools/participants/status.rb
+++ app/components/schools/participants/status.rb
@@ -5,15 +5,12 @@ module Schools
     class Status < BaseComponent
       def initialize(participant_profile:)
         @participant_profile = participant_profile
+        @induction_record = nil
       end
 
     private
 
-      attr_reader :participant_profile
-
-      delegate :school_cohort, to: :participant_profile
-      delegate :core_induction_programme?, to: :school_cohort
-      delegate :delivery_partner, to: :school_cohort
+      attr_reader :participant_profile, :induction_record
 
       def heading
         t :header, scope: translation_scope
@@ -24,62 +21,14 @@ module Schools
       end
 
       def translation_scope
-        @translation_scope ||= "schools.participants.status.#{profile_status}"
+        @translation_scope ||= "schools.participants.status.#{record_state}"
       end
 
-      def profile_status
-        return awaiting_validation_status if participant_profile.ecf_participant_validation_data.blank?
-        return :eligible_cip if core_induction_programme?
-
-        FeatureFlag.active?(:eligibility_notifications) ? fip_validation_status : :checking_eligibility
-      end
-
-      def fip_validation_status
-        if (eligibility = participant_profile.ecf_participant_eligibility)
-          return fip_eligible_status if eligibility.eligible_status? || eligibility.duplicate_profile_reason?
-          return fip_ineligible_status(eligibility) if eligibility.ineligible_status?
-          return fip_manual_check_status(eligibility) if eligibility.manual_check_status?
-          return fip_manual_check_status(eligibility) if eligibility.manual_check_status?
-        end
-        :checking_eligibility
-      end
-
-      def fip_eligible_status
-        delivery_partner ? :eligible_fip : :eligible_fip_no_partner
-      end
-
-      def fip_manual_check_status(eligibility)
-        if eligibility.no_qts_reason?
-          participant_profile.ect? ? :fip_ect_no_qts : :checking_eligibility
-        else
-          :checking_eligibility
-        end
-      end
-
-      def fip_ineligible_status(eligibility)
-        case eligibility.reason
-        when "previous_induction"
-          :ineligible_previous_induction
-        when "previous_participation"
-          :ero_mentor
-        when "active_flags"
-          :ineligible_flag
-        else
-          :ineligible_generic
-        end
-      end
-
-      def awaiting_validation_status
-        return :details_required if latest_email&.delivered?
-        return :request_for_details_failed if latest_email&.failed?
-
-        :request_to_be_sent
-      end
-
-      def latest_email
-        return @latest_email if defined?(@latest_email)
-
-        @latest_email = Email.associated_with(participant_profile).tagged_with(:request_for_details).latest
+      def record_state
+        @record_state ||= SchoolParticipantsStatusTagStatus.new(
+          participant_profile:,
+          induction_record:,
+        ).record_state
       end
     end
   end
---
+++ spec/services/school_participants_status_tag_status_spec.rb
@@ -0,0 +1,174 @@
+# frozen_string_literal: true
+
+RSpec.describe SchoolParticipantsStatusTagStatus, with_feature_flags: { eligibility_notifications: "active" } do
+  subject { described_class.new(participant_profile:).record_state }
+
+  context "when an email has been sent but the participant has not validated" do
+    let(:participant_profile) { create(:ect_participant_profile, :email_sent) }
+
+    it { is_expected.to eq :details_required } # "details_required"
+  end
+
+  context "when an email bounced" do
+    let(:participant_profile) { create(:ect_participant_profile, :email_bounced) }
+
+    it { is_expected.to eq :request_for_details_failed } #  "request_for_details_failed"
+  end
+
+  context "when no email has been sent" do
+    let(:participant_profile) { create(:ect_participant_profile) }
+
+    it { is_expected.to eq :request_to_be_sent } #  request_to_be_sent
+  end
+
+  context "when the participant is doing FIP" do
+    let(:school_cohort) { create(:school_cohort, :fip) }
+
+    context "when the participant is an ECT" do
+      let(:participant_profile) { create(:ect_participant_profile, school_cohort:) }
+      let!(:validation_data) { create(:ecf_participant_validation_data, participant_profile:) }
+
+      context "when the participant is eligible" do
+        let!(:eligibility) { create(:ecf_participant_eligibility, participant_profile:) }
+
+        it { is_expected.to eq :eligible_fip_no_partner } #  eligible_fip_no_partner
+      end
+
+      context "when the participant has no QTS" do
+        let!(:eligibility) { create(:ecf_participant_eligibility, :no_qts_state, participant_profile:) }
+
+        it { is_expected.to eq :fip_ect_no_qts } # fip_ect_no_qts
+      end
+
+      context "when the participant has a previous induction" do
+        let!(:eligibility) { create(:ecf_participant_eligibility, :previous_induction_state, participant_profile:) }
+
+        it { is_expected.to eq :ineligible_previous_induction } # ineligible_previous_induction
+      end
+
+      context "when the participant has a TRN mismatch" do
+        let!(:eligibility) { create(:ecf_participant_eligibility, :different_trn_state, participant_profile:) }
+
+        it { is_expected.to eq :checking_eligibility } #  checking_eligibility
+      end
+
+      context "when the participant has active flags and manual check status" do
+        let!(:eligibility) { create(:ecf_participant_eligibility, :active_flags_state, participant_profile:) }
+
+        it { is_expected.to eq :checking_eligibility } #  checking_eligibility
+      end
+    end
+
+    context "when the participant is a mentor" do
+      let(:participant_profile) { create(:mentor_participant_profile, school_cohort:) }
+      let!(:validation_data) { create(:ecf_participant_validation_data, participant_profile:) }
+
+      context "when the participant is eligible" do
+        let!(:eligibility) { create(:ecf_participant_eligibility, participant_profile:) }
+
+        it { is_expected.to eq :eligible_fip_no_partner } #  eligible_fip_no_partner
+      end
+
+      context "when the participant has a previous participation (ERO)" do
+        let!(:eligibility) { create(:ecf_participant_eligibility, :previous_participation_state, participant_profile:) }
+
+        it { is_expected.to eq :ero_mentor } #  ero_mentor - TODO: does this check mentor participation
+      end
+
+      context "when the participant has a TRN mismatch" do
+        let!(:eligibility) { create(:ecf_participant_eligibility, :different_trn_state, participant_profile:) }
+
+        it { is_expected.to eq :checking_eligibility } #  checking_eligibility
+      end
+
+      context "when the participant is a duplicate profile" do
+        let(:participant_profile) { create(:mentor_participant_profile, :secondary_profile, school_cohort:) }
+        let!(:eligibility) { create(:ecf_participant_eligibility, :secondary_profile_state, participant_profile:) }
+
+        before { participant_profile.reload }
+
+        it { is_expected.to eq :eligible_fip_no_partner } #  eligible_fip_no_partner
+      end
+
+      context "when the participant has active flags and manual check status" do
+        let!(:eligibility) { create(:ecf_participant_eligibility, :active_flags_state, participant_profile:) }
+
+        it { is_expected.to eq :checking_eligibility } #  checking_eligibility
+      end
+    end
+  end
+
+  context "when the participant is doing CIP" do
+    let(:school_cohort) { create(:school_cohort, :cip) }
+
+    context "when the participant is an ECT" do
+      let(:participant_profile) { create(:ect_participant_profile, school_cohort:) }
+      let!(:validation_data) { create(:ecf_participant_validation_data, participant_profile:) }
+
+      context "when the participant is eligible" do
+        let!(:eligibility) { create(:ecf_participant_eligibility, participant_profile:) }
+
+        it { is_expected.to eq :eligible_cip } #  eligible_cip
+      end
+
+      context "when the participant has no QTS" do
+        let!(:eligibility) { create(:ecf_participant_eligibility, :no_qts_state, participant_profile:) }
+
+        it { is_expected.to eq :eligible_cip } #  eligible_cip
+      end
+
+      context "when the participant has a previous induction" do
+        let!(:eligibility) { create(:ecf_participant_eligibility, :previous_induction_state, participant_profile:) }
+
+        it { is_expected.to eq :eligible_cip } #  eligible_cip
+      end
+
+      context "when the participant has a TRN mismatch" do
+        let!(:eligibility) { create(:ecf_participant_eligibility, :different_trn_state, participant_profile:) }
+
+        it { is_expected.to eq :eligible_cip } #  eligible_cip
+      end
+
+      context "when the participant has active flags and manual check status" do
+        let!(:eligibility) { create(:ecf_participant_eligibility, :active_flags_state, participant_profile:) }
+
+        it { is_expected.to eq :eligible_cip } #  eligible_cip
+      end
+    end
+
+    context "when the participant is a mentor" do
+      let(:participant_profile) { create(:mentor_participant_profile, school_cohort:) }
+      let!(:validation_data) { create(:ecf_participant_validation_data, participant_profile:) }
+
+      context "when the participant is eligible" do
+        let!(:eligibility) { create(:ecf_participant_eligibility, participant_profile:) }
+
+        it { is_expected.to eq :eligible_cip } #  eligible_cip
+      end
+
+      context "when the participant has no QTS" do
+        let!(:eligibility) { create(:ecf_participant_eligibility, :no_qts_state, participant_profile:) }
+
+        it { is_expected.to eq :eligible_cip } #  eligible_cip
+      end
+
+      context "when the participant has a previous participation (ERO)" do
+        let!(:eligibility) { create(:ecf_participant_eligibility, :previous_participation_state, participant_profile:) }
+
+        it { is_expected.to eq :eligible_cip } #  eligible_cip
+      end
+
+      context "when the participant has a TRN mismatch" do
+        let!(:eligibility) { create(:ecf_participant_eligibility, :different_trn_state, participant_profile:) }
+
+        it { is_expected.to eq :eligible_cip } #  eligible_cip
+      end
+
+      context "when the participant has active flags and manual check status" do
+        let!(:eligibility) { create(:ecf_participant_eligibility, :active_flags_state, participant_profile:) }
+
+        it { is_expected.to eq :eligible_cip } #  eligible_cip
+      end
+    end
+  end
+end
