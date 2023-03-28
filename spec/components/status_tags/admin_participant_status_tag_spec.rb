190308585cf71e9fa35e2049ab300e7007a63d98
matt <mattonfoot@gmail.com>
2023-03-28 13:53:45 +0100

    add mentor with duplicate profile scenarios and align admin tests

--- spec/components/status_tags/admin_participant_status_tag_spec.rb
+++ spec/components/status_tags/admin_participant_status_tag_spec.rb
@@ -1,107 +1,85 @@
 # frozen_string_literal: true
 
-RSpec.describe StatusTags::AdminParticipantStatusTag, type: :component do
-  let!(:participant_profile) { create :ect_participant_profile }
-  let(:component) { described_class.new participant_profile: }
-
-  subject { render_inline(component) }
+RSpec.describe StatusTags::AdminParticipantStatusTag, :with_training_record_state_examples, type: :component do
+  subject { render_inline described_class.new(participant_profile:) }
 
   context "when the request for details has not been sent yet" do
+    let(:participant_profile) { ect_on_fip_no_validation }
     it { is_expected.to have_selector(".govuk-tag.govuk-tag--grey", exact_text: "Contacted for information") }
   end
 
   context "with a request for details email record" do
-    let!(:email) { create :email, tags: %i[request_for_details], associated_with: participant_profile, status: email_status }
-
     context "which has been successfully delivered" do
-      let(:email_status) { :delivered }
-
+      let(:participant_profile) { ect_on_fip_details_request_delivered }
       it { is_expected.to have_selector(".govuk-tag.govuk-tag--grey", exact_text: "Contacted for information") }
     end
 
     context "which has failed to be deliver" do
-      let(:email_status) { Email::FAILED_STATUSES.sample }
-
+      let(:participant_profile) { ect_on_fip_details_request_failed }
       it { is_expected.to have_selector(".govuk-tag.govuk-tag--grey", exact_text: "Check email address") }
     end
 
     context "which is still pending" do
-      let(:email_status) { :submitted }
-
+      let(:participant_profile) { ect_on_fip_details_request_submitted }
       it { is_expected.to have_selector(".govuk-tag.govuk-tag--grey", exact_text: "Contacted for information") }
     end
   end
 
   context "mentor with multiple profiles" do
-    let(:school_cohort) { create(:school_cohort) }
-
     context "when the primary profile is eligible" do
-      let(:participant_profile) { create(:mentor_participant_profile, :primary_profile, school_cohort:) }
-      let!(:ecf_participant_eligibility) { create(:ecf_participant_eligibility, participant_profile:) }
-
-      before { participant_profile.reload }
+      let(:participant_profile) { mentor_profile_duplicity_primary }
 
       it { is_expected.to have_selector(".govuk-tag.govuk-tag--green", exact_text: "Eligible: Mentor at main school") }
     end
 
     context "when the secondary profile is ineligible because it is a duplicate" do
-      let(:participant_profile) { create(:mentor_participant_profile, :secondary_profile, school_cohort:) }
-      let!(:ecf_participant_eligibility) { create(:ecf_participant_eligibility, :secondary_profile_state, participant_profile:) }
-
-      before { participant_profile.reload }
-
+      let(:participant_profile) { mentor_profile_duplicity_secondary }
       it { is_expected.to have_selector(".govuk-tag.govuk-tag--green", exact_text: "Eligible: Mentor at additional school") }
     end
   end
 
   context "full induction programme participant" do
     context "has submitted validation data" do
-      let(:school_cohort) { create(:school_cohort, :fip) }
-      let(:participant_profile) { create(:ect_participant_profile, school_cohort:) }
-      let!(:ecf_participant_eligibility) { create(:ecf_participant_eligibility, participant_profile:) }
-
+      let(:participant_profile) { ect_on_fip }
       it { is_expected.to have_selector(".govuk-tag.govuk-tag--green", exact_text: "Eligible to start") }
     end
 
     context "was a participant in early roll out" do
-      let(:school_cohort) { create(:school_cohort, :fip) }
-      let(:participant_profile) { create(:mentor_participant_profile, school_cohort:) }
-      let!(:ecf_participant_eligibility) { create(:ecf_participant_eligibility, :previous_participation_state, participant_profile:) }
-
+      let(:participant_profile) { mentor_ineligible_previous_participation }
       it { is_expected.to have_selector(".govuk-tag.govuk-tag--green", exact_text: "Eligible to start: ERO") }
+    end
+
+    context "has a withdrawn status" do
+      context "when there is not induction record to use" do
+        let(:participant_profile) { ect_on_fip_withdrawn_no_induction_record }
+        it { is_expected.to have_selector(".govuk-tag.govuk-tag--red", exact_text: "Withdrawn by provider") }
+      end
+
+      context "when an active induction record is available" do
+        let(:participant_profile) { ect_on_fip_enrolled_after_withdraw }
+        it { is_expected.to have_selector(".govuk-tag.govuk-tag--green", exact_text: "Eligible to start") }
+      end
     end
   end
 
   context "core induction programme participant" do
     context "has submitted validation data" do
-      let(:school_cohort) { create(:school_cohort, :cip) }
-      let(:participant_profile) { create(:ect_participant_profile, school_cohort:) }
-      let!(:ecf_participant_eligibility) { create(:ecf_participant_eligibility, :manual_check, participant_profile:) }
-
+      let(:participant_profile) { ect_on_fip_no_tra_record }
       it { is_expected.to have_selector(".govuk-tag.govuk-tag--orange", exact_text: "DfE checking eligibility") }
     end
 
     context "has a previous induction reason" do
-      let(:school_cohort) { create(:school_cohort, :cip) }
-      let(:participant_profile) { create(:ect_participant_profile, school_cohort:) }
-      let!(:ecf_participant_eligibility) { create(:ecf_participant_eligibility, :previous_induction_state, participant_profile:) }
-
+      let(:participant_profile) { ect_on_cip_ineligible_previous_induction }
       it { is_expected.to have_selector(".govuk-tag.govuk-tag--red", exact_text: "Not eligible: NQT+1") }
     end
 
     context "has no QTS reason" do
-      let(:school_cohort) { create(:school_cohort, :cip) }
-      let(:participant_profile) { create(:ect_participant_profile, school_cohort:) }
-      let!(:ecf_participant_eligibility) { create(:ecf_participant_eligibility, :no_qts_state, participant_profile:) }
-
+      let(:participant_profile) { ect_on_cip_manual_check_no_qts }
       it { is_expected.to have_selector(".govuk-tag.govuk-tag--red", exact_text: "Not eligible: No QTS") }
     end
 
     context "has an ineligible status" do
-      let(:school_cohort) { create(:school_cohort, :cip) }
-      let(:participant_profile) { create(:ect_participant_profile, school_cohort:) }
-      let!(:ecf_participant_eligibility) { create(:ecf_participant_eligibility, :ineligible, participant_profile:) }
-
+      let(:participant_profile) { ect_on_cip_ineligible_previous_participation }
       it { is_expected.to have_selector(".govuk-tag.govuk-tag--red", exact_text: "Not eligible") }
     end
 
