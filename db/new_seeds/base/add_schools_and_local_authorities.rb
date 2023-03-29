99d87dd7670b99a226a07fb6fd96b54f35e21059
Steve Lorek <steve@stevelorek.com>
2023-03-29 16:27:09 +0100

    Create schools in all cohorts. Small refactor

--- db/new_seeds/base/add_schools_and_local_authorities.rb
+++ db/new_seeds/base/add_schools_and_local_authorities.rb
@@ -2,16 +2,19 @@
 
 require Rails.root.join("db/new_seeds/util/seed_utils")
 
-def add_school_to_local_authority(school:, local_authority:, lead_providers:, cohorts:, nomination_email: false)
+@cohorts = Cohort.all
+@lead_providers = LeadProvider.all
+
+def add_school_to_local_authority(school:, local_authority:, nomination_email: false)
   FactoryBot.create(:seed_school_local_authority, school:, local_authority:)
   FactoryBot.create(:seed_induction_coordinator_profile, :with_user).tap do |induction_coordinator_profile|
     FactoryBot.create(:seed_induction_coordinator_profiles_school, induction_coordinator_profile:, school:)
 
-    cohorts.sample(Random.rand(1..cohorts.length)).each do |cohort|
+    @cohorts.sample(Random.rand(1..@cohorts.length)).each do |cohort|
       FactoryBot.create(:seed_school_cohort, school:, cohort:)
     end
 
-    lead_providers.sample.tap do |lead_provider|
+    @lead_providers.sample.tap do |lead_provider|
       scenarios = Random.rand(1..4).times.map do
         NewSeeds::Scenarios::Participants::Mentors::MentoringMultipleEctsWithSameProvider
           .new(school:, lead_provider:)
@@ -32,12 +35,9 @@ end
 # create some local authorities
 local_authorities = FactoryBot.create_list(:local_authority, seed_quantity(:local_authorities))
 
-cohorts = Cohort.where(start_year: [2021, 2022])
-lead_providers = LeadProvider.all
-
 # add some random schools to each LA
 local_authorities.each do |local_authority|
-  add_school_to_local_authority(school: FactoryBot.create(:seed_school), local_authority:, cohorts:, lead_providers:)
+  add_school_to_local_authority(school: FactoryBot.create(:seed_school), local_authority:)
 end
 
 # and add some with the old 'test' school format so they're easily findable in dev
@@ -51,8 +51,6 @@ end
       primary_contact_email: "cpd-test+school-#{i}@digital.education.gov.uk",
     ),
     local_authority: local_authorities.sample,
-    cohorts:,
-    lead_providers:,
 
     # this reimplements a feature of the legacy seeds
     # where 'ZZ Test School 3' has a NominationEmail record
