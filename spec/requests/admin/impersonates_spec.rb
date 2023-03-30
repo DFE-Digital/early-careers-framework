6b7a80c95aa18b461b43641376d741487b916dac
Abeer Salameh <cwrw@users.noreply.github.com>
2023-03-30 18:51:54 +0300

    Revert "Send impersonator back to starting point when done impersonating"

--- spec/requests/admin/impersonates_spec.rb
+++ spec/requests/admin/impersonates_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe "Admin::Participants", type: :request do
   let(:admin_user) { create(:user, :admin) }
   let(:other_admin_user) { create(:user, :admin) }
   let!(:cohort) { create(:cohort, :current) }
-  let!(:induction_coordinator) { create(:user, :induction_coordinator) }
+  let!(:user) { create(:user, :induction_coordinator) }
 
   before do
     sign_in admin_user
@@ -14,51 +14,54 @@ RSpec.describe "Admin::Participants", type: :request do
 
   describe "POST /admin/impersonate" do
     it "sets impersonation session" do
-      when_i_impersonate(induction_coordinator)
-      expect(session[:impersonated_user_id]).to eql(induction_coordinator.id.to_s)
+      when_i_impersonate(user)
+      expect(session[:impersonated_user_id]).to eql(user.id.to_s)
     end
 
     it "redirects to impersonated user start page" do
-      when_i_impersonate(induction_coordinator)
+      when_i_impersonate(user)
       follow_redirect!
-      expect(response).to redirect_to(schools_choose_programme_path(induction_coordinator.school, cohort.start_year))
+      expect(response).to redirect_to(schools_choose_programme_path(user.school, cohort.start_year))
     end
 
     it "errors if you impersonate yourself" do
       when_i_impersonate(admin_user)
-      expect(response).to redirect_to(admin_school_path(induction_coordinator.school))
+      follow_redirect!
+      expect(response).to redirect_to "/admin/schools"
       expect(flash[:warning]).to eql("You cannot impersonate yourself")
     end
 
     it "errors if you impersonate an admin" do
       when_i_impersonate(other_admin_user)
-      expect(response).to redirect_to(admin_school_path(induction_coordinator.school))
+      follow_redirect!
+      expect(response).to redirect_to "/admin/schools"
       expect(flash[:warning]).to eql("You cannot impersonate another admin user")
     end
   end
 
   describe "DELETE /admin/impersonate" do
     before do
-      when_i_impersonate(induction_coordinator)
+      when_i_impersonate(user)
     end
 
     it "clears impersonation session" do
-      when_i_stop_impersonating(induction_coordinator)
+      when_i_stop_impersonating(user)
       expect(session[:impersonated_user_id]).to be_blank
     end
 
-    it "redirects to impersonation starting point" do
-      when_i_stop_impersonating(induction_coordinator)
-      expect(response).to redirect_to(admin_school_path(induction_coordinator.school))
+    it "redirects to support user start page" do
+      when_i_stop_impersonating(user)
+      follow_redirect!
+      expect(response).to redirect_to("/admin/schools")
     end
   end
 
 private
 
   def when_i_impersonate(user)
-    post "/admin/impersonate",
-         params: { impersonated_user_id: user&.id },
-         headers: { "HTTP_REFERER" => admin_school_path(induction_coordinator.school) }
+    post "/admin/impersonate", params: {
+      impersonated_user_id: user&.id,
+    }
   end
 
   def when_i_stop_impersonating(user)
