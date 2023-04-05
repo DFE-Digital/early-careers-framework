85e90e9b2ad10058716cfe1e796d2d7303b8ca5c
Lorenzo Tello <ltello8a@gmail.com>
2023-04-05 20:13:38 +0200

    fix tests

--- spec/support/shared_examples/manage_fip_partnered_participants.rb
+++ spec/support/shared_examples/manage_fip_partnered_participants.rb
@@ -21,9 +21,6 @@ RSpec.shared_examples "manage fip participants example", js: true do
     scenario "Induction coordinators can view and manage participant" do
       given_i_am_taken_to_fip_induction_dashboard
       when_i_navigate_to_participants_dashboard
-      click_on "Not training"
-      then_i_can_view_ineligible_participants
-
       when_i_click_on_the_participants_name "Ineligible With-mentor"
       then_i_am_taken_to_view_details_page
       then_i_can_view_ineligible_participant_status
@@ -40,9 +37,6 @@ RSpec.shared_examples "manage fip participants example", js: true do
     scenario "Induction coordinators can view and manage participant" do
       given_i_am_taken_to_fip_induction_dashboard
       when_i_navigate_to_participants_dashboard
-      click_on "Not training"
-      then_i_can_view_ineligible_participants
-
       when_i_click_on_the_participants_name "Ineligible Without-mentor"
       then_i_am_taken_to_view_details_page
       then_i_can_view_ineligible_participant_status
@@ -59,9 +53,6 @@ RSpec.shared_examples "manage fip participants example", js: true do
     scenario "Induction coordinators can view and manage participant" do
       given_i_am_taken_to_fip_induction_dashboard
       when_i_navigate_to_participants_dashboard
-      click_on "Not training"
-      then_i_can_view_ineligible_participants
-
       when_i_click_on_the_participants_name "Ineligible mentor"
       then_i_am_taken_to_view_details_page
       then_i_can_view_ineligible_participant_status
@@ -78,9 +69,6 @@ RSpec.shared_examples "manage fip participants example", js: true do
     scenario "Induction coordinators can view and manage participant" do
       given_i_am_taken_to_fip_induction_dashboard
       when_i_navigate_to_participants_dashboard
-      click_on "Mentors"
-      then_i_can_view_eligible_participants
-
       when_i_click_on_the_participants_name "ero mentor"
       then_i_am_taken_to_view_details_page
       then_i_can_see_ero_status
@@ -98,8 +86,6 @@ RSpec.shared_examples "manage fip participants example", js: true do
     scenario "Induction coordinators can view and manage participant" do
       given_i_am_taken_to_fip_induction_dashboard
       when_i_navigate_to_participants_dashboard
-      then_i_can_view_eligible_participants
-
       when_i_click_on_the_participants_name "Eligible With-mentor"
       then_i_am_taken_to_view_details_page
       then_i_can_view_eligible_fip_partnered_ect_status
@@ -116,8 +102,6 @@ RSpec.shared_examples "manage fip participants example", js: true do
     scenario "Induction coordinators can view and manage participant" do
       given_i_am_taken_to_fip_induction_dashboard
       when_i_navigate_to_participants_dashboard
-      then_i_can_view_eligible_participants
-
       when_i_click_on_the_participants_name "Eligible Without-mentor"
       then_i_am_taken_to_view_details_page
       then_i_can_view_eligible_fip_partnered_ect_status
@@ -134,11 +118,9 @@ RSpec.shared_examples "manage fip participants example", js: true do
     scenario "Induction coordinators can view and manage participant" do
       given_i_am_taken_to_fip_induction_dashboard
       when_i_navigate_to_participants_dashboard
-      then_i_can_view_eligible_participants
-
       when_i_click_on_the_participants_name "Eligible mentor"
       then_i_am_taken_to_view_details_page
-      then_i_can_view_eligible_fip_partnered_ect_status
+      then_i_can_view_eligible_fip_partnered_mentor_status
     end
   end
 
@@ -153,8 +135,6 @@ RSpec.shared_examples "manage fip participants example", js: true do
     scenario "Induction coordinators can view and manage participant" do
       given_i_am_taken_to_fip_induction_dashboard
       when_i_navigate_to_participants_dashboard
-      then_i_can_view_contacted_for_info_participants
-
       when_i_click_on_the_participants_name "CFI With-mentor"
       then_i_am_taken_to_view_details_page
       then_i_can_view_contacted_for_info_status
@@ -171,8 +151,6 @@ RSpec.shared_examples "manage fip participants example", js: true do
     scenario "Induction coordinators can view and manage participant" do
       given_i_am_taken_to_fip_induction_dashboard
       when_i_navigate_to_participants_dashboard
-      then_i_can_view_contacted_for_info_participants
-
       when_i_click_on_the_participants_name "CFI Without-mentor"
       then_i_am_taken_to_view_details_page
       then_i_can_view_contacted_for_info_bounced_email_status
@@ -189,8 +167,6 @@ RSpec.shared_examples "manage fip participants example", js: true do
     scenario "Induction coordinators can view and manage participant" do
       given_i_am_taken_to_fip_induction_dashboard
       when_i_navigate_to_participants_dashboard
-      then_i_can_view_contacted_for_info_participants
-
       when_i_click_on_the_participants_name "CFI Mentor"
       then_i_am_taken_to_view_details_page
       then_i_can_view_contacted_for_info_status
@@ -208,9 +184,6 @@ RSpec.shared_examples "manage fip participants example", js: true do
     scenario "Induction coordinators can view and manage participant" do
       given_i_am_taken_to_fip_induction_dashboard
       when_i_navigate_to_participants_dashboard
-
-      then_i_can_view_details_being_checked_participants
-
       when_i_click_on_the_participants_name "DBC With-Mentor"
       then_i_am_taken_to_view_details_page
       then_i_can_view_details_being_checked_status
@@ -227,8 +200,6 @@ RSpec.shared_examples "manage fip participants example", js: true do
     scenario "Induction coordinators can view and manage participant" do
       given_i_am_taken_to_fip_induction_dashboard
       when_i_navigate_to_participants_dashboard
-      then_i_can_view_details_being_checked_participants
-
       when_i_click_on_the_participants_name "DBC Without-Mentor"
       then_i_am_taken_to_view_details_page
       then_i_can_view_details_being_checked_status
@@ -245,8 +216,6 @@ RSpec.shared_examples "manage fip participants example", js: true do
     scenario "Induction coordinators can view and manage participant" do
       given_i_am_taken_to_fip_induction_dashboard
       when_i_navigate_to_participants_dashboard
-      then_i_can_view_details_being_checked_participants
-
       when_i_click_on_the_participants_name "DBC Mentor"
       then_i_am_taken_to_view_details_page
       then_i_can_view_details_being_checked_status
