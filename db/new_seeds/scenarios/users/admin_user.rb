c8159befb176c4674f39737b4c852d29e42dc9da
Peter Yates <peter.yates@graphia.co.uk>
2023-04-07 20:00:01 +0100

    Seed a super-user

--- db/new_seeds/scenarios/users/admin_user.rb
+++ db/new_seeds/scenarios/users/admin_user.rb
@@ -4,7 +4,7 @@ module NewSeeds
   module Scenarios
     module Users
       class AdminUser
-        attr_reader :user, :new_user_attributes
+        attr_reader :user, :new_user_attributes, :admin_profile
 
         def initialize(user: nil, full_name: nil, email: nil)
           @user = user
@@ -14,7 +14,15 @@ module NewSeeds
         def build
           admin_user = user || FactoryBot.create(:seed_user, **new_user_attributes)
 
-          FactoryBot.create(:seed_admin_profile, user: admin_user)
+          @admin_profile = FactoryBot.create(:seed_admin_profile, user: admin_user)
+
+          self
+        end
+
+        def with_super_user
+          admin_profile.update!(super_user: true)
+
+          self
         end
       end
     end
