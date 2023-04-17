5752dfbe4867606a9811569c0e82a79cbce7fea6
cwrw <abeer.salameh@gmail.com>
2023-04-17 23:08:37 +0100

    Amend Call off contract script
    Allow the script to accept a CSV or default to generated seed data.
    Using either example seed data or csv row data to create a call off
    contract and the relevant bands per lead provider and cohort.
    
    Also ensure band d has the correct output_payment_percantage and
    service_fee_percentage.

--- app/services/importers/create_call_off_contract.rb
+++ app/services/importers/create_call_off_contract.rb
@@ -3,45 +3,107 @@
 module Importers
   class CreateCallOffContract
     def call
-      LeadProvider.all.each do |lp|
-        [cohort_2021, cohort_2022, cohort_2023].each do |cohort|
-          sample_call_off_contract = CallOffContract.find_or_create_by!(
-            lead_provider: lp,
-            version: example_contract_data[:version] || "0.0.1",
-            uplift_target: example_contract_data[:uplift_target],
-            uplift_amount: example_contract_data[:uplift_amount],
-            recruitment_target: example_contract_data[:recruitment_target],
-            revised_target: example_contract_data[:revised_target],
-            set_up_fee: example_contract_data[:set_up_fee],
-            raw: example_contract_data.to_json,
-            cohort:,
-          )
-
-          %i[band_a band_b band_c band_d].each do |band|
-            src = example_contract_data[band]
-            ParticipantBand.find_or_create_by!(
-              call_off_contract: sample_call_off_contract,
-              min: src[:min],
-              max: src[:max],
-              per_participant: src[:per_participant],
-            )
-          end
-        end
+      if path_to_csv.present?
+        create_call_off_contracts_from_csv
+      else
+        create_default_call_off_contracts
       end
     end
 
   private
 
-    def cohort_2021
-      @cohort_2021 ||= Cohort.find_or_create_by!(start_year: 2021)
+    attr_reader :path_to_csv, :logger
+
+    def initialize(path_to_csv: nil, logger: Rails.logger)
+      @path_to_csv = path_to_csv
+      @logger = logger
     end
 
-    def cohort_2022
-      @cohort_2022 ||= Cohort.find_or_create_by!(start_year: 2022)
+    def create_default_call_off_contracts
+      raise "Do not seed default call off contracts in Production!" if Rails.env.production?
+
+      LeadProvider.all.each do |lead_provider|
+        [cohort_current, cohort_previous, cohort_next].each do |cohort|
+          create_call_off_contract_and_bands(lead_provider:, cohort:, contract_data: example_contract_data)
+        end
+      end
     end
 
-    def cohort_2023
-      @cohort_2023 ||= Cohort.find_or_create_by!(start_year: 2023)
+    def create_call_off_contracts_from_csv
+      check_headers!
+
+      ActiveRecord::Base.transaction do
+        rows.each do |row|
+          cohort = Cohort.find_by!(start_year: row["cohort-start-year"].to_i)
+          lead_provider = LeadProvider.joins(:cohorts).find_by!(name: row["lead-provider-name"], cohorts: { start_year: row["cohort-start-year"].to_i })
+
+          call_off_contract = CallOffContract.find_by(
+            lead_provider:,
+            cohort:,
+          )
+
+          next if call_off_contract
+
+          contract_data = build_contract_data(row)
+          create_call_off_contract_and_bands(lead_provider:, cohort:, contract_data:)
+        end
+      end
+    end
+
+    def create_call_off_contract_and_bands(lead_provider:, cohort:, contract_data:)
+      call_off_contract = create_call_off_contract!(lead_provider:, contract_data:, cohort:)
+
+      %i[band_a band_b band_c band_d].each do |band|
+        band_data = contract_data[band]
+        create_participant_band!(call_off_contract:, band_data:, band_d: band == :band_d)
+      end
+    end
+
+    def check_headers!
+      unless %w[lead-provider-name cohort-start-year uplift-target uplift-amount recruitment-target revised-target set-up-fee band-a-min band-a-max band-a-per-participant band-b-min band-b-max band-b-per-participant band-c-min band-c-max band-c-per-participant band-d-min band-d-max band-d-per-participant].all? { |header| rows.headers.include?(header) }
+        raise NameError, "Invalid headers"
+      end
+    end
+
+    def rows
+      @rows ||= CSV.read(path_to_csv, headers: true)
+    end
+
+    def create_call_off_contract!(lead_provider:, contract_data:, cohort:)
+      CallOffContract.create!(
+        lead_provider:,
+        cohort:,
+        uplift_target: contract_data[:uplift_target],
+        uplift_amount: contract_data[:uplift_amount],
+        recruitment_target: contract_data[:recruitment_target],
+        revised_target: contract_data[:revised_target],
+        set_up_fee: contract_data[:set_up_fee],
+        raw: contract_data.to_json,
+      )
+    end
+
+    def create_participant_band!(call_off_contract:, band_data:, band_d: false)
+      attributes = {
+        call_off_contract:,
+        min: band_data[:min],
+        max: band_data[:max],
+        per_participant: band_data[:per_participant],
+      }
+
+      attributes.merge!(output_payment_percantage: 100, service_fee_percentage: 0) if band_d
+      ParticipantBand.create!(**attributes)
+    end
+
+    def cohort_current
+      @cohort_current ||= Cohort.current || FactoryBot.create(:cohort, :current)
+    end
+
+    def cohort_previous
+      @cohort_previous ||= Cohort.previous || FactoryBot.create(:cohort, :previous)
+    end
+
+    def cohort_next
+      @cohort_next ||= Cohort.next || FactoryBot.create(:cohort, :next)
     end
 
     def example_contract_data
@@ -52,6 +114,7 @@ module Importers
         revised_target: (4500 * 1.02).to_i,
         set_up_fee: 0,
         band_a: {
+          min: 0,
           max: 10,
           per_participant: 995,
         },
@@ -69,6 +132,36 @@ module Importers
           min: 31,
           max: 40,
           per_participant: 966,
+        },
+      }
+    end
+
+    def build_contract_data(row)
+      {
+        uplift_target: row["uplift-target"],
+        uplift_amount: row["uplift-amount"],
+        recruitment_target: row["recruitment-target"],
+        revised_target: row["revised-target"],
+        set_up_fee: row["set-up-fee"],
+        band_a: {
+          min: row["band-a-min"],
+          max: row["band-a-max"],
+          per_participant: row["band-a-per-participant"],
+        },
+        band_b: {
+          min: row["band-b-min"],
+          max: row["band-b-max"],
+          per_participant: row["band-b-per-participant"],
+        },
+        band_c: {
+          min: row["band-c-min"],
+          max: row["band-c-max"],
+          per_participant: row["band-c-per-participant"],
+        },
+        band_d: {
+          min: row["band-d-min"],
+          max: row["band-d-max"],
+          per_participant: row["band-d-per-participant"],
         },
       }
     end
