c0deff5485524b0b7f96170e442630d4c36af1f4
Fabio Volpe <volpefabio@outlook.com>
2023-04-17 15:21:42 +0100

    spec fix

--- spec/features/schools/participants/add_participants/add_ect_with_appropriate_body_spec.rb
+++ spec/features/schools/participants/add_participants/add_ect_with_appropriate_body_spec.rb
@@ -152,6 +152,7 @@ private
       "state_name" => "Active",
       "dob" => Date.new(1998, 11, 22),
       "qualified_teacher_status" => { "qts_date" => 1.year.ago },
+      "induction_start_date" => Date.new(2022, 9, 1),
       "induction" => {
         "start_date" => 1.month.ago,
         "status" => "Active",
