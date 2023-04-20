9a7cd40c685853196f2bb2506560cdbb94071a37
Tony Headford <tony@binarycircus.com>
2023-04-20 17:24:10 +0100

    Add more magic dates for testing scenarios

--- app/services/dqt_record_check.rb
+++ app/services/dqt_record_check.rb
@@ -99,40 +99,53 @@ private
   end
 
   def magic_date_range
-    (Date.new(1900, 1, 1)..Date.new(1900, 1, 3))
+    magic_results.keys.map { |k| Date.new(1900, 1, k) }
+    # (Date.new(1900, 1, 1)..Date.new(1900, 1, 23))
   end
 
   def magic_response
-    magic_results[date_of_birth.day - 1]
+    magic_results[date_of_birth.day] || magic_results[1]
   end
 
   def magic_results
-    [
+    {
       # all matches - use birthdate 1/1/1900
-      CheckResult.new(magic_dqt_record, true, true, true, true, 4), # dqt_record/TRN/name/DoB/Nino/total matched
+      1 => CheckResult.new(magic_dqt_record, true, true, true, true, 4), # dqt_record/TRN/name/DoB/Nino/total matched
       # name and nino don't match  - use birthdate 2/1/1900
-      CheckResult.new(magic_dqt_record, true, false, true, false, 2),
+      2 => CheckResult.new(magic_dqt_record, true, false, true, false, 2),
       # did not match - use birthdate 3/1/1900
-      check_failure(:no_match_found),
-    ]
+      3 => check_failure(:no_match_found),
+      # matched but no QTS - use birthdate 4/1/1900
+      4 => CheckResult.new(magic_dqt_record(qts_date: nil), true, true, true, true, 4),
+      # matched but no induction - use birthdate 4/1/1900
+      5 => CheckResult.new(magic_dqt_record(with_induction: false), true, true, true, true, 4),
+      # matched but active flags - use birthdate 6/1/1900
+      6 => CheckResult.new(magic_dqt_record(active_alert: true), true, true, true, true, 4),
+      # all matches 21 cohort start date - use 21/1/1900
+      21 => CheckResult.new(magic_dqt_record(induction_start_date: Date.new(2021, 9, 1)), true, true, true, true, 4),
+      # all matches 22 cohort start date - use 22/1/1900
+      22 => CheckResult.new(magic_dqt_record(induction_start_date: Date.new(2022, 9, 1)), true, true, true, true, 4),
+      # all matches 23 cohort start date - use 23/1/1900
+      23 => CheckResult.new(magic_dqt_record(induction_start_date: Date.new(2023, 9, 1)), true, true, true, true, 4),
+    }
   end
 
-  def magic_dqt_record(with_induction: true)
+  def magic_dqt_record(with_induction: true, active_alert: false, qts_date: 2.years.ago, induction_start_date: 1.month.ago)
     record = {
       "trn" => TeacherReferenceNumber.new(trn).formatted_trn,
       "name" => full_name,
       "dob" => date_of_birth,
       "ni_number" => nino,
-      "active_alert" => false,
+      "active_alert" => active_alert,
       "state_name" => "Active",
       "qualified_teacher_status" => {
-        "qts_date" => 2.years.ago,
+        "qts_date" => qts_date,
       },
     }
 
     if with_induction
       record.merge!("induction" => {
-        "start_date" => 1.month.ago,
+        "start_date" => induction_start_date,
         "status" => "In Progress",
       })
     end
