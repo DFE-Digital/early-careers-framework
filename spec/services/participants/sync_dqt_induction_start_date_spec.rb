e02bb0bbc4ef471f0c11eadaef12f177a9d2745e
Fabio Volpe <volpefabio@outlook.com>
2023-04-24 11:40:19 +0100

    save error message specs

--- spec/services/participants/sync_dqt_induction_start_date_spec.rb
+++ spec/services/participants/sync_dqt_induction_start_date_spec.rb
@@ -75,6 +75,45 @@ RSpec.describe Participants::SyncDqtInductionStartDate, with_feature_flags: { co
     end
   end
 
+  context "when the school hasn't set the destination cohort" do
+    let(:dqt_induction_start_date) { Date.new(2023, 10, 2) }
+    let(:participant_profile) { create(:ect_participant_profile, induction_start_date: nil) }
+    let!(:induction_record) { create(:induction_record, participant_profile:) }
+    let!(:cohort) { create(:cohort, start_year: 2023) }
+
+    it "does not change the participant and save the error" do
+      expect {
+        described_class.call(dqt_induction_start_date, participant_profile)
+      }.to not_change(participant_profile, :induction_start_date)
+       .and not_change { participant_profile.induction_records.latest.cohort.start_year }
+
+      error = SyncDqtInductionStartDateError.find_by(participant_profile:)
+      expect(error).to be_present
+      expect(error.error_message)
+        .to include("Target school cohort")
+        .and include("not setup")
+    end
+  end
+
+  context "when an error is already present from a previous job" do
+    let(:dqt_induction_start_date) { Date.new(2023, 10, 2) }
+    let(:participant_profile) { create(:ect_participant_profile, induction_start_date: nil) }
+    let!(:induction_record) { create(:induction_record, participant_profile:) }
+    let!(:cohort) { create(:cohort, start_year: 2023) }
+    let!(:school_cohort) { create(:school_cohort, :with_induction_programme, :with_ecf_standard_schedule, cohort:, school: induction_record.school) }
+    let!(:error) { SyncDqtInductionStartDateError.create!(participant_profile:, error_message: "test message") }
+
+    it "delete the error if the participant is successfully processed" do
+      expect {
+        described_class.call(dqt_induction_start_date, participant_profile)
+      }.to change(participant_profile, :induction_start_date).to(dqt_induction_start_date)
+       .and change { participant_profile.induction_records.latest.cohort.start_year }.to(2023)
+
+      error = SyncDqtInductionStartDateError.find_by(participant_profile:)
+      expect(error).not_to be_present
+    end
+  end
+
   context "when 'cohortless_dashboard' feature flag is disabled" do
     let(:dqt_induction_start_date) { Date.new(2023, 10, 2) }
     let(:participant_profile) { create(:ect_participant_profile, induction_start_date: nil) }
