b7097bc0f8c9e389a24ebb783b8d49667dcaaa2e
Peter Yates <peter.yates@graphia.co.uk>
2023-05-10 11:43:40 +0100

    Seed two schools per delivery partner in dev envs
    This will allow them to simulate transferring a teacher between two schools

--- db/new_seeds/base/add_environment_specific_data.rb
+++ db/new_seeds/base/add_environment_specific_data.rb
@@ -4,7 +4,7 @@
 # familiarise themselves with the school user interface.
 if Rails.env.in?(%w[development deployed_development])
   delivery_partner_credentials = {
-    "Ambition Institute" => "ambitition-institute-sit@example.com",
+    "Ambition Institute" => "ambition-institute-sit-%d@example.com",
   }
 
   cohort = Cohort.current
@@ -12,11 +12,13 @@ if Rails.env.in?(%w[development deployed_development])
   delivery_partner_credentials.each do |name, email|
     delivery_partner = FactoryBot.create(:seed_delivery_partner, name: "#{name} Delivery Partner")
 
-    NewSeeds::Scenarios::Schools::School
-      .new(name: "#{name} Test School")
-      .build
-      .with_partnership_in(cohort:, delivery_partner:)
-      .with_an_induction_tutor(full_name: "#{name} Test SIT", email:)
-      .with_school_cohort_and_programme(cohort:, programme_type: :fip)
+    [1, 2].each do |i|
+      NewSeeds::Scenarios::Schools::School
+        .new(name: "#{name} Test School #{i}")
+        .build
+        .with_partnership_in(cohort:, delivery_partner:)
+        .with_an_induction_tutor(full_name: "#{name} Test SIT #{i}", email: email % Array.wrap(i))
+        .with_school_cohort_and_programme(cohort:, programme_type: :fip)
+    end
   end
 end
