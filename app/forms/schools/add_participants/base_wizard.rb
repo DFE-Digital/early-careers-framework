048a4ac1305e43f40d2e5108eadfd14f623e60e3
matt <mattonfoot@gmail.com>
2023-05-10 13:44:49 +0100

    fix view path when training is required to be set up

--- app/forms/schools/add_participants/base_wizard.rb
+++ app/forms/schools/add_participants/base_wizard.rb
@@ -119,7 +119,7 @@ module Schools
       # path to the most appropriate start point to set up training for the transfer
       def need_training_path
         if cohort_to_place_participant == ::Cohort.active_registration_cohort
-          schools_cohort_setup_start(school_id: school.slug, cohort_id: cohort_to_place_participant)
+          schools_cohort_setup_start_path(school_id: school.slug, cohort_id: cohort_to_place_participant)
         else
           schools_choose_programme_path(school_id: school.slug, cohort_id: cohort_to_place_participant)
         end
