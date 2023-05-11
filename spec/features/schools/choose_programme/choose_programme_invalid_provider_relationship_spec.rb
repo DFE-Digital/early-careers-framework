26851a5fb068b864cec6502dc7e28003cb1370c2
Lorenzo Tello <ltello8a@gmail.com>
2023-05-11 04:25:12 +0200

    fix tests

--- spec/features/schools/choose_programme/choose_programme_invalid_provider_relationship_spec.rb
+++ spec/features/schools/choose_programme/choose_programme_invalid_provider_relationship_spec.rb
@@ -12,51 +12,23 @@ RSpec.feature "Schools should be able to choose their programme", type: :feature
     and_i_am_signed_in_as_an_induction_coordinator
     when_i_start_programme_selection_for_next_cohort
     then_i_am_taken_to_what_we_need_to_know_to_setup_academic_year
+    when_i_click_continue
+    then_i_am_taken_to_ects_expected_in_next_academic_year_page
   end
 
   context "school cohort provider relationship for 2022 is invalid" do
-    scenario "school continues with same lead provider" do
-      when_i_choose_ects_expected
-      and_i_click_continue
-      then_i_am_taken_to_the_change_provider_page
-      and_i_see_the_lead_provider
-      and_i_see_the_delivery_partner
-
-      when_i_choose_no
-      and_i_click_continue
-      then_i_am_taken_to_the_provider_relationship_invalid_page
-      and_i_click_continue
-      then_i_am_taken_to_the_use_different_delivery_partner
-
-      when_i_choose_yes
-      and_i_click_continue
-      then_i_am_taken_to_the_appropriate_body_appointed_page
-
-      when_i_choose_no
-      and_i_click_continue
-      then_i_am_taken_to_the_complete_page
-
-      when_i_click_on_the_return_to_your_training_link
-      then_i_am_taken_to_the_manage_your_training_page
-      and_i_see_the_lead_provider
-      and_i_see_delivery_partner_to_be_confirmed
-    end
-
     scenario "school continues with different lead provider" do
       when_i_choose_ects_expected
       and_i_click_continue
-      then_i_am_taken_to_the_change_provider_page
-      and_i_see_the_lead_provider
-      and_i_see_the_delivery_partner
+      then_i_am_taken_to_the_lp_dp_relationship_has_changed_page
 
-      when_i_choose_no
-      and_i_click_continue
-      then_i_am_taken_to_the_provider_relationship_invalid_page
-      and_i_click_continue
-      then_i_am_taken_to_the_use_different_delivery_partner
+      when_i_click_the_confirm_button
+      then_i_am_taken_to_what_changes_page
 
-      when_i_choose_no
+      when_i_choose_to_form_a_new_partnership
       and_i_click_continue
+      then_i_am_taken_to_the_form_a_new_partnership_confirmation_page
+      when_i_click_the_confirm_button
       then_i_am_taken_to_the_appropriate_body_appointed_page
 
       when_i_choose_no
@@ -65,7 +37,7 @@ RSpec.feature "Schools should be able to choose their programme", type: :feature
 
       when_i_click_on_the_return_to_your_training_link
       then_i_am_taken_to_the_manage_your_training_page
-      and_i_see_training_provider_to_be_confirmed
+      and_i_see_the_lead_provider
       and_i_see_delivery_partner_to_be_confirmed
     end
   end
