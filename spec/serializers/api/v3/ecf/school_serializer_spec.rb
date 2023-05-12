2082bf0562db253071ea36fd851643225f0415b8
cwrw <abeer.salameh@gmail.com>
2023-05-12 02:46:11 +0100

    [CPLD-2165] Amend school serialiser to accept school
    Rename school cohort serialiser to school serialiser as it now
    accepts a school instead.
    
    We will pass in the cohort to filter induction programme choices
    as well as determine partnership

--- spec/serializers/api/v3/ecf/school_serializer_spec.rb
+++ spec/serializers/api/v3/ecf/school_serializer_spec.rb
@@ -5,13 +5,12 @@ require "rails_helper"
 module Api
   module V3
     module ECF
-      RSpec.describe SchoolCohortSerializer do
+      RSpec.describe SchoolSerializer do
         describe "serialization" do
-          let(:school_cohort) { create(:school_cohort) }
-          let(:school) { school_cohort.school }
-          let(:cohort) { school_cohort.cohort }
+          let(:school) { create(:school) }
+          let(:cohort) { create(:cohort) }
 
-          subject { described_class.new(school_cohort) }
+          subject { described_class.new(school, params: { cohort: }) }
 
           it "sets the ID as the school ID" do
             expect(subject.serializable_hash[:data][:id]).to eq(school.id)
@@ -35,7 +34,7 @@ module Api
 
           context "when the school is in partnership" do
             let(:lead_provider) { create(:lead_provider) }
-            let(:school_cohort) { create(:school_cohort, lead_provider:) }
+            let!(:school_cohort) { create(:school_cohort, lead_provider:, school:, cohort:) }
 
             it "returns in_partnership as true" do
               expect(subject.serializable_hash[:data][:attributes][:in_partnership]).to be true
@@ -48,11 +47,21 @@ module Api
             end
           end
 
-          it "returns the school cohort induction_programme_choice" do
-            expect(subject.serializable_hash[:data][:attributes][:induction_programme_choice]).to eq(school_cohort.induction_programme_choice)
+          context "when the school is already in the cohort" do
+            let!(:school_cohort) { create(:school_cohort, school:, cohort:) }
+            it "returns the school cohort induction_programme_choice" do
+              expect(subject.serializable_hash[:data][:attributes][:induction_programme_choice]).to eq(school_cohort.induction_programme_choice)
+            end
+          end
+
+          context "when school is not yet in the cohort" do
+            it "returns the an unknown induction programme choice" do
+              expect(subject.serializable_hash[:data][:attributes][:induction_programme_choice]).to eq("not_yet_known")
+            end
           end
 
           context "when the school is updated last" do
+            let(:school_cohort) { create(:school_cohort, school:, cohort:) }
             before do
               school_cohort
               Timecop.travel(1.day.from_now) { school.touch }
@@ -64,6 +73,8 @@ module Api
           end
 
           context "when the school cohort is updated last" do
+            let(:school_cohort) { create(:school_cohort, school:, cohort:) }
+
             before do
               school_cohort
               Timecop.travel(1.day.from_now) { school_cohort.touch }
@@ -71,6 +82,12 @@ module Api
 
             it "returns the school cohort updated_at" do
               expect(subject.serializable_hash[:data][:attributes][:updated_at]).to eq(school_cohort.updated_at.rfc3339)
+            end
+          end
+
+          context "when there is no school cohort" do
+            it "returns the school updated_at" do
+              expect(subject.serializable_hash[:data][:attributes][:updated_at]).to eq(school.updated_at.rfc3339)
             end
           end
         end
