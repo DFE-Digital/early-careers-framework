c7ccb126ba85c23b5219c4b017a248b239dd01d4
cwrw <abeer.salameh@gmail.com>
2023-05-16 09:55:36 +0100

    Disable papertrail in migrations as versions table is created later

--- db/migrate/20210302031858_add_cohorts_to_partnerships.rb
+++ db/migrate/20210302031858_add_cohorts_to_partnerships.rb
@@ -2,6 +2,10 @@
 
 class AddCohortsToPartnerships < ActiveRecord::Migration[6.1]
   def change
+    PaperTrail.request.disable_model(Cohort)
+
     add_reference :partnerships, :cohort, null: false, default: Cohort.find_or_create_by!(start_year: 2021).id, foreign_key: true
+
+    PaperTrail.request.enable_model(Cohort)
   end
 end
