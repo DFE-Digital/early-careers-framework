69db099cc236bf1677a439d0ea7e9c0b6a0b4011
matt <mattonfoot@gmail.com>
2023-05-18 15:42:26 +0100

    guard against mentors without induction records

--- app/services/dashboard/participants.rb
+++ app/services/dashboard/participants.rb
@@ -12,16 +12,8 @@ module Dashboard
       @mentors = process_participants
     end
 
-    def dashboard_school_cohorts
-      SchoolCohort.dashboard_for_school(school:, latest_year:)
-    end
-
     def ects
       @ects ||= mentors.values.flatten.compact + orphan_ects
-    end
-
-    def ects_mentored_by(mentor)
-      mentors[dashboard_mentoring_mentor(mentor)]
     end
 
     def no_qts
@@ -29,25 +21,14 @@ module Dashboard
     end
 
     def orphan_mentors
-      @orphan_mentors ||= school_mentors_not_mentoring.map do |school_mentor|
-        dashboard_mentor(school_mentor.participant_profile_id)
-      end
+      @orphan_mentors ||= induction_records.select(&:mentor?) - mentors.keys
+    end
+
+    def dashboard_school_cohorts
+      SchoolCohort.dashboard_for_school(school:, latest_year:)
     end
 
   private
-
-    def dashboard_mentoring_mentor(mentor)
-      return mentor if mentor.is_a?(Dashboard::Mentor)
-
-      mentors.keys.detect { |dashboard_mentor| dashboard_mentor.participant_profile_id == mentor.id }
-    end
-
-    def dashboard_mentor(profile_id)
-      induction_record = induction_records.detect { |ir| ir.participant_profile_id == profile_id }
-      participant_profile = ParticipantProfile.find(profile_id)
-
-      Dashboard::Mentor.new(induction_record:, participant_profile:)
-    end
 
     # List of relevant (current or transferring_in or transferred) induction record of each of the participant of
     # the school in the cohorts displayed by the dashboard
@@ -65,6 +46,12 @@ module Dashboard
       end
     end
 
+    def induction_record_of_profile(profile_id)
+      induction_records.detect do |induction_record|
+        induction_record.participant_profile_id == profile_id
+      end
+    end
+
     def no_qts?(induction_record)
       !induction_record.training_status_withdrawn? &&
         (induction_record.active? || induction_record.claimed_by_another_school?) &&
@@ -73,26 +60,16 @@ module Dashboard
         induction_record.participant_no_qts?
     end
 
-    def profile_ids_of_mentors_mentoring
-      mentors.keys.map(&:participant_profile_id)
-    end
-
     def process_participants
       induction_records
         .select(&:ect?)
         .group_by(&:mentor_profile_id)
         .each_with_object({}) do |(mentor_profile_id, ects), hash|
         if mentor_profile_id
-          hash[dashboard_mentor(mentor_profile_id)] = ects
+          hash[induction_record_of_profile(mentor_profile_id)] = ects
         else
           @orphan_ects = ects
         end
-      end
-    end
-
-    def school_mentors_not_mentoring
-      school.school_mentors.reject do |school_mentor|
-        profile_ids_of_mentors_mentoring.include?(school_mentor.participant_profile_id)
       end
     end
   end
