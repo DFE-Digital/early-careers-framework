5c8178e0ba89cecea23832fbd2838927eb517763
matt <mattonfoot@gmail.com>
2023-05-18 10:06:35 +0100

    check correct school language files in specs

--- spec/components/status_tags/school_participant_status_tag_spec.rb
+++ spec/components/status_tags/school_participant_status_tag_spec.rb
@@ -5,40 +5,19 @@ RSpec.describe StatusTags::SchoolParticipantStatusTag, type: :component do
 
   subject(:label) { render_inline component }
 
-  context "When the feature flag :school_participant_status_language is not active" do
-    context "The language file" do
-      TrainingRecordState.record_states.each_key do |key|
-        it "includes :#{key} as a language entry" do
-          expect(I18n.t("status_tags.school_participant_status").keys).to include key.to_sym
-        end
-      end
-    end
-
-    I18n.t("status_tags.school_participant_status").each do |key, value|
-      context "and :#{key} is the determined state" do
-        before { allow(component).to receive(:record_state).and_return(key) }
-        it { is_expected.to have_text value[:label] }
-        it { is_expected.to have_text Array.wrap(value[:description]).join("\n  ").gsub("%{contact_us}", "contact us\n") }
+  context "The language file" do
+    TrainingRecordState.record_states.each_key do |key|
+      it "includes :#{key} as a language entry" do
+        expect(I18n.t("status_tags.school_participant_status").keys).to include key.to_sym
       end
     end
   end
 
-  context "When the feature flag :school_participant_status_language is active", with_feature_flags: { school_participant_status_language: "active" } do
-    context "The language file" do
-      TrainingRecordState.record_states.each_key do |key|
-        it "includes :#{key} as a language entry" do
-          expect(I18n.t("status_tags.school_participant_status_detailed").keys).to include key.to_sym
-        end
-      end
-    end
-
-    I18n.t("status_tags.school_participant_status_detailed").each do |key, value|
-      context "and :#{key} is the determined state" do
-        before { allow(component).to receive(:record_state).and_return(key) }
-        it { is_expected.to have_text value[:label] }
-        it { is_expected.to have_selector(".govuk-tag.govuk-tag--#{value[:colour]}", exact_text: value[:label]) }
-        it { is_expected.to have_text Array.wrap(value[:description]).join("\n  ").gsub("%{contact_us}", "contact us\n") }
-      end
+  I18n.t("status_tags.school_participant_status").each do |key, value|
+    context "and :#{key} is the determined state" do
+      before { allow(component).to receive(:record_state).and_return(key) }
+      it { is_expected.to have_text value[:label] }
+      it { is_expected.to have_text Array.wrap(value[:description]).join("\n  ").gsub("%{contact_us}", "contact us\n") }
     end
   end
 end
