14635188421f724c926d7560805a513f96d0d5c2
Peter Yates <peter.yates@graphia.co.uk>
2023-05-24 11:59:29 +0100

    Add a scope to order cohorts by start year

--- spec/models/cohort_spec.rb
+++ spec/models/cohort_spec.rb
@@ -23,6 +23,13 @@ RSpec.describe Cohort, type: :model do
         expect(Cohort.between_2021_and(2030).to_sql).to include(expected)
       end
     end
+
+    describe ".ordered_by_year" do
+      it "orders the cohorts by year ascending" do
+        expect(Cohort.ordered_by_year.to_sql).to include(%(ORDER BY "cohorts"."start_year" ASC))
+        expect(Cohort.ordered_by_year.map(&:start_year)).to eql([2020, 2021, 2022, 2023, 2024])
+      end
+    end
   end
 
   describe ".current" do
