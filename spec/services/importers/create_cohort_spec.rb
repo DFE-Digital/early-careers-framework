0652f4a4511f7f4882a62ceb49bd071a525f1f74
matt <mattonfoot@gmail.com>
2023-06-15 17:34:56 +0100

    avoid existing cohorts when testing crete cohort service

--- spec/services/importers/create_cohort_spec.rb
+++ spec/services/importers/create_cohort_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe Importers::CreateCohort do
   let(:csv) { Tempfile.new("data.csv") }
   let(:path_to_csv) { csv.path }
 
+  let(:start_year) { Cohort.ordered_by_start_year.last.start_year + 50 }
+
   subject(:importer) { described_class.new(path_to_csv:) }
 
   describe "#call" do
@@ -13,14 +15,11 @@ RSpec.describe Importers::CreateCohort do
       before do
         csv.write "start-year,registration-start-date,academic-year-start-date,npq-registration-start-date"
         csv.write "\n"
-        csv.write "2020,2020/05/10,2020/09/01,"
-        csv.write "\n"
-        csv.write "2021,2021/05/10,2021/09/01,"
-        csv.write "\n"
-        csv.write "2022,2022/05/10,2022/09/01,"
-        csv.write "\n"
-        csv.write "2023,2023/05/10,2023/09/01,"
-        csv.write "\n"
+        4.times.each do |n|
+          year = start_year + n
+          csv.write "#{year},#{year}/05/10,#{year}/09/01,"
+          csv.write "\n"
+        end
         csv.close
       end
 
@@ -33,43 +32,45 @@ RSpec.describe Importers::CreateCohort do
       it "sets the correct start year on the record" do
         importer.call
 
-        expect(Cohort.order(:start_year).last.start_year).to eq(2023)
+        expect(Cohort.ordered_by_start_year.last.start_year).to eq(start_year + 3)
       end
 
       it "sets the correct registration start date on the record" do
         importer.call
 
-        cohort = Cohort.find_by(start_year: 2022)
+        cohort = Cohort.find_by(start_year: start_year + 1)
 
-        expect(cohort.registration_start_date).to eq(Date.parse("10/05/2022"))
+        expect(cohort.registration_start_date).to eq(Date.parse("10/05/#{start_year + 1}"))
       end
 
       it "only creates one cohort record per year" do
+        original_cohort_count = Cohort.count
         importer.call
 
-        expect(Cohort.select("start_year").group("start_year").pluck(:start_year).size).to be 4
+        expect(Cohort.select("start_year").group("start_year").pluck(:start_year).size).to be original_cohort_count + 4
       end
     end
 
     context "with existing cohorts" do
-      let!(:cohort_2023) { create(:cohort, start_year: 2023, registration_start_date: Date.new(2023, 5, 1), academic_year_start_date: Date.new(2023, 8, 31)) }
+      let!(:cohort) { FactoryBot.create(:seed_cohort, start_year:, registration_start_date: Date.new(start_year, 5, 1), academic_year_start_date: Date.new(start_year, 8, 31)) }
 
       before do
         csv.write "start-year,registration-start-date,academic-year-start-date,npq-registration-start-date"
         csv.write "\n"
-        csv.write "2023,2023/05/10,2023/09/01,2023/04/01"
+        csv.write "#{start_year},#{start_year}/05/10,#{start_year}/09/01,#{start_year}/04/01"
         csv.write "\n"
         csv.close
       end
 
       it "updates the cohort values" do
+        original_cohort_count = Cohort.count
         importer.call
 
-        expect(Cohort.count).to eq(1)
-        expect(cohort_2023.reload).to have_attributes(
-          registration_start_date: Date.new(2023, 5, 10),
-          academic_year_start_date: Date.new(2023, 9, 1),
-          npq_registration_start_date: Date.new(2023, 4, 1),
+        expect(Cohort.count).to eq(original_cohort_count)
+        expect(cohort.reload).to have_attributes(
+          registration_start_date: Date.new(start_year, 5, 10),
+          academic_year_start_date: Date.new(start_year, 9, 1),
+          npq_registration_start_date: Date.new(start_year, 4, 1),
         )
       end
     end
