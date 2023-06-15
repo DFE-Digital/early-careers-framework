b350a6868f22d019d69042b5486a9b6883d07dcf
Peter Yates <peter.yates@graphia.co.uk>
2023-06-15 12:05:25 +0100

    Transactions instead of trucations

--- spec/cypress/app_commands/clean.rb
+++ spec/cypress/app_commands/clean.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 # cleaning the database using database_cleaner
-DatabaseCleaner.clean_with :truncation
+DatabaseCleaner.clean_with :transaction
 
 Rails.logger.info "APPCLEANED" # used by log_fail.rb
