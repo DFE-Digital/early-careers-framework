f4deaa20eeeeee91253ef4974d2771aa61fa71c1
Leandro Alemão <leandro.costantini@gmail.com>
2023-06-19 10:50:47 +0100

    [CPDLP-2262] Address PR comments

--- spec/jobs/participant_outcomes/batch_send_latest_outcomes_job_spec.rb
+++ spec/jobs/participant_outcomes/batch_send_latest_outcomes_job_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe ParticipantOutcomes::BatchSendLatestOutcomesJob do
   let(:outcome_1) { double(id: 1) }
   let(:outcome_2) { double(id: 2) }
   let(:outcomes) { [outcome_1, outcome_2] }
-  let(:default_batch_size) { described_class::DEFAULT_BATCH_SIZE }
   let(:queue_double) { double(detect: nil) }
 
   before do
@@ -19,7 +18,7 @@ RSpec.describe ParticipantOutcomes::BatchSendLatestOutcomesJob do
       let(:batch_size) { 2 }
 
       it "enqueues the send job for each record" do
-        described_class.perform_now
+        described_class.perform_now(batch_size)
 
         expect(ParticipantOutcomes::SendToQualifiedTeachersApiJob).to(have_been_enqueued.exactly(:once).with(participant_outcome_id: 1))
         expect(ParticipantOutcomes::SendToQualifiedTeachersApiJob).to(have_been_enqueued.exactly(:once).with(participant_outcome_id: 2))
@@ -35,10 +34,6 @@ RSpec.describe ParticipantOutcomes::BatchSendLatestOutcomesJob do
         expect(ParticipantOutcomes::SendToQualifiedTeachersApiJob).to have_been_enqueued.exactly(:once).with(participant_outcome_id: 1)
         expect(ParticipantOutcomes::SendToQualifiedTeachersApiJob).not_to have_been_enqueued.with(participant_outcome_id: 2)
       end
-    end
-
-    it "does not requeue itself" do
-      expect { described_class.perform_now(default_batch_size) }.not_to have_enqueued_job(described_class)
     end
   end
 
