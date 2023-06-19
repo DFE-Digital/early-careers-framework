2f5aceb9f74ebd9bf3c1516e7df317e669b2d7c7
matt <mattonfoot@gmail.com>
2023-06-19 10:12:33 +0100

    add transferred participant scenario

--- spec/services/dashboard/participants_spec.rb
+++ spec/services/dashboard/participants_spec.rb
@@ -23,6 +23,23 @@ RSpec.describe Dashboard::Participants, :with_default_schedules do
       .build
       .add_induction_record(induction_programme: transfer_1.induction_programme_from, training_status: :deferred)
   end
+  # TODO: copied from NewSeeds::Scenarios::Participants::TrainingRecordStates#ect_on_fip_transferred as they are not yet set up to receive a school cohort
+  let!(:transferred_induction_record) do
+    transferred_to_school_cohort = NewSeeds::Scenarios::Schools::School.new.build
+                                                                       .chosen_fip_but_not_partnered(cohort: ect_1_induction_record_1.school_cohort.cohort)
+                                                                       .school_cohort
+
+    transfer_date = 1.month.ago
+
+    travel_to(2.months.ago) do
+      builder = NewSeeds::Scenarios::Participants::Ects::Ect
+        .new(school_cohort: ect_1_induction_record_1.school_cohort)
+        .build
+      relevant_induction_record = builder.add_induction_record(induction_programme: ect_1_induction_record_1.induction_programme, induction_status: "leaving", end_date: transfer_date, school_transfer: true)
+      builder.add_induction_record(induction_programme: transferred_to_school_cohort.default_induction_programme, start_date: transfer_date, school_transfer: true)
+      relevant_induction_record
+    end
+  end
   let!(:not_mentoring_induction_record) do
     NewSeeds::Scenarios::Participants::Mentors::MentorWithNoEcts
       .new(school_cohort: ect_1_induction_record_1.school_cohort)
@@ -48,7 +65,7 @@ RSpec.describe Dashboard::Participants, :with_default_schedules do
     end
 
     it "returns a unique entry per ect mentor not mentoring or ects not training" do
-      expect(subject).to contain_exactly(deferred_induction_record, not_mentoring_induction_record)
+      expect(subject).to contain_exactly(deferred_induction_record, not_mentoring_induction_record, transferred_induction_record)
     end
   end
 end
