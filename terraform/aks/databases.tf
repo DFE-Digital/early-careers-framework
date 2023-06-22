39d34d861955987f2571730eed1030dae6c30741
Peter Yates <peter.yates@graphia.co.uk>
2023-06-22 15:59:52 +0100

    Add Redis AKS config

---
+++ terraform/aks/databases.tf
@@ -0,0 +1,15 @@
+module "redis" {
+  source = "git::https://github.com/DFE-Digital/terraform-modules.git//aks/redis?ref=testing"
+
+  namespace             = var.namespace
+  environment           = local.environment
+  azure_resource_prefix = var.azure_resource_prefix
+  service_name          = local.service_name
+  service_short         = var.service_short
+  config_short          = var.config_short
+
+  cluster_configuration_map = module.cluster_data.configuration_map
+
+  use_azure               = var.deploy_azure_backing_services
+  azure_enable_monitoring = var.enable_monitoring
+}
