cadb66872ddde27855310cd1c73b6f5e708183c4
Peter Yates <peter.yates@graphia.co.uk>
2023-06-22 16:10:03 +0100

    Add PostgreSQL AKS config

--- terraform/aks/databases.tf
+++ terraform/aks/databases.tf
@@ -13,3 +13,20 @@ module "redis" {
   use_azure               = var.deploy_azure_backing_services
   azure_enable_monitoring = var.enable_monitoring
 }
+
+module "postgres" {
+  source = "git::https://github.com/DFE-Digital/terraform-modules.git//aks/postgres?ref=testing"
+
+  namespace             = var.namespace
+  environment           = local.environment
+  azure_resource_prefix = var.azure_resource_prefix
+  service_name          = local.service_name
+  service_short         = var.service_short
+  config_short          = var.config_short
+
+  cluster_configuration_map = module.cluster_data.configuration_map
+
+  use_azure               = var.deploy_azure_backing_services
+  azure_enable_monitoring = var.enable_monitoring
+  azure_extensions        = ["citext", "pg_stat_statements", "pgcrypto", "plpgsql"]
+}
