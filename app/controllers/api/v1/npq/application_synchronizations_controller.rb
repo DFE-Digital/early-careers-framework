ce85a55603d3c61c1c79b22df3801c443c9881b9
Javier Fernandez <javier.fernandez@education.gov.uk>
2023-06-23 18:19:12 +0100

    method name change

--- app/controllers/api/v1/npq/application_synchronizations_controller.rb
+++ app/controllers/api/v1/npq/application_synchronizations_controller.rb
@@ -2,14 +2,14 @@ module Api
   module V1
     module NPQ
       class ApplicationSynchronizationsController < ApiController
-        before_action :all_applications
+        before_action :set_npq_applications
         def index
           render json: json_accounts_serializer_class.new(@npq_applications).serializable_hash.to_json
         end
 
         private
 
-        def all_applications
+        def set_npq_applications
           @npq_applications = NPQApplication.all.select(:lead_provider_approval_status, :id, :participant_identity_id)
         end
 
