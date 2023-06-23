5f5abade43bee2164d0e158cbbcc39e6dff56b42
Frankie Roberto <frankie@frankieroberto.com>
2023-06-23 15:01:47 +0100

    Add table of stats on admin overview page

--- app/components/primary_nav_component.rb
+++ app/components/primary_nav_component.rb
@@ -2,8 +2,12 @@
 
 class PrimaryNavComponent < ViewComponent::Base
   include ViewComponent::SlotableV2
-
+  attr_reader :wide
   renders_many :nav_items, "NavItemComponent"
+
+  def initialize(wide: false)
+    @wide = wide
+  end
 
   class NavItemComponent < ViewComponent::Base
     attr_reader :path
