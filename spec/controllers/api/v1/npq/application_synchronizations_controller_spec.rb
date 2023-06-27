6876d113e630853b12aa2222b7ba78af5ff366df
Javier Fernandez <javier.fernandez@education.gov.uk>
2023-06-27 14:50:05 +0100

    remove authenticate spec

--- spec/controllers/api/v1/npq/application_synchronizations_controller_spec.rb
+++ spec/controllers/api/v1/npq/application_synchronizations_controller_spec.rb
@@ -7,31 +7,6 @@ class DummyController < Api::V1::NPQ::ApplicationSynchronizationsController
 end
 
 describe DummyController, type: :controller do
-  describe "#authenticate" do
-    before do
-      controller.response              = response
-      request.headers["Authorization"] = bearer_token
-      controller.authenticate
-    end
-
-    context "when authorization header not provided or invalid" do
-      let(:bearer_token) { "Bearer invalid" }
-
-      it "requests authentication via the http header" do
-        expect(response.status).to eq(401)
-      end
-    end
-
-    context "when authorization header is provided" do
-      let(:cpd_lead_provider) { create(:cpd_lead_provider) }
-      let(:token) { NPQRegistrationApiToken.create_with_random_token!(cpd_lead_provider_id: cpd_lead_provider.id) }
-      let(:bearer_token) { "Bearer #{token}" }
-
-      it "requests authentication via the http header" do
-        expect(response.status).to eq(200)
-      end
-    end
-  end
   describe "Get#index" do
     before do
       controller.response              = response
