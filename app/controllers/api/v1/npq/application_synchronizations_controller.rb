93c18bde28fea136a606e810c80638ad6d23b865
Javier Fernandez <javier.fernandez@education.gov.uk>
2023-06-28 17:36:04 +0100

    Responsed limited application to NPQ

--- app/controllers/api/v1/npq/application_synchronizations_controller.rb
+++ app/controllers/api/v1/npq/application_synchronizations_controller.rb
@@ -12,7 +12,8 @@ module Api
       private
 
         def set_npq_applications
-          @npq_applications = NPQApplication.all.select(:lead_provider_approval_status, :id, :participant_identity_id)
+          filtered_ids = params[:ids].split(',')
+          @npq_applications = NPQApplication.where(id: filtered_ids).select(:lead_provider_approval_status, :id, :participant_identity_id)
         end
 
         def json_accounts_serializer_class
