30578de640e628fddbb94d4b278b98bbabe301d9
cwrw <abeer.salameh@gmail.com>
2023-07-05 21:11:27 +0100

    Check if column exists from removing
    This migration is failing in envs the column does not
    exist in. Add exists true to ensure migration passes

--- db/migrate/20230705152210_remove_accidental_columns_from_versions.rb
+++ db/migrate/20230705152210_remove_accidental_columns_from_versions.rb
@@ -3,7 +3,7 @@
 class RemoveAccidentalColumnsFromVersions < ActiveRecord::Migration[6.1]
   def change
     StrongMigrations.disable_check(:remove_column)
-    remove_column :versions, "{:null=>false}", :string
+    remove_column :versions, "{:null=>false}", :string, if_exists: true
     StrongMigrations.enable_check(:remove_column)
   end
 end
