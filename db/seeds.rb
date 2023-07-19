89aa454befab1a589a101584ca140a6b953e35da
Peter Yates <peter.yates@graphia.co.uk>
2023-07-19 17:32:10 +0100

    Add review env to list of non-prod environments

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,13 @@ if ENV["LEGACY_SEEDS"] == "true"
   load Rails.root.join(*seed_path, "initial_seed.rb").to_s
   load Rails.root.join(*seed_path, "schedules.rb").to_s
 
-  if %w[development deployed_development test sandbox].include?(Rails.env)
+  if %w[development deployed_development test sandbox review].include?(Rails.env)
     %w[test_data dummy_structures appropriate_bodies].each do |seed|
       load Rails.root.join(*seed_path, "#{seed}.rb").to_s
     end
   end
 
-  if Rails.env.development? || Rails.env.deployed_development?
+  if Rails.env.development? || Rails.env.deployed_development? || Rails.env.review?
     FeatureFlag::PERMANENT_SETTINGS.each do |feature|
       FeatureFlag.activate(feature)
     end
