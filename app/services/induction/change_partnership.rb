f1bc0cdb182e3b5d36b1999e9748a4896604f23d
Tony Headford <tony@binarycircus.com>
2023-07-31 13:50:52 +0100

    Move unpartnered participants to new partnership programme

--- app/services/induction/change_partnership.rb
+++ app/services/induction/change_partnership.rb
@@ -2,20 +2,21 @@
 
 class Induction::ChangePartnership < BaseService
   def call
-    default_induction_programme = school_cohort.default_induction_programme
+    ActiveRecord::Base.transaction do
+      default_induction_programme = school_cohort.default_induction_programme
 
-    if default_induction_programme.present? &&
-        default_induction_programme.full_induction_programme?
+      if default_induction_programme.present? &&
+          default_induction_programme.full_induction_programme?
 
-      if default_induction_programme.partnership.blank?
-        # there is no partnership, we presume there was none when the school chose FIP
-        # so we can just update the programme
-        default_induction_programme.update!(partnership:)
+        if default_induction_programme.partnership.blank?
+          # there is no partnership, we presume there was none when the school chose FIP
+          # so we can just update the programme
+          default_induction_programme.update!(partnership:)
+          move_unpartnered_participants
 
-      elsif default_induction_programme.partnership != partnership &&
-          default_induction_programme.partnership.challenged?
+        elsif default_induction_programme.partnership != partnership &&
+            default_induction_programme.partnership.challenged?
 
-        ActiveRecord::Base.transaction do
           # existing FIP partnership replaced so create a new programme and migrate
           # all the participants to the new one
           programme = InductionProgramme.create!(school_cohort:,
@@ -26,10 +27,12 @@ class Induction::ChangePartnership < BaseService
                                                             to_programme: programme)
 
           school_cohort.update!(default_induction_programme: programme)
+          move_unpartnered_participants
         end
+      else
+        Induction::SetCohortInductionProgramme.call(school_cohort:, programme_choice: "full_induction_programme")
+        move_unpartnered_participants
       end
-    else
-      Induction::SetCohortInductionProgramme.call(school_cohort:, programme_choice: "full_induction_programme")
     end
   end
 
@@ -40,5 +43,21 @@ private
   def initialize(school_cohort:, partnership:)
     @school_cohort = school_cohort
     @partnership = partnership
+  end
+
+  def move_unpartnered_participants
+    unpartnered_fip_programmes.each do |programme|
+      Induction::MigrateParticipantsToNewProgramme.call(from_programme: programme,
+                                                        to_programme: school_cohort.default_induction_programme)
+    end
+  end
+
+  def unpartnered_fip_programmes
+    school_cohort
+      .induction_programmes
+      .full_induction_programme
+      .joins(:active_induction_records)
+      .left_joins(:partnership)
+      .where("partnerships.id IS NULL OR partnerships.challenged_at IS NOT NULL")
   end
 end
