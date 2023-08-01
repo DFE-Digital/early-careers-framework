1aff0e94ff36661b7723786832f50098dc1acd19
Peter Yates <peter.yates@graphia.co.uk>
2023-08-01 17:07:13 +0100

    Add staging to env-specific settings
    We're going to be using staging in place of development so we can keep a
    1:1 mapping between Rails and deployed environment names. As such, there
    are some special bits of functionality that are enabled on a
    per-environment basis and staging needs to be added to them

--- db/new_seeds/base/add_feature_flags.rb
+++ db/new_seeds/base/add_feature_flags.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-if Rails.env.development? || Rails.env.deployed_development? || Rails.env.review?
+if Rails.env.in?(%w[development deployed_development review staging])
   Rails.logger.info("activating permanent feature flags")
 
   FeatureFlag::PERMANENT_SETTINGS.each do |feature|
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,13 @@ if ENV["LEGACY_SEEDS"] == "true"
   load Rails.root.join(*seed_path, "initial_seed.rb").to_s
   load Rails.root.join(*seed_path, "schedules.rb").to_s
 
-  if %w[development deployed_development test sandbox review].include?(Rails.env)
+  if Rails.env.in?(%w[development deployed_development test sandbox staging review])
     %w[test_data dummy_structures appropriate_bodies].each do |seed|
       load Rails.root.join(*seed_path, "#{seed}.rb").to_s
     end
   end
 
-  if Rails.env.development? || Rails.env.deployed_development? || Rails.env.review?
+  if Rails.env.in?(%w[development deployed_development staging review])
     FeatureFlag::PERMANENT_SETTINGS.each do |feature|
       FeatureFlag.activate(feature)
     end
