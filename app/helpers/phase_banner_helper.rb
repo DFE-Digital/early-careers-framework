bbec615709ac66c4154e32f20a719d9809222a63
Naomi Lockhart <naomi@lockyy.com>
2023-08-01 17:08:44 +0100

    Add notification banner for maintenance

--- app/helpers/phase_banner_helper.rb
+++ app/helpers/phase_banner_helper.rb
@@ -16,4 +16,15 @@ module PhaseBannerHelper
       "sandbox" => "yellow",
     }.fetch(env, nil)
   end
+
+  def maintenance_banner_dismissed?
+    dismissed_until = cookies[:dismiss_maintenance_banner_until]
+    return if dismissed_until.blank?
+
+    Time.zone.parse(dismissed_until) <= 1.week.from_now
+  rescue StandardError => e
+    Rails.logger.error "Error parsing maintenance banner dismissal cookie: #{e.message}"
+    Sentry.capture_exception(e)
+    false
+  end
 end
