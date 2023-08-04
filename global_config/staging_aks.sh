144cb35cb0d22c828f9b9b80c400d6a19eacb75f
Peter Yates <peter.yates@graphia.co.uk>
2023-08-04 18:12:05 +0100

    Remove duplicated setting of DEPLOY_ENV

--- global_config/staging_aks.sh
+++ global_config/staging_aks.sh
@@ -2,6 +2,6 @@ AZURE_RESOURCE_PREFIX=s189t01
 AZURE_SUBSCRIPTION=s189-teacher-services-cloud-test
 CONFIG=staging_aks
 CONFIG_SHORT=st
-DEPLOY_ENV=staging_aks
+DEPLOY_ENV=${CONFIG}
 ENV_TAG=Test
 PLATFORM=aks
