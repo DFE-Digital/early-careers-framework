068c2ed0fb6b2485483cad2afa8ceaf90c483754
Ross Oliver <ross.oliver36@gmail.com>
2023-08-07 14:22:36 +0100

    Add library to deduplicate ECF profiles
    We have a number of duplicate profiles in the database and we want to
    reconcile these records.
    
    Add library to reconcile a duplicate profile with the primary record.
    Currently handles school changes, validation/eligibility merging,
    declaration reconciliation and serialization then deletion of the
    duplicate record.
    
    Rake task usage:
    
    ```
    bundle exec rake 'participant_profile_deduplicator:dedup_dry_run[<primary-profile-uuid>,<duplicate-profile-uuid>]'
    ```

---
+++ lib/participant_profile_deduplicator.rb
@@ -0,0 +1,174 @@
+# frozen_string_literal: true
+
+class ParticipantProfileDeduplicator
+  CONFLICTING_DECLARATION_STATES = %w[submitted eligible payable paid].freeze
+
+  attr_reader :primary_profile_id, :duplicate_profile_id, :dry_run
+
+  def initialize(primary_profile_id, duplicate_profile_id, dry_run: true)
+    @primary_profile_id = primary_profile_id
+    @duplicate_profile_id = duplicate_profile_id
+    @dry_run = dry_run
+  end
+
+  def dedup!
+    Rails.logger.info("~~~ DRY RUN ~~~") if dry_run
+
+    ensure_lead_providers_match
+    ensure_schedules_match
+    ensure_training_programmes_match
+
+    ActiveRecord::Base.transaction do
+      handle_school_change!
+      reconcile_remaining_induction_records!
+      transfer_validation_data!
+      transfer_participant_eligibility!
+      reconcile_declarations!
+      delete_duplicate!
+
+      raise ActiveRecord::Rollback if dry_run
+    end
+  end
+
+private
+
+  def ensure_lead_providers_match
+    return if primary_profile.lead_provider.id == duplicate_profile.lead_provider.id
+
+    raise "Only duplicates with the same lead_provider are supported"
+  end
+
+  def ensure_schedules_match
+    return if primary_profile.latest_induction_record.schedule_id == duplicate_profile.latest_induction_record.schedule_id
+
+    raise "Only duplicates with the same schedule are supported"
+  end
+
+  def ensure_training_programmes_match
+    primary_training_programmes = primary_profile.induction_records.map { |ir| ir.induction_programme.training_programme }.flatten
+    duplicate_training_programmes = duplicate_profile.induction_records.map { |ir| ir.induction_programme.training_programme }.flatten
+
+    raise "Only duplicates with the same training programme are supported" if primary_training_programmes.difference(duplicate_training_programmes).any?
+  end
+
+  def handle_school_change!
+    return unless primary_profile.school != duplicate_profile.school
+
+    primary_profile.induction_records.oldest.update!(school_transfer: true)
+
+    log_info("Primary profile oldest induction record set as school transfer")
+
+    duplicate_induction_record = duplicate_profile.latest_induction_record
+    end_date = primary_profile.induction_records.oldest.start_date - 1.minute
+
+    duplicate_induction_record.update!(
+      participant_profile_id: primary_profile.id,
+      induction_status: :leaving,
+      end_date:,
+      preferred_identity: preferred_identity(duplicate_induction_record),
+    )
+
+    log_info("Preferred identity updated on duplicate profile latest induction record") if duplicate_induction_record.saved_change_to_preferred_identity_id?
+    log_info("Duplicate profile latest induction record transferred. End date: #{end_date}")
+  end
+
+  def reconcile_remaining_induction_records!
+    duplicate_profile.induction_records.each do |induction_record|
+      induction_record.update!(
+        participant_profile_id: primary_profile.id,
+        preferred_identity_id: preferred_identity(induction_record).id,
+      )
+
+      log_info("Preferred identity updated on duplicate profile induction record") if induction_record.saved_change_to_preferred_identity_id?
+      log_info("Duplicate profile induction record transferred")
+    end
+  end
+
+  def transfer_validation_data!
+    return if primary_profile.ecf_participant_validation_data.present? || duplicate_profile.ecf_participant_validation_data.nil?
+
+    log_info("Validation data transferred")
+
+    duplicate_profile.ecf_participant_validation_data.update!(participant_profile_id: primary_profile.id)
+  end
+
+  def transfer_participant_eligibility!
+    return if primary_profile.ecf_participant_eligibility.present? || duplicate_profile.ecf_participant_eligibility.nil?
+
+    duplicate_profile.ecf_participant_eligibility.update!(participant_profile_id: primary_profile.id)
+
+    log_info("Eligibility transferred")
+  end
+
+  def reconcile_declarations!
+    duplicate_profile.participant_declarations.each do |declaration|
+      void_most_recent_conflicting_declaration!(declaration)
+      declaration.update!(
+        participant_profile_id: primary_profile.id,
+        user_id: primary_profile.user_id,
+      )
+
+      log_info("User changed on declaration (#{declaration.id})") if declaration.saved_change_to_user_id?
+      log_info("Transferred declaration: #{declaration.declaration_type}, #{declaration.state} (#{declaration.id})")
+    end
+  end
+
+  def void_most_recent_conflicting_declaration!(declaration)
+    conflicting_declaration = conflicting_declaration(declaration)
+
+    return unless conflicting_declaration
+
+    most_recent_declaration = [declaration, conflicting_declaration].max_by(&:created_at)
+    log_info("Voided conflicting declaration: #{most_recent_declaration.declaration_type}, #{most_recent_declaration.state} (#{most_recent_declaration.id})")
+    VoidParticipantDeclaration.new(most_recent_declaration).call
+  end
+
+  def conflicting_declaration(declaration)
+    return unless declaration.state.in?(CONFLICTING_DECLARATION_STATES)
+
+    primary_profile.participant_declarations.find do |d|
+      d.declaration_type == declaration.declaration_type && d.state == declaration.state
+    end
+  end
+
+  def delete_duplicate!
+    Finance::ECF::DeletedDuplicate.create!(
+      data: serialized_duplicate,
+      primary_participant_profile: primary_profile,
+    )
+
+    log_info("Destroyed duplicate profile")
+
+    duplicate_profile.reload
+    duplicate_profile.validation_decisions.destroy_all
+    duplicate_profile.participant_profile_states.destroy_all
+    duplicate_profile.participant_profile_schedules.destroy_all
+    duplicate_profile.ecf_participant_validation_data&.destroy!
+    duplicate_profile.ecf_participant_eligibility&.destroy!
+    duplicate_profile.destroy!
+  end
+
+  def serialized_duplicate
+    @serialized_duplicate ||= Finance::ECF::DuplicateSerializer.new(duplicate_profile).serializable_hash
+  end
+
+  def primary_profile
+    @primary_profile ||= ParticipantProfile::ECF.find(primary_profile_id)
+  end
+
+  def duplicate_profile
+    @duplicate_profile ||= ParticipantProfile::ECF.find(duplicate_profile_id)
+  end
+
+  def preferred_identity(duplicate_induction_record)
+    if duplicate_induction_record.preferred_identity&.user_id == primary_profile.induction_records.oldest.preferred_identity.user_id
+      duplicate_induction_record.preferred_identity
+    else
+      primary_profile.latest_induction_record.preferred_identity
+    end
+  end
+
+  def log_info(info)
+    Rails.logger.info(info)
+  end
+end
---
+++ spec/lib/participant_profile_deduplicator_spec.rb
@@ -0,0 +1,214 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+require "participant_profile_deduplicator"
+
+describe ParticipantProfileDeduplicator do
+  let(:primary_profile) { create(:ect) }
+  let(:duplicate_profile) { create(:ect, :eligible_for_funding, school_cohort: primary_profile.school_cohort) }
+  let(:dry_run) { false }
+  let!(:instance) { described_class.new(primary_profile.id, duplicate_profile.id, dry_run:) }
+
+  before { allow(Rails.logger).to receive(:info) }
+
+  describe "#dedup!" do
+    subject(:dedup!) { instance.dedup! }
+
+    context "when dry_run is true" do
+      let(:dry_run) { true }
+
+      it "does not make any changes, but logs out as if it does" do
+        expect { dedup! }.not_to change(ParticipantProfile::ECF, :count)
+        expect(Rails.logger).to have_received(:info).with("~~~ DRY RUN ~~~")
+        expect(Rails.logger).to have_received(:info).with("Destroyed duplicate profile")
+      end
+    end
+
+    context "when the lead providers are different" do
+      let(:duplicate_profile) { create(:ect) }
+
+      it { expect { dedup! }.to raise_error("Only duplicates with the same lead_provider are supported") }
+    end
+
+    context "when the schedules are different" do
+      before { duplicate_profile.latest_induction_record.update(schedule: create(:schedule, name: "other")) }
+
+      it { expect { dedup! }.to raise_error("Only duplicates with the same schedule are supported") }
+    end
+
+    context "when the training programmes are different" do
+      before { create(:induction_record, participant_profile: primary_profile) }
+
+      it { expect { dedup! }.to raise_error("Only duplicates with the same training programme are supported") }
+    end
+
+    it "destroys the duplicate profile and associated data" do
+      duplicate_induction_record = create(:induction_record, participant_profile: duplicate_profile)
+      validation_decision = duplicate_profile.validation_decisions.build(validation_step: "test")
+      participant_profile_state = create(:participant_profile_state, participant_profile: duplicate_profile)
+      schedule = ParticipantProfileSchedule.create!(participant_profile: duplicate_profile, schedule: Finance::Schedule.first)
+      validation_data = duplicate_profile.ecf_participant_validation_data
+      eligibility = duplicate_profile.ecf_participant_eligibility
+
+      expect { dedup! }.to change(ParticipantProfile::ECF, :count).by(-1)
+
+      expect { eligibility.reload }.not_to raise_error
+      expect { validation_data.reload }.not_to raise_error
+
+      expect { schedule.reload }.to raise_error(ActiveRecord::RecordNotFound)
+      expect { participant_profile_state.reload }.to raise_error(ActiveRecord::RecordNotFound)
+      expect { validation_decision.reload }.to raise_error(ActiveRecord::RecordNotFound)
+      expect { duplicate_profile.reload }.to raise_error(ActiveRecord::RecordNotFound)
+
+      expect(Rails.logger).to have_received(:info).with("Destroyed duplicate profile")
+    end
+
+    context "when the school has changed" do
+      let(:duplicate_profile) { create(:ect) }
+      let!(:oldest_induction_record) do
+        travel_to(3.days.ago) do
+          preferred_identity = primary_profile.latest_induction_record.preferred_identity
+          induction_programme = primary_profile.latest_induction_record.induction_programme
+          create(:induction_record, participant_profile: primary_profile, preferred_identity:, induction_programme:)
+        end
+      end
+      let!(:duplicate_induction_record) { duplicate_profile.latest_induction_record }
+      let(:primary_latest_induction_record) { primary_profile.latest_induction_record }
+      let(:primary_oldest_induction_record) { primary_profile.induction_records.oldest }
+
+      before { duplicate_profile.latest_induction_record.update!(partnership: primary_latest_induction_record.partnership) }
+
+      it "transfers the latest induction record" do
+        end_date = primary_oldest_induction_record.start_date - 1.minute
+        preferred_identity = primary_latest_induction_record.preferred_identity
+
+        dedup!
+
+        duplicate_induction_record.reload
+        expect(duplicate_induction_record).to have_attributes(
+          participant_profile_id: primary_profile.id,
+          induction_status: "leaving",
+          end_date:,
+          preferred_identity:,
+        )
+
+        expect(Rails.logger).to have_received(:info).with("Duplicate profile latest induction record transferred. End date: #{end_date}")
+        expect(Rails.logger).to have_received(:info).with("Preferred identity updated on duplicate profile latest induction record")
+      end
+
+      it "sets school_transfer to true on the primary profile's oldest induction record" do
+        expect { dedup! }.to change { primary_oldest_induction_record.reload.school_transfer }.from(false).to(true)
+
+        expect(Rails.logger).to have_received(:info).with("Primary profile oldest induction record set as school transfer")
+      end
+
+      context "when the duplicate preferred_identity#user is the same as the primary" do
+        before do
+          user = primary_latest_induction_record.preferred_identity.user
+          duplicate_induction_record.preferred_identity.update!(user:)
+        end
+
+        it "does not update the preferred identity on the transferred induction record" do
+          expect { dedup! }.not_to change { duplicate_induction_record.reload.preferred_identity }
+
+          expect(Rails.logger).not_to have_received(:info).with("Preferred identity updated on duplicate profile induction record")
+        end
+      end
+    end
+
+    context "when the duplicate has multiple induction records" do
+      before { create(:induction_record, participant_profile: duplicate_profile) }
+
+      it "transfers all of the induction records" do
+        expect { dedup! }.to change(primary_profile.induction_records, :count).by(duplicate_profile.induction_records.count)
+      end
+    end
+
+    it "retains a serialized record of the duplicate" do
+      expect { dedup! }.to change { Finance::ECF::DeletedDuplicate.count }.by(1)
+
+      serialized_duplicate = Finance::ECF::DeletedDuplicate.last
+
+      expect(serialized_duplicate.data.dig("data", "id")).to eq(duplicate_profile.id)
+      expect(serialized_duplicate.primary_participant_profile).to eq(primary_profile)
+    end
+
+    it "transfers ecf_participant_validation_data from the duplicate to the primary" do
+      duplicate_validation_data = duplicate_profile.ecf_participant_validation_data
+
+      expect { dedup! }.to change { duplicate_validation_data.reload.participant_profile_id }.to(primary_profile.id)
+
+      expect(Rails.logger).to have_received(:info).with("Validation data transferred")
+    end
+
+    it "transfers ecf_participant_eligibility from the duplicate to the primary" do
+      duplicate_eligibility = duplicate_profile.ecf_participant_eligibility
+
+      expect { dedup! }.to change { duplicate_eligibility.reload.participant_profile_id }.to(primary_profile.id)
+
+      expect(Rails.logger).to have_received(:info).with("Eligibility transferred")
+    end
+
+    context "when there are declarations" do
+      let(:primary_profile) { create(:ect, :eligible_for_funding) }
+
+      let!(:duplicate_declaration) do
+        travel_to(1.day.ago) do
+          create(:ect_participant_declaration,
+                 :submitted,
+                 declaration_type: "retained-1",
+                 participant_profile: duplicate_profile,
+                 cpd_lead_provider: duplicate_profile.lead_provider.cpd_lead_provider)
+        end
+      end
+      let!(:conflicting_declaration) do
+        create(:ect_participant_declaration,
+               :submitted,
+               declaration_type: "retained-1",
+               participant_profile: primary_profile,
+               cpd_lead_provider: primary_profile.lead_provider.cpd_lead_provider)
+      end
+
+      it "transfers declarations from the duplicate to the primary" do
+        dedup!
+
+        expect(duplicate_declaration.reload).to have_attributes(
+          participant_profile_id: primary_profile.id,
+          user_id: primary_profile.user_id,
+        )
+
+        expect(Rails.logger).to have_received(:info).with("User changed on declaration (#{duplicate_declaration.id})")
+        expect(Rails.logger).to have_received(:info).with("Transferred declaration: retained-1, submitted (#{duplicate_declaration.id})")
+      end
+
+      context "when the user in the duplicate declaration matches the primary profile user" do
+        before { duplicate_declaration.update!(user_id: primary_profile.user_id) }
+
+        it "does not log out the user change" do
+          dedup!
+          expect(Rails.logger).not_to have_received(:info).with("User changed on declaration (#{duplicate_declaration.id})")
+        end
+      end
+
+      it "voids the later declaration when there are conflicts" do
+        expect { dedup! }.to change { conflicting_declaration.reload.state }.to("voided")
+
+        expect(Rails.logger).to have_received(:info).with("Voided conflicting declaration: retained-1, submitted (#{conflicting_declaration.id})")
+      end
+    end
+
+    context "when the primary profile already has ecf_participant_validation_data/ecf_participant_eligibility" do
+      let(:primary_profile) { create(:ect, :eligible_for_funding) }
+
+      it "destroys the ecf_participant_validation_data/ecf_participant_eligibility on the duplicate" do
+        validation_data = duplicate_profile.ecf_participant_validation_data
+        eligibility = duplicate_profile.ecf_participant_eligibility
+
+        dedup!
+
+        expect { eligibility.reload }.to raise_error(ActiveRecord::RecordNotFound)
+        expect { validation_data.reload }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+  end
+end
