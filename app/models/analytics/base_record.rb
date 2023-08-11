a08b48e133fd7f1f98ea48a82afefa82cd672605
Peter Yates <peter.yates@graphia.co.uk>
2023-08-11 12:43:20 +0100

    Use old base_record connection settings on PaaS

--- app/models/analytics/base_record.rb
+++ app/models/analytics/base_record.rb
@@ -5,6 +5,10 @@ module Analytics
     self.abstract_class = true
 
     # FIXME: remove deployed_development when we've switched to Azure
-    connects_to database: { writing: :analytics } unless Rails.env.review? || Rails.env.deployed_development?
+    if ENV.key?("VCAP_APPLICATION")
+      connects_to database: { writing: :analytics } if %w[test development production].include? Rails.env
+    else
+      connects_to database: { writing: :analytics } unless Rails.env.review? || Rails.env.deployed_development?
+    end
   end
 end
