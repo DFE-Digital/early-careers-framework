fd4b7e94d930381c418e5e99335642af3d6afd9f
Tony Headford <tony@binarycircus.com>
2023-08-14 14:55:49 +0100

    Change migration class name to use DQT not Dqt

--- db/migrate/20230424093735_create_sync_dqt_induction_start_date_error.rb
+++ db/migrate/20230424093735_create_sync_dqt_induction_start_date_error.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class CreateSyncDqtInductionStartDateError < ActiveRecord::Migration[6.1]
+class CreateSyncDQTInductionStartDateError < ActiveRecord::Migration[6.1]
   def change
     create_table :sync_dqt_induction_start_date_errors, if_not_exists: true do |t|
       t.belongs_to :participant_profile, null: false, foreign_key: true, type: :uuid, index: { name: "dqt_sync_participant_profile_id" }
