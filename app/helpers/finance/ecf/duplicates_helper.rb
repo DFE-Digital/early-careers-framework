13aab29c1b020e8360765bc1a8f1a225e7eb6b81
Frankie Roberto <frankie@frankieroberto.com>
2023-08-17 13:09:34 +0100

    Fix tables

--- app/helpers/finance/ecf/duplicates_helper.rb
+++ app/helpers/finance/ecf/duplicates_helper.rb
@@ -12,46 +12,46 @@ module Finance
       end
 
       def header_row_for(row)
-        row.cell(header: true, text: "Profile type")
-        row.cell(header: true, text: "Participant ID")
-        row.cell(header: true, text: "Profile ID")
-        row.cell(header: true, text: "TRN")
-        row.cell(header: true, text: "Cohort")
-        row.cell(header: true, text: "Schedule")
-        row.cell(header: true, text: "Induction status")
-        row.cell(header: true, text: "Training status")
-        row.cell(header: true, text: "Lead Provider")
-        row.cell(header: true, text: "School")
-        row.cell(header: true, text: "Starts on")
-        row.cell(header: true, text: "Ends on")
-        row.cell(header: true, text: "Declaration count")
+        row.with_cell(header: true, text: "Profile type")
+        row.with_cell(header: true, text: "Participant ID")
+        row.with_cell(header: true, text: "Profile ID")
+        row.with_cell(header: true, text: "TRN")
+        row.with_cell(header: true, text: "Cohort")
+        row.with_cell(header: true, text: "Schedule")
+        row.with_cell(header: true, text: "Induction status")
+        row.with_cell(header: true, text: "Training status")
+        row.with_cell(header: true, text: "Lead Provider")
+        row.with_cell(header: true, text: "School")
+        row.with_cell(header: true, text: "Starts on")
+        row.with_cell(header: true, text: "Ends on")
+        row.with_cell(header: true, text: "Declaration count")
       end
 
       def row_for(row, participant_profile)
-        row.cell { tag_for(participant_profile) }
-        row.cell do
+        row.with_cell { tag_for(participant_profile) }
+        row.with_cell do
           participant_profile.participant_id
         end
-        row.cell(text: participant_profile.id)
-        row.cell(text: participant_profile.teacher_profile_trn.to_s)
-        row.cell(text: participant_profile.cohort)
-        row.cell(text: participant_profile.schedule_identifier)
-        row.cell(text: participant_profile.induction_status)
-        row.cell(text: participant_profile.training_status)
-        row.cell(text: participant_profile.provider_name)
-        row.cell(text: participant_profile.school_name)
-        row.cell(text: participant_profile.start_date.to_fs(:govuk))
-        row.cell(text: participant_profile.end_date&.to_fs(:govuk))
-        row.cell(text: participant_profile.declaration_count)
+        row.with_cell(text: participant_profile.id)
+        row.with_cell(text: participant_profile.teacher_profile_trn.to_s)
+        row.with_cell(text: participant_profile.cohort)
+        row.with_cell(text: participant_profile.schedule_identifier)
+        row.with_cell(text: participant_profile.induction_status)
+        row.with_cell(text: participant_profile.training_status)
+        row.with_cell(text: participant_profile.provider_name)
+        row.with_cell(text: participant_profile.school_name)
+        row.with_cell(text: participant_profile.start_date.to_fs(:govuk))
+        row.with_cell(text: participant_profile.end_date&.to_fs(:govuk))
+        row.with_cell(text: participant_profile.declaration_count)
       end
 
       def comparative_table_row_for(primary_profile:, duplicate_profile:, header:, row:, method:)
         primary_value = primary_profile.public_send(method)
         duplicate_value = duplicate_profile.public_send(method)
         classes = primary_value == duplicate_value ? [] : ["govuk-tag--red"]
-        row.cell(header: true, text: header)
-        row.cell(text: primary_value, classes:)
-        row.cell(text: duplicate_value, classes:)
+        row.with_cell(header: true, text: header)
+        row.with_cell(text: primary_value, classes:)
+        row.with_cell(text: duplicate_value, classes:)
       end
     end
   end
