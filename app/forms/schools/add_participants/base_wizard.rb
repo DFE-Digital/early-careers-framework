ff1eb46510fce555f3385fe04054a398a2a687a2
Naomi Lockhart <naomi@lockyy.com>
2023-08-30 16:38:38 +0100

    Add ability to reactivate withdrawn ECTs
    This works within the same school they were withdrawn from but not from other schools

--- app/forms/schools/add_participants/base_wizard.rb
+++ app/forms/schools/add_participants/base_wizard.rb
@@ -160,22 +160,36 @@ module Schools
 
         return false if adding_ect_as_mentor?
 
+        return false if email_owner_participant_profile_withdrawn_from_current_school?
+        return false if email_owner_induction_record_for_current_school&.training_status_withdrawn?
+
         return true unless transfer?
 
         @email_owner != existing_user
       end
 
+      def email_owner_participant_profile
+        @email_owner.teacher_profile&.ecf_profiles&.first
+      end
+
+      def email_owner_participant_profile_withdrawn_from_current_school?
+        email_owner_participant_profile.school == school &&
+          email_owner_participant_profile.training_status_withdrawn?
+      end
+
+      def email_owner_induction_record_for_current_school
+        return if email_owner_participant_profile.blank?
+
+        Induction::FindBy.call(
+          participant_profile: email_owner_participant_profile,
+          school:,
+        )
+      end
+
       def email_used_in_the_same_school?
-        if email_in_use?
-          participant_profile = @email_owner.teacher_profile&.ecf_profiles&.first
-          if participant_profile
-            Induction::FindBy.call(participant_profile:)&.school == school
-          else
-            false
-          end
-        else
-          false
-        end
+        return false unless email_in_use?
+
+        email_owner_induction_record_for_current_school.present?
       end
 
       ## handling transfers
@@ -545,6 +559,10 @@ module Schools
 
       def adding_ect_as_mentor?
         participant_type == "mentor" && !@email_owner&.teacher_profile&.participant_profiles&.mentors&.exists?
+      end
+
+      def added_email_participant_withdrawn?
+        participant_type == "ect" && @email_owner&.teacher_profile&.participant_profiles&.withdrawn?
       end
     end
   end
