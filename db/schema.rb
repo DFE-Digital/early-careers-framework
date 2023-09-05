7712d05c80259b8f2a340de0d3dad51c1bd6ccbf
Leandro Alemão <leandro.costantini@gmail.com>
2023-09-05 17:00:30 +0100

    [CPDLP-2414] Add a new field to statements table to capture marked as paid timestamp

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_21_154316) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_05_155638) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "fuzzystrmatch"
@@ -1025,6 +1025,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_21_154316) do
     t.boolean "output_fee", default: true
     t.string "contract_version", default: "0.0.1"
     t.decimal "reconcile_amount", default: "0.0", null: false
+    t.datetime "marked_as_paid_at"
     t.index ["cohort_id"], name: "index_statements_on_cohort_id"
     t.index ["cpd_lead_provider_id"], name: "index_statements_on_cpd_lead_provider_id"
   end
