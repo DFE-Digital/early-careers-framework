be4ab2b8d3ec5088f3420ec7678103a2f035361f
Tony Headford <tony@binarycircus.com>
2023-09-07 09:00:19 +0100

    use load_from_hash! to purge old jobs

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -36,6 +36,6 @@ end
 # Sidekiq Cron
 if Sidekiq.server?
   Rails.application.config.after_initialize do
-    Sidekiq::Cron::Job.load_from_hash(YAML.load_file(Rails.root.join("config/sidekiq_cron_schedule.yml")))
+    Sidekiq::Cron::Job.load_from_hash!(YAML.load_file(Rails.root.join("config/sidekiq_cron_schedule.yml")))
   end
 end
