a0c1c39f4a41935b3a07446b6aa800998d0430ca
Frankie Roberto <frankie@frankieroberto.com>
2023-09-11 17:40:14 +0100

    Update tests

--- spec/features/schools/participants/transferring_participants/fip_to_fip/mentor_different_partner_spec.rb
+++ spec/features/schools/participants/transferring_participants/fip_to_fip/mentor_different_partner_spec.rb
@@ -202,7 +202,7 @@ RSpec.describe "Transferring a mentor with a different provider", type: :feature
   end
 
   def then_i_should_be_on_what_we_need_page
-    expect(page).to have_selector("h1", text: "What we need from you")
+    expect(page).to have_selector("h1", text: "What we need to know about this mentor")
   end
 
   def then_i_should_be_on_full_name_page
--- spec/features/schools/participants/transferring_participants/fip_to_fip/unhappy_path_cannot_validate_spec.rb
+++ spec/features/schools/participants/transferring_participants/fip_to_fip/unhappy_path_cannot_validate_spec.rb
@@ -127,7 +127,7 @@ RSpec.describe "transferring participants", type: :feature, js: true do
       end
 
       def then_i_should_be_on_what_we_need_page
-        expect(page).to have_selector("h1", text: "What we need from you")
+        expect(page).to have_selector("h1", text: "What we need to know about this ECT")
       end
 
       def then_i_should_be_on_full_name_page
