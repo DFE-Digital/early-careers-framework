0ef985c5792a2eaec1964159f9b7b7202c65145c
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2023-09-13 17:41:49 +0100

    [CPDLP-2224] Remove service fees from ECF 2021 contract/cohort statements from October 2023 statement onwards

---
+++ app/services/oneoffs/remove_fees_from_ecf_contracts.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+module Oneoffs
+  class RemoveFeesFromECFContracts
+    attr_reader :cohort_year, :from_date
+
+    def initialize(cohort_year:, from_date:)
+      @cohort_year = cohort_year
+      @from_date = from_date.to_date
+    end
+
+    def call
+      statements.each do |statement|
+        lead_provider = statement.lead_provider
+        version = statement.contract_version
+
+        old_contract = CallOffContract.where(
+          version:,
+          lead_provider:,
+          cohort:,
+        ).first!
+
+        # Contract already set to zero
+        next if old_contract.monthly_service_fee.to_s == "0.0"
+
+        new_version = increment_version(version)
+
+        new_contract = CallOffContract.find_or_initialize_by(
+          version: new_version,
+          lead_provider:,
+          cohort:,
+        )
+
+        new_contract.uplift_target = old_contract.uplift_target
+        new_contract.uplift_amount = old_contract.uplift_amount
+        new_contract.recruitment_target = old_contract.recruitment_target
+        new_contract.set_up_fee = old_contract.set_up_fee
+        new_contract.revised_target = old_contract.revised_target
+        new_contract.monthly_service_fee = 0.0
+        new_contract.save!
+
+        if new_contract.participant_bands.empty?
+          old_contract.participant_bands.each do |old_band|
+            new_band = old_band.dup
+            new_band.call_off_contract = new_contract
+            new_band.save!
+          end
+        end
+
+        statement.update!(contract_version: new_version)
+      end
+    end
+
+    def cohort
+      @cohort ||= Cohort.find_by!(start_year: cohort_year)
+    end
+
+    def statements
+      @statements ||= Finance::Statement::ECF.where(cohort:).where("payment_date >= ?", from_date)
+    end
+
+    def increment_version(version)
+      n = (version.split(".") || []).last.to_i
+      "0.0.#{n + 1}"
+    end
+  end
+end
