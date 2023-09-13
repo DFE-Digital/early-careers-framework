7bcda311cc29da5f581935b3ed46761ab29830a0
Lorenzo Tello <ltello8a@gmail.com>
2023-09-13 23:10:57 +0200

    CST-1961: refactor admin features to replace/edit SIT details using new service objects

--- app/controllers/admin/schools/induction_coordinators_controller.rb
+++ app/controllers/admin/schools/induction_coordinators_controller.rb
@@ -6,17 +6,16 @@ module Admin
     skip_after_action :verify_policy_scoped
     before_action :set_school
 
+    attr_reader :school
+
     def new
-      @induction_tutor_form = NominateInductionTutorForm.new
+      @induction_tutor_form = NominateInductionTutorForm.new(school:)
     end
 
     def create
-      @induction_tutor_form = NominateInductionTutorForm.new(tutor_form_params)
+      @induction_tutor_form = NominateInductionTutorForm.new(school:, **tutor_form_params)
       if @induction_tutor_form.valid?(%i[full_name email])
-        CreateInductionTutor.call(school: @school,
-                                  email: @induction_tutor_form.email,
-                                  full_name: @induction_tutor_form.full_name)
-
+        @induction_tutor_form.save!
         set_success_message(content: "New induction tutor added. They will get an email with next steps.", title: "Success")
         redirect_to admin_school_path(@school)
       else
@@ -25,16 +24,13 @@ module Admin
     end
 
     def edit
-      @induction_tutor_form = NominateInductionTutorForm.new(school_induction_tutor_attributes)
+      @induction_tutor_form = EditInductionTutorForm.new(school_induction_tutor_attributes)
     end
 
     def update
-      @induction_tutor_form = NominateInductionTutorForm.new(school_induction_tutor_attributes.merge(tutor_form_params))
+      @induction_tutor_form = EditInductionTutorForm.new(school_induction_tutor_attributes.merge(tutor_form_params))
 
-      if @induction_tutor_form.valid?(%i[full_name email])
-        UpdateInductionTutor.call(school: @school,
-                                  full_name: @induction_tutor_form.full_name,
-                                  email: @induction_tutor_form.email)
+      if @induction_tutor_form.save
         set_success_message(content: "Induction tutor details updated", title: "Success")
         redirect_to admin_school_path(@school)
       else
@@ -49,20 +45,11 @@ module Admin
     end
 
     def school_induction_tutor_attributes
-      user = @school.induction_tutor
-      if user
-        {
-          user_id: user.id,
-          email: user.email,
-          full_name: user.full_name,
-        }
-      else
-        {}
-      end
-    end
-
-    def school_using_this_email(email)
-      Identity.find_user_by(email:).schools.first
+      {
+        induction_tutor: @school.induction_tutor,
+        email: @school.induction_tutor&.email,
+        full_name: @school.induction_tutor&.full_name,
+      }
     end
 
     def tutor_form_params
