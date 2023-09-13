952c5e09511b84ef33e11e762ef8e26ef9d1d659
Lorenzo Tello <ltello8a@gmail.com>
2023-09-13 22:27:02 +0200

    CST-1961: rewrite service to nominate/replace induction tutors

--- app/forms/nominate_induction_tutor_form.rb
+++ app/forms/nominate_induction_tutor_form.rb
@@ -5,70 +5,93 @@ class NominateInductionTutorForm
   include ActiveRecord::AttributeAssignment
   include ActiveModel::Serialization
 
-  attr_accessor :full_name, :email, :token, :school_id, :user_id
+  attr_accessor :email, :full_name, :school, :token
 
-  validates :full_name, presence: true, on: :full_name
-  validates :email, presence: true, notify_email: true, on: :email
-  validate :different_name, on: :full_name
-  validate :email_is_not_in_use, on: :email
-  validate :name_matches, on: :email
+  # Validations
+  validates :email, presence: true, notify_email: true, on: %i[email]
+  validates :full_name, presence: true, on: %i[full_name email]
+  validates :school, presence: true, on: %i[email]
+  validate :not_current_sit, on: %i[email]
+  validate :name_matches, on: %i[email]
 
+  # Instance Methods
   def attributes
-    {
-      full_name:,
-      email:,
-      token:,
-    }
+    { email:, full_name:, token: }
   end
 
-  def school
-    if school_id
-      School.find school_id
-    else
-      NominationEmail.find_by(token:).school
+  def save!
+    raise errors.full_messages unless valid?(:email)
+
+    ActiveRecord::Base.transaction do
+      remove_current_sits
+      ensure_user_persisted
+      add_school_to_sit
+      inform_user
     end
-  end
 
-  def existing_name
-    user_scope = user_id.present? ? User.induction_coordinators.where.not(id: user_id) : User.induction_coordinators
-    existing_user = user_scope.find_by(email:)
-    return if existing_user.blank?
-
-    existing_user.full_name
-  end
-
-  def name_different?
-    existing_name.present? && existing_name != full_name
-  end
-
-  def email_already_taken?
-    Identity.find_user_by(email:)
+    true
   end
 
 private
 
-  def email_is_not_in_use
-    if email_already_taken?
-      other_school = school_using_this_email(email)
-      if other_school.present?
-        errors.add(:email, "The email address #{email} is already used by a user at #{other_school.name}")
-      else
-        errors.add(:email, "The email address #{email} is already in use")
-      end
-    end
+  def add_school_to_sit
+    sit_profile.schools << school
   end
 
-  def different_name
-    if name_different?
-      errors.add(:full_name, "An induction tutor has already been nominated using this email address, with the name #{existing_name}. The name you entered is #{full_name}.")
-    end
+  def different_name?
+    user.full_name != full_name
+  end
+
+  def ensure_user_persisted
+    user.save! unless user.persisted?
+  end
+
+  def inform_user
+    SchoolMailer.with(sit_profile:, school:, start_url:, step_by_step_url:)
+                .nomination_confirmation_email.deliver_later
   end
 
   def name_matches
-    errors.add(:full_name, :does_not_match) if name_different?
+    if different_name?
+      errors.add(:full_name, "A user with a different name (#{user.full_name}) has already been registered with this email address. Please, change the name or email you have entered.")
+    end
   end
 
-  def school_using_this_email(email)
-    Identity.find_user_by(email:).schools.first
+  def not_current_sit
+    errors.add(:email, "The user with email #{user.email} is already an induction coordinator at #{school.name}") if sit_at_school?
+  end
+
+  def remove_current_sits
+    school.induction_coordinators.each do |sit|
+      next sit.induction_coordinator_profile.schools.delete(school) if sit.schools.count > 1
+      next sit.induction_coordinator_profile.destroy! if sit.teacher_profile.present? || sit.npq_registered?
+      sit.destroy!
+    end
+  end
+
+  def sit_at_school?
+    school.induction_coordinators.include?(user) if school
+  end
+
+  def sit_profile
+    @sit_profile ||= user.induction_coordinator_profile || user.create_induction_coordinator_profile!
+  end
+
+  def start_url
+    Rails.application.routes.url_helpers.root_url(
+      host: Rails.application.config.domain,
+      **UTMService.email(:new_induction_tutor),
+    )
+  end
+
+  def step_by_step_url
+    Rails.application.routes.url_helpers.step_by_step_url(
+      host: Rails.application.config.domain,
+      **UTMService.email(:new_induction_tutor),
+    )
+  end
+
+  def user
+    @user ||= Identity.find_user_by(email:) || User.new(email:, full_name:)
   end
 end
--- spec/forms/nominate_induction_tutor_form_spec.rb
+++ spec/forms/nominate_induction_tutor_form_spec.rb
@@ -3,70 +3,165 @@
 require "rails_helper"
 
 RSpec.describe NominateInductionTutorForm, type: :model do
-  let(:nomination_email) { create(:nomination_email) }
-  let(:token) { nomination_email.token }
   let(:school) { nomination_email.school }
-  let(:name) { Faker::Name.name }
+  let(:full_name) { Faker::Name.name }
   let(:email) { Faker::Internet.email }
 
   describe "validations" do
-    it { is_expected.to validate_presence_of(:full_name).with_message("Enter a full name").on(%i[full_name check_name]) }
-    it { is_expected.to validate_presence_of(:email).with_message("Enter an email address").on(%i[email create]) }
+    it { is_expected.to validate_presence_of(:email).with_message("Enter an email address").on(%i[email]) }
+    it { is_expected.to validate_presence_of(:full_name).with_message("Enter a full name").on(%i[full_name email]) }
+    it { is_expected.to validate_presence_of(:school).with_message("can't be blank").on(%i[email]) }
 
-    it "validates that the email address is not in use by an ECT" do
-      create(:ect, user: create(:user, email:))
-      form = NominateInductionTutorForm.new(token:, full_name: name, email:)
-      expect(form.valid?(:email)).to be false
-      expect(form.errors[:email].first).to eq("The email address #{email} is already in use")
-      expect(form.email_already_taken?).to be_truthy
+    context "when the person is already a SIT at the school" do
+      let(:induction_tutor) { NewSeeds::Scenarios::Schools::School.new.build.with_an_induction_tutor.induction_tutor }
+      let(:form) { described_class.new(full_name:, email: induction_tutor.email, school: induction_tutor.schools.first) }
+
+      it "an error is added" do
+        expect(form.valid?(:email)).to be_falsey
+        expect(form.errors[:email].first).to eq("The user with email #{form.email} is already an induction coordinator at #{form.school.name}")
+      end
     end
 
-    it "allows the email to be in use by a mentor" do
-      create(:mentor, user: create(:user, email:))
+    context "when the name provided doesn't match the intended new SIT's name" do
+      let(:form) { described_class.new(full_name: "Another name", email:, school:) }
 
-      form = NominateInductionTutorForm.new(token:, full_name: name, email:)
-      expect(form).to be_valid
+      before do
+        create(:ect, user: create(:user, email:, full_name:))
+      end
+
+      it "an error is added" do
+        expect(form.valid?(:email)).to be_falsey
+        expect(form.errors[:full_name].first).to eq("A user with a different name (#{full_name}) has already been registered with this email address. Please, change the name or email you have entered.")
+      end
     end
 
-    it "allows the email to be in use by an induction tutor" do
-      create(:user, :induction_coordinator, full_name: name, email:)
-      form = NominateInductionTutorForm.new(token:, full_name: name, email:)
-      expect(form).to be_valid
+    context "when the email provided is in use by a mentor" do
+      let(:form) { described_class.new(full_name:, email:, school:) }
+
+      before do
+        create(:mentor, user: create(:user, email:, full_name:))
+      end
+
+      it "is valid" do
+        expect(form).to be_valid(:email)
+      end
     end
 
-    it "allows the email to be in use by a NPQ registrant" do
-      npq_user = create(:user, full_name: name, email:)
-      create(:npq_participant_profile, user: npq_user)
+    context "when the email provided is in use by an induction tutor" do
+      let(:form) { described_class.new(full_name:, email:, school:) }
 
-      form = NominateInductionTutorForm.new(token:, full_name: name, email:)
-      expect(form).to be_valid
+      before do
+        create(:user, :induction_coordinator, full_name:, email:)
+      end
+
+      it "is valid" do
+        expect(form).to be_valid(:email)
+      end
     end
 
-    it "validates that the name matches if the email matches an induction tutor" do
-      create(:user, :induction_coordinator, full_name: "One Name", email:)
-      form = NominateInductionTutorForm.new(token:, full_name: "Different Name", email:)
-      expect(form.valid?(:email)).to be false
-      expect(form.errors[:full_name].first).to eq("The name you entered does not match our records")
-      expect(form.name_different?).to be_truthy
+    context "when the email provided is in use by a NPQ registrant" do
+      let(:form) { described_class.new(full_name:, email:, school:) }
+
+      before do
+        create(:npq_participant_profile, user: create(:user, full_name:, email:))
+      end
+
+      it "is valid" do
+        expect(form).to be_valid(:email)
+      end
+    end
+
+    context "when the email provided is in use by a non-registered person" do
+      let(:form) { described_class.new(full_name:, email:, school:) }
+
+      it "is valid" do
+        expect(form).to be_valid(:email)
+      end
     end
   end
 
-  describe "#school" do
-    context "from token accessor" do
-      it "returns the correct school" do
-        form = NominateInductionTutorForm.new(token:)
+  describe "#save!" do
+    let(:start_url) { Rails.application.routes.url_helpers.root_url(host: Rails.application.config.domain, **UTMService.email(:new_induction_tutor)) }
+    let(:step_by_step_url) { Rails.application.routes.url_helpers.step_by_step_url(host: Rails.application.config.domain, **UTMService.email(:new_induction_tutor)) }
 
-        expect(form.school).to eql school
+    subject { described_class.new(full_name:, email:, school:) }
+
+    context "when the previous SIT manages more than a school" do
+      let(:previous_sit) { NewSeeds::Scenarios::Schools::School.new.build.with_an_induction_tutor.induction_tutor }
+
+      before do
+        previous_sit.induction_coordinator_profile.schools << school
+      end
+
+      it "removes the school from their list" do
+        expect { subject.save! }.to change { previous_sit.schools.size }.by(-1)
       end
     end
 
-    context "from school_id accessor" do
-      let(:school) { create(:school) }
-      it "returns the correct school" do
-        form = NominateInductionTutorForm.new(school_id: school.id)
+    context "when the previous SIT manages only one school" do
+      let(:previous_sit) { NewSeeds::Scenarios::Schools::School.new.build.with_an_induction_tutor.induction_tutor }
+      let(:school) { previous_sit.schools.first }
+      let(:sit_profile) { previous_sit.induction_coordinator_profile }
 
-        expect(form.school).to eql school
+      context "when the previous SIT has a teacher_profile" do
+        before do
+          create(:teacher_profile, user: previous_sit)
+        end
+
+        it "destroys their SIT profile" do
+          expect { subject.save! }.to change { previous_sit.reload.induction_coordinator_profile }.from(sit_profile).to(nil)
+        end
       end
+
+      context "when the previous SIT is NPQ registered" do
+        before do
+          create(:npq_participant_profile, user: previous_sit)
+        end
+
+        it "destroys their SIT profile" do
+          expect { subject.save! }.to change { previous_sit.reload.induction_coordinator_profile }.from(sit_profile).to(nil)
+        end
+      end
+
+      context "when the previous SIT has no other profiles" do
+        it "removes the SIT from the service" do
+          expect { subject.save! }.to change { User.exists?(previous_sit.id) }.from(true).to(false)
+        end
+      end
+    end
+
+    context "when the new SIT is not registered yet" do
+      it "they get registered" do
+        expect { subject.save! }.to change { User.exists?(email:, full_name:) }.from(false).to(true)
+      end
+    end
+
+    context "when the new SIT has not SIT profile" do
+      let(:new_sit) { create(:user, full_name:, email:) }
+
+      it "one is created for them" do
+        expect { subject.save! }.to change { new_sit.reload.induction_coordinator_profile.present? }.from(false).to(true)
+      end
+    end
+
+    context "when the new SIT manages no school" do
+      let(:new_sit) { create(:user, full_name:, email:) }
+
+      it "a first school is added to their list" do
+        expect { subject.save! }.to change { new_sit.reload.schools.size }.by(1)
+      end
+    end
+
+    context "when the new SIT manages other schools" do
+      let(:new_sit) { NewSeeds::Scenarios::Schools::School.new.build.with_an_induction_tutor(full_name:, email:).induction_tutor }
+
+      it "the school is added to their list" do
+        expect { subject.save! }.to change { new_sit.reload.schools.size }.by(1)
+      end
+    end
+
+    it "sends an email to the new SIT" do
+      expect { subject.save! }.to have_enqueued_mail(SchoolMailer, :nomination_confirmation_email)
     end
   end
 end
