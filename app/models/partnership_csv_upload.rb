b4e47ccd3cba5c7f4fae3bd37d9309790f84dc0f
cwrw <abeer.salameh@gmail.com>
2023-09-13 22:42:15 +0100

    Remove csv attachment in partnership csv upload model

--- app/models/partnership_csv_upload.rb
+++ app/models/partnership_csv_upload.rb
@@ -2,14 +2,9 @@
 
 class PartnershipCsvUpload < ApplicationRecord
   has_paper_trail
-  has_one_attached :csv
   belongs_to :lead_provider, optional: true
   belongs_to :delivery_partner, optional: true
   belongs_to :cohort
-
-  validate :csv_validation
-
-  MAX_FILE_SIZE = 2.megabytes
 
   def invalid_schools
     @invalid_schools ||= find_school_errors
@@ -25,9 +20,7 @@ class PartnershipCsvUpload < ApplicationRecord
   end
 
   def urns
-    @urns ||= csv.open { |csv| csv.readlines.map(&:chomp) }
-                 .map { |s| strip_bom(s) }
-                 .uniq
+    uploaded_urns.uniq
   end
 
   # NOTE: this method is intended for short term use while we migrate the urn
@@ -45,18 +38,6 @@ class PartnershipCsvUpload < ApplicationRecord
   end
 
 private
-
-  def csv_validation
-    return unless csv.attached?
-
-    if csv.filename.extension.downcase != "csv"
-      errors.add(:base, "File must be a CSV")
-    end
-
-    if csv.byte_size > MAX_FILE_SIZE
-      errors.add(:base, "File must be less than 2mb.")
-    end
-  end
 
   def find_school_errors
     errors = []
