123f1cec2c6251d1c40ce5db9f710de4593873d1
Leandro Alemão <leandro.costantini@gmail.com>
2023-09-21 10:22:24 +0100

    [CPDLP-2387] Address PR comment

--- spec/components/finance/statements/npq_details_table_spec.rb
+++ spec/components/finance/statements/npq_details_table_spec.rb
@@ -8,8 +8,7 @@ RSpec.describe Finance::Statements::NPQDetailsTable, type: :component do
   let(:npq_lead_provider)   { cpd_lead_provider.npq_lead_provider }
   let(:statement)           { create(:npq_statement, cpd_lead_provider:) }
   let(:participant_profile) { create(:npq_application, :accepted, :eligible_for_funding, npq_course:, npq_lead_provider:).profile }
-  let!(:npq_course)              { create(:npq_leadership_course, identifier: "npq-leading-teaching") }
-  let!(:contract)                { create(:npq_contract, npq_lead_provider:, cohort:, monthly_service_fee: nil) }
+  let!(:npq_course)         { create(:npq_leadership_course, identifier: "npq-leading-teaching") }
 
   let!(:participant_declaration) do
     travel_to statement.deadline_date do
@@ -19,10 +18,13 @@ RSpec.describe Finance::Statements::NPQDetailsTable, type: :component do
 
   let(:rendered) { render_inline(described_class.new(statement:)) }
 
+  before do
+    npq_contract = NPQContract.find_by(npq_lead_provider:, cohort:, npq_course:)
+    npq_contract.update!(monthly_service_fee: nil)
+  end
+
   it "has the correct text" do
-    expect(rendered).to have_text("Total starts")
-    expect(rendered).to have_text(2)
-    expect(rendered).to have_text("Total net VAT")
-    expect(rendered).to have_text("£1,532.63")
+    expect(rendered).to have_text("Total starts\n          \n            \n              \n                \n                  1")
+    expect(rendered).to have_text("Total net VAT\n          £1,372.63")
   end
 end
