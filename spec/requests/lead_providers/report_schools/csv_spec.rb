0daaac331214351265d72b84c6ccbe075f6a5b9a
Frankie Roberto <frankie@frankieroberto.com>
2023-09-26 15:04:18 +0100

    Update test

--- spec/requests/lead_providers/report_schools/csv_spec.rb
+++ spec/requests/lead_providers/report_schools/csv_spec.rb
@@ -29,7 +29,7 @@ RSpec.describe "Lead Provider school reporting: uploading csv", type: :request d
   describe "POST /lead-providers/report-schools/csv" do
     context "with no file selected" do
       it "renders :new with error message" do
-        expect_any_instance_of(AnalyticsDataLayer).to receive(:add).with(csv_file_errors: %w[please_select_a_csv_file_to_upload])
+        expect_any_instance_of(AnalyticsDataLayer).to receive(:add).with(csv_file_errors: %w[select_a_csv_file_to_upload])
 
         post "/lead-providers/report-schools/csv", params: {}
 
