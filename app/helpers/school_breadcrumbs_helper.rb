463c0389bc213938495265e5c5fa546c2bc016ef
Frankie Roberto <frankie@frankieroberto.com>
2023-10-03 14:32:46 +0100

    Code style fix

--- app/helpers/school_breadcrumbs_helper.rb
+++ app/helpers/school_breadcrumbs_helper.rb
@@ -4,7 +4,7 @@ module SchoolBreadcrumbsHelper
   def breadcrumbs(active_school = nil, active_participant = nil)
     breadcrumbs = base_breadcrumbs
     breadcrumbs.merge!({ active_school.name => schools_dashboard_path(school_id: active_school) }) if active_school.present?
-    breadcrumbs.merge!({ "Manage mentors and ECTs" => school_participants_path(school_id: active_school)}) if active_participant.present?
+    breadcrumbs.merge!({ "Manage mentors and ECTs" => school_participants_path(school_id: active_school) }) if active_participant.present?
     breadcrumbs
   end
 
