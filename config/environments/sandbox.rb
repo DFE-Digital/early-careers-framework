1d56f48bc9759b9dbeb5c48dea69c59ec61e0821
Ross Oliver <ross.oliver36@gmail.com>
2023-10-05 11:27:54 +0100

    Remove error_handler
    We are finding this disagrees witth Rubocop and need to investigate why.

--- config/environments/sandbox.rb
+++ config/environments/sandbox.rb
@@ -53,9 +53,6 @@ Rails.application.configure do
                            read_timeout: 0.2, # Defaults to 1 second
                            write_timeout: 0.2, # Defaults to 1 second
                            reconnect_attempts: 1, # Defaults to 0
-                           error_handler: lambda { |_method, _returning, exception|
-                                            Sentry.capture_exception(exception)
-                                          },
                        }
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
