f43cf828017d47781d80249202564be47e144fd5
Javier Fernandez <javier.fernandez@education.gov.uk>
2023-10-11 13:44:42 +0100

    Updated appliction synchronization controller with limit management

--- app/controllers/api/v1/npq/application_synchronizations_controller.rb
+++ app/controllers/api/v1/npq/application_synchronizations_controller.rb
@@ -15,11 +15,24 @@ module Api
           Api::V1::NPQ::ApplicationSynchronizationSerializer
         end
 
+        def updated_applications
+          NPQApplication.where("updated_at >= ?", 1.week.ago).select(:lead_provider_approval_status, :id, :participant_identity_id)
+        end
+
+        def applications_by_ids
+          ecf_ids = params[:ecf_ids].split(',')
+          remaining_applications_count = 1000 - NPQApplication.where("updated_at >= ?", 1.week.ago).count
+          applications_to_fetch_count = [remaining_applications_count, NPQApplication.count].min
+          if applications_to_fetch_count > 0
+            NPQApplication.where(id: ecf_ids).limit(applications_to_fetch_count).select(:lead_provider_approval_status, :id, :participant_identity_id)
+          end
+        end
+
         # TODO: This needs to be optimized.
         # It will work fine for now because we are fetching records
         # that have been change within a week.
         def set_npq_applications
-          @npq_applications = NPQApplication.where("updated_at >= ?", 1.week.ago).select(:lead_provider_approval_status, :id, :participant_identity_id)
+          @npq_applications = applications_by_ids + updated_applications
         end
       end
     end
