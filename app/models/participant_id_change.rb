a072e1c5ad661b4565cd5d6c8037e8dcd41d1d82
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2023-10-12 16:19:12 +0100

    [CPDLP-2467] Dont allow duplicate participant_id_change entries

--- app/models/participant_id_change.rb
+++ app/models/participant_id_change.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ParticipantIdChange < ApplicationRecord
+  has_paper_trail
+
   belongs_to :user
 
   belongs_to :from_participant, class_name: "User"
--- spec/models/participant_id_change_spec.rb
+++ spec/models/participant_id_change_spec.rb
@@ -3,6 +3,10 @@
 require "rails_helper"
 
 RSpec.describe ParticipantIdChange, type: :model do
+  it "enables paper trail" do
+    is_expected.to be_versioned
+  end
+
   describe "associations" do
     it { is_expected.to belong_to(:user).class_name("User") }
     it { is_expected.to belong_to(:from_participant).class_name("User") }
