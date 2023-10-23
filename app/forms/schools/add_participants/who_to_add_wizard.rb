ebdf61ac56b199a59f92e5c3b48771508ddeb6e3
Fabio Volpe <volpefabio@gmail.com>
2023-10-23 13:00:44 +0100

    removed `cohortless_dashboard` flag

--- app/forms/schools/add_participants/who_to_add_wizard.rb
+++ app/forms/schools/add_participants/who_to_add_wizard.rb
@@ -43,12 +43,6 @@ module Schools
 
         return true if desired_cohort.start_year <= Cohort.current.start_year
 
-        # if Cohort.within_next_registration_period? && desired_cohort == Cohort.next
-        #   FeatureFlag.active?(:cohortless_dashboard, for: school)
-        # else
-        #   false
-        # end
-
         Cohort.within_next_registration_period? && desired_cohort == Cohort.next
       end
 
