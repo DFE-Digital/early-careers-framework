6382886be363f657e7a438bacdf275e08c01636c
Fabio Volpe <volpefabio@gmail.com>
2023-10-31 11:26:09 +0000

    spec fix

--- spec/mailers/school_mailer_spec.rb
+++ spec/mailers/school_mailer_spec.rb
@@ -76,7 +76,7 @@ RSpec.describe SchoolMailer, type: :mailer do
     end
 
     it "renders the right headers" do
-      expect(Cohort).to receive(:current).and_return(cohort).once
+      expect(Cohort).to receive(:active_registration_cohort).and_return(cohort).once
       expect(nomination_email.from).to eq(["mail@example.com"])
       expect(nomination_email.to).to eq([primary_contact_email])
     end
