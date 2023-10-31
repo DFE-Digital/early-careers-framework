66160b5a0ed5f84b6150efda145d22d4869349ad
Leonidas Apostolidis <leoapost@gmail.com>
2023-10-31 15:28:10 +0000

    Extract the induction statuses activation logic to a service class

--- app/controllers/admin/participants/change_induction_status_controller.rb
+++ app/controllers/admin/participants/change_induction_status_controller.rb
@@ -7,11 +7,7 @@ module Admin::Participants
     def edit; end
 
     def confirm_induction_status
-      @participant_profile = retrieve_participant_profile
-      @relevant_induction_record = Induction::FindBy.new(participant_profile: @participant_profile).call
-
-      update_the_participant_profile unless @participant_profile.active_record?
-      update_the_induction_record if %w[withdrawn leaving].include?(@relevant_induction_record&.induction_status)
+      Induction::InductionStatusesActivator.call(participant_profile: @participant_profile)
 
       flash[:success] = {
         title: "Induction status changed successfully",
@@ -28,20 +24,6 @@ module Admin::Participants
         policy_scope(ParticipantProfile).find(params[:participant_id]).tap do |participant_profile|
           authorize participant_profile, policy_class: participant_profile.policy_class
         end
-    end
-
-    def update_the_participant_profile
-      @participant_profile.update(status: :active)
-    end
-
-    def update_the_induction_record
-      induction_record_changes = { induction_status: :active }
-      induction_record_changes[:school_transfer] = true if @relevant_induction_record&.leaving_induction_status?
-
-      Induction::ChangeInductionRecord.call(
-        induction_record: @relevant_induction_record,
-        changes: induction_record_changes,
-      )
     end
   end
 end
