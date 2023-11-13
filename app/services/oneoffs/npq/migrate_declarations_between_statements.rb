c027c2082d10a61cf8ffd5728facc7a2d6a58c47
Ross Oliver <ross.oliver36@gmail.com>
2023-11-13 14:19:08 +0000

    Add oneoff service to migrate declarations between statements
    We occasionally need to migrate declarations between
    different finance statements.
    
    When doing this, we expect both statements to be output
    statements and after migrating the from statement should be
    updated to `output_fee: false`.
    
    We only want to allow migration of NPQ declarations between
    NPQ statements and only when they are of the same cohort.
    We need to ensure there is a to/from statement for each
    provider.

---
+++ app/services/oneoffs/npq/migrate_declarations_between_statements.rb
@@ -0,0 +1,92 @@
+# frozen_string_literal: true
+
+require "has_recordable_information"
+
+module Oneoffs::NPQ
+  class MigrateDeclarationsBetweenStatements
+    class StatementMismatchError < StandardError; end
+
+    include HasRecordableInformation
+
+    def initialize(cohort:, from_statement_name:, to_statement_name:)
+      @cohort = cohort
+      @from_statement_name = from_statement_name
+      @to_statement_name = to_statement_name
+    end
+
+    def migrate(dry_run: true)
+      reset_recorded_info
+      ensure_statements_align!
+      record_summary_info(dry_run)
+
+      ActiveRecord::Base.transaction do
+        migrate_declarations_between_statements!
+
+        raise ActiveRecord::Rollback if dry_run
+      end
+
+      recorded_info
+    end
+
+  private
+
+    attr_reader :cohort, :from_statement_name, :to_statement_name
+
+    def migrate_declarations_between_statements!
+      each_statements_by_provider do |provider, from_statement, to_statement|
+        record_declarations_info(provider, from_statement.participant_declarations)
+        migrate_line_items!(from_statement, to_statement)
+        from_statement.update!(output_fee: false)
+      end
+    end
+
+    def migrate_line_items!(from_statement, to_statement)
+      from_statement.statement_line_items.update!(statement_id: to_statement.id)
+    end
+
+    def each_statements_by_provider
+      from_statements_by_provider.each do |provider, from_statement|
+        to_statement = to_statements_by_provider[provider]
+        yield(provider, from_statement, to_statement)
+      end
+    end
+
+    def record_summary_info(dry_run)
+      record_info("~~~ DRY RUN ~~~") if dry_run
+      record_info("Migrating declarations from #{from_statement_name} to #{to_statement_name} for #{provider_count} providers")
+    end
+
+    def record_declarations_info(provider, declarations)
+      record_info("Migrating #{declarations.size} declarations for #{provider.name}")
+    end
+
+    def ensure_statements_align!
+      statements_mismatch = from_statements_by_provider.keys.sort != to_statements_by_provider.keys.sort
+      statements_empty = from_statements_by_provider.empty? && to_statements_by_provider.empty?
+
+      raise StatementMismatchError, "There is a mismatch between to/from statements" if statements_mismatch
+      raise StatementMismatchError, "No statements were found" if statements_empty
+    end
+
+    def provider_count
+      from_statements_by_provider.count
+    end
+
+    def from_statements_by_provider
+      @from_statements_by_provider ||= statements_by_provider(from_statement_name)
+    end
+
+    def to_statements_by_provider
+      @to_statements_by_provider ||= statements_by_provider(to_statement_name)
+    end
+
+    def statements_by_provider(statement_name)
+      Finance::Statement::NPQ
+        .includes(:cohort)
+        .where(cohort:, name: statement_name)
+        .output
+        .group_by(&:npq_lead_provider)
+        .transform_values(&:first)
+    end
+  end
+end
