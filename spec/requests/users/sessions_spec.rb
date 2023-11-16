b60da27e3f6bca0e1b0b09be5e08d36c299aef34
Fabio Volpe <volpefabio@gmail.com>
2023-11-16 14:19:21 +0000

    use of `disable_cohort_setup_check`

--- spec/requests/users/sessions_spec.rb
+++ spec/requests/users/sessions_spec.rb
@@ -226,10 +226,6 @@ RSpec.describe "Users::Sessions", type: :request do
       let(:school) { user.schools.first }
       let!(:cohort) { create :cohort, :current }
 
-      # before do
-      #   allow(Cohort).to receive(:active_registration_cohort).and_return(cohort)
-      # end
-
       it "redirects to correct dashboard" do
         inside_registration_window(cohort:) do
           post "/users/sign_in_with_token", params: { login_token: user.login_token }
