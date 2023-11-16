88aebb2ce644ed5b908b62aa6d93b24162853a81
Fabio Volpe <volpefabio@gmail.com>
2023-11-16 14:18:42 +0000

    use of `disable_cohort_setup_check`

--- spec/requests/schools/core_programme/materials_spec.rb
+++ spec/requests/schools/core_programme/materials_spec.rb
@@ -11,7 +11,6 @@ RSpec.describe "Schools::CoreProgramme::Materials", type: :request do
   let(:cip) { create :core_induction_programme }
 
   before do
-    allow(Cohort).to receive(:active_registration_cohort).and_return(cohort)
     sign_in user
   end
 
