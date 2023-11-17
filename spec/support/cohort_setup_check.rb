6ff2b14190691ed2504a1cb3fe921376cd05be7f
Fabio Volpe <655705+lazydevorg@users.noreply.github.com>
2023-11-17 15:32:46 +0000

    Update spec/support/cohort_setup_check.rb
    Co-authored-by: Tony Headford <tony@binarycircus.com>

--- spec/support/cohort_setup_check.rb
+++ spec/support/cohort_setup_check.rb
@@ -3,6 +3,4 @@
 # prevent sending the user to setup the cohort when testing other journies
 def disable_cohort_setup_check
   allow_any_instance_of(Schools::BaseController).to receive(:redirect_to_setup_cohort)
-  # allow_any_instance_of(Schools::DashboardController).to receive(:set_up_new_cohort?).and_return(false)
-  # allow_any_instance_of(Schools::DashboardController).to receive(:check_school_cohorts).and_return(false)
 end
