8bd4d9e23183e093b8038e2d81dece0c3481a041
Lorenzo Tello <ltello8a@gmail.com>
2023-11-23 16:03:15 +0000

    CST-2134: when adding a sit as mentor user the typed name rather than the current one

--- spec/features/schools/participants/add_participants/sit_add_ect_spec.rb
+++ spec/features/schools/participants/add_participants/sit_add_ect_spec.rb
@@ -7,48 +7,52 @@ RSpec.describe "SIT adding themself as ECT", js: true do
   include ManageTrainingSteps
 
   before do
-    given_there_is_a_school_that_has_chosen_cip
-    set_participant_data
-    given_there_is_a_school_that_has_chosen_fip_and_partnered
-    and_i_am_signed_in_as_an_induction_coordinator
-    and_i_click_on(Cohort.current.description)
-    then_i_am_taken_to_fip_induction_dashboard
+    outside_auto_assignment_window do
+      given_there_is_a_school_that_has_chosen_cip
+      set_participant_data
+      given_there_is_a_school_that_has_chosen_fip_and_partnered
+      and_i_am_signed_in_as_an_induction_coordinator
+      and_i_click_on(Cohort.current.description)
+      then_i_am_taken_to_fip_induction_dashboard
 
-    set_dqt_validation_result
+      set_dqt_validation_result
+    end
   end
 
   scenario "SIT aborts process as it can't add themself as ECT" do
-    when_i_navigate_to_participants_dashboard
-    when_i_click_to_add_a_new_ect_or_mentor
-    then_i_should_be_on_the_who_to_add_page
+    outside_auto_assignment_window do
+      when_i_navigate_to_participants_dashboard
+      when_i_click_to_add_a_new_ect_or_mentor
+      then_i_should_be_on_the_who_to_add_page
 
-    when_i_select_to_add_a "ECT"
-    when_i_click_on_continue
+      when_i_select_to_add_a "ECT"
+      when_i_click_on_continue
 
-    then_i_am_taken_to_the_what_we_need_from_you_page
+      then_i_am_taken_to_the_what_we_need_from_you_page
 
-    when_i_click_on_continue
-    then_i_am_taken_to_add_ect_name_page
+      when_i_click_on_continue
+      then_i_am_taken_to_add_ect_name_page
 
-    when_i_add_ect_name
-    when_i_click_on_continue
-    then_i_am_taken_to_add_teachers_trn_page
+      when_i_add_ect_name
+      when_i_click_on_continue
+      then_i_am_taken_to_add_teachers_trn_page
 
-    when_i_add_the_trn
-    when_i_click_on_continue
-    then_i_am_taken_to_add_date_of_birth_page
+      when_i_add_the_trn
+      when_i_click_on_continue
+      then_i_am_taken_to_add_date_of_birth_page
 
-    when_i_add_a_date_of_birth
-    when_i_click_on_continue
-    then_i_am_taken_to_add_ect_or_mentor_email_page
+      when_i_add_a_date_of_birth
+      when_i_click_on_continue
+      then_i_am_taken_to_add_ect_or_mentor_email_page
 
-    when_i_add_sits_email
-    when_i_click_on_continue
-    then_i_am_taken_to_you_cant_add_yourself_as_ect_page
+      when_i_add_ect_or_mentor_email(email: @induction_coordinator_profile.user.email)
+      when_i_click_on_continue
+      then_i_am_taken_to_you_cant_add_yourself_as_ect_page
 
-    when_i_choose_to_cancel
-    and_i_click_on_continue
-    then_i_am_taken_to_manage_your_training_page
+      when_i_choose_to_cancel
+      and_i_click_on_continue
+      then_i_am_taken_to_manage_your_training_page
+    end
   end
 
   scenario "SIT chooses to add themself as Mentor instead" do
@@ -77,7 +81,7 @@ RSpec.describe "SIT adding themself as ECT", js: true do
       when_i_click_on_continue
       then_i_am_taken_to_add_ect_or_mentor_email_page
 
-      when_i_add_sits_email
+      when_i_add_ect_or_mentor_email(email: @induction_coordinator_profile.user.email)
       when_i_click_on_continue
       then_i_am_taken_to_you_cant_add_yourself_as_ect_page
 
@@ -90,7 +94,7 @@ RSpec.describe "SIT adding themself as ECT", js: true do
 
       when_i_choose_summer_term_this_cohort
       and_i_click_on_continue
-      then_i_am_taken_to_choose_sit_partnership_page
+      then_i_am_taken_to_choose_mentor_partnership_page
 
       when_i_choose_current_providers
       and_i_click_on_continue
@@ -100,7 +104,7 @@ RSpec.describe "SIT adding themself as ECT", js: true do
       then_i_am_taken_to_yourself_as_mentor_confirmation_page
 
       when_i_click_on_view_ects_and_mentors
-      then_i_see_the_sit_name
+      then_i_see_the_participant_name
     end
   end
 end
