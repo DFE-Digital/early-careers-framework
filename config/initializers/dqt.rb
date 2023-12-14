31789d271ab31b15bf9c38c6e0a6270f157f0876
Fabio Volpe <volpefabio@gmail.com>
2023-12-14 13:47:18 +0000

    Remove DQT module related files
    Deleted various files associated with the DQT module as they are no longer needed in the current application context. This includes files associated with API configuration, client requests-responses, and version-specific files. The changes were made keeping the codebase clean and lean, removing outdated or unused components.

--- config/initializers/dqt.rb
+++ config/initializers/dqt.rb
@@ -1,15 +1,3 @@
 # frozen_string_literal: true
 
-require "dqt"
-
-DQT.configure do |config|
-  def json_to_hash(json)
-    JSON.parse(json.gsub("=>", ":"), symbolize_names: true)
-  end
-
-  config.client.headers = { Authorization: Rails.application.config.dqt_client_api_key } if Rails.application.config.respond_to?(:dqt_client_api_key)
-  config.client.host = Rails.application.config.dqt_client_host if Rails.application.config.respond_to?(:dqt_client_host)
-  config.client.params = json_to_hash(Rails.application.config.dqt_client_params) if Rails.application.config.respond_to?(:dqt_client_params) && Rails.application.config.dqt_client_params.present?
-end
-
 require "full_dqt"
