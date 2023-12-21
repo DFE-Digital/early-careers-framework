59a6b39a678a2ede97f2e2a94a2bb373fdec2a3c
Peter Yates <peter.yates@graphia.co.uk>
2023-12-21 11:17:41 +0000

    Add migration config for Azure
    The long name will be `migration` and the short name `mg`
    
    The tfvars is copied from sandbox but we don't need domains/external
    hostnames because this is just for internal use.

---
+++ global_config/migration_aks.sh
@@ -0,0 +1,9 @@
+AZURE_RESOURCE_PREFIX=s189p01
+AZURE_SUBSCRIPTION=s189-teacher-services-cloud-production
+CONFIG=migration_aks
+CONFIG_SHORT=mg
+CONFIG_LONG=migration
+DEPLOY_ENV=${CONFIG}
+ENV_TAG=Production
+PLATFORM=aks
+NAMESPACE=cpd-production
---
+++ terraform/aks/workspace_variables/migration_aks_backend.tfvars
@@ -0,0 +1,4 @@
+resource_group_name  = "s189p01-cpdecf-mg-rg"
+storage_account_name = "s189p01cpdecftfstatemgsa"
+container_name       = "cpdecf-tfstate"
+key                  = "terraform.tfstate"
