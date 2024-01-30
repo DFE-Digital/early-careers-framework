3e6a1c0a2ded900a6d23e5e8a958fd09bb830512
Fabio Volpe <volpefabio@gmail.com>
2024-01-30 18:14:33 +0000

    track_validation_error added

--- app/controllers/finance/banding_tracker/provider_choices_controller.rb
+++ app/controllers/finance/banding_tracker/provider_choices_controller.rb
@@ -11,7 +11,11 @@ module Finance
       def create
         choose_provider_form.attributes = choose_provider_params
 
-        return redirect_to finance_banding_tracker_provider_path(id: choose_provider_form.id) if choose_provider_form.valid?
+        if choose_provider_form.valid?
+          return redirect_to finance_banding_tracker_provider_path(id: choose_provider_form.id)
+        else
+          track_validation_error(choose_provider_form)
+        end
 
         @providers = LeadProvider.name_order
         render :new
