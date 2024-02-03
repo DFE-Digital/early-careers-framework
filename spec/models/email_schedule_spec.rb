cc40b8574fae3a86c39e41f507c0aca384f1611e
Leonidas Apostolidis <leoapost@gmail.com>
2024-02-03 01:02:19 +0000

    Add more validations to EmailSchedule model
    1. Ensure schedule date is in the future
    2. Allow only one email is scheduled per day. Can be bypassed by skipping
    the validations if we need to manually schedule more emails.

--- spec/models/email_schedule_spec.rb
+++ spec/models/email_schedule_spec.rb
@@ -6,6 +6,25 @@ RSpec.describe EmailSchedule, type: :model do
   it { is_expected.to validate_inclusion_of(:mailer_name).in_array(EmailSchedule::MAILERS.keys.map(&:to_s)) }
   it { is_expected.to validate_presence_of(:scheduled_at) }
 
+  describe "custom validations" do
+    describe "#validate_future_schedule_date" do
+      let(:past_scheduled_email) { build(:seed_email_factory, scheduled_at: Time.current.yesterday.midday) }
+      let(:future_scheduled_email) { build(:seed_email_factory, scheduled_at: Time.current.tomorrow.midday) }
+
+      it "adds an error when scheduled_at is not in the future" do
+        past_scheduled_email.valid?
+
+        expect(past_scheduled_email.errors[:scheduled_at]).to include("The schedule date must be in the future")
+      end
+
+      it "does not add an error when scheduled_at is in the future" do
+        future_scheduled_email.valid?
+
+        expect(future_scheduled_email.errors[:scheduled_at]).to be_empty
+      end
+    end
+  end
+
   describe "scopes" do
     let!(:scheduled_today) { create(:seed_email_factory, :scheduled_for_today) }
     let!(:scheduled_later) { create(:seed_email_factory) }
