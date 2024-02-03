80947208e3b253431662082f9f930e0080074d7f
Leonidas Apostolidis <leoapost@gmail.com>
2024-02-03 01:41:44 +0000

    Create CRUD pages the EmailSchedule

---
+++ app/controllers/admin/performance/email_schedules_controller.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+class Admin::Performance::EmailSchedulesController < Admin::BaseController
+  skip_after_action :verify_authorized
+  skip_after_action :verify_policy_scoped
+  before_action :set_email_schedule, only: %i[show edit update delete]
+
+  def index
+    @upcoming_emails = EmailSchedule.queued.order(:scheduled_at)
+    @recently_sent = EmailSchedule.where.not(status: :queued).order(scheduled_at: :desc).limit(20)
+  end
+
+  def show; end
+
+  def new
+    @email_schedule = EmailSchedule.new
+  end
+
+  def edit; end
+
+  def create
+    @email_schedule = EmailSchedule.new(email_schedule_params)
+
+    if @email_schedule.save
+      set_success_message content: "Email has been scheduled"
+      redirect_to admin_performance_email_schedules_url
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @email_schedule.update(email_schedule_params)
+      set_success_message content: "Email scheduled has been updated"
+      redirect_to admin_performance_email_schedules_url
+    else
+      render :edit
+    end
+  end
+
+  def delete
+    @email_schedule.destroy!
+    redirect_to admin_performance_email_schedules_url, status: :see_other
+  end
+
+private
+
+  def set_email_schedule
+    @email_schedule = EmailSchedule.find(params[:id])
+  end
+
+  def email_schedule_params
+    params.fetch(:email_schedule, {}).permit(:mailer_name, :scheduled_at)
+  end
+end
