f2c3a9684e0e1ebdcd1e020332b9ba1708c53f45
Leonidas Apostolidis <leoapost@gmail.com>
2024-02-03 01:07:31 +0000

    Allow to create EmailSchedule factories scheduled at any date

--- spec/factories/seeds/email_schedule_factory.rb
+++ spec/factories/seeds/email_schedule_factory.rb
@@ -6,19 +6,30 @@ FactoryBot.define do
     scheduled_at { 1.week.from_now }
     status { "queued" }
 
+    trait :skip_validations do
+      to_create { |instance| instance.save(validate: false) }
+    end
+
     trait(:scheduled_for_today) do
       scheduled_at { Date.current }
       status { "queued" }
+
+      skip_validations
     end
 
     trait(:sending) do
       scheduled_at { Date.current }
       status { "sending" }
+
+      skip_validations
     end
 
     trait(:sent) do
       scheduled_at { 1.month.ago }
       status { "sent" }
+      actual_email_count { rand(15_000) }
+
+      skip_validations
     end
 
     trait(:valid) {}
