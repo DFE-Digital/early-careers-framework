31a37b04cc5076bdd4504110207c1b2e34b7ceb9
Leonidas Apostolidis <leoapost@gmail.com>
2024-02-06 14:14:42 +0000

    Move the estimated emails to be sent to a helper method

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -121,4 +121,10 @@ module AdminHelper
 
     govuk_link_to(text, template_url, target: "_blank")
   end
+
+  def estimate_bulk_emails_count(email_schedule)
+    BulkMailers::SchoolReminderComms
+      .new(cohort: Cohort.containing_date(email_schedule.scheduled_at), dry_run: true)
+      .send(email_schedule.mailer_method)
+  end
 end
