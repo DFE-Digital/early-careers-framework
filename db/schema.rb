3f8cee06cfb0acab7c25612255bbfef9a8877bb8
Tony Headford <tony@binarycircus.com>
2024-02-06 17:11:23 +0000

    retimestamp migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_25_143240) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_06_170145) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "fuzzystrmatch"
