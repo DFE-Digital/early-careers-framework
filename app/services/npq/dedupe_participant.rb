501240661c71db7f0cc50212c1a17b1bd1fbec61
Ross Oliver <ross.oliver36@gmail.com>
2024-02-08 16:51:48 +0000

    Prevent flip/flop when deduping NPQ participants
    Currently, if we call `NPQP::DedupeParticipant` with an `NPQApplication` that
    is associated with the oldest `TeacherProfile` it will always transfer this
    user onto a more recent `TeacherProfile` (as we exclude the `from_user` from
    the `to_user` lookup).
    
    This results in users flip/flopping on `TeacherProfile` records. Instead, we
    should never exclude the oldest `TeacherProfile` and only transfer if the
    `NPQApplication` user is more recent.
    
    As the `Identity::Transfer` class prevents duplicate `ParticipantIdChange`
    records from being created we can omit this check from the
    `NPQ::DedupeParticipant` service.

--- app/services/npq/dedupe_participant.rb
+++ app/services/npq/dedupe_participant.rb
@@ -10,45 +10,39 @@ module NPQ
 
     validates :npq_application, presence: { message: I18n.t(:missing_npq_application) }
     validates :trn, presence: { message: I18n.t(:missing_trn) }
-    validates :to_user, presence: { message: I18n.t(:missing_to_user) }
-    validates :from_user, presence: { message: I18n.t(:missing_from_user) }
+    validates :primary_user_for_trn, presence: { message: I18n.t(:missing_primary_user_for_trn) }
+    validates :application_user, presence: { message: I18n.t(:missing_application_user) }
     validate :trn_validated
-    validate :dedupe_already_taken
+    validate :duplication_exists
 
     def call
       return if invalid?
 
-      Identity::Transfer.call(from_user:, to_user:)
+      Identity::Transfer.call(from_user: application_user, to_user: primary_user_for_trn)
     end
 
   private
 
-    def from_user
-      @from_user ||= User.find_by(id: npq_application&.user_id)
+    def application_user
+      @application_user ||= User.find_by(id: npq_application&.user_id)
     end
 
-    def to_user
-      @to_user ||= TeacherProfile
+    def primary_user_for_trn
+      @primary_user_for_trn ||= TeacherProfile
         .joins(:user)
         .includes(:user)
         .oldest_first
         .where(trn:)
-        .where.not(users: { id: from_user&.id })
         .first
         &.user
     end
 
-    def trn_validated
-      return if npq_application&.teacher_reference_number_verified?
-
-      errors.add(:trn, I18n.t(:trn_not_validated))
+    def duplication_exists
+      errors.add(:trn, I18n.t(:no_dedupe_required)) if primary_user_for_trn == application_user
     end
 
-    def dedupe_already_taken
-      return if from_user&.participant_id_changes.blank?
-      return unless from_user.participant_id_changes.where(from_participant: [from_user, to_user], to_participant: [from_user, to_user]).any?
-
-      errors.add(:trn, I18n.t(:dedupe_has_already_taken_place))
+    def trn_validated
+      errors.add(:trn, I18n.t(:trn_not_validated)) unless npq_application&.teacher_reference_number_verified?
     end
   end
 end
--- spec/services/npq/dedupe_participant_spec.rb
+++ spec/services/npq/dedupe_participant_spec.rb
@@ -7,98 +7,69 @@ RSpec.describe NPQ::DedupeParticipant do
   let(:npq_lead_provider) { cpd_lead_provider.npq_lead_provider }
   let(:npq_application) { create(:npq_application, :eligible_for_funding, npq_lead_provider:) }
   let(:trn) { npq_application&.teacher_reference_number }
-  let(:user) { npq_application&.user.presence || create(:user) }
-  let!(:teacher_profile) { create(:teacher_profile, user:, trn:) }
-  let!(:same_trn_teacher_profile) { create(:teacher_profile, trn:) }
+  let!(:application_teacher_profile) { travel_to(1.day.from_now) { create(:teacher_profile, user: npq_application.user, trn:) } }
+  let!(:primary_teacher_profile) { create(:teacher_profile, trn:) }
 
-  let(:params) do
-    {
-      npq_application:,
-      trn:,
-    }
+  subject(:service) { described_class.new(npq_application:, trn:) }
+
+  def expect_error(attribute, message)
+    expect(service).to be_invalid
+    expect(service.errors.messages_for(attribute)).to include(message)
   end
 
-  subject(:service) { described_class.new(params) }
+  describe "validations" do
+    it { is_expected.to be_valid }
 
-  describe "#call" do
     context "when the npq application is missing" do
+      let(:application_teacher_profile) {}
       let(:npq_application) {}
 
-      it "is invalid returning a meaningful error message" do
-        is_expected.to be_invalid
-
-        expect(service.errors.messages_for(:npq_application)).to include("The npq application must be present")
-      end
+      it { expect_error(:npq_application, "The npq application must be present") }
     end
 
     context "when the trn is missing" do
       let(:trn) {}
 
-      it "is invalid returning a meaningful error message" do
-        is_expected.to be_invalid
-
-        expect(service.errors.messages_for(:trn)).to include("The teacher reference number (TRN) must be present")
-      end
+      it { expect_error(:trn, "The teacher reference number (TRN) must be present") }
     end
 
-    context "when the from_user is missing" do
-      before do
-        allow(npq_application).to receive(:user_id).and_return(nil)
-      end
+    context "when the application user is missing" do
+      before { allow(npq_application).to receive(:user_id).and_return(nil) }
 
-      it "is invalid returning a meaningful error message" do
-        is_expected.to be_invalid
-
-        expect(service.errors.messages_for(:from_user)).to include("The user to be deduped from must be present")
-      end
+      it { expect_error(:application_user, "The application must have a user") }
     end
 
-    context "when the to_user is missing" do
-      before do
-        same_trn_teacher_profile.update!(trn: "A23456")
-      end
+    context "when the primary_user_for_trn is missing" do
+      before { TeacherProfile.destroy_all }
 
-      it "is invalid returning a meaningful error message" do
-        is_expected.to be_invalid
-
-        expect(service.errors.messages_for(:to_user)).to include("The user to be deduped to must be present")
-      end
+      it { expect_error(:primary_user_for_trn, "There must be a primary user for this trn") }
     end
 
     context "when the TRN has not been validated yet" do
-      before do
-        npq_application.update!(teacher_reference_number_verified: false)
-      end
+      before { npq_application.update!(teacher_reference_number_verified: false) }
 
-      it "is invalid returning a meaningful error message" do
-        is_expected.to be_invalid
-
-        expect(service.errors.messages_for(:trn)).to include("Teacher reference number (TRN) has not been validated")
-      end
+      it { expect_error(:trn, "Teacher reference number (TRN) has not been validated") }
     end
 
-    context "when a dedupe with same users already took place" do
-      before do
-        user.participant_id_changes.create!(from_participant: user, to_participant: same_trn_teacher_profile.user)
-      end
+    context "when there application_user is the primary_user_for_trn (not a duplicate)" do
+      let!(:application_teacher_profile) {}
+      let!(:primary_teacher_profile) { create(:teacher_profile, trn:, user: npq_application.user) }
 
-      it "is invalid returning a meaningful error message" do
-        is_expected.to be_invalid
+      it { expect_error(:trn, "There is no duplication in this instance") }
+    end
+  end
 
-        expect(service.errors.messages_for(:trn)).to include("Deduplication has already taken place from these users")
-      end
+  describe "#call" do
+    it "calls Identity::Transfer" do
+      expect(Identity::Transfer).to receive(:call).with(from_user: application_teacher_profile.user, to_user: primary_teacher_profile.user)
+
+      service.call
     end
 
-    context "with correct params" do
-      it "is valid" do
-        is_expected.to be_valid
-      end
+    context "when the dedupe has previously been performed" do
+      before { described_class.new(npq_application:, trn:).call }
 
-      it "calls Identity::Transfer with correct params" do
-        expect(Identity::Transfer).to receive(:call).with(from_user: user, to_user: same_trn_teacher_profile.user)
-
-        subject.call
-      end
+      it { expect { service.call }.not_to change(ParticipantIdChange, :count) }
     end
   end
 end
