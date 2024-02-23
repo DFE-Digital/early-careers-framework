b7fd46f2632d60936a51240ffafa3e1e5eccd4dd
Tony Headford <tony@binarycircus.com>
2024-02-23 14:34:00 +0000

    Using destroy_all does not always seems to work so switching to destroy! each element

--- app/services/archive/destroy_ecf_profile_data.rb
+++ app/services/archive/destroy_ecf_profile_data.rb
@@ -36,14 +36,17 @@ module Archive
       ActiveRecord::Base.transaction do
         participant_profile.ecf_participant_validation_data&.destroy!
         participant_profile.ecf_participant_eligibility&.destroy!
-        participant_profile.participant_profile_states.destroy_all
-        participant_profile.participant_profile_schedules.destroy_all
-        participant_profile.participant_declarations.destroy_all
-        participant_profile.induction_records.destroy_all
-        participant_profile.validation_decisions.destroy_all
-        participant_profile.deleted_duplicates.destroy_all
+        # for some reason .destroy_all doesn't always seems to destroy the objects and
+        # doesn't fail loudly. Don't want to use .delete_all either as we want callbacks
+        # to trigger for analytics at least
+        participant_profile.participant_profile_states.each(&:destroy!)
+        participant_profile.participant_profile_schedules.each(&:destroy!)
+        participant_profile.participant_declarations.each(&:destroy!)
+        participant_profile.induction_records.each(&:destroy!)
+        participant_profile.validation_decisions.each(&:destroy!)
+        participant_profile.deleted_duplicates.each(&:destroy!)
 
-        participant_profile.school_mentors.destroy_all if participant_profile.mentor?
+        participant_profile.school_mentors.each(&:destroy!) if participant_profile.mentor?
         participant_profile.destroy!
       end
     end
