acab470976e8726a624c440f3d998b8689d8bd96
Tony Headford <tony@binarycircus.com>
2024-02-27 14:48:05 +0000

    Switch to using delete_all and no_touching for the bulk of the destruction

--- app/services/archive/destroy_ecf_profile_data.rb
+++ app/services/archive/destroy_ecf_profile_data.rb
@@ -34,19 +34,21 @@ module Archive
 
     def destroy_profile!
       ActiveRecord::Base.transaction do
-        participant_profile.ecf_participant_validation_data&.destroy!
-        participant_profile.ecf_participant_eligibility&.destroy!
-        # for some reason .destroy_all doesn't always seems to destroy the objects and
-        # doesn't fail loudly. Don't want to use .delete_all either as we want callbacks
-        # to trigger for analytics at least
-        participant_profile.participant_profile_states.each(&:destroy!)
-        participant_profile.participant_profile_schedules.each(&:destroy!)
-        participant_profile.participant_declarations.each(&:destroy!)
-        participant_profile.induction_records.each(&:destroy!)
-        participant_profile.validation_decisions.each(&:destroy!)
-        participant_profile.deleted_duplicates.each(&:destroy!)
+        ActiveRecord::Base.no_touching do
+          participant_profile.ecf_participant_validation_data&.delete
+          participant_profile.ecf_participant_eligibility&.delete
 
-        participant_profile.school_mentors.each(&:destroy!) if participant_profile.mentor?
+          participant_profile.participant_profile_states.delete_all(:delete_all)
+          participant_profile.participant_profile_schedules.delete_all(:delete_all)
+          participant_profile.participant_declarations.delete_all(:delete_all)
+          participant_profile.induction_records.delete_all(:delete_all)
+          participant_profile.validation_decisions.delete_all(:delete_all)
+          participant_profile.deleted_duplicates.delete_all(:delete_all)
+
+          participant_profile.school_mentors.delete_all(:delete_all) if participant_profile.mentor?
+        end
+
+        # allow callbacks and touching for this bit so we get analytics
         participant_profile.destroy!
       end
     end
