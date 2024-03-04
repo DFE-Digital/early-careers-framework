c2f57833f547146134726f1becc9acbe07818a35
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2024-03-04 10:30:43 +0000

    [CPDLP-2796] Identity::PrimaryUser only chooses non-archived users

--- spec/services/oneoffs/flipflop_users_cleanup_spec.rb
+++ spec/services/oneoffs/flipflop_users_cleanup_spec.rb
@@ -30,7 +30,7 @@ RSpec.describe Oneoffs::FlipflopUsersCleanup do
       expect(result[1]).to eql([user2.id, "TRN does not exist"])
       expect(result[2]).to eql([user3.id, "already the primary user"])
       expect(result[3]).to eql([user4.id, "transfer to user [#{user3.id}]"])
-      expect(result[4]).to eql([user5.id, "to_user [#{user1.id}] is archived"])
+      expect(result[4]).to eql([user5.id, "already the primary user"])
     end
   end
 
