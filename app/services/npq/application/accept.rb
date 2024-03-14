e434ef643bf1a3697e33ffd32bae18f885e1c2b9
Leandro Alemão <leandro.costantini@gmail.com>
2024-03-14 11:42:11 +0000

    [CPDLP-2849] Fix bug when accepting a NPQ application where another for the same course has already been accepted

--- app/services/npq/application/accept.rb
+++ app/services/npq/application/accept.rb
@@ -106,8 +106,14 @@ module NPQ
         @participant_profile ||= ParticipantProfile.find(npq_application.id)
       end
 
+      def trn
+        @trn ||= npq_application.teacher_reference_number_verified? ? npq_application.teacher_reference_number : user.teacher_profile&.trn
+      end
+
+      # Make sure existing or the new teacher profile has a valid TRN so we fetch same_trn_user & other_accepted_applications_with_same_course correctly
       def teacher_profile
-        @teacher_profile ||= user.teacher_profile || user.build_teacher_profile
+        @teacher_profile ||= (user.teacher_profile&.assign_attributes(trn:)
+                              user.teacher_profile) || user.build_teacher_profile(trn:)
       end
 
       def user
