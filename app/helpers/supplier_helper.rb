94be831cd32426aa878955a020b6c780f3c4db9d
matt <mattonfoot@gmail.com>
2024-03-21 11:25:37 +0000

    Add support UI to see lead provider suppliers, there users and there delivery partners

--- app/helpers/supplier_helper.rb
+++ app/helpers/supplier_helper.rb
@@ -2,6 +2,6 @@
 
 module SupplierHelper
   def supplier_link(supplier)
-    supplier.is_a?(LeadProvider) ? supplier.name : govuk_link_to(supplier.name, edit_admin_delivery_partner_path(supplier))
+    govuk_link_to(supplier.name, supplier.is_a?(LeadProvider) ? admin_lead_provider_path(supplier) : edit_admin_delivery_partner_path(supplier))
   end
 end
