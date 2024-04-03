1c688edbcde260e107ae2354f69e212b88f039f8
JackS <jack.scotti@education.gov.uk>
2024-04-03 12:01:11 +0100

    Fix test

--- spec/features/start_page_spec.rb
+++ spec/features/start_page_spec.rb
@@ -4,13 +4,8 @@ require "rails_helper"
 
 RSpec.feature "Start user journey", type: :feature, js: true, rutabaga: false do
   scenario "Start page is accessible" do
-    given_i_am_on_the_start_page
-    then_the_page_is_accessible
-  end
-
-  scenario "Root URL should be the Start page" do
     given_i_am_at_the_root_of_the_service
-    then_i_am_on_the_start_page
+    then_the_page_is_accessible
   end
 
 private
