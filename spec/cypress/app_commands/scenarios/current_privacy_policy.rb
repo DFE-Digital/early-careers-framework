be23be631be1a01224b63230897da6fb0ef0243a
Leonidas Apostolidis <leoapost@gmail.com>
2024-04-09 15:44:30 +0100

    Remove Cypress

--- spec/cypress/app_commands/scenarios/current_privacy_policy.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.create :privacy_policy
-PrivacyPolicy::Publish.call
--- spec/cypress/support/step_definitions/misc.js
+++
@@ -1,9 +0,0 @@
-import { Then } from "cypress-cucumber-preprocessor/steps";
-
-Then("the page should be accessible", () => {
-  cy.checkA11y();
-});
-
-Then("the Swagger documentation should be visible", () => {
-  cy.get("section.models").should("be.visible");
-});
