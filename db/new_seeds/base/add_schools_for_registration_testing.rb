e69d2e6aeab7af4ad557cf65241cb88026279cfa
Tony Headford <tony@binarycircus.com>
2024-04-19 15:31:02 +0100

    more seeds and fix scenario

--- db/new_seeds/base/add_schools_for_registration_testing.rb
+++ db/new_seeds/base/add_schools_for_registration_testing.rb
@@ -4,233 +4,142 @@ def padded_urn(num)
   num.to_s.rjust(6, "0")
 end
 
-previous_cohort = Cohort.previous
-current_cohort = Cohort.current
-cohorts_name = sprintf("%02d/%02d", previous_cohort.start_year % 100, current_cohort.start_year % 100)
-next_urn = 10
+previous_cohort = Cohort.within_next_registration_period? ? Cohort.current : Cohort.previous
+current_cohort = Cohort.active_registration_cohort
+
+next_urn = 110
 
 ActiveRecord::Base.transaction do
-  # schools without cohorts or partnerships set up
-  NewSeeds::Scenarios::Schools::School.new(name: "Cohortless Empty School FIP", urn: padded_urn(next_urn))
-    .build
-    .with_an_induction_tutor(full_name: "Ethel Emptyfip", email: "ethel.emptyfip@example.com")
+  # Previous cohort
+  # School that ran FIP "last" year
+  begin
+    school = NewSeeds::Scenarios::Schools::School.new(name: "Reg - FIP #{previous_cohort.academic_year} School", urn: padded_urn(next_urn))
+      .build
+      .with_an_induction_tutor(full_name: "Freddy Fip", email: "freddy.fip@example.com")
+      .chosen_fip_and_partnered_in(cohort: previous_cohort)
 
-  next_urn += 1
+    next_urn += 1
+    school_cohort = school.school_cohorts[previous_cohort.start_year]
 
-  NewSeeds::Scenarios::Schools::School.new(name: "Cohortless Empty School CIP", urn: padded_urn(next_urn))
-    .build
-    .with_an_induction_tutor(full_name: "Eddie Emptycip", email: "eddie.emptycip@example.com")
-
-  next_urn += 1
-
-  # Schools with a partnership set up with a cohort only with registered mentors and ECTS
-  school = NewSeeds::Scenarios::Schools::School.new(name: "Cohortless School #{previous_cohort.start_year}-FIP", urn: padded_urn(next_urn))
-    .build
-    .with_an_induction_tutor(full_name: "Terry Fip", email: "terry.fip@example.com")
-    .chosen_fip_and_partnered_in(cohort: previous_cohort)
-
-  next_urn += 1
-  school_cohort = school.school_cohorts[previous_cohort.start_year]
-
-  2.times do
-    NewSeeds::Scenarios::Participants::Ects::Ect.new(school_cohort:)
+    NewSeeds::Scenarios::Participants::Mentors::MentorWithSomeEcts.new(school_cohort:)
       .build
       .with_induction_record(induction_programme: school_cohort.default_induction_programme)
       .with_validation_data
       .with_eligibility
+      .with_mentees
+  end
 
-    NewSeeds::Scenarios::Participants::Mentors::MentorWithNoEcts.new(school_cohort:)
+  # School that ran CIP "last" year
+  begin
+    school = NewSeeds::Scenarios::Schools::School.new(name: "Reg - CIP #{previous_cohort.academic_year} School", urn: padded_urn(next_urn))
+      .build
+      .with_an_induction_tutor(full_name: "Colin Cip", email: "colin.cip@example.com")
+      .chosen_cip_with_materials_in(cohort: previous_cohort)
+
+    next_urn += 1
+    school_cohort = school.school_cohorts[previous_cohort.start_year]
+
+    NewSeeds::Scenarios::Participants::Mentors::MentorWithSomeEcts.new(school_cohort:)
       .build
       .with_induction_record(induction_programme: school_cohort.default_induction_programme)
       .with_validation_data
       .with_eligibility
+      .with_mentees
   end
 
-  school = NewSeeds::Scenarios::Schools::School.new(name: "Cohortless School #{previous_cohort.start_year}-CIP", urn: padded_urn(next_urn))
-    .build
-    .with_an_induction_tutor(full_name: "Carly Cip", email: "carly.cip@example.com")
-    .chosen_cip_with_materials_in(cohort: previous_cohort)
+  # School that ran DIY "last" year
+  begin
+    school = NewSeeds::Scenarios::Schools::School.new(name: "Reg - DIY #{previous_cohort.academic_year} School", urn: padded_urn(next_urn))
+      .build
+      .with_an_induction_tutor(full_name: "Debbie Diy", email: "debbie.diy@example.com")
+      .chosen_diy_in(cohort: previous_cohort)
 
-  next_urn += 1
-  school_cohort = school.school_cohorts[previous_cohort.start_year]
+    next_urn += 1
+  end
 
-  2.times do
-    NewSeeds::Scenarios::Participants::Ects::Ect.new(school_cohort:)
+  # School that had no ECTs "last" year
+  begin
+    school = NewSeeds::Scenarios::Schools::School.new(name: "Reg - No ECTs #{previous_cohort.academic_year} School", urn: padded_urn(next_urn))
+      .build
+      .with_an_induction_tutor(full_name: "Norma Noects", email: "norma.noects@example.com")
+      .chosen_no_ects_in(cohort: previous_cohort)
+
+    next_urn += 1
+  end
+
+  # School that didnt engage "last" year
+  begin
+    school = NewSeeds::Scenarios::Schools::School.new(name: "Reg - Cohortless #{previous_cohort.academic_year} School", urn: padded_urn(next_urn))
+      .build
+      .with_an_induction_tutor(full_name: "Claire Cohortless", email: "claire.cohortless@example.com")
+
+    next_urn += 1
+  end
+
+  #### Current registration cohort
+  # School that has chosen FIP
+  begin
+    school = NewSeeds::Scenarios::Schools::School.new(name: "Reg - FIP #{current_cohort.academic_year} School", urn: padded_urn(next_urn))
+      .build
+      .with_an_induction_tutor(full_name: "Felicity Fip", email: "felicity.fip@example.com")
+      .chosen_fip_and_partnered_in(cohort: current_cohort)
+
+    next_urn += 1
+    school_cohort = school.school_cohorts[current_cohort.start_year]
+
+    NewSeeds::Scenarios::Participants::Mentors::MentorWithSomeEcts.new(school_cohort:)
       .build
       .with_induction_record(induction_programme: school_cohort.default_induction_programme)
       .with_validation_data
       .with_eligibility
+      .with_mentees
+  end
 
-    NewSeeds::Scenarios::Participants::Mentors::MentorWithNoEcts.new(school_cohort:)
+  # School that has chosen CIP
+  begin
+    school = NewSeeds::Scenarios::Schools::School.new(name: "Reg - CIP #{current_cohort.academic_year} School", urn: padded_urn(next_urn))
+      .build
+      .with_an_induction_tutor(full_name: "Cathy Cip", email: "cathy.cip@example.com")
+      .chosen_cip_with_materials_in(cohort: current_cohort)
+
+    next_urn += 1
+    school_cohort = school.school_cohorts[current_cohort.start_year]
+
+    NewSeeds::Scenarios::Participants::Mentors::MentorWithSomeEcts.new(school_cohort:)
       .build
       .with_induction_record(induction_programme: school_cohort.default_induction_programme)
       .with_validation_data
       .with_eligibility
+      .with_mentees
   end
 
-  # Schools with a partnership set up with a cohort only with no registered mentors or ECTs
-  school = NewSeeds::Scenarios::Schools::School.new(name: "Cohortless School #{current_cohort.start_year}-FIP", urn: padded_urn(next_urn))
-    .build
-    .with_an_induction_tutor(full_name: "Tabitha Fip", email: "tabitha.fip@example.com")
-    .chosen_fip_and_partnered_in(cohort: current_cohort)
+  # School that has chosen DIY
+  begin
+    school = NewSeeds::Scenarios::Schools::School.new(name: "Reg - DIY #{current_cohort.academic_year} School", urn: padded_urn(next_urn))
+      .build
+      .with_an_induction_tutor(full_name: "Daniel Diy", email: "daniel.diy@example.com")
+      .chosen_diy_in(cohort: current_cohort)
 
-  next_urn += 1
-
-  school = NewSeeds::Scenarios::Schools::School.new(name: "Cohortless Second School #{current_cohort.start_year}-FIP", urn: padded_urn(next_urn))
-    .build
-    .with_an_induction_tutor(full_name: "Margaret Fip", email: "margaret.fip@example.com")
-    .chosen_fip_and_partnered_in(cohort: current_cohort)
-
-  next_urn += 1
-
-  school = NewSeeds::Scenarios::Schools::School.new(name: "Cohortless School #{current_cohort.start_year}-CIP", urn: padded_urn(next_urn))
-    .build
-    .with_an_induction_tutor(full_name: "Charlie Cip", email: "charlie.cip@example.com")
-    .chosen_cip_with_materials_in(cohort: current_cohort)
-
-  next_urn += 1
-
-  # Schools with a partnership set up with cohorts with registered ECTs and registered Mentors
-  school = NewSeeds::Scenarios::Schools::School.new(name: "Cohortless School #{previous_cohort.academic_year}-FIP", urn: padded_urn(next_urn))
-    .build
-    .with_an_induction_tutor(full_name: "Tracey Fip", email: "tracey.fip@example.com")
-    .chosen_fip_and_partnered_in(cohort: previous_cohort)
-    .chosen_fip_and_partnered_in(cohort: current_cohort)
-
-  next_urn += 1
-
-  # Schools with a partnership set up with cohorts with registered ECTs and registered Mentors
-  school = NewSeeds::Scenarios::Schools::School.new(name: "Cohortless School #{cohorts_name}-FIP", urn: padded_urn(next_urn))
-    .build
-    .with_an_induction_tutor(full_name: "Mary Fip", email: "mary.fip@example.com")
-    .chosen_fip_and_partnered_in(cohort: previous_cohort)
-    .chosen_fip_and_partnered_in(cohort: current_cohort)
-
-  next_urn += 1
-
-  [previous_cohort.start_year, current_cohort.start_year].each do |year|
-    school_cohort = school.school_cohorts[year]
-
-    2.times do
-      NewSeeds::Scenarios::Participants::Ects::Ect.new(school_cohort:)
-        .build
-        .with_induction_record(induction_programme: school_cohort.default_induction_programme)
-        .with_validation_data
-        .with_eligibility
-
-      NewSeeds::Scenarios::Participants::Mentors::MentorWithNoEcts.new(school_cohort:)
-        .build
-        .with_induction_record(induction_programme: school_cohort.default_induction_programme)
-        .with_validation_data
-        .with_eligibility
-    end
+    next_urn += 1
   end
 
-  school = NewSeeds::Scenarios::Schools::School.new(name: "Cohortless School #{cohorts_name}-CIP", urn: padded_urn(next_urn))
-    .build
-    .with_an_induction_tutor(full_name: "Colin Cip", email: "colin.cip@example.com")
-    .chosen_cip_with_materials_in(cohort: previous_cohort)
-    .chosen_cip_with_materials_in(cohort: current_cohort)
+  # School that had no ECTs "last" year
+  begin
+    school = NewSeeds::Scenarios::Schools::School.new(name: "Reg - No ECTs #{current_cohort.academic_year} School", urn: padded_urn(next_urn))
+      .build
+      .with_an_induction_tutor(full_name: "Nigel Noects", email: "nigel.noects@example.com")
+      .chosen_no_ects_in(cohort: current_cohort)
 
-  next_urn += 1
-
-  [previous_cohort.start_year, current_cohort.start_year].each do |year|
-    school_cohort = school.school_cohorts[year]
-
-    2.times do
-      NewSeeds::Scenarios::Participants::Ects::Ect.new(school_cohort:)
-        .build
-        .with_induction_record(induction_programme: school_cohort.default_induction_programme)
-        .with_validation_data
-        .with_eligibility
-
-      NewSeeds::Scenarios::Participants::Mentors::MentorWithNoEcts.new(school_cohort:)
-        .build
-        .with_induction_record(induction_programme: school_cohort.default_induction_programme)
-        .with_validation_data
-        .with_eligibility
-    end
+    next_urn += 1
   end
 
-  # Schools with a partnership set up with cohorts with registered ECTs but no Mentors
-  school = NewSeeds::Scenarios::Schools::School.new(name: "Cohortless School #{cohorts_name}-FIP No Mentors", urn: padded_urn(next_urn))
-    .build
-    .with_an_induction_tutor(full_name: "Trevor Fip-NoMentors", email: "trevor.fip.nomentors@example.com")
-    .chosen_fip_and_partnered_in(cohort: previous_cohort)
-    .chosen_fip_and_partnered_in(cohort: current_cohort)
+  # School that hasn't engaged
+  begin
+    school = NewSeeds::Scenarios::Schools::School.new(name: "Reg - Cohortless #{current_cohort.academic_year} School", urn: padded_urn(next_urn))
+      .build
+      .with_an_induction_tutor(full_name: "Craig Cohortless", email: "craig.cohortless@example.com")
 
-  next_urn += 1
+    next_urn += 1
 
-  [previous_cohort.start_year, current_cohort.start_year].each do |year|
-    school_cohort = school.school_cohorts[year]
-
-    2.times do
-      NewSeeds::Scenarios::Participants::Ects::Ect.new(school_cohort:)
-        .build
-        .with_induction_record(induction_programme: school_cohort.default_induction_programme)
-        .with_validation_data
-        .with_eligibility
-    end
-  end
-
-  school = NewSeeds::Scenarios::Schools::School.new(name: "Cohortless School #{cohorts_name}-CIP No Mentors", urn: padded_urn(next_urn))
-    .build
-    .with_an_induction_tutor(full_name: "Coleen Cip-NoMentors", email: "coleen.cip.nomentors@example.com")
-    .chosen_cip_with_materials_in(cohort: previous_cohort)
-    .chosen_cip_with_materials_in(cohort: current_cohort)
-
-  next_urn += 1
-
-  [previous_cohort.start_year, current_cohort.start_year].each do |year|
-    school_cohort = school.school_cohorts[year]
-
-    2.times do
-      NewSeeds::Scenarios::Participants::Mentors::MentorWithNoEcts.new(school_cohort:)
-        .build
-        .with_induction_record(induction_programme: school_cohort.default_induction_programme)
-        .with_validation_data
-        .with_eligibility
-    end
-  end
-
-  # Schools with a partnership set up with cohorts with no ECTs and registered Mentors
-  school = NewSeeds::Scenarios::Schools::School.new(name: "Cohortless School #{cohorts_name}-FIP-NoECTs", urn: padded_urn(next_urn))
-    .build
-    .with_an_induction_tutor(full_name: "Thomas Fip-NoECTs", email: "thomas.fip.noects@example.com")
-    .chosen_fip_and_partnered_in(cohort: previous_cohort)
-    .chosen_fip_and_partnered_in(cohort: current_cohort)
-
-  next_urn += 1
-
-  [previous_cohort.start_year, current_cohort.start_year].each do |year|
-    school_cohort = school.school_cohorts[year]
-
-    2.times do
-      NewSeeds::Scenarios::Participants::Mentors::MentorWithNoEcts.new(school_cohort:)
-        .build
-        .with_induction_record(induction_programme: school_cohort.default_induction_programme)
-        .with_validation_data
-        .with_eligibility
-    end
-  end
-
-  school = NewSeeds::Scenarios::Schools::School.new(name: "Cohortless School #{cohorts_name}-CIP-NoECTs", urn: padded_urn(next_urn))
-    .build
-    .with_an_induction_tutor(full_name: "Clare Cip-NoECTs", email: "clare.cip.noects@example.com")
-    .chosen_cip_with_materials_in(cohort: previous_cohort)
-    .chosen_cip_with_materials_in(cohort: current_cohort)
-
-  next_urn += 1
-
-  [previous_cohort.start_year, current_cohort.start_year].each do |year|
-    school_cohort = school.school_cohorts[year]
-
-    2.times do
-      NewSeeds::Scenarios::Participants::Mentors::MentorWithNoEcts.new(school_cohort:)
-        .build
-        .with_induction_record(induction_programme: school_cohort.default_induction_programme)
-        .with_validation_data
-        .with_eligibility
-    end
   end
 end
