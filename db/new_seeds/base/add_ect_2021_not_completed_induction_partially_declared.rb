e3a96eefd85ebe6772f7563b24a157889940eccb
Ross Oliver <ross.oliver36@gmail.com>
2024-05-02 10:53:05 +0100

    Fix seeds/partnerships

--- db/new_seeds/base/add_ect_2021_not_completed_induction_partially_declared.rb
+++ db/new_seeds/base/add_ect_2021_not_completed_induction_partially_declared.rb
@@ -8,17 +8,35 @@ cohort_2024 = Cohort.find_by!(start_year: 2024)
 
 course_identifier = "ecf-induction"
 
+delivery_partner = FactoryBot.create(:delivery_partner, name: "Test ECT Delivery Partner")
+school = FactoryBot.create(:school, name: "Test ECT School")
+
+ProviderRelationship.create!(delivery_partner:, lead_provider:, cohort: cohort_2021)
+created_partnership = Partnerships::Create.new({
+  cohort: 2021,
+  school_id: school.id,
+  lead_provider_id: lead_provider.id,
+  delivery_partner_id: delivery_partner.id,
+}).call
+raise RuntimeError unless created_partnership
+
+ProviderRelationship.create!(delivery_partner:, lead_provider:, cohort: cohort_2024)
+created_partnership = Partnerships::Create.new({
+  cohort: 2024,
+  school_id: school.id,
+  lead_provider_id: lead_provider.id,
+  delivery_partner_id: delivery_partner.id,
+}).call
+raise RuntimeError unless created_partnership
+
 seed_quantity(:ects_2021_not_completed_induction_partially_declared).times do
   # Create participant in 2021 cohort.
   participant_identity = FactoryBot.create(:participant_identity)
   user = participant_identity.user
-  participant_profile = FactoryBot.create(:ect, cohort: cohort_2021, lead_provider:, user:)
+  school_cohort_2021 = SchoolCohort.find_by!(school:, cohort: cohort_2021)
+  participant_profile = FactoryBot.create(:ect, cohort: cohort_2021, school_cohort: school_cohort_2021, lead_provider:, user:)
 
   # Create declarations against 2021.
   state = ParticipantDeclaration.states.values.sample
   FactoryBot.create(:participant_declaration, participant_profile:, state:, course_identifier:, cpd_lead_provider:)
-
-  # Setup 2024 induction programme to allow change schedule.
-  school = participant_profile.school
-  FactoryBot.create(:school_cohort, :cip, :with_induction_programme, cohort: cohort_2024, lead_provider:, school:)
 end
--- db/new_seeds/base/add_mentor_2021_not_completed_induction_partially_declared.rb
+++ db/new_seeds/base/add_mentor_2021_not_completed_induction_partially_declared.rb
@@ -8,11 +8,33 @@ cohort_2024 = Cohort.find_by!(start_year: 2024)
 
 course_identifier = "ecf-mentor"
 
+delivery_partner = FactoryBot.create(:delivery_partner, name: "Test Mentor Delivery Partner")
+school = FactoryBot.create(:school, name: "Test Mentor School")
+
+ProviderRelationship.create!(delivery_partner:, lead_provider:, cohort: cohort_2021)
+created_partnership = Partnerships::Create.new({
+  cohort: 2021,
+  school_id: school.id,
+  lead_provider_id: lead_provider.id,
+  delivery_partner_id: delivery_partner.id,
+}).call
+raise RuntimeError unless created_partnership
+
+ProviderRelationship.create!(delivery_partner:, lead_provider:, cohort: cohort_2024)
+created_partnership = Partnerships::Create.new({
+  cohort: 2024,
+  school_id: school.id,
+  lead_provider_id: lead_provider.id,
+  delivery_partner_id: delivery_partner.id,
+}).call
+raise RuntimeError unless created_partnership
+
 seed_quantity(:mentors_2021_not_completed_training_partially_declared).times do
   # Create participant in 2021 cohort.
   participant_identity = FactoryBot.create(:participant_identity)
   user = participant_identity.user
-  participant_profile = FactoryBot.create(:mentor, cohort: cohort_2021, lead_provider:, user:)
+  school_cohort_2021 = SchoolCohort.find_by!(school:, cohort: cohort_2021)
+  participant_profile = FactoryBot.create(:mentor, cohort: cohort_2021, school_cohort: school_cohort_2021, lead_provider:, user:)
 
   # Switch to Mentor schedule (not sure why the factory puts it as ECF in the first place).
   schedule = Finance::Schedule::Mentor.find_by!(cohort: cohort_2021)
@@ -22,8 +44,4 @@ seed_quantity(:mentors_2021_not_completed_training_partially_declared).times do
   # Create declarations against 2021.
   state = ParticipantDeclaration.states.values.sample
   FactoryBot.create(:participant_declaration, participant_profile:, state:, course_identifier:, cpd_lead_provider:)
-
-  # Setup 2024 induction programme to allow change schedule.
-  school = participant_profile.school
-  FactoryBot.create(:school_cohort, :cip, :with_induction_programme, cohort: cohort_2024, lead_provider:, school:)
 end
