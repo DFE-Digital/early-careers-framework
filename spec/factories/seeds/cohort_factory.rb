da983ef8f121cf4698e3b70f1a663392dd065709
Ross Oliver <ross.oliver36@gmail.com>
2024-05-03 08:26:30 +0100

    Force 2024 registration to start
    Bump the registation date into the past so that it triggers the 2024 registration.

--- spec/factories/seeds/cohort_factory.rb
+++ spec/factories/seeds/cohort_factory.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
   factory(:seed_cohort, class: "Cohort") do
     sequence(:start_year) { Faker::Number.unique.between(from: 2050, to: 3025) }
 
-    registration_start_date { Date.new(start_year, 5, 10) }
+    registration_start_date { Date.new(start_year, 5, 1) }
     academic_year_start_date { Date.new(start_year, 9, 1) }
     automatic_assignment_period_end_date { Date.new(start_year + 1, 3, 31) }
 
