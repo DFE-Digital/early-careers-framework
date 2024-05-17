184d369088f3e200d73eb009ed117f16b48c2ca6
Pablo Manrubia <pmanrubia@gmail.com>
2024-05-17 09:07:34 +0100

    Remove duplication from serializer

--- app/serializers/api/v1/npq_participant_serializer.rb
+++ app/serializers/api/v1/npq_participant_serializer.rb
@@ -18,31 +18,15 @@ module Api
       attribute(:participant_id, &:id)
 
       attribute(:npq_courses) do |object, params|
-        scope = object.npq_profiles
-        scope = scope.includes(npq_application: [:npq_course])
+        npq_profiles = npq_profiles(object, params)
 
-        if params[:cpd_lead_provider]
-          scope = scope.joins(npq_application: { npq_lead_provider: [:cpd_lead_provider] })
-          scope = scope.where(npq_applications: { npq_lead_providers: { cpd_lead_provider: params[:cpd_lead_provider] } })
-        else
-          scope = ParticipantProfile::NPQ.none
-        end
-
-        scope.map { |npq_profile| npq_profile.npq_application.npq_course.identifier }
+        npq_profiles.map { |npq_profile| npq_profile.npq_application.npq_course.identifier }
       end
 
       attribute :funded_places, if: -> { FeatureFlag.active?(:npq_capping) } do |object, params|
-        scope = object.npq_profiles
-        scope = scope.includes(npq_application: [:npq_course])
+        npq_profiles = npq_profiles(object, params)
 
-        if params[:cpd_lead_provider]
-          scope = scope.joins(npq_application: { npq_lead_provider: [:cpd_lead_provider] })
-          scope = scope.where(npq_applications: { npq_lead_providers: { cpd_lead_provider: params[:cpd_lead_provider] } })
-        else
-          scope = ParticipantProfile::NPQ.none
-        end
-
-        scope.map do |npq_profile|
+        npq_profiles.map do |npq_profile|
           {
             "npq_course": npq_profile.npq_application.npq_course.identifier,
             "funded_place:": npq_profile.npq_application.funded_place,
@@ -57,6 +41,19 @@ module Api
 
       attribute(:updated_at) do |object|
         object.updated_at.rfc3339
+      end
+
+      def self.npq_profiles(object, params)
+        scope = object.npq_profiles
+        scope = scope.includes(npq_application: [:npq_course])
+
+        if params[:cpd_lead_provider]
+          scope = scope.joins(npq_application: { npq_lead_provider: [:cpd_lead_provider] })
+          scope = scope.where(npq_applications: { npq_lead_providers: { cpd_lead_provider: params[:cpd_lead_provider] } })
+        else
+          scope = ParticipantProfile::NPQ.none
+        end
+        scope
       end
     end
   end
