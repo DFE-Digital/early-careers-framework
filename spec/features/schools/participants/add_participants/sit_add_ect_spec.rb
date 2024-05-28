0d7ad435c0ddcbbbf3acd5ba35db7d5f1c7c0746
Steve Laing <steve.laing@gmail.com>
2024-05-28 14:53:26 +0100

    Skip start term step in next cohort registration period
    When a participant has no induction start date from DQT and the current date is within the registration window for the next cohort, automatically assign them.

--- spec/features/schools/participants/add_participants/sit_add_ect_spec.rb
+++ spec/features/schools/participants/add_participants/sit_add_ect_spec.rb
@@ -7,20 +7,18 @@ RSpec.describe "SIT adding themself as ECT", js: true do
   include ManageTrainingSteps
 
   before do
-    outside_auto_assignment_window do
-      given_there_is_a_school_that_has_chosen_cip
-      set_participant_data
-      given_there_is_a_school_that_has_chosen_fip_and_partnered
-      and_i_am_signed_in_as_an_induction_coordinator
-      and_i_click_on(Cohort.current.description)
-      then_i_am_taken_to_fip_induction_dashboard
+    given_there_is_a_school_that_has_chosen_cip
+    set_participant_data
+    given_there_is_a_school_that_has_chosen_fip_and_partnered
+    and_i_am_signed_in_as_an_induction_coordinator
+    and_i_click_on(Cohort.current.description)
+    then_i_am_taken_to_fip_induction_dashboard
 
-      set_dqt_validation_result
-    end
+    set_dqt_validation_result
   end
 
   scenario "SIT aborts process as it can't add themself as ECT" do
-    outside_auto_assignment_window do
+    inside_auto_assignment_window do
       when_i_navigate_to_ect_dashboard
       when_i_click_to_add_a_new_ect
       then_i_should_be_on_the_who_to_add_page
@@ -56,7 +54,7 @@ RSpec.describe "SIT adding themself as ECT", js: true do
   end
 
   scenario "SIT chooses to add themself as Mentor instead" do
-    outside_auto_assignment_window do
+    inside_auto_assignment_window do
       when_i_navigate_to_ect_dashboard
       when_i_click_to_add_a_new_ect
       then_i_should_be_on_the_who_to_add_page
@@ -90,10 +88,6 @@ RSpec.describe "SIT adding themself as ECT", js: true do
       then_i_am_taken_to_add_yourself_as_mentor_confirmation_page
 
       when_i_click_on_confirm
-      then_i_am_taken_to_sit_mentor_start_training_page
-
-      when_i_choose_summer_term_this_cohort
-      and_i_click_on_continue
       then_i_am_taken_to_choose_mentor_partnership_page
 
       when_i_choose_current_providers
