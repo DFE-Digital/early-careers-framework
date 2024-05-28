c60b981c1e1b37ad55a8f6dd5dc0b60e8e097652
Steve Laing <steve.laing@gmail.com>
2024-05-28 14:53:26 +0100

    Skip start term step in next cohort registration period
    When a participant has no induction start date from DQT and the current date is within the registration window for the next cohort, automatically assign them.

--- spec/features/schools/participants/add_participants/sit_add_ect_without_induction_start_date_spec.rb
+++ spec/features/schools/participants/add_participants/sit_add_ect_without_induction_start_date_spec.rb
@@ -3,7 +3,7 @@
 require "rails_helper"
 require_relative "../../training_dashboard/manage_training_steps"
 
-RSpec.describe "SIT adds ECT participant with no induction start date", js: true do
+RSpec.describe "SIT adds ECT participant", js: true do
   include ManageTrainingSteps
 
   before do
@@ -18,7 +18,7 @@ RSpec.describe "SIT adds ECT participant with no induction start date", js: true
     end
   end
 
-  scenario "when inside the registration period" do
+  scenario "Participant doesn't have an induction start date" do
     inside_registration_window do
       when_i_navigate_to_ect_dashboard
       when_i_click_to_add_a_new_ect
@@ -50,74 +50,6 @@ RSpec.describe "SIT adds ECT participant with no induction start date", js: true
       when_i_click_confirm_and_add
       then_i_am_taken_to_the_confirmation_page
       and_the_participant_has_been_added_to_the_next_cohort
-    end
-  end
-
-  scenario "when outside the registration period" do
-    outside_auto_assignment_window do
-      when_i_navigate_to_ect_dashboard
-      when_i_click_to_add_a_new_ect
-      then_i_should_be_on_the_who_to_add_page
-
-      when_i_select_to_add_a "ECT"
-      when_i_click_on_continue
-
-      then_i_am_taken_to_the_what_we_need_from_you_page
-
-      when_i_click_on_continue
-      then_i_am_taken_to_add_ect_name_page
-
-      when_i_add_ect_name
-      when_i_click_on_continue
-      then_i_am_taken_to_add_teachers_trn_page
-
-      when_i_add_the_trn
-      when_i_click_on_continue
-      then_i_am_taken_to_add_date_of_birth_page
-
-      when_i_add_a_date_of_birth
-      when_i_click_on_continue
-      then_i_am_taken_to_add_ect_or_mentor_email_page
-
-      when_i_add_ect_or_mentor_email(email: @participant_data[:email])
-      when_i_click_on_continue
-
-      then_i_see_i_cannot_add_participant_yet
-    end
-  end
-
-  scenario "when training setup is needed" do
-    inside_registration_window do
-      @school_cohort_next.update!(induction_programme_choice: :design_our_own)
-
-      when_i_navigate_to_ect_dashboard
-      when_i_click_to_add_a_new_ect
-      then_i_should_be_on_the_who_to_add_page
-
-      when_i_select_to_add_a "ECT"
-      when_i_click_on_continue
-
-      then_i_am_taken_to_the_what_we_need_from_you_page
-
-      when_i_click_on_continue
-      then_i_am_taken_to_add_ect_name_page
-
-      when_i_add_ect_name
-      when_i_click_on_continue
-      then_i_am_taken_to_add_teachers_trn_page
-
-      when_i_add_the_trn
-      when_i_click_on_continue
-      then_i_am_taken_to_add_date_of_birth_page
-
-      when_i_add_a_date_of_birth
-      when_i_click_on_continue
-      then_i_am_taken_to_add_ect_or_mentor_email_page
-
-      when_i_add_ect_or_mentor_email(email: @participant_data[:email])
-      when_i_click_on_continue
-
-      then_i_see_that_training_setup_is_needed
     end
   end
 
