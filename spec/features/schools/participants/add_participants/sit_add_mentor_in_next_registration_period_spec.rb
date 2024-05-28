6fac284a907cb8d5a815a69fe84be37671f51f2d
Steve Laing <steve.laing@gmail.com>
2024-05-28 14:53:26 +0100

    Skip start term step in next cohort registration period
    When a participant has no induction start date from DQT and the current date is within the registration window for the next cohort, automatically assign them.

--- spec/features/schools/participants/add_participants/sit_add_mentor_in_next_registration_period_spec.rb
+++ spec/features/schools/participants/add_participants/sit_add_mentor_in_next_registration_period_spec.rb
@@ -3,16 +3,20 @@
 require "rails_helper"
 require_relative "../../training_dashboard/manage_training_steps"
 
-RSpec.describe "SIT adding mentor inside registration period", js: true do
+RSpec.describe "SIT adding mentor", js: true do
   include ManageTrainingSteps
 
-  scenario "when training setup is complete" do
+  before do
     inside_registration_window do
       given_there_is_a_school_that_has_chosen_cip
       given_there_is_a_school_that_has_chosen_fip_for_current_and_next_cohorts_and_partnered
       set_participant_data
       set_dqt_validation_result
+    end
+  end
 
+  scenario "Induction tutor adds a new mentor to current providers" do
+    inside_registration_window do
       given_there_is_a_sit
 
       when_i_sign_in_as_sit
@@ -56,49 +60,6 @@ RSpec.describe "SIT adding mentor inside registration period", js: true do
       when_i_click_on "View your mentors"
       then_i_see_the_mentor_name
       and_the_mentor_has_been_added_to_the_next_cohort
-    end
-  end
-
-  scenario "when training setup is needed" do
-    inside_registration_window do
-      given_there_is_a_school_that_has_chosen_cip
-      given_there_is_a_school_that_has_chosen_fip_and_needs_training_setup
-      set_participant_data
-      set_dqt_validation_result
-
-      given_there_is_a_sit
-
-      when_i_sign_in_as_sit
-      and_i_click_on(Cohort.current.description)
-      then_i_am_taken_to_fip_induction_dashboard
-
-      when_i_navigate_to_mentors_dashboard
-      and_i_click_to_add_a_new_mentor
-      then_i_should_be_on_the_who_to_add_page
-
-      when_i_select_to_add_a "Mentor"
-      and_i_click_on_continue
-      then_i_am_taken_to_the_what_we_need_from_mentor_page
-
-      when_i_click_on_continue
-      then_i_am_taken_to_add_mentor_full_name_page
-
-      when_i_add_mentor_name
-      and_i_click_on_continue
-      then_i_am_taken_to_add_teachers_trn_page
-
-      when_i_add_the_trn
-      and_i_click_on_continue
-      then_i_am_taken_to_add_date_of_birth_page
-
-      when_i_add_a_date_of_birth
-      and_i_click_on_continue
-      then_i_am_taken_to_add_ect_or_mentor_email_page
-
-      when_i_add_ect_or_mentor_email
-      and_i_click_on_continue
-
-      then_i_see_that_training_setup_is_needed
     end
   end
 
