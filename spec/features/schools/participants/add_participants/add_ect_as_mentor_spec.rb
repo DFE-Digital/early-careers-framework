e591a5735cd83164683ad6f75363beeb432b044e
Steve Laing <steve.laing@gmail.com>
2024-05-29 15:50:13 +0100

    Add registration is not yet open scenarios

--- spec/features/schools/participants/add_participants/add_ect_as_mentor_spec.rb
+++ spec/features/schools/participants/add_participants/add_ect_as_mentor_spec.rb
@@ -59,4 +59,36 @@ RSpec.describe "Add ECT as mentor", js: true do
       then_i_see_the_mentor_name
     end
   end
+
+  scenario "Induction tutor tries to add an ECT that's a mentor in another school outside auto assignment period" do
+    outside_auto_assignment_window do
+      when_i_navigate_to_mentors_dashboard
+      when_i_click_to_add_a_new_mentor
+      then_i_should_be_on_the_who_to_add_page
+
+      when_i_select_to_add_a "Mentor"
+      when_i_click_on_continue
+      then_i_am_taken_to_the_what_we_need_to_know_about_this_mentor_page
+
+      when_i_click_on_continue
+      then_i_am_taken_to_add_mentor_full_name_page
+
+      when_i_add_mentor_name
+      when_i_click_on_continue
+      then_i_am_taken_to_add_teachers_trn_page
+
+      when_i_add_the_trn
+      when_i_click_on_continue
+      then_i_am_taken_to_add_date_of_birth_page
+
+      when_i_add_a_date_of_birth
+      when_i_click_on_continue
+      then_i_am_taken_to_add_ect_or_mentor_email_page
+
+      when_i_add_ect_or_mentor_email
+      when_i_click_on_continue
+
+      then_i_see_i_cannot_add_participant_yet
+    end
+  end
 end
