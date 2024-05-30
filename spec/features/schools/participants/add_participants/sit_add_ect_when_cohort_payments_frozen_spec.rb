28723a12254db5eff2ba0cb3bd5e6de3ae4f654b
Steve Laing <steve.laing@gmail.com>
2024-05-30 17:38:17 +0100

    Add scenario for Cohort payments frozen
    This covers the journey of a participant who would be added to a Cohort where payments are frozen.

---
+++ spec/features/schools/participants/add_participants/sit_add_ect_when_cohort_payments_frozen_spec.rb
@@ -0,0 +1,68 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+require_relative "../../training_dashboard/manage_training_steps"
+
+RSpec.describe "SIT adding an ECT", js: true do
+  include ManageTrainingSteps
+
+  before do
+    inside_auto_assignment_window do
+      given_there_is_a_school_that_has_chosen_cip
+      given_there_is_a_school_that_has_chosen_fip_for_previous_and_current_cohort_and_partnered
+      and_i_am_signed_in_as_an_induction_coordinator
+      and_i_click_on(Cohort.current.description)
+      then_i_am_taken_to_fip_induction_dashboard
+
+      # @cohort here refers to the previous cohort
+      @cohort.update!(payments_frozen_at: 1.day.ago)
+      set_participant_data
+      # induction start date is in the previous cohort, which has had payments frozen
+      @participant_data[:start_date] = Date.new(Cohort.previous.start_year, 9, 1)
+      set_dqt_validation_result
+    end
+  end
+
+  scenario "when target cohort payments are frozen" do
+    inside_auto_assignment_window do
+      when_i_navigate_to_ect_dashboard
+      when_i_click_to_add_a_new_ect
+      then_i_should_be_on_the_who_to_add_page
+
+      when_i_select_to_add_a "ECT"
+      when_i_click_on_continue
+
+      then_i_am_taken_to_the_what_we_need_from_you_page
+
+      when_i_click_on_continue
+      then_i_am_taken_to_add_ect_name_page
+
+      when_i_add_ect_name
+      when_i_click_on_continue
+      then_i_am_taken_to_add_teachers_trn_page
+
+      when_i_add_the_trn
+      when_i_click_on_continue
+      then_i_am_taken_to_add_date_of_birth_page
+
+      when_i_add_a_date_of_birth
+      when_i_click_on_continue
+      then_i_am_taken_to_add_ect_or_mentor_email_page
+
+      when_i_add_ect_or_mentor_email(email: @participant_data[:email])
+      when_i_click_on_continue
+
+      when_i_click_confirm_and_add
+      then_i_see_confirmation_that_the_participant_has_been_added
+      and_the_participant_has_been_added_to_the_next_cohort
+    end
+  end
+
+  def then_i_see_confirmation_that_the_participant_has_been_added
+    expect(page).to have_content("#{@participant_data[:full_name]} has been added as an ECT")
+  end
+
+  def and_the_participant_has_been_added_to_the_next_cohort
+    expect(ParticipantProfile::ECT.last.school_cohort.cohort).to eq(Cohort.active_registration_cohort)
+  end
+end
