2a62b02b2d0ba10fd909deec4d2dc5216b1174f3
Lorenzo Tello <ltello8a@gmail.com>
2024-05-31 16:13:35 +0100

    CST-2565: move mentors out of frozen cohort when assigned an ect in unfrozen cohort

--- app/services/induction/change_mentor.rb
+++ app/services/induction/change_mentor.rb
@@ -8,7 +8,7 @@ class Induction::ChangeMentor < BaseService
         changes: { mentor_profile: },
       )
       induction_record.participant_profile.update!(mentor_profile:)
-
+      amend_mentor_cohort if sit_mentor_out_of_frozen_cohort?
       send_training_materials_if_needed
 
       mentor_profile&.user&.touch
@@ -24,6 +24,28 @@ private
     @mentor_profile = mentor_profile
   end
 
+  def amend_mentor_cohort
+    Induction::AmendParticipantCohort.new(participant_profile: mentor_profile,
+                                          source_cohort_start_year: mentor_profile.schedule.cohort.start_year,
+                                          target_cohort_start_year: Cohort.active_registration_cohort.start_year).save
+  end
+
+  def cip_materials
+    induction_record.induction_programme.core_induction_programme
+  end
+
+  def ect_in_payments_frozen_cohort?
+    induction_record.cohort.payments_frozen?
+  end
+
+  def send_training_materials_if_needed
+    return unless sit_name
+
+    if induction_record.enrolled_in_cip? && cip_materials.present?
+      send_training_materials
+    end
+  end
+
   def send_training_materials
     MentorMailer.with(
       mentor_profile:,
@@ -34,19 +56,13 @@ private
      .deliver_later
   end
 
-  def cip_materials
-    induction_record.induction_programme.core_induction_programme
+  def sit_mentor_out_of_frozen_cohort?
+    return false if ect_in_payments_frozen_cohort?
+
+    mentor_profile.eligible_to_change_cohort_and_continue_training?(cohort: Cohort.active_registration_cohort)
   end
 
   def sit_name
     induction_record.school.induction_tutor&.full_name
-  end
-
-  def send_training_materials_if_needed
-    return unless sit_name
-
-    if induction_record.enrolled_in_cip? && cip_materials.present?
-      send_training_materials
-    end
   end
 end
