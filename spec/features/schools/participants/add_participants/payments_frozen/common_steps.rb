db0bd3005109f19b2840af92e0d899020839ff77
Steve Laing <steve.laing@gmail.com>
2024-06-04 14:04:03 +0100

    Add scenario for transfering ECT when payments are frozen
    This feature emulates a SIT transfering an existing ECT between schools where the school cohorts have payments frozen.

--- spec/features/schools/participants/add_participants/payments_frozen/common_steps.rb
+++ spec/features/schools/participants/add_participants/payments_frozen/common_steps.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 def given_there_is_a_school_that_has_chosen_fip_for_four_cohorts_and_partnered
-  school = create(:school, name: "FIP School")
-  delivery_partner = create(:delivery_partner, name: "Amazing Delivery Team")
-  lead_provider = create(:lead_provider, name: "Big Provider Ltd")
-
   (2021..2024).each do |year|
     cohort = Cohort.find_or_create_by!(start_year: year)
     school_cohort = SchoolCohort.create!(cohort:, school:, induction_programme_choice: "full_induction_programme")
@@ -14,7 +10,6 @@ def given_there_is_a_school_that_has_chosen_fip_for_four_cohorts_and_partnered
     induction_programme.update!(partnership:)
 
     # Magic instance vars necessary for subsequent steps (!)
-    @school ||= school
     @school_cohort ||= school_cohort
     @induction_programme ||= induction_programme
   end
@@ -24,8 +19,52 @@ def earliest_cohort
   Cohort.find_by(start_year: 2021)
 end
 
+def school
+  @school ||= FactoryBot.create(:school, name: "FIP School")
+end
+
+def target_school
+  @target_school ||= FactoryBot.create(:school, name: "Target Fip School")
+end
+
+def lead_provider
+  @lead_provider ||= FactoryBot.create(:lead_provider, name: "Big Provider Ltd")
+end
+
+def delivery_partner
+  @delivery_partner ||= FactoryBot.create(:delivery_partner, name: "Amazing Delivery Team")
+end
+
 def and_the_earliest_cohort_has_payments_frozen
   earliest_cohort.update!(payments_frozen_at: 1.day.ago)
+end
+
+def and_there_is_another_school_that_has_chosen_fip_in_the_payments_frozen_cohort_and_partnered
+  target_school_cohort = create(:school_cohort, school: target_school,
+                                cohort: earliest_cohort, induction_programme_choice: "full_induction_programme")
+  induction_programme = create(:induction_programme, :fip, school_cohort: target_school_cohort, partnership: nil)
+  target_school_cohort.update!(default_induction_programme: induction_programme)
+  partnership = create(:partnership, school: target_school,
+                       lead_provider:, delivery_partner:, cohort: earliest_cohort, challenge_deadline: 2.weeks.ago)
+  induction_programme.update!(partnership:)
+
+  current_target_school_cohort = create(:school_cohort, school: target_school,
+                                        cohort: Cohort.active_registration_cohort, induction_programme_choice: "full_induction_programme")
+  current_induction_programme = create(:induction_programme, :fip, school_cohort: current_target_school_cohort, partnership: nil)
+  current_partnership = create(:partnership, school: target_school)
+  current_induction_programme.update!(partnership: current_partnership)
+end
+
+def and_i_am_signed_in_as_an_induction_coordinator_for_the_transfer_school
+  induction_coordinator_profile = create(
+    :induction_coordinator_profile,
+    schools: [target_school],
+    user: create(:user, full_name: "Carl Coordinator"),
+  )
+  create(:participant_identity, user: induction_coordinator_profile.user)
+  privacy_policy = create(:privacy_policy)
+  privacy_policy.accept!(induction_coordinator_profile.user)
+  sign_in_as induction_coordinator_profile.user
 end
 
 def when_i_complete_all_the_wizard_steps
