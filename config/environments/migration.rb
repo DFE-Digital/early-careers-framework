5ac64f872b24329d6919451afe00467d6bc7e655
Ross Oliver <ross.oliver36@gmail.com>
2024-06-07 10:19:51 +0100

    Add X-With-Server-Date header support to migration env
    We need this to be able to test some of the declaration scenarios.

--- config/environments/migration.rb
+++ config/environments/migration.rb
@@ -5,4 +5,7 @@ require Rails.root.join("config/environments/production")
 Rails.application.configure do
   config.log_level = :debug
   config.ssl_options = { redirect: { exclude: ->(request) { request.path.include?("/check") } } }
+
+  # Used to handle HTTP_X_WITH_SERVER_DATE header for server side datetime overwrite
+  config.middleware.use TimeTraveler
 end
