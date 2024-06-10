414ce9326cba7492a1a7ae353b6babb8fd93b161
Steve Laing <steve.laing@gmail.com>
2024-06-10 17:54:03 +0100

    Use mid_cohort spec flag
    Ensures we don't hit date related failures in spec

--- spec/features/schools/participants/add_participants/sit_add_ect_spec.rb
+++ spec/features/schools/participants/add_participants/sit_add_ect_spec.rb
@@ -3,7 +3,7 @@
 require "rails_helper"
 require_relative "../../training_dashboard/manage_training_steps"
 
-RSpec.describe "SIT adding themself as ECT", js: true do
+RSpec.describe "SIT adding themself as ECT", js: true, mid_cohort: true do
   include ManageTrainingSteps
 
   before do
@@ -18,87 +18,83 @@ RSpec.describe "SIT adding themself as ECT", js: true do
   end
 
   scenario "SIT aborts process as it can't add themself as ECT" do
-    inside_auto_assignment_window do
-      when_i_navigate_to_ect_dashboard
-      when_i_click_to_add_a_new_ect
-      then_i_should_be_on_the_who_to_add_page
+    when_i_navigate_to_ect_dashboard
+    when_i_click_to_add_a_new_ect
+    then_i_should_be_on_the_who_to_add_page
 
-      when_i_select_to_add_a "ECT"
-      when_i_click_on_continue
+    when_i_select_to_add_a "ECT"
+    when_i_click_on_continue
 
-      then_i_am_taken_to_the_what_we_need_from_you_page
+    then_i_am_taken_to_the_what_we_need_from_you_page
 
-      when_i_click_on_continue
-      then_i_am_taken_to_add_ect_name_page
+    when_i_click_on_continue
+    then_i_am_taken_to_add_ect_name_page
 
-      when_i_add_ect_name
-      when_i_click_on_continue
-      then_i_am_taken_to_add_teachers_trn_page
+    when_i_add_ect_name
+    when_i_click_on_continue
+    then_i_am_taken_to_add_teachers_trn_page
 
-      when_i_add_the_trn
-      when_i_click_on_continue
-      then_i_am_taken_to_add_date_of_birth_page
+    when_i_add_the_trn
+    when_i_click_on_continue
+    then_i_am_taken_to_add_date_of_birth_page
 
-      when_i_add_a_date_of_birth
-      when_i_click_on_continue
-      then_i_am_taken_to_add_ect_or_mentor_email_page
+    when_i_add_a_date_of_birth
+    when_i_click_on_continue
+    then_i_am_taken_to_add_ect_or_mentor_email_page
 
-      when_i_add_ect_or_mentor_email(email: @induction_coordinator_profile.user.email)
-      when_i_click_on_continue
-      then_i_am_taken_to_you_cant_add_yourself_as_ect_page
+    when_i_add_ect_or_mentor_email(email: @induction_coordinator_profile.user.email)
+    when_i_click_on_continue
+    then_i_am_taken_to_you_cant_add_yourself_as_ect_page
 
-      when_i_choose_to_cancel
-      and_i_click_on_continue
-      then_i_am_taken_to_manage_your_training_page
-    end
+    when_i_choose_to_cancel
+    and_i_click_on_continue
+    then_i_am_taken_to_manage_your_training_page
   end
 
   scenario "SIT chooses to add themself as Mentor instead" do
-    inside_auto_assignment_window do
-      when_i_navigate_to_ect_dashboard
-      when_i_click_to_add_a_new_ect
-      then_i_should_be_on_the_who_to_add_page
+    when_i_navigate_to_ect_dashboard
+    when_i_click_to_add_a_new_ect
+    then_i_should_be_on_the_who_to_add_page
 
-      when_i_select_to_add_a "ECT"
-      when_i_click_on_continue
+    when_i_select_to_add_a "ECT"
+    when_i_click_on_continue
 
-      then_i_am_taken_to_the_what_we_need_from_you_page
+    then_i_am_taken_to_the_what_we_need_from_you_page
 
-      when_i_click_on_continue
-      then_i_am_taken_to_add_ect_name_page
+    when_i_click_on_continue
+    then_i_am_taken_to_add_ect_name_page
 
-      when_i_add_ect_name
-      when_i_click_on_continue
-      then_i_am_taken_to_add_teachers_trn_page
+    when_i_add_ect_name
+    when_i_click_on_continue
+    then_i_am_taken_to_add_teachers_trn_page
 
-      when_i_add_the_trn
-      when_i_click_on_continue
-      then_i_am_taken_to_add_date_of_birth_page
+    when_i_add_the_trn
+    when_i_click_on_continue
+    then_i_am_taken_to_add_date_of_birth_page
 
-      when_i_add_a_date_of_birth
-      when_i_click_on_continue
-      then_i_am_taken_to_add_ect_or_mentor_email_page
+    when_i_add_a_date_of_birth
+    when_i_click_on_continue
+    then_i_am_taken_to_add_ect_or_mentor_email_page
 
-      when_i_add_ect_or_mentor_email(email: @induction_coordinator_profile.user.email)
-      when_i_click_on_continue
-      then_i_am_taken_to_you_cant_add_yourself_as_ect_page
+    when_i_add_ect_or_mentor_email(email: @induction_coordinator_profile.user.email)
+    when_i_click_on_continue
+    then_i_am_taken_to_you_cant_add_yourself_as_ect_page
 
-      when_i_choose_to_add_myself_as_mentor
-      and_i_click_on_continue
-      then_i_am_taken_to_add_yourself_as_mentor_confirmation_page
+    when_i_choose_to_add_myself_as_mentor
+    and_i_click_on_continue
+    then_i_am_taken_to_add_yourself_as_mentor_confirmation_page
 
-      when_i_click_on_confirm
-      then_i_am_taken_to_choose_mentor_partnership_page
+    when_i_click_on_confirm
+    then_i_am_taken_to_choose_mentor_partnership_page
 
-      when_i_choose_current_providers
-      and_i_click_on_continue
-      then_i_am_taken_to_check_answers_page
+    when_i_choose_current_providers
+    and_i_click_on_continue
+    then_i_am_taken_to_check_answers_page
 
-      when_i_click_confirm_and_add
-      then_i_am_taken_to_yourself_as_mentor_confirmation_page
+    when_i_click_confirm_and_add
+    then_i_am_taken_to_yourself_as_mentor_confirmation_page
 
-      when_i_click_on_view_mentors
-      then_i_see_the_participant_name
-    end
+    when_i_click_on_view_mentors
+    then_i_see_the_participant_name
   end
 end
