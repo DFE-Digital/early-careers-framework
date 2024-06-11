8d067deedf39cff8c6cb1dd31faac4574bd5c2b4
Lorenzo Tello <ltello8a@gmail.com>
2024-06-11 23:53:37 +0100

    re-validated participants cannot be placed in frozen cohorts

---
+++ app/services/induction/amend_cohort_after_eligibility_checks.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+# Change the cohort of an ECF participant whose eligibility has been recalculated.
+# If the participant has become eligible and is currently in a payments-frozen cohort,
+# transfer them to the currently active registration cohort.
+module Induction
+  class AmendCohortAfterEligibilityChecks
+    include ActiveModel::Model
+
+    attr_accessor :participant_profile
+
+    def call
+      amend_participant_cohort if eligible_participant_in_frozen_cohort?
+    end
+
+  private
+
+    def amend_participant_cohort
+      Induction::AmendParticipantCohort.new(participant_profile:,
+                                            source_cohort_start_year: participant_profile.schedule&.cohort&.start_year,
+                                            target_cohort_start_year: Cohort.active_registration_cohort.start_year)
+                                       .save
+    end
+
+    def eligible_participant_in_frozen_cohort?
+      return false unless participant_profile.eligible?
+      return false if participant_profile.ecf_participant_eligibility&.reason != "none"
+
+      participant_profile.schedule&.cohort&.payments_frozen?
+    end
+  end
+end
