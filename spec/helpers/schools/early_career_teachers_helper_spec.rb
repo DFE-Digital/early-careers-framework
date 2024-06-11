ddd18f0916495cb1c3c72edb89bc890882469af2
Lorenzo Tello <ltello8a@gmail.com>
2024-06-11 15:24:37 +0100

    add missing specs

--- spec/helpers/schools/early_career_teachers_helper_spec.rb
+++ spec/helpers/schools/early_career_teachers_helper_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Schools::EarlyCareerTeachersHelper, type: :helper do
   describe "#completed_participants" do
     let(:participant_1) { double("ParticipantProfile", induction_completion_date: 3.days.ago) }
     let(:participant_2) { double("ParticipantProfile", induction_completion_date: nil) }
-    let(:participant_3) { double("ParticipantProfile", induction_completion_date: 1.days.ago) }
+    let(:participant_3) { double("ParticipantProfile", induction_completion_date: 1.day.ago) }
 
     context "when all participants are completed" do
       let(:participants) { [participant_1, participant_3] }
