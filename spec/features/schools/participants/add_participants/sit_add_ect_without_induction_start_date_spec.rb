b1f59799624a10cf2d839706bb5731f641b65695
Lorenzo Tello <ltello8a@gmail.com>
2024-06-12 23:00:53 +0100

    participants registered inside the registration window for next year are placed in the next cohort

---
+++ spec/features/schools/participants/add_participants/sit_add_ect_without_induction_start_date_spec.rb
@@ -0,0 +1,140 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+require_relative "../../training_dashboard/manage_training_steps"
+
+RSpec.describe "SIT adds ECT participant with no induction start date", js: true do
+  include ManageTrainingSteps
+
+  before do
+    inside_registration_window do
+      set_participant_data
+      given_there_is_a_school_that_has_chosen_fip_for_current_and_next_cohorts_and_partnered
+      and_i_am_signed_in_as_an_induction_coordinator
+      and_i_click_on(Cohort.current.description)
+      then_i_am_taken_to_fip_induction_dashboard
+
+      set_dqt_validation_result
+    end
+  end
+
+  scenario "when inside the registration period" do
+    inside_registration_window do
+      when_i_navigate_to_ect_dashboard
+      when_i_click_to_add_a_new_ect
+      then_i_should_be_on_the_who_to_add_page
+
+      when_i_select_to_add_a "ECT"
+      when_i_click_on_continue
+
+      then_i_am_taken_to_the_what_we_need_from_you_page
+
+      when_i_click_on_continue
+      then_i_am_taken_to_add_ect_name_page
+
+      when_i_add_ect_name
+      when_i_click_on_continue
+      then_i_am_taken_to_add_teachers_trn_page
+
+      when_i_add_the_trn
+      when_i_click_on_continue
+      then_i_am_taken_to_add_date_of_birth_page
+
+      when_i_add_a_date_of_birth
+      when_i_click_on_continue
+      then_i_am_taken_to_add_ect_or_mentor_email_page
+
+      when_i_add_ect_or_mentor_email(email: @participant_data[:email])
+      when_i_click_on_continue
+
+      when_i_click_confirm_and_add
+      then_i_am_taken_to_the_confirmation_page
+      and_the_participant_has_been_added_to_the_next_cohort
+    end
+  end
+
+  scenario "when outside the registration period" do
+    outside_auto_assignment_window do
+      when_i_navigate_to_ect_dashboard
+      when_i_click_to_add_a_new_ect
+      then_i_should_be_on_the_who_to_add_page
+
+      when_i_select_to_add_a "ECT"
+      when_i_click_on_continue
+
+      then_i_am_taken_to_the_what_we_need_from_you_page
+
+      when_i_click_on_continue
+      then_i_am_taken_to_add_ect_name_page
+
+      when_i_add_ect_name
+      when_i_click_on_continue
+      then_i_am_taken_to_add_teachers_trn_page
+
+      when_i_add_the_trn
+      when_i_click_on_continue
+      then_i_am_taken_to_add_date_of_birth_page
+
+      when_i_add_a_date_of_birth
+      when_i_click_on_continue
+      then_i_am_taken_to_add_ect_or_mentor_email_page
+
+      when_i_add_ect_or_mentor_email(email: @participant_data[:email])
+      when_i_click_on_continue
+      when_i_choose_autumn_term_this_cohort
+      when_i_click_on_continue
+
+      then_i_see_i_cannot_add_participant_yet
+    end
+  end
+
+  scenario "when training setup is needed" do
+    inside_registration_window do
+      @school_cohort_next.update!(induction_programme_choice: :design_our_own)
+
+      when_i_navigate_to_ect_dashboard
+      when_i_click_to_add_a_new_ect
+      then_i_should_be_on_the_who_to_add_page
+
+      when_i_select_to_add_a "ECT"
+      when_i_click_on_continue
+
+      then_i_am_taken_to_the_what_we_need_from_you_page
+
+      when_i_click_on_continue
+      then_i_am_taken_to_add_ect_name_page
+
+      when_i_add_ect_name
+      when_i_click_on_continue
+      then_i_am_taken_to_add_teachers_trn_page
+
+      when_i_add_the_trn
+      when_i_click_on_continue
+      then_i_am_taken_to_add_date_of_birth_page
+
+      when_i_add_a_date_of_birth
+      when_i_click_on_continue
+
+      then_i_see_that_training_setup_is_needed
+    end
+  end
+
+  def then_i_am_taken_to_the_confirmation_page
+    expect(page).to have_content("#{@participant_data[:full_name]} has been added as an ECT")
+  end
+
+  def and_the_participant_has_been_added_to_the_next_cohort
+    expect(@school.ecf_participant_profiles.last.school_cohort).to eq(@school_cohort_next)
+  end
+
+  def set_participant_data
+    @participant_data = {
+      trn: "1234567",
+      full_name: "Sally Teacher",
+      date_of_birth: Date.new(1998, 3, 22),
+      email: "sally@school.com",
+      nino: "AB123456A",
+      start_date: nil,
+    }
+  end
+end
--- spec/features/schools/participants/add_participants/sit_add_mentor_spec.rb
+++ spec/features/schools/participants/add_participants/sit_add_mentor_spec.rb
@@ -68,7 +68,7 @@ RSpec.describe "SIT adding mentor", js: true do
   end
 
   scenario "Induction tutor adds themself as mentor using their own email address when email and TRN belongs to an existing mentor" do
-    outside_auto_assignment_window do
+    inside_auto_assignment_window do
       given_there_is_a_sit_and_mentor_in_difference_schools
 
       when_i_sign_in_as_sit
@@ -170,6 +170,47 @@ RSpec.describe "SIT adding mentor", js: true do
 
       when_i_click_on "View your mentors"
       then_i_see_the_mentor_name
+    end
+  end
+
+  scenario "Induction tutor adds a new mentor outside auto assignment window" do
+    outside_auto_assignment_window do
+      given_there_is_a_sit
+
+      when_i_sign_in_as_sit
+      and_i_click_on(Cohort.current.description)
+      then_i_am_taken_to_fip_induction_dashboard
+
+      when_i_navigate_to_mentors_dashboard
+      and_i_click_to_add_a_new_mentor
+      then_i_should_be_on_the_who_to_add_page
+
+      when_i_select_to_add_a "Mentor"
+      and_i_click_on_continue
+      then_i_am_taken_to_the_what_we_need_from_mentor_page
+
+      when_i_click_on_continue
+      then_i_am_taken_to_add_mentor_full_name_page
+
+      when_i_add_mentor_name
+      and_i_click_on_continue
+      then_i_am_taken_to_add_teachers_trn_page
+
+      when_i_add_the_trn
+      and_i_click_on_continue
+      then_i_am_taken_to_add_date_of_birth_page
+
+      when_i_add_a_date_of_birth
+      and_i_click_on_continue
+      then_i_am_taken_to_add_ect_or_mentor_email_page
+
+      when_i_add_ect_or_mentor_email
+      and_i_click_on_continue
+      then_i_am_taken_to_mentor_start_training_page
+
+      when_i_choose_autumn_term_this_cohort
+      and_i_click_on_continue
+      then_i_see_i_cannot_add_participant_yet
     end
   end
 end
