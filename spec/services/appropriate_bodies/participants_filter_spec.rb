f1df38b8311b8ae6bb0c5cda7849ba02fe44962a
Abeer Salameh <abeer.salameh@gmail.com>
2024-06-28 08:12:28 +0100

    Change appropriate body view to show active registration cohort
    Currently we show the current cohort which is calculated from the
    `academic_year_start_date` which is in September. We would like to
    change that to be from the registration start date which is in June
    instead.

--- spec/services/appropriate_bodies/participants_filter_spec.rb
+++ spec/services/appropriate_bodies/participants_filter_spec.rb
@@ -3,16 +3,26 @@
 require "rails_helper"
 
 RSpec.describe AppropriateBodies::ParticipantsFilter do
+  let(:cohort) { Cohort.active_registration_cohort }
   let(:appropriate_body_user) { create(:user, :appropriate_body) }
   let(:appropriate_body) { appropriate_body_user.appropriate_bodies.first }
 
-  let(:participant_profile1) { create(:ect_participant_profile) }
+  let(:participant_profile1) { create(:ect_participant_profile, cohort:) }
+  let(:school_cohort1) { create(:school_cohort, cohort:) }
   let(:induction_programme1) { create(:induction_programme, :fip) }
-  let!(:induction_record1) { create(:induction_record, participant_profile: participant_profile1, appropriate_body:, induction_programme: induction_programme1) }
+  let!(:induction_record1) { create(:induction_record, participant_profile: participant_profile1, appropriate_body:, induction_programme: induction_programme1, school_cohort: school_cohort1) }
 
   let(:participant_profile2) { create(:ect_participant_profile, training_status: "deferred") }
+  let(:school_cohort2) { create(:school_cohort, cohort:) }
   let(:induction_programme2) { create(:induction_programme, :cip) }
-  let!(:induction_record2) { create(:induction_record, participant_profile: participant_profile2, appropriate_body:, induction_programme: induction_programme2, training_status: "deferred") }
+  let!(:induction_record2) { create(:induction_record, participant_profile: participant_profile2, appropriate_body:, induction_programme: induction_programme2, training_status: "deferred", school_cohort: school_cohort2) }
+
+  # participant to be ignored
+  let(:older_cohort) { Cohort.find_by(start_year: 2021) }
+  let(:another_participant_profile) { create(:ect_participant_profile, cohort: older_cohort) }
+  let(:another_school_cohort) { create(:school_cohort, cohort: older_cohort) }
+  let(:another_induction_programme) { create(:induction_programme, :fip) }
+  let!(:another_induction_record) { create(:induction_record, participant_profile: another_participant_profile, appropriate_body:, induction_programme: another_induction_programme, school_cohort: another_school_cohort) }
 
   let(:collection) { AppropriateBodies::InductionRecordsQuery.new(appropriate_body:).induction_records }
   let(:params) { {} }
