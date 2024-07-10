e181f1fb158859d1740e4f8e14ad1913abe62878
Steve Laing <steve.laing@gmail.com>
2024-07-10 14:35:26 +0100

    Remove a couple of redundant methods

--- app/services/form_data/change_lead_provider_store.rb
+++ app/services/form_data/change_lead_provider_store.rb
@@ -2,14 +2,6 @@
 
 module FormData
   class ChangeLeadProviderStore < DataStore
-    def school_id
-      get(:school_id)
-    end
-
-    def participant_id
-      get(:participant_id)
-    end
-
     def store_attrs(step, attrs)
       set(step, attrs)
     end
