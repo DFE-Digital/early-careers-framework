16ea190fe49ba20d4f51c5de439f326292342416
Lorenzo Tello <ltello8a@gmail.com>
2024-10-16 18:57:37 +0100

    bug fix: do not archive participants in a closing cohort with archivable and non-archivable declarations

--- app/models/participant_profile/ect.rb
+++ app/models/participant_profile/ect.rb
@@ -11,9 +11,37 @@ class ParticipantProfile::ECT < ParticipantProfile::ECF
   }
 
   def self.archivable_from_frozen_cohort(restrict_to_participant_ids: [])
-    super(restrict_to_participant_ids:)
-      .where(induction_completion_date: nil)
-      .where("induction_start_date IS NULL OR induction_start_date < make_date(cohorts.start_year, 9, 1)")
+    archivable_states = %i[ineligible voided submitted].freeze
+
+    # ECTs that have no FIP induction records
+    not_fip = InductionRecord.joins(:induction_programme, participant_profile: { schedule: :cohort })
+                             .where.not(cohorts: { payments_frozen_at: nil })
+                             .where(participant_profiles: { induction_completion_date: nil })
+                             .where(participant_profiles: { type: "ParticipantProfile::ECT" })
+                             .where("participant_profiles.induction_start_date IS NULL OR participant_profiles.induction_start_date < make_date(cohorts.start_year, 9, 1)")
+                             .where.not(induction_programme: { training_programme: :full_induction_programme })
+    not_fip = not_fip.where(participant_profile_id: restrict_to_participant_ids) if restrict_to_participant_ids.any?
+    not_fip_ids = not_fip.pluck(:participant_profile_id).uniq
+
+    # ECTs that have at least one non-archivable declaration
+    with_unarchivable_declaration = joins(:participant_declarations, schedule: :cohort)
+                                      .where.not(cohorts: { payments_frozen_at: nil })
+                                      .where(induction_completion_date: nil)
+                                      .where("induction_start_date IS NULL OR induction_start_date < make_date(cohorts.start_year, 9, 1)")
+                                      .where.not(id: not_fip_ids)
+                                      .where.not(participant_declarations: { state: archivable_states })
+                                      .distinct
+    with_unarchivable_declaration = with_unarchivable_declaration.where(id: restrict_to_participant_ids) if restrict_to_participant_ids.any?
+    with_unarchivable_declaration_ids = with_unarchivable_declaration.pluck(:id)
+
+    query = joins(schedule: :cohort)
+              .where.not(cohorts: { payments_frozen_at: nil })
+              .where(induction_completion_date: nil)
+              .where("induction_start_date IS NULL OR induction_start_date < make_date(cohorts.start_year, 9, 1)")
+              .where.not(id: not_fip_ids + with_unarchivable_declaration_ids)
+    query = query.where(id: restrict_to_participant_ids) if restrict_to_participant_ids.any?
+
+    query
   end
 
   def completed_training?
