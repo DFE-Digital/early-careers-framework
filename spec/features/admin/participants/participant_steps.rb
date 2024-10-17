e29aa91ede5888c36fdca895e9f79aa4b88014dd
Leandro Alemão <leandro.costantini@gmail.com>
2024-10-17 17:07:44 +0100

    [CPDLP-3589] Fix broken tests

--- spec/features/admin/participants/participant_steps.rb
+++ spec/features/admin/participants/participant_steps.rb
@@ -29,7 +29,7 @@ module ParticipantSteps
     and_i_am_signed_in_as_an_admin
     and_i_have_added_an_ect
     and_i_have_added_a_mentor
-
+    and_i_have_added_a_npq
     when_i_visit_admin_participants_dashboard
     then_i_should_see_a_list_of_participants
   end
