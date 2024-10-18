13e1648f4a52e967d5573e7ba798f0f64035ccdc
Ross Oliver <ross.oliver36@gmail.com>
2024-10-18 11:17:47 +0100

    Generate known tokens for parity check on migration DB restore
    We hit an issue when running the original token provider whereby the keys would
    generate but not work in the migration environment. This was due to the keys
    being generated using the NPQ reg migration environment secret_key_base but
    verified in ECF using a different secret_key_base.
    
    To get around this, we are going to use known tokens; each migration keyvault
    has the same set of tokens indexed by the lead provider `ecf_id`. After
    restoring the migration database we will create the known-tokens for the parity
    check service to use.

---
+++ app/services/migration/parity_check/token_provider.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module Migration
+  class ParityCheck::TokenProvider
+    class UnsupportedEnvironmentError < RuntimeError; end
+
+    def generate!
+      raise UnsupportedEnvironmentError, "The parity check functionality is disabled for this environment" unless enabled?
+
+      known_tokens_by_lead_provider_ecf_id.each do |id, token|
+        cpd_lead_provider = NPQLeadProvider.find_by!(id:).cpd_lead_provider
+        create_with_known_token!(token:, cpd_lead_provider:) if cpd_lead_provider
+      end
+    end
+
+  private
+
+    def known_tokens_by_lead_provider_ecf_id
+      JSON.parse(ENV["PARITY-CHECK-KEYS"].to_s)
+    rescue JSON::ParserError
+      {}
+    end
+
+    def create_with_known_token!(token:, cpd_lead_provider:)
+      LeadProviderApiToken.create_with_known_token!(token, cpd_lead_provider:)
+    end
+
+    def enabled?
+      Rails.env.migration?
+    end
+  end
+end
