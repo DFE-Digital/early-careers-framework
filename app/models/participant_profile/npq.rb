0ae3a65a237d5572ce3b0de658648b6acc6ffa6d
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2024-12-18 17:01:17 +0000

    [CPDLP-3814] remove NPQCourse model and table

--- app/models/participant_profile/npq.rb
+++ app/models/participant_profile/npq.rb
@@ -41,7 +41,6 @@ class ParticipantProfile::NPQ < ParticipantProfile
   # self.ignored_columns = %i[mentor_profile_id school_cohort_id]
   belongs_to :cohort, optional: true
   belongs_to :school, optional: true
-  belongs_to :npq_course, optional: true
 
   has_many :participant_declarations, class_name: "ParticipantDeclaration::NPQ", foreign_key: :participant_profile_id
 
