16dcfa460daaf95c6d27c8b593a104867dd71b36
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2024-12-18 11:00:02 +0000

    [CPDLP-3815] comment for clarification

--- spec/models/finance/schedule/ecf_spec.rb
+++ spec/models/finance/schedule/ecf_spec.rb
@@ -32,6 +32,7 @@ RSpec.describe Finance::Schedule::ECF, type: :model do
     end
   end
 
+  # 'db/data/schedules/schedules.csv' has data for 2021 to 2024
   Cohort.where(start_year: 2021..2024).find_each do |cohort|
     context "for #{cohort.start_year} cohort" do
       let(:cohort) { Cohort.find_by(start_year: cohort.start_year) }
