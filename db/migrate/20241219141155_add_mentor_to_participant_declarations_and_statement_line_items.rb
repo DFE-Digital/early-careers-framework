6d7a2fcca7da9284866d1a46bfd5d3e214eb52aa
Ross Oliver <ross.oliver36@gmail.com>
2024-12-19 14:34:13 +0000

    Populate `mentor` fields and update queries

--- db/migrate/20241219141155_add_mentor_to_participant_declarations_and_statement_line_items.rb
+++ db/migrate/20241219141155_add_mentor_to_participant_declarations_and_statement_line_items.rb
@@ -5,11 +5,15 @@ class AddMentorToParticipantDeclarationsAndStatementLineItems < ActiveRecord::Mi
     add_column :participant_declarations, :mentor, :boolean, default: false, null: false
     add_column :statement_line_items, :mentor, :boolean, default: false, null: false
 
-    ParticipantDeclaration::ECF.includes(:participant_profile, :statement_line_items).find_each do |declaration|
-      mentor = declaration.participant_profile.mentor?
-      declaration.update!(mentor:)
-      declaration.statement_line_items.update_all(mentor:)
-    end
+    mentor_declarations = ParticipantDeclaration::ECF
+      .includes(:participant_profile, :statement_line_items)
+      .where(participant_profiles: { type: "ParticipantProfile::Mentor" })
+
+    mentor_declarations.update_all(mentor: true)
+
+    mentor_statement_line_items = Finance::StatementLineItem.where(participant_declaration_id: mentor_declarations.pluck(:id))
+    
+    mentor_statement_line_items.update_all(mentor: true)
   end
 
   def down
