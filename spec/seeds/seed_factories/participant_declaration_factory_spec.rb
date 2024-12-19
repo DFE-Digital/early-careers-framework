7d81b8334663c00c195efea6f85992d59b09f966
Ross Oliver <ross.oliver36@gmail.com>
2024-12-19 11:37:23 +0000

    Update queries to exclude `ParticipantDeclaration::Mentor`
    Update factories to create correct mentor/ect declarations. Update calculators
    and reports to only include ECT declarations (as mentor declarations will be on
    a separate calculator).

--- spec/seeds/seed_factories/participant_declaration_factory_spec.rb
+++ spec/seeds/seed_factories/participant_declaration_factory_spec.rb
@@ -2,10 +2,15 @@
 
 require_relative "./shared_factory_examples"
 
-RSpec.describe("seed_ecf_participant_declaration, seed_npq_participant_declaration") do
+RSpec.describe("seed_ect_participant_declaration, seed_npq_participant_declaration") do
   it_behaves_like("a seed factory") do
-    let(:factory_name) { :seed_ecf_participant_declaration }
-    let(:factory_class) { ParticipantDeclaration::ECF }
+    let(:factory_name) { :seed_ect_participant_declaration }
+    let(:factory_class) { ParticipantDeclaration::ECT }
+  end
+
+  it_behaves_like("a seed factory") do
+    let(:factory_name) { :seed_mentor_participant_declaration }
+    let(:factory_class) { ParticipantDeclaration::Mentor }
   end
 
   it_behaves_like("a seed factory") do
