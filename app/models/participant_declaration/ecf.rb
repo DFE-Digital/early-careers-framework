a434ded65d2e6a3143f7af56a7cf015ee46b2013
Ross Oliver <ross.oliver36@gmail.com>
2025-01-10 13:13:14 +0000

    Add and maintain `temp_type` on `ParticipantDeclaration::ECF`
    We are splitting `ECF` declarations into `ECT` and `Mentor` declarations. As
    calculating the declaration type takes a few minutes, in order to do it without
    downtime or data errors we are going to add a `temp_type` column that we can
    populate and maintain ahead of the switch.
    
    Once shipped, we can then switch the data of `new_type` to `type` quickly and
    start referencing the new column.

--- app/models/participant_declaration/ecf.rb
+++ app/models/participant_declaration/ecf.rb
@@ -3,6 +3,8 @@
 class ParticipantDeclaration::ECF < ParticipantDeclaration
   has_many :statements, class_name: "Finance::Statement::ECF", through: :statement_line_items
 
+  before_save :set_temp_type
+
   def ecf?
     true
   end
@@ -12,5 +14,12 @@ class ParticipantDeclaration::ECF < ParticipantDeclaration
       declaration_type == "started" &&
       %w[paid awaiting_clawback clawed_back].include?(state) &&
       (sparsity_uplift || pupil_premium_uplift)
+  end
+
+  def set_temp_type
+    return unless participant_profile
+    return unless temp_type.nil?
+
+    self.temp_type = participant_profile.type.sub("ParticipantProfile", "ParticipantDeclaration")
   end
 end
