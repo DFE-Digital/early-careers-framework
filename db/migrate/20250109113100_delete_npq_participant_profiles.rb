6a0eb39b4137c01446b898c7155375e9206a245c
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2025-01-14 12:10:07 +0000

    [CPDLP-3817] batch migration

--- db/migrate/20250109113100_delete_npq_participant_profiles.rb
+++ db/migrate/20250109113100_delete_npq_participant_profiles.rb
@@ -4,9 +4,9 @@ class DeleteNPQParticipantProfiles < ActiveRecord::Migration[7.1]
   def up
     npq_type = "ParticipantProfile::NPQ"
 
-    ParticipantProfileState.includes(:participant_profile).where(participant_profile: { type: npq_type }).delete_all
-    ParticipantProfileSchedule.includes(:participant_profile).where(participant_profile: { type: npq_type }).delete_all
-    ProfileValidationDecision.includes(:participant_profile).where(participant_profile: { type: npq_type }).delete_all
+    ParticipantProfileState.includes(:participant_profile).where(participant_profile: { type: npq_type }).in_batches(of: 10_000) { |batch| batch.delete_all }
+    ParticipantProfileSchedule.includes(:participant_profile).where(participant_profile: { type: npq_type }).in_batches(of: 10_000) { |batch| batch.delete_all }
+    ProfileValidationDecision.includes(:participant_profile).where(participant_profile: { type: npq_type }).in_batches(of: 10_000) { |batch| batch.delete_all }
     ParticipantProfile.where(type: npq_type).in_batches(of: 10_000) { |batch| batch.delete_all }
   end
 end
