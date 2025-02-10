56f86723d33d2a4fdd91729c48e4d6db4bfd6492
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2025-02-10 18:27:39 +0000

    [CPDLP-4009] Bringing everything together into the statements show page

--- app/controllers/finance/ecf/statements_controller.rb
+++ app/controllers/finance/ecf/statements_controller.rb
@@ -6,7 +6,14 @@ module Finance
       def show
         @ecf_lead_provider = lead_provider_scope.find(params[:payment_breakdown_id])
         @statement = @ecf_lead_provider.statements.find(params[:id])
-        @calculator = StatementCalculator.new(statement: @statement)
+
+        if @statement.cohort.mentor_funding?
+          @mentor_calculator = Mentor::StatementCalculator.new(statement: @statement)
+          @ect_calculator = ECT::StatementCalculator.new(statement: @statement)
+        else
+          @calculator = StatementCalculator.new(statement: @statement)
+        end
+
         set_important_message(title: t("finance.statements.payment_authorisations.banner.title"), content: t("finance.statements.payment_authorisations.banner.content", statement_marked_as_paid_at: @statement.marked_as_paid_at.strftime("%-I:%M%P on %-e %b %Y"))) if authorising_for_payment_banner_visible?(@statement)
       end
 
