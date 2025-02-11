35a8cba1338b73f912156654ce038ca2d5c3c2fa
Leandro Alemão <leandro.costantini@gmail.com>
2025-02-11 11:03:07 +0000

    [CPDLP-3937] Improve test coverage on mentors `statement_calculator` specs

--- spec/services/finance/ecf/mentor/statement_calculator_spec.rb
+++ spec/services/finance/ecf/mentor/statement_calculator_spec.rb
@@ -42,6 +42,33 @@ RSpec.describe Finance::ECF::Mentor::StatementCalculator, mid_cohort: true do
     end
   end
 
+  describe "#started_fee_per_declaration" do
+    let(:output_breakdown) do
+      [
+        {
+          started_additions: 4,
+          started_subtractions: 0,
+        },
+        {
+          completed_additions: 2,
+          completed_subtractions: 1,
+        },
+      ]
+    end
+
+    let(:output_calculator) { instance_double("Finance::ECF::Mentor::OutputCalculator") }
+
+    before do
+      allow(Finance::ECF::Mentor::OutputCalculator).to receive(:new).and_return(output_calculator)
+      allow(output_calculator).to receive(:output_breakdown).and_return(output_breakdown)
+      allow(output_calculator).to receive(:fee_for_declaration).and_return(500)
+    end
+
+    it "returns correct value" do
+      expect(subject.started_fee_per_declaration).to eql(500)
+    end
+  end
+
   describe "#additions_for_started" do
     let(:output_breakdown) do
       [
@@ -160,6 +187,54 @@ RSpec.describe Finance::ECF::Mentor::StatementCalculator, mid_cohort: true do
     it "returns only voided mentor declarations" do
       expect(subject.voided_declarations.size).to eql(5)
       expect(subject.voided_declarations.pluck(:state, :type).uniq.flatten).to eql(["voided", "ParticipantDeclaration::Mentor"])
+    end
+  end
+
+  describe "#voided_count" do
+    before do
+      declarations = create_list(
+        :mentor_participant_declaration, 5,
+        state: :voided
+      )
+
+      declarations.each do |dec|
+        Finance::StatementLineItem.create!(
+          statement:,
+          participant_declaration: dec,
+          state: dec.state,
+        )
+      end
+
+      declarations = create_list(
+        :ect_participant_declaration, 5,
+        state: :voided
+      )
+
+      declarations.each do |dec|
+        Finance::StatementLineItem.create!(
+          statement:,
+          participant_declaration: dec,
+          state: dec.state,
+        )
+      end
+    end
+
+    it "returns the # of voided mentor declarations" do
+      expect(subject.voided_count).to eql(5)
+    end
+  end
+
+  describe "#fee_for_declaration" do
+    let(:output_calculator) { instance_double("Finance::ECF::Mentor::OutputCalculator", output_breakdown: []) }
+
+    before do
+      allow(Finance::ECF::Mentor::OutputCalculator).to receive(:new).with(statement:).and_return(output_calculator)
+    end
+
+    it "calls OutputCalculator#fee_for_declaration with correct params" do
+      expect(output_calculator).to receive(:fee_for_declaration).with(type: "started")
+
+      subject.fee_for_declaration(type: "started")
     end
   end
 end
