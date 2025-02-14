6e2eed38e8a9c73d89461bdfa3a71aa2b389d90c
Ross Oliver <ross.oliver36@gmail.com>
2025-02-14 08:33:57 +0000

    Expire session cookie after 2 weeks
    To be inline with our cookie consent form we want to expire the session cookie after 2 weeks.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,6 @@
 # frozen_string_literal: true
 
-Rails.application.config.session_store :active_record_store, key: "_ecf_session"
+# We time travel a lot in our feature tests which results
+# in failures if the session expires.
+expire_after = Rails.env.test? ? nil : 2.weeks
+Rails.application.config.session_store :active_record_store, key: "_ecf_session", expire_after:
