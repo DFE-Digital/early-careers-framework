17e57c832d38cbea064827b9d5b4b4aeb4074114
Ross Oliver <ross.oliver36@gmail.com>
2025-02-18 13:09:57 +0000

    Add report-only Content Security Policy
    We want to re-introduce the Content Security Policy, however we want it to be
    configured to report only initially so that we can monitor for any violations
    before enforcing it.
    
    Add CSP headers and re-implement security headers without the `secure_headers`
    gem.
    
    Implement separate, locked down CSP for API endpoints.
    
    Update inline scripts and styles to use a nonce.

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -6,22 +6,38 @@
 # See the Securing Rails Applications Guide for more information:
 # https://guides.rubyonrails.org/security.html#content-security-policy-header
 
-# Rails.application.configure do
-#   config.content_security_policy do |policy|
-#     policy.default_src :self, :https
-#     policy.font_src    :self, :https, :data
-#     policy.img_src     :self, :https, :data
-#     policy.object_src  :none
-#     policy.script_src  :self, :https
-#     policy.style_src   :self, :https
-#     # Specify URI for violation reports
-#     # policy.report_uri "/csp-violation-report-endpoint"
-#   end
-#
-#   # Generate session nonces for permitted importmap, inline scripts, and inline styles.
-#   config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
-#   config.content_security_policy_nonce_directives = %w(script-src style-src)
-#
-#   # Report violations without enforcing the policy.
-#   # config.content_security_policy_report_only = true
-# end
+Rails.application.configure do
+  # Default policy for the application; covers static pages and the
+  # admin/finance dashboards.
+  config.content_security_policy do |policy|
+    policy.default_src :self
+    policy.font_src    :self, :data
+    policy.img_src     :self, :data, :blob, "https://www.googletagmanager.com/td"
+    policy.object_src  :none
+    policy.script_src  :self, "https://www.googletagmanager.com/gtm.js", "https://www.googletagmanager.com/gtag/js", "https://static.zdassets.com/ekr/snippet.js"
+    policy.style_src   :self
+    policy.connect_src :self, "*.google-analytics.com"
+    policy.frame_src   :self, "https://www.googletagmanager.com/ns.html"
+
+    # I haven't figured out why yet, but a couple of the feature tests
+    # fail when this is set; its very strange.
+    policy.report_uri  "/csp_reports" unless Rails.env.test?
+  end
+
+  # Generate session nonces for permitted importmap, inline scripts, and inline styles.
+  config.content_security_policy_nonce_generator = ->(_) { SecureRandom.base64(16) }
+  config.content_security_policy_nonce_directives = %w[script-src style-src]
+
+  # Report violations without enforcing the policy.
+  config.content_security_policy_report_only = true
+
+  # Security-related HTTP headers.
+  config.action_dispatch.default_headers = {
+    "X-Frame-Options" => "DENY",
+    "X-XSS-Protection" => "0",
+    "X-Content-Type-Options" => "nosniff",
+    "X-Permitted-Cross-Domain-Policies" => "none",
+    "Referrer-Policy" => "origin-when-cross-origin, strict-origin-when-cross-origin",
+    "X-Download-Options" => "noopen",
+  }
+end
