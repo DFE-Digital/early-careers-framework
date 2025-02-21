106e69da2b9f2e59beb6398ee0634bab31166f14
Ross Oliver <ross.oliver36@gmail.com>
2025-02-21 14:02:31 +0000

    Exclude 2025 and later participants from delivery partners
    We want to exclude participants in the 2025 cohort from the delivery partners
    participants view. We also want to hide the 2025 and later cohorts from the
    academic year dropdown.
    
    This is indended to encourage lead providers to carry out their contractual
    duties and ensure there is no ownness on the DfE to share details of
    participants in 2025 and later cohorts.

--- spec/services/delivery_partners/participants_filter_spec.rb
+++ spec/services/delivery_partners/participants_filter_spec.rb
@@ -3,25 +3,33 @@
 require "rails_helper"
 
 RSpec.describe DeliveryPartners::ParticipantsFilter do
-  let(:school_cohort1) { create(:school_cohort, cohort: Cohort.current) }
+  let(:school_cohort1) { create(:school_cohort, cohort: Cohort.find_by!(start_year: 2023)) }
   let(:partnership1) { create(:partnership, lead_provider: create(:lead_provider, name: "LP1")) }
   let(:induction_programme1) { create(:induction_programme, partnership: partnership1) }
   let(:participant_profile1) { create(:mentor_participant_profile) }
   let!(:induction_record1) { create(:induction_record, participant_profile: participant_profile1, induction_programme: induction_programme1, school_cohort: school_cohort1) }
 
-  let(:school_cohort2) { create(:school_cohort, cohort: Cohort.next) }
+  let(:school_cohort2) { create(:school_cohort, cohort: Cohort.find_by!(start_year: 2024)) }
   let(:partnership2) { create(:partnership, lead_provider: create(:lead_provider, name: "LP2")) }
   let(:induction_programme2) { create(:induction_programme, partnership: partnership2) }
   let(:participant_profile2) { create(:ect_participant_profile, training_status: "deferred") }
   let!(:induction_record2) { create(:induction_record, participant_profile: participant_profile2, induction_programme: induction_programme2, school_cohort: school_cohort2, training_status: "deferred") }
 
-  let(:delivery_partner) { create(:delivery_partner, partnerships: [partnership1, partnership2]) }
+  let(:school_cohort3) { create(:school_cohort, cohort: Cohort.find_by!(start_year: 2025)) }
+  let(:partnership3) { create(:partnership, lead_provider: create(:lead_provider, name: "LP3")) }
+  let(:induction_programme3) { create(:induction_programme, partnership: partnership3) }
+  let(:participant_profile3) { create(:ect_participant_profile, training_status: "deferred") }
+  let!(:induction_record3) { create(:induction_record, participant_profile: participant_profile3, induction_programme: induction_programme3, school_cohort: school_cohort3) }
+
+  let(:delivery_partner) { create(:delivery_partner, partnerships: [partnership1, partnership2, partnership3]) }
 
   let(:collection) { DeliveryPartners::InductionRecordsQuery.new(delivery_partner:).induction_records }
   let(:params) { {} }
   let(:training_record_states) { DetermineTrainingRecordState.call(induction_records: collection) }
 
-  subject { described_class.new(collection:, params:, training_record_states:).scope.to_a }
+  let(:instance) { described_class.new(collection:, params:, training_record_states:) }
+
+  subject { instance.scope.to_a }
 
   context "when there are no filters provided" do
     it { is_expected.to contain_exactly(induction_record1, induction_record2) }
@@ -98,7 +106,13 @@ RSpec.describe DeliveryPartners::ParticipantsFilter do
       it { is_expected.to be_empty }
     end
 
-    context "with academic_year" do
+    context "with academic_year not present in the options" do
+      let(:params) { { academic_year: 2025 } }
+
+      it { is_expected.to be_empty }
+    end
+
+    context "with academic_year present in the options" do
       let(:params) { { academic_year: school_cohort1.start_year } }
 
       it { is_expected.to contain_exactly(induction_record1) }
@@ -123,5 +137,32 @@ RSpec.describe DeliveryPartners::ParticipantsFilter do
 
       it { is_expected.to contain_exactly(induction_record1) }
     end
+  end
+
+  describe "#role_options" do
+    subject(:options) { instance.role_options }
+
+    it { expect(options.map(&:id)).to eq(["", "ect", "mentor"]) }
+    it { expect(options.map(&:name)).to eq(["", "Early career teacher", "Mentor"]) }
+  end
+
+  describe "#academic_year_options" do
+    subject(:options) { instance.academic_year_options }
+
+    let(:expected_values) { [""] + Cohort.order(:start_year).pluck(:start_year).excluding(2025) }
+
+    before { expect(Cohort.where(start_year: 2025)).to be_exists }
+
+    it { expect(options.map(&:id)).to eq(expected_values) }
+    it { expect(options.map(&:name)).to eq(expected_values) }
+  end
+
+  describe "#status_options" do
+    subject(:options) { instance.status_options }
+
+    let(:expected_values) { I18n.t("status_tags.delivery_partner_participant_status").values.uniq }
+
+    it { expect(options.map(&:id)).to eq([""] + expected_values.pluck(:id)) }
+    it { expect(options.map(&:name)).to eq([""] + expected_values.pluck(:label)) }
   end
 end
