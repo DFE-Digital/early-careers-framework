18748c742897701a532a7fd86ffd3fa15a48158c
Leandro Alemão <leandro.costantini@gmail.com>
2025-03-03 14:20:04 +0000

    [CPDLP-4054] Enforce Content Security Policy in the app

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
   config.content_security_policy_nonce_directives = %w[script-src style-src]
 
   # Report violations without enforcing the policy.
-  config.content_security_policy_report_only = true
+  config.content_security_policy_report_only = false
 
   # Security-related HTTP headers.
   config.action_dispatch.default_headers = {
