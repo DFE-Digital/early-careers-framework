f4f187a307a362a189870fe23fd81ddbd8a1a625
Ross Oliver <ross.oliver36@gmail.com>
2025-04-04 11:09:48 +0100

    Add ProgrammeTypeMappings service
    As part of the work to reword programme type changes across the user-facing
    portions of the application we want to centralise the mappings to/from new/old
    values we display.
    
    Add the `ProgrammeTypeMappings` service to centralise the programme type
    changes.

---
+++ app/services/programme_type_mappings.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+class ProgrammeTypeMappings
+  class << self
+    def mappings_enabled?
+      FeatureFlag.active?(:programme_type_changes_2025)
+    end
+
+    def withdrawal_reason(reason:)
+      return reason unless mappings_enabled?
+
+      case reason
+      when "school-left-fip"
+        "switched-to-school-led"
+      else
+        reason
+      end
+    end
+
+    def training_programme(training_programme:)
+      return training_programme unless mappings_enabled?
+
+      case training_programme
+      when "full_induction_programme", "school_funded_fip"
+        "provider_led"
+      when "core_induction_programme", "design_our_own"
+        "school_led"
+      else
+        training_programme
+      end
+    end
+  end
+end
---
+++ spec/services/programme_type_mappings_spec.rb
@@ -0,0 +1,101 @@
+# frozen_string_literal: true
+
+RSpec.describe ProgrammeTypeMappings do
+  describe "mappings_enabled?" do
+    subject { described_class }
+
+    context "when the programme_type_changes_2025 feature flag is enabled" do
+      before { FeatureFlag.activate(:programme_type_changes_2025) }
+
+      it { is_expected.to be_mappings_enabled }
+    end
+
+    context "when the programme_type_changes_2025 feature flag is disabled" do
+      before { FeatureFlag.deactivate(:programme_type_changes_2025) }
+
+      it { is_expected.not_to be_mappings_enabled }
+    end
+  end
+
+  describe "mappings" do
+    before { allow(described_class).to receive(:mappings_enabled?) { mappings_enabled } }
+
+    describe ".withdrawal_reason" do
+      subject { described_class.withdrawal_reason(reason:) }
+
+      context "when mappings are enabled" do
+        let(:mappings_enabled) { true }
+
+        context "when reason is 'school-left-fip'" do
+          let(:reason) { "school-left-fip" }
+
+          it { is_expected.to eq("switched-to-school-led") }
+        end
+
+        context "when reason is not 'school-left-fip'" do
+          let(:reason) { "other" }
+
+          it { is_expected.to eq(reason) }
+        end
+      end
+
+      context "when mappings are disabled" do
+        let(:mappings_enabled) { false }
+
+        context "when reason is 'school-left-fip'" do
+          let(:reason) { "school-left-fip" }
+
+          it { is_expected.to eq(reason) }
+        end
+      end
+    end
+
+    describe ".training_programme" do
+      subject { described_class.training_programme(training_programme:) }
+
+      context "when mappings are enabled" do
+        let(:mappings_enabled) { true }
+
+        %i[full_induction_programme school_funded_fip].each do |induction_programme_type|
+          context "when training_programme is '#{induction_programme_type}'" do
+            let(:training_programme) { build(:induction_programme, induction_programme_type).training_programme }
+
+            it { is_expected.to eq("provider_led") }
+          end
+        end
+
+        %i[core_induction_programme design_our_own].each do |induction_programme_type|
+          context "when training_programme is '#{induction_programme_type}'" do
+            let(:training_programme) { build(:induction_programme, induction_programme_type).training_programme }
+
+            it { is_expected.to eq("school_led") }
+          end
+        end
+
+        context "when training_programme is anything else" do
+          let(:training_programme) { :anything_else }
+
+          it { is_expected.to eq(training_programme) }
+        end
+      end
+
+      context "when mappings are disabled" do
+        let(:mappings_enabled) { false }
+
+        %i[fip school_funded_fip cip design_our_own].each do |induction_programme_type|
+          context "when training_programme is '#{induction_programme_type}'" do
+            let(:training_programme) { build(:induction_programme, induction_programme_type).training_programme }
+
+            it { is_expected.to eq(training_programme) }
+          end
+        end
+
+        context "when training_programme is anything else" do
+          let(:training_programme) { :anything_else }
+
+          it { is_expected.to eq(training_programme) }
+        end
+      end
+    end
+  end
+end
