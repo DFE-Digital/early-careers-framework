929a8069630ef40fe1ed86ddf3fa4894e5467d78
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2025-04-04 17:45:58 +0100

    [CPDLP-4159] updated partnership guidance page to show school led when feature is on

--- spec/features/lead_providers/partnership_guidance_spec.rb
+++ spec/features/lead_providers/partnership_guidance_spec.rb
@@ -11,6 +11,26 @@ RSpec.feature "Lead Provider landing pages", type: :feature, js: true do
     and_the_page_is_accessible
   end
 
+  context "when 'programme_type_changes_2025' is disabled" do
+    before { FeatureFlag.deactivate("programme_type_changes_2025") }
+
+    scenario "Learning how to manage partnerships" do
+      given_i_am_on_the_lead_provider_landing_page
+      when_i_click_the_link_to_the_guidance_page
+      then_i_should_see_core_induction_programme
+    end
+  end
+
+  context "when 'programme_type_changes_2025' is enabled" do
+    before { FeatureFlag.activate("programme_type_changes_2025") }
+
+    scenario "Learning how to manage partnerships" do
+      given_i_am_on_the_lead_provider_landing_page
+      when_i_click_the_link_to_the_guidance_page
+      then_i_should_see_school_led
+    end
+  end
+
 private
 
   def when_i_click_the_link_to_the_guidance_page
@@ -25,5 +45,15 @@ private
   def then_i_am_on_the_lead_providers_partnership_guide_page
     @guidance_page = Pages::LeadProviderPartnershipGuidancePage.new
     expect(@guidance_page).to be_displayed
+  end
+
+  def then_i_should_see_core_induction_programme
+    @guidance_page = Pages::LeadProviderPartnershipGuidancePage.new
+    expect(@guidance_page).to have_content("They can only use the accredited materials (core induction programme).")
+  end
+
+  def then_i_should_see_school_led
+    @guidance_page = Pages::LeadProviderPartnershipGuidancePage.new
+    expect(@guidance_page).to have_content("They can only use the accredited materials (school led).")
   end
 end
