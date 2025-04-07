c98d7515b83bfc53a4ccce01dbf8553d5b3694cb
Leandro Alemão <leandro.costantini@gmail.com>
2025-04-07 09:39:34 +0100

    [CPDLP-4158] Add `training_programme_friendly_name` method

--- app/services/programme_type_mappings.rb
+++ app/services/programme_type_mappings.rb
@@ -29,5 +29,41 @@ class ProgrammeTypeMappings
         training_programme
       end
     end
+
+    def training_programme_friendly_name(name, short: false)
+      long_names = if mappings_enabled?
+                     {
+                       "full_induction_programme" => "Provider led",
+                       "core_induction_programme" => "School led",
+                       "design_our_own" => "School led",
+                       "school_funded_fip" => "Provider led",
+                     }
+                   else
+                     {
+                       "full_induction_programme" => "Full induction programme",
+                       "core_induction_programme" => "Core induction programme",
+                       "design_our_own" => "Design our own",
+                       "school_funded_fip" => "School funded full induction programme",
+                     }
+                   end
+
+      short_names = if mappings_enabled?
+                      {
+                        "full_induction_programme" => "Provider led",
+                        "core_induction_programme" => "School led",
+                        "design_our_own" => "School led",
+                        "school_funded_fip" => "Provider led",
+                      }
+                    else
+                      {
+                        "full_induction_programme" => "FIP",
+                        "core_induction_programme" => "CIP",
+                        "design_our_own" => "Design our own",
+                        "school_funded_fip" => "School funded FIP",
+                      }
+                    end
+
+      short ? short_names.fetch(name) : long_names.fetch(name)
+    end
   end
 end
