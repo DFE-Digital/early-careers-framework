fea751b5713e1de4082674ca88cfd3738c718ce0
paul senior <paul.senior@education.gov.uk>
2025-05-20 15:39:34 +0100

    chore(2234-ecf-plan-for-dr-test-may-2025): amended envt var file

--- global_config/staging.sh
+++ global_config/staging.sh
@@ -3,6 +3,6 @@ AZURE_SUBSCRIPTION=s189-teacher-services-cloud-test
 CONFIG=staging
 CONFIG_SHORT=st
 CONFIG_LONG=staging
-DEPLOY_ENV=${CONFIG}
+DEPLOY_ENV=staging
 ENV_TAG=Test
 NAMESPACE=cpd-development
