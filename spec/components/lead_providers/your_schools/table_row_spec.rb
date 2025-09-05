d754d8312e7950e9a58d2546c3685d036af3c229
Peter Yates <peter.yates@graphia.co.uk>
2025-09-05 12:25:27 +0100

    Inspect rendered_content instead of render_inline
    This is the way suggested by ViewComponent's guide - not entirely sure
    why but the result of render_inline didn't contain all the classes,
    perhaps it's just a trimmed down version to keep things light?
    
    https://viewcomponent.org/guide/testing.html

--- spec/components/lead_providers/your_schools/table_row_spec.rb
+++ spec/components/lead_providers/your_schools/table_row_spec.rb
@@ -7,17 +7,29 @@ RSpec.describe LeadProviders::YourSchools::TableRow, type: :component do
   let(:cohort) { partnership.cohort }
   let(:school_cohort) { create(:school_cohort, school:, cohort:) }
 
-  let(:component) { described_class.new partnership:, participant_counts: }
-  subject { render_inline(component) }
+  before { render_inline(described_class.new(partnership:, participant_counts:)) }
 
-  it { is_expected.to have_link school.name, href: lead_providers_partnership_path(partnership) }
-  it { is_expected.to have_content school.urn }
-  it { is_expected.to have_content partnership.delivery_partner.name }
+  it "renders a link to the school partnership" do
+    expect(rendered_content).to have_link school.name, href: lead_providers_partnership_path(partnership)
+  end
+
+  it "displays the school URN" do
+    expect(rendered_content).to have_content school.urn
+  end
+
+  it "displays the delivery partner name" do
+    expect(rendered_content).to have_content partnership.delivery_partner.name
+  end
 
   context "with ECTs for the partnership" do
     let(:participant_counts) { { partnership.id => { ect_count: 5, mentor_count: 2 } } }
 
-    it { is_expected.to have_css "td.govuk-table__cell--numeric", text: 5 }
-    it { is_expected.to have_css "td.govuk-table__cell--numeric", text: 2 }
+    it "displays the ECT count" do
+      expect(rendered_content).to have_css "td.govuk-table__cell--numeric", text: 5
+    end
+
+    it "displays the mentor count" do
+      expect(rendered_content).to have_css "td.govuk-table__cell--numeric", text: 2
+    end
   end
 end
