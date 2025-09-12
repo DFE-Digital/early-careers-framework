b9c786ddbfee0105d4732e2c2a8650b4b42a2fd6
Tony Headford <tony@binarycircus.com>
2025-09-12 09:17:02 +0100

    Make var names clearer

--- app/services/data_stage/support_scripts.rb
+++ app/services/data_stage/support_scripts.rb
@@ -9,12 +9,12 @@ module DataStage
     end
 
     def close_school_with_no_successor(urn:, leaving_date: Time.zone.now)
-      ds = DataStage::School.find_by!(urn:)
-      s = ds.counterpart
+      staged_school = DataStage::School.find_by!(urn:)
+      live_school = staged_school.counterpart
 
       ActiveRecord::Base.transaction do
         # mark anyone still at the school as having left
-        s.school_cohorts.each do |school_cohort|
+        live_school.school_cohorts.each do |school_cohort|
           school_cohort.induction_programmes.each do |induction_programme|
             induction_programme.induction_records.where(induction_status: %i[active completed]).find_each do |induction_record|
               induction_record.leaving!(leaving_date, transferring_out: true)
@@ -23,20 +23,20 @@ module DataStage
         end
 
         # remove induction coordinator(s) from the school (only removes the association with the school)
-        s.induction_coordinator_profiles_schools.each(&:destroy!)
+        live_school.induction_coordinator_profiles_schools.each(&:destroy!)
 
-        # remove school mentors
-        s.school_mentors.each(&:destroy!)
+        # remove school mentors (only school_mentor records not the participant_profiles)
+        live_school.school_mentors.each(&:destroy!)
 
         # update school state
-        ds.create_or_sync_counterpart!
+        staged_school.create_or_sync_counterpart!
 
         # mark school changes as done
-        ds.school_changes.unprocessed.each do |school_change|
+        staged_school.school_changes.unprocessed.each do |school_change|
           school_change.update!(handled: true)
         end
 
-        logger.info("Closed #{s.name_and_urn}")
+        logger.info("Closed #{live_school.name_and_urn}")
       end
     end
   end
