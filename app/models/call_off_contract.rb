60d4e77d97154cbca33073906219663898e6105e
Ross Oliver <ross.oliver36@gmail.com>
2026-01-21 15:45:16 +0000

    Analysis of contracts in ECF
    Adds a new `/contracts` page that builds a tree diagram of the contract
    structure in ECF.
    
    It breaks down contracts by lead provider, cohort, version and statement.
    It also flags what changes between contract versions for a LP/cohort.
    
    You need to connect to the snapshot database to use this:
    
    ```
    make ci production konduit-snapshot
    ```

--- app/models/call_off_contract.rb
+++ app/models/call_off_contract.rb
@@ -27,5 +27,47 @@ class CallOffContract < ApplicationRecord
     !uplift_amount.nil?
   end
 
+  def describe
+    previous_contract = self.class
+      .not_flagged_as_unused
+      .where(
+        lead_provider:,
+        cohort:,
+      )
+      .to_a
+      .sort_by { |c| Finance::ECF::ContractVersion.new(c.version).numerical_value }
+      .reject { |c| Finance::ECF::ContractVersion.new(c.version).numerical_value >= Finance::ECF::ContractVersion.new(version).numerical_value }
+      .last
+
+    attributes = self.attributes.except("id", "lead_provider_id", "cohort_id", "created_at", "updated_at", "version", "raw", "revised_target")
+
+    participant_bands.min_nulls_first.each.with_index do |band, index|
+      letter = ("A".ord + index).chr
+      attributes["band_#{letter}"] = "#{band.per_participant}, #{band.min}-#{band.max}, #{band.output_payment_percentage}%, #{band.service_fee_percentage}%"
+    end
+
+    if previous_contract
+      previous_attributes = previous_contract.attributes.except("id", "lead_provider_id", "cohort_id", "created_at", "updated_at", "version", "raw", "revised_target")
+      previous_contract.participant_bands.min_nulls_first.each.with_index do |band, index|
+        letter = ("A".ord + index).chr
+        previous_attributes["band_#{letter}"] = "#{band.per_participant}, #{band.min}-#{band.max}, #{band.output_payment_percentage}%, #{band.service_fee_percentage}%"
+      end
+
+      attributes = attributes.reject { |k, v| previous_attributes[k].presence == v.presence }
+    end
+
+    result = attributes.map do |key, value|
+      if key == "monthly_service_fee" && value.nil?
+        "#{key.humanize}: default"
+      else
+        "#{key.humanize}: #{value}"
+      end
+    end
+
+    return ["No changes"] if result.empty?
+
+    result
+  end
+
   delegate :set_up_recruitment_basis, to: :band_a
 end
--- app/models/mentor_call_off_contract.rb
+++ app/models/mentor_call_off_contract.rb
@@ -3,4 +3,24 @@
 class MentorCallOffContract < ApplicationRecord
   belongs_to :lead_provider
   belongs_to :cohort
+
+  def describe
+    previous_contract = self.class
+      .where(
+        lead_provider:,
+        cohort:,
+      )
+      .to_a
+      .sort_by { |c| Finance::ECF::ContractVersion.new(c.version).numerical_value }
+      .reject { |c| Finance::ECF::ContractVersion.new(c.version).numerical_value >= Finance::ECF::ContractVersion.new(version).numerical_value }
+      .last
+
+    attributes = self.attributes.except("id", "lead_provider_id", "cohort_id", "created_at", "updated_at", "version", "raw")
+
+    if previous_contract
+      attributes = attributes.reject { |k, v| previous_contract.attributes[k].presence == v.presence }
+    end
+
+    attributes.map { |key, value| "#{key.humanize}: #{value}" }
+  end
 end
