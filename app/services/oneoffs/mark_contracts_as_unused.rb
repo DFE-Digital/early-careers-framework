18468bfef0ccf32a980a35dcc6a753959702ab72
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2026-01-30 14:37:25 +0000

    [3176] Update Oneoffs::MarkContractsAsUnused to include MentorCallOffContract

--- app/services/oneoffs/mark_contracts_as_unused.rb
+++ app/services/oneoffs/mark_contracts_as_unused.rb
@@ -10,6 +10,7 @@ module Oneoffs
     EQUALITY_CHECK_IGNORE = {
       call_off_contract: %w[id raw created_at updated_at],
       participant_bands: %w[id call_off_contract_id created_at updated_at],
+      mentor_call_off_contract: %w[id raw created_at updated_at],
     }.freeze
 
     def perform_change(dry_run: true)
@@ -18,10 +19,13 @@ module Oneoffs
       record_info("~~~ DRY RUN ~~~") if dry_run
 
       ActiveRecord::Base.transaction do
-        mark_as_unused!(orphans, "no associated finance statement")
-        mark_as_unused!(confirmed_duplicates, "duplicates")
-
+        mark_contracts_as_unused!(orphan_contracts, "no associated finance statement")
+        mark_contracts_as_unused!(confirmed_duplicate_contracts, "duplicates")
         record_divergent_duplicate_info
+
+        mark_mentor_contracts_as_unused!(orphan_mentor_contracts, "no associated finance statement")
+        mark_mentor_contracts_as_unused!(confirmed_duplicate_mentor_contracts, "duplicates")
+        record_mentor_divergent_duplicate_info
 
         raise ActiveRecord::Rollback if dry_run
       end
@@ -31,7 +35,7 @@ module Oneoffs
 
   private
 
-    def mark_as_unused!(call_off_contracts, reason)
+    def mark_contracts_as_unused!(call_off_contracts, reason)
       record_info("Marking #{call_off_contracts.size} contracts as unused (#{reason})")
 
       call_off_contracts.each do |call_off_contract|
@@ -39,8 +43,8 @@ module Oneoffs
       end
     end
 
-    def orphans
-      @orphans ||= call_off_contracts
+    def orphan_contracts
+      @orphan_contracts ||= call_off_contracts
         .joins(:lead_provider)
         .joins("LEFT JOIN statements ON
           statements.cpd_lead_provider_id = lead_providers.cpd_lead_provider_id
@@ -49,25 +53,25 @@ module Oneoffs
         .where(statements: { id: nil })
     end
 
-    def confirmed_duplicates
-      @confirmed_duplicates ||= potential_duplicates.flat_map do |original, duplicates|
+    def confirmed_duplicate_contracts
+      @confirmed_duplicate_contracts ||= potential_duplicate_contracts.flat_map do |original, duplicates|
         duplicates.select { |dup| call_off_contract_equal?(original, dup) }
       end
     end
 
-    def divergent_duplicates
-      @divergent_duplicates ||= potential_duplicates.values.flatten - confirmed_duplicates
+    def divergent_duplicate_contracts
+      @divergent_duplicate_contracts ||= potential_duplicate_contracts.values.flatten - confirmed_duplicate_contracts
     end
 
     def record_divergent_duplicate_info
-      divergent_duplicates.each do |divergent_duplicate|
-        original = potential_duplicates.find { |_, duplicates| duplicates.include?(divergent_duplicate) }.first
+      divergent_duplicate_contracts.each do |divergent_duplicate|
+        original = potential_duplicate_contracts.find { |_, duplicates| duplicates.include?(divergent_duplicate) }.first
         record_info("Skipping duplicate (#{divergent_duplicate.id}) due to attributes differing with original (#{original.id})")
       end
     end
 
-    def potential_duplicates
-      @potential_duplicates ||= duplicated_attributes.each_with_object({}) do |duplicate_condition, hash|
+    def potential_duplicate_contracts
+      @potential_duplicate_contracts ||= duplicated_contract_attributes.each_with_object({}) do |duplicate_condition, hash|
         duplicates = call_off_contracts.where(duplicate_condition).order(:created_at).to_a
         original = duplicates.shift
         hash[original] = duplicates
@@ -86,15 +90,8 @@ module Oneoffs
       contracts_equal && bands_equal
     end
 
-    def attributes_equal?(original, duplicate, ignore_key)
-      original_attributes = original.attributes.except(*EQUALITY_CHECK_IGNORE[ignore_key])
-      duplicate_attributes = duplicate.attributes.except(*EQUALITY_CHECK_IGNORE[ignore_key])
-
-      original_attributes == duplicate_attributes
-    end
-
-    def duplicated_attributes
-      @duplicated_attributes ||= call_off_contracts
+    def duplicated_contract_attributes
+      @duplicated_contract_attributes ||= call_off_contracts
         .group(*DUPLICATE_ATTRIBUTES)
         .having("COUNT(*) > 1")
         .pluck(*DUPLICATE_ATTRIBUTES)
@@ -103,6 +100,72 @@ module Oneoffs
 
     def call_off_contracts
       @call_off_contracts ||= CallOffContract.not_flagged_as_unused
+    end
+
+    def mark_mentor_contracts_as_unused!(contracts, reason)
+      record_info("Marking #{contracts.size} mentor contracts as unused (#{reason})")
+
+      contracts.each do |contract|
+        contract.update!(version: "#{MentorCallOffContract::UNUSED_VERSION_PREFIX}#{contract.version}")
+      end
+    end
+
+    def orphan_mentor_contracts
+      @orphan_mentor_contracts ||= mentor_call_off_contracts
+        .joins(:lead_provider)
+        .joins("LEFT JOIN statements ON
+          statements.cpd_lead_provider_id = lead_providers.cpd_lead_provider_id
+          AND statements.cohort_id = mentor_call_off_contracts.cohort_id
+          AND statements.mentor_contract_version = mentor_call_off_contracts.version")
+        .where(statements: { id: nil })
+    end
+
+    def confirmed_duplicate_mentor_contracts
+      @confirmed_duplicate_mentor_contracts ||= potential_duplicate_mentor_contracts.flat_map do |original, duplicates|
+        duplicates.select { |dup| mentor_call_off_contract_equal?(original, dup) }
+      end
+    end
+
+    def divergent_duplicate_mentor_contracts
+      @divergent_duplicate_mentor_contracts ||= potential_duplicate_mentor_contracts.values.flatten - confirmed_duplicate_mentor_contracts
+    end
+
+    def record_mentor_divergent_duplicate_info
+      divergent_duplicate_mentor_contracts.each do |divergent_duplicate|
+        original = potential_duplicate_mentor_contracts.find { |_, duplicates| duplicates.include?(divergent_duplicate) }.first
+        record_info("Skipping mentor duplicate (#{divergent_duplicate.id}) due to attributes differing with original (#{original.id})")
+      end
+    end
+
+    def potential_duplicate_mentor_contracts
+      @potential_duplicate_mentor_contracts ||= duplicated_mentor_contract_attributes.each_with_object({}) do |duplicate_condition, hash|
+        duplicates = mentor_call_off_contracts.where(duplicate_condition).order(:created_at).to_a
+        original = duplicates.shift
+        hash[original] = duplicates
+      end
+    end
+
+    def mentor_call_off_contract_equal?(original, duplicate)
+      attributes_equal?(original, duplicate, :mentor_call_off_contract)
+    end
+
+    def duplicated_mentor_contract_attributes
+      @duplicated_mentor_contract_attributes ||= mentor_call_off_contracts
+        .group(*DUPLICATE_ATTRIBUTES)
+        .having("COUNT(*) > 1")
+        .pluck(*DUPLICATE_ATTRIBUTES)
+        .map { |values| DUPLICATE_ATTRIBUTES.zip(values).to_h.symbolize_keys }
+    end
+
+    def mentor_call_off_contracts
+      @mentor_call_off_contracts ||= MentorCallOffContract.not_flagged_as_unused
+    end
+
+    def attributes_equal?(original, duplicate, ignore_key)
+      original_attributes = original.attributes.except(*EQUALITY_CHECK_IGNORE[ignore_key])
+      duplicate_attributes = duplicate.attributes.except(*EQUALITY_CHECK_IGNORE[ignore_key])
+
+      original_attributes == duplicate_attributes
     end
   end
 end
